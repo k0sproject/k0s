0efb020c185b295469211e681d73571214a7a639
jasmingacic <jasmin.gacic@gmail.com>
2020-10-07 00:37:07 +0200

    invoking component init in goroutine
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

--- pkg/component/manager.go
+++ pkg/component/manager.go
@@ -1,5 +1,13 @@
 package component
 
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
 // Manager manages components
 type Manager struct {
 	components []Component
@@ -19,11 +27,30 @@ func (m *Manager) Add(component Component) {
 
 // Init initializes all managed components
 func (m *Manager) Init() error {
+	logrus.Debug("********Starting init")
+
+	start := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(len(m.components))
+	errors := make(chan error)
+	wgDone := make(chan bool)
+
 	for _, comp := range m.components {
-		if err := comp.Init(); err != nil {
-			return err
-		}
+		go execute(comp, errors, wgDone, &wg)
 	}
+
+	go wait(wgDone, &wg)
+
+	select {
+	case <-wgDone:
+		break
+	case err := <-errors:
+		fmt.Println("Error: ", err)
+		close(errors)
+		break
+	}
+
+	logrus.Debug("********finished init:", time.Since(start))
 	return nil
 }
 
@@ -43,4 +70,17 @@ func (m *Manager) Stop() error {
 		m.components[i].Stop()
 	}
 	return nil
+}
+
+func execute(c Component, fatalErrors chan<- error, wgDone chan<- bool, wg *sync.WaitGroup) {
+	err := c.Init()
+	if err != nil {
+		fatalErrors <- err
+	}
+	wg.Done()
+}
+
+func wait(wgDone chan<- bool, wg *sync.WaitGroup) {
+	wg.Wait()
+	close(wgDone)
 }
