e7437dc0fcbe0cdf090d2ab2b4a02a481a2d509e
Leo Sjöberg <phroggyy@users.noreply.github.com>
2020-10-08 15:44:50 +0300

    Remove BIRD check from calico (#254)
    * Remove BIRD check from calico
    
    Signed-off-by: Leo Sjöberg <lsjoberg@mirantis.com>
    
    Co-authored-by: Leo Sjöberg <lsjoberg@mirantis.com>

--- inttest/basic/basic_test.go
+++ inttest/basic/basic_test.go
@@ -3,7 +3,10 @@ package basic
 import (
 	"context"
 	"github.com/stretchr/testify/suite"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
 	"testing"
+	"time"
 
 	"github.com/Mirantis/mke/inttest/common"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -36,10 +39,22 @@ func (s *BasicSuite) TestMkeGetsUp() {
 	s.T().Logf("found %d pods in kube-system", podCount)
 	s.Greater(podCount, 0, "expecting to see few pods in kube-system namespace")
 
+	s.Nil(s.WaitForCalicoReady(kc), "calico did not start")
+}
+
+func (s *BasicSuite) WaitForCalicoReady(kc *kubernetes.Clientset) error {
+	s.T().Log("waiting to see calico ready in kube API")
+	return wait.PollImmediate(1*time.Second, 5*time.Minute, func() (done bool, err error) {
+		ds, err := kc.AppsV1().DaemonSets("kube-system").Get(context.TODO(), "calico-node", v1.GetOptions{})
+		if err != nil {
+			return false, nil
+		}
+
+		return ds.Status.NumberReady == ds.Status.DesiredNumberScheduled, nil
+	})
 }
 
 func TestBasicSuite(t *testing.T) {
-
 	s := BasicSuite{
 		common.FootlooseSuite{
 			ControllerCount: 1,
