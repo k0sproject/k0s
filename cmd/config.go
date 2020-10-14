7c071be3b727f4d03fa234bdc2caf74dd7da51d8
Leo Sjöberg <phroggyy@users.noreply.github.com>
2020-10-14 15:27:16 +0300

    Add a command to output a default MKE config (#283)
    Signed-off-by: Leo Sjöberg <lsjoberg@mirantis.com>
    
    Co-authored-by: Leo Sjöberg <lsjoberg@mirantis.com>

---
+++ cmd/config.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/Mirantis/mke/pkg/apis/v1beta1"
+	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v2"
+)
+
+func ConfigCommand() *cli.Command {
+	return &cli.Command{
+		Name:      "default-config",
+		Usage:     "Output the default MKE configuration yaml to stdout",
+		Action:    buildConfig,
+	}
+}
+
+func buildConfig(ctx *cli.Context) error {
+	conf, _ := yaml.Marshal(v1beta1.DefaultClusterConfig())
+	fmt.Print(string(conf))
+	return nil
+}
