a665a799f1242ae9bf6fd9ff96be46f1e9927896
jasmingacic <jasmin.gacic@gmail.com>
2020-11-03 14:58:11 +0100

    interim commit with no real success
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

--- inttest/docker-test/start.sh
+++ inttest/docker-test/start.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
-make -C ../../
-cp ../../mke ./mke
-docker build . -t mke 
-docker run -d --rm --privileged -p 6443:6443 -v /var/lib/mke mke mke server --enable-worker
+# make -C ../../
+# cp ../../mke ./mke
 
-sleep 2 
-container_id=`docker ps  --filter "ancestor=mke" -q`
+# docker network create --subnet=172.18.0.0/16 k0snet
+docker build . -t mke 
+docker run -dt --rm --privileged -p 6443:6443 -v /var/lib/mke --net k0snet --ip 172.18.0.22 --name master mke mke server --enable-worker
+
+sleep 5
+container_id=`docker ps -f name=master -q`
 docker cp $container_id:/var/lib/mke/pki/admin.conf kubeconfig
+docker exec $container_id /bin/sh fixresolv.sh
 export KUBECONFIG=kubeconfig
+
+# sleep 120
+
+token=`docker exec $container_id mke token create`
+
+docker run -dt --rm --privileged -v /var/lib/mke --net k0snet --ip 172.18.0.23 --name worker1 mke mke worker $token
+# container_id=`docker ps -f name=master -q`
+# docker exec $container_id /bin/sh fixresolv.sh
+# container_id=`docker ps -f name=worker1 -q`
+# docker exec $container_id /bin/sh fixresolv.sh
+
+# coredns=`kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -n kube-system  | grep coredns`
+# kubectl delete pods -n kube-system $coredns
+
+# docker run -dt --rm --privileged -v /var/lib/mke --net k0snet --ip 172.18.0.24 --name worker2 mke mke worker $token
+# container_id=`docker ps -f name=worker2 -q`
+# docker exec $container_id /bin/sh fixresolv.sh
+
+
+while [[ $(kubectl get nodes  -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True" ]]; do sleep 1; done
+
+kubectl get nodes 
+kubectl get pods --all-namespaces 
