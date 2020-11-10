1629769fd3bdca2a0a6c1b78753f01db25913f08
Natanael Copa <ncopa@mirantis.com>
2020-11-10 16:16:05 +0100

    Add fallback for MachineID based on hostname
    Generate a machine id from md5sum of hostname in case /etc/machine-id is
    missing.
    
    fixes #353
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- pkg/util/machineid.go
+++ pkg/util/machineid.go
@@ -1,8 +1,23 @@
 package util
 
-import "github.com/denisbrodbeck/machineid"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"os"
+
+	"github.com/denisbrodbeck/machineid"
+)
 
 // MachineID returns protected id for the current machine
 func MachineID() (string, error) {
-	return machineid.ProtectedID("k0sproject-k0s")
+	id, err := machineid.ProtectedID("k0sproject-k0s")
+	if err != nil {
+		name, err := os.Hostname()
+		if err != nil {
+			return "", err
+		}
+		sum := md5.Sum([]byte(name))
+		id = hex.EncodeToString(sum[:])
+	}
+	return id, err
 }
