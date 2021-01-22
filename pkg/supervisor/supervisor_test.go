67a12da91b3b0caa804bac42f7715132e2f0563e
Natanael Copa <ncopa@mirantis.com>
2021-01-22 11:33:55 +0100

    Add simple unit test for supervisor
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

---
+++ pkg/supervisor/supervisor_test.go
@@ -0,0 +1,32 @@
+package supervisor
+
+import "testing"
+
+func TestSupervisor(t *testing.T) {
+	var testSupervisors = []Supervisor{
+		Supervisor{
+			Name:    "supervisor-test-sleep",
+			BinPath: "/bin/sh",
+			RunDir:  ".",
+			Args:    []string{"-c", "sleep 1s"},
+		},
+		Supervisor{
+			Name:    "supervisor-test-fail",
+			BinPath: "/bin/false",
+			RunDir:  ".",
+		},
+		Supervisor{
+			Name:    "supervisor-test-non-executable",
+			BinPath: "/tmp",
+			RunDir:  ".",
+		},
+	}
+
+	for _, s := range testSupervisors {
+		s.Supervise()
+		err := s.Stop()
+		if err != nil {
+			t.Errorf("Failed to stop %s: %w", s.Name, err)
+		}
+	}
+}
