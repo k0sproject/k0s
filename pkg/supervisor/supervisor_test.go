c6ff643b561552d5dd249a9356cc44eeb20b4e5a
Natanael Copa <ncopa@mirantis.com>
2021-01-22 13:53:23 +0100

    Add test for a failing init of rundir
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- pkg/supervisor/supervisor_test.go
+++ pkg/supervisor/supervisor_test.go
@@ -2,34 +2,58 @@ package supervisor
 
 import "testing"
 
+type SupervisorTest struct {
+	shouldFail bool
+	proc       Supervisor
+}
+
 func TestSupervisor(t *testing.T) {
-	var testSupervisors = []Supervisor{
-		Supervisor{
-			Name:    "supervisor-test-sleep",
-			BinPath: "/bin/sh",
-			RunDir:  ".",
-			Args:    []string{"-c", "sleep 1s"},
+	var testSupervisors = []SupervisorTest{
+		SupervisorTest{
+			shouldFail: false,
+			proc: Supervisor{
+				Name:    "supervisor-test-sleep",
+				BinPath: "/bin/sh",
+				RunDir:  ".",
+				Args:    []string{"-c", "sleep 1s"},
+			},
 		},
-		Supervisor{
-			Name:    "supervisor-test-fail",
-			BinPath: "/bin/false",
-			RunDir:  ".",
+		SupervisorTest{
+			shouldFail: false,
+			proc: Supervisor{
+				Name:    "supervisor-test-fail",
+				BinPath: "/bin/false",
+				RunDir:  ".",
+			},
 		},
-		Supervisor{
-			Name:    "supervisor-test-non-executable",
-			BinPath: "/tmp",
-			RunDir:  ".",
+		SupervisorTest{
+			shouldFail: false,
+			proc: Supervisor{
+				Name:    "supervisor-test-non-executable",
+				BinPath: "/tmp",
+				RunDir:  ".",
+			},
+		},
+		SupervisorTest{
+			shouldFail: true,
+			proc: Supervisor{
+				Name:    "supervisor-test-rundir-fail",
+				BinPath: "/tmp",
+				RunDir:  "/bin/sh/foo/bar",
+			},
 		},
 	}
 
 	for _, s := range testSupervisors {
-		err := s.Supervise()
-		if err != nil {
-			t.Errorf("Failed to start %s: %w", s.Name, err)
+		err := s.proc.Supervise()
+		if err != nil && !s.shouldFail {
+			t.Errorf("Failed to start %s: %w", s.proc.Name, err)
+		} else if err == nil && s.shouldFail {
+			t.Errorf("%s should fail but didn't", s.proc.Name)
 		}
-		err = s.Stop()
+		err = s.proc.Stop()
 		if err != nil {
-			t.Errorf("Failed to stop %s: %w", s.Name, err)
+			t.Errorf("Failed to stop %s: %w", s.proc.Name, err)
 		}
 	}
 }
