3f4ca48bf9b9794cf80073c4eebbddb532f63a2c
Jussi Nummelin <jnummelin@mirantis.com>
2021-02-09 09:30:56 +0000

    Changed kubectl command to use PersistentPreRun hooks to set kubeconfig env. Init funcs are problematic since they do not allow to control the order of things happening.
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- cmd/kubectl.go
+++ cmd/kubectl.go
@@ -18,27 +18,44 @@ package cmd
 import (
 	"os"
 
-	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 	kubectl "k8s.io/kubectl/pkg/cmd"
 )
 
 var (
-	kubectlCmd = kubectl.NewDefaultKubectlCommand()
+	kubectlCmd = kubectl.NewKubectlCommand(os.Stdin, os.Stdout, os.Stderr)
 )
 
 func init() {
-	//Get relevant Vars from constant package
-	k0sVars = constant.GetConfig(dataDir)
-	kubenv := os.Getenv("KUBECONFIG")
-	if kubenv == "" {
-		// Verify we can read the config before pushing it to env
-		file, err := os.OpenFile(k0sVars.AdminKubeConfigPath, os.O_RDONLY, 0600)
-		if err != nil {
-			logrus.Errorf("cannot read admin kubeconfig at %s, is the server running?", k0sVars.AdminKubeConfigPath)
-			return
+	kubectlCmd.Aliases = []string{"kc"}
+	// Get handle on the original kubectl prerun so we can call it later
+	originalPreRunE := kubectlCmd.PersistentPreRunE
+	kubectlCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		// Call parents pre-run if exists, cobra does not do this automatically
+		// See: https://github.com/spf13/cobra/issues/216
+		if parent := cmd.Parent(); parent != nil {
+			if parent.PersistentPreRun != nil {
+				parent.PersistentPreRun(parent, args)
+			}
+			if parent.PersistentPreRunE != nil {
+				err := parent.PersistentPreRunE(parent, args)
+				if err != nil {
+					return err
+				}
+			}
 		}
-		file.Close()
-		os.Setenv("KUBECONFIG", k0sVars.AdminKubeConfigPath)
+		kubenv := os.Getenv("KUBECONFIG")
+		if kubenv == "" {
+			// Verify we can read the config before pushing it to env
+			file, err := os.OpenFile(k0sVars.AdminKubeConfigPath, os.O_RDONLY, 0600)
+			if err != nil {
+				logrus.Errorf("cannot read admin kubeconfig at %s, is the server running?", k0sVars.AdminKubeConfigPath)
+				return err
+			}
+			defer file.Close()
+			os.Setenv("KUBECONFIG", k0sVars.AdminKubeConfigPath)
+		}
+		return originalPreRunE(cmd, args)
 	}
 }
