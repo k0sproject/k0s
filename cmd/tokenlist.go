ba2b5b7f2dbd9058a45c96cfee3780005a8ca4a7
Jussi Nummelin <jnummelin@mirantis.com>
2021-02-12 13:43:59 +0000

    Add token list command
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

---
+++ cmd/tokenlist.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2020 Mirantis, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/k0sproject/k0s/pkg/token"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	tokenListCmd.Flags().StringVar(&tokenRole, "role", "", "Either worker,controller or empty for all roles")
+}
+
+var (
+	tokenListCmd = &cobra.Command{
+		Use:     "list",
+		Short:   "List join tokens",
+		Example: `k0s token list --role worker // list worker tokens`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			manager, err := token.NewManager(filepath.Join(k0sVars.AdminKubeConfigPath))
+			if err != nil {
+				return err
+			}
+
+			tokens, err := manager.List(tokenRole)
+			if err != nil {
+				return err
+			}
+			if len(tokens) == 0 {
+				fmt.Println("No k0s join tokens found")
+				return nil
+			}
+
+			//fmt.Printf("Tokens: %v \n", tokens)
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"ID", "Role", "Expires at"})
+			table.SetAutoWrapText(false)
+			table.SetAutoFormatHeaders(true)
+			table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+			table.SetAlignment(tablewriter.ALIGN_LEFT)
+			table.SetCenterSeparator("")
+			table.SetColumnSeparator("")
+			table.SetRowSeparator("")
+			table.SetHeaderLine(false)
+			table.SetBorder(false)
+			table.SetTablePadding("\t") // pad with tabs
+			table.SetNoWhiteSpace(true)
+			for _, t := range tokens {
+				table.Append(t.ToArray())
+			}
+
+			table.Render()
+
+			return nil
+		},
+	}
+)
--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/olekukonko/tablewriter v0.0.2
 	github.com/onsi/ginkgo v1.14.1 // indirect
 	github.com/onsi/gomega v1.10.2 // indirect
 	github.com/pkg/errors v0.9.1
