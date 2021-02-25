128f068073d6a2835fbe808abc736171e6e6b7bb
Natanael Copa <ncopa@mirantis.com>
2021-02-25 15:15:54 +0100

    Refactor InitMainController to be variadic in inttest
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- inttest/dualstack/dualstack_test.go
+++ inttest/dualstack/dualstack_test.go
@@ -71,7 +71,7 @@ func (ds *DualstackSuite) getKubeConfig(node string) *restclient.Config {
 func (ds *DualstackSuite) SetupSuite() {
 	ds.FootlooseSuite.SetupSuite()
 	ds.putFile("/tmp/k0s.yaml", k0sConfigWithAddon)
-	ds.Require().NoError(ds.InitMainController([]string{"--config=/tmp/k0s.yaml"}))
+	ds.Require().NoError(ds.InitMainController("--config=/tmp/k0s.yaml"))
 	ds.Require().NoError(ds.RunWorkers("/var/lib/k0s"))
 	client, err := k8s.NewForConfig(ds.getKubeConfig("controller0"))
 	ds.Require().NoError(err)
