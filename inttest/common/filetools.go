6c6f792fbc216568a1e540008a723fe5a8db16b8
Natanael Copa <ncopa@mirantis.com>
2021-03-23 13:18:17 +0100

    inttest: clean up disconnect of ssh
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- inttest/common/filetools.go
+++ inttest/common/filetools.go
@@ -6,6 +6,7 @@ import "fmt"
 func (s *FootlooseSuite) GetFileFromController(controllerIdx int, path string) string {
 	sshCon, err := s.SSH(s.ControllerNode(controllerIdx))
 	s.Require().NoError(err)
+	defer sshCon.Disconnect()
 	content, err := sshCon.ExecWithOutput(fmt.Sprintf("cat %s", path))
 	s.Require().NoError(err)
 
