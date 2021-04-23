51f9981edd8877238552e255ba97801fc948a311
Natanael Copa <ncopa@mirantis.com>
2021-04-23 11:06:37 +0200

    Store kubelet config as a template in ConfigMap
    We need to set Client CA path and volume plugin dir relative the data
    dir, and it needs to be set on the worker side. This means that we
    cannot save the full path in the ConfigMap. Instead we store a template
    in ConfigMap which is rendered on the worker side before writing the
    config.
    
    Based on work of @jnummelin
    
    Fixes #771
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- pkg/component/controller/kubeletconfig.go
+++ pkg/component/controller/kubeletconfig.go
@@ -81,11 +81,9 @@ func (k *KubeletConfig) Run() error {
 
 func (k *KubeletConfig) run(dnsAddress string) (*bytes.Buffer, error) {
 	manifest := bytes.NewBuffer([]byte{})
-	clientCAFile := filepath.Join(k.k0sVars.CertRootDir, "ca.crt")
 	volumePluginDir := k.k0sVars.KubeletVolumePluginDir
-	defaultProfile := getDefaultProfile(dnsAddress, clientCAFile, volumePluginDir, k.clusterSpec.Network.DualStack.Enabled)
-	winClientCAFile := k.k0sVars.WindowsCertRootDir + "\\ca.crt"
-	winDefaultProfile := getDefaultProfile(dnsAddress, winClientCAFile, volumePluginDir, k.clusterSpec.Network.DualStack.Enabled)
+	defaultProfile := getDefaultProfile(dnsAddress, volumePluginDir, k.clusterSpec.Network.DualStack.Enabled)
+	winDefaultProfile := getDefaultProfile(dnsAddress, volumePluginDir, k.clusterSpec.Network.DualStack.Enabled)
 	if err := k.writeConfigMapWithProfile(manifest, "default", defaultProfile); err != nil {
 		return nil, fmt.Errorf("can't write manifest for default profile config map: %v", err)
 	}
@@ -97,7 +95,7 @@ func (k *KubeletConfig) run(dnsAddress string) (*bytes.Buffer, error) {
 		formatProfileName("default-windows"),
 	}
 	for _, profile := range k.clusterSpec.WorkerProfiles {
-		profileConfig := getDefaultProfile(dnsAddress, clientCAFile, volumePluginDir, false) // Do not add dualstack feature gate to the custom profiles
+		profileConfig := getDefaultProfile(dnsAddress, volumePluginDir, false) // Do not add dualstack feature gate to the custom profiles
 		merged, err := mergeProfiles(&profileConfig, profile.Values)
 		if err != nil {
 			return nil, fmt.Errorf("can't merge profile `%s` with default profile: %v", profile.Name, err)
@@ -169,7 +167,7 @@ func (k *KubeletConfig) writeRbacRoleBindings(w io.Writer, configMapNames []stri
 	return tw.WriteToBuffer(w)
 }
 
-func getDefaultProfile(dnsAddress string, clientCAFile string, volumePluginDir string, dualStack bool) unstructuredYamlObject {
+func getDefaultProfile(dnsAddress string, volumePluginDir string, dualStack bool) unstructuredYamlObject {
 	// the motivation to keep it like this instead of the yaml template:
 	// - it's easier to merge programatically defined structure
 	// - apart from map[string]interface there is no good way to define free-form mapping
@@ -185,7 +183,7 @@ func getDefaultProfile(dnsAddress string, clientCAFile string, volumePluginDir s
 				"enabled":  true,
 			},
 			"x509": map[string]interface{}{
-				"clientCAFile": clientCAFile,
+				"clientCAFile": "{{.ClientCAFile}}",
 			},
 		},
 		"authorization": map[string]interface{}{
@@ -208,7 +206,7 @@ func getDefaultProfile(dnsAddress string, clientCAFile string, volumePluginDir s
 			"TLS_RSA_WITH_AES_128_GCM_SHA256",
 		},
 		"volumeStatsAggPeriod": "0s",
-		"volumePluginDir":      volumePluginDir,
+		"volumePluginDir":      "{{.VolumePluginDir}}",
 		"failSwapOn":           false,
 		"rotateCertificates":   true,
 		"serverTLSBootstrap":   true,
