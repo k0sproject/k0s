ec27bb99e5724b1633ba27e28c45742153eb5d5b
Jussi Nummelin <jnummelin@mirantis.com>
2021-04-28 20:45:09 +0000

    Exclude calico images from airgap list on armv7
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- pkg/airgap/images.go
+++ pkg/airgap/images.go
@@ -1,6 +1,8 @@
 package airgap
 
 import (
+	"runtime"
+
 	"github.com/k0sproject/k0s/pkg/apis/v1beta1"
 	"github.com/k0sproject/k0s/pkg/constant"
 )
@@ -12,10 +14,7 @@ var pauseImage = v1beta1.ImageSpec{
 
 // GetImageURIs returns all image tags
 func GetImageURIs(spec *v1beta1.ClusterImages) []string {
-	return []string{
-		spec.Calico.CNI.URI(),
-		spec.Calico.KubeControllers.URI(),
-		spec.Calico.Node.URI(),
+	images := []string{
 		spec.Konnectivity.URI(),
 		spec.CoreDNS.URI(),
 		spec.KubeProxy.URI(),
@@ -24,4 +23,12 @@ func GetImageURIs(spec *v1beta1.ClusterImages) []string {
 		spec.KubeRouter.CNI.URI(),
 		spec.KubeRouter.CNIInstaller.URI(),
 	}
+	// Calico images do not currently support armv7, thus we need to exclude them from the list if we're running this on arm
+	if runtime.GOARCH != "arm" {
+		images = append(images,
+			spec.Calico.CNI.URI(),
+			spec.Calico.KubeControllers.URI(),
+			spec.Calico.Node.URI())
+	}
+	return images
 }
