bea1d8bc8ff49acf34c02a43e65519a5dd6bc32a
Natanael Copa <ncopa@mirantis.com>
2021-09-16 10:51:40 +0200

    Bump kubernetes to 1.22.2 (CVE-2021-25741)
    Fixes:
    - CVE-2021-25741: Symlink Exchange Can Allow Host Filesystem Access
      https://groups.google.com/g/kubernetes-announce/c/-e9OlTcED5E
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- docs/configuration.md
+++ docs/configuration.md
@@ -78,7 +78,7 @@ spec:
       version: v0.3.7
     kubeproxy:
       image: k8s.gcr.io/kube-proxy
-      version: v1.22.1
+      version: v1.22.2
     coredns:
       image: docker.io/coredns/coredns
       version: 1.7.0
--- inttest/sonobuoy/signetwork_test.go
+++ inttest/sonobuoy/signetwork_test.go
@@ -69,7 +69,7 @@ func (s *NetworkSuite) TestSigNetwork() {
 		`--e2e-focus=\[sig-network\].*\[Conformance\]`,
 		`--e2e-skip=\[Serial\]`,
 		"--e2e-parallel=y",
-		"--kube-conformance-image-version=v1.22.1",
+		"--kube-conformance-image-version=v1.22.2",
 	}
 	s.T().Log("running sonobuoy, this may take a while")
 	sonoFinished := make(chan bool)
