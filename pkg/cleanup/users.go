c2e34d2cc67473d196e37becb511e44440c7abc7
Natanael Copa <ncopa@mirantis.com>
2021-09-22 14:14:27 +0200

    reset: simplify user cleanup
    We don't need test if users exists since the
    install.DeleteControllerUsers will test that for us.
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- pkg/cleanup/users.go
+++ pkg/cleanup/users.go
@@ -1,7 +1,6 @@
 package cleanup
 
 import (
-	iusers "github.com/k0sproject/k0s/internal/pkg/users"
 	"github.com/k0sproject/k0s/pkg/config"
 	"github.com/k0sproject/k0s/pkg/install"
 	"github.com/sirupsen/logrus"
@@ -18,18 +17,7 @@ func (u *users) Name() string {
 
 // NeedsToRun detects controller users
 func (u *users) NeedsToRun() bool {
-	cfg, err := config.GetNodeConfig(u.Config.cfgFile, u.Config.k0sVars)
-	if err != nil {
-		return false
-	}
-
-	users := install.GetControllerUsers(cfg)
-	for _, user := range users {
-		if exists, _ := iusers.CheckIfUserExists(user); exists {
-			return true
-		}
-	}
-	return false
+	return true
 }
 
 // Run removes all controller users that are present on the host
@@ -38,6 +26,7 @@ func (u *users) Run() error {
 	cfg, err := config.GetNodeConfig(u.Config.cfgFile, u.Config.k0sVars)
 	if err != nil {
 		logger.Errorf("failed to get cluster setup: %v", err)
+		return nil
 	}
 	if err := install.DeleteControllerUsers(cfg); err != nil {
 		// don't fail, just notify on delete error
