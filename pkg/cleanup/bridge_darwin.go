0f06f1fab639d0318c7c82a5448000c023134d64
Alexey Makhov <makhov.alex@gmail.com>
2021-09-24 13:16:43 +0300

    Revert "darwin bridge"
    This reverts commit c00c1ab90beeaf966a577af59527a9297f94bf69.
    
    Signed-off-by: Alexey Makhov <makhov.alex@gmail.com>

--- pkg/cleanup/bridge_darwin.go
+++ pkg/cleanup/bridge_darwin.go
@@ -11,11 +11,6 @@ func (b *bridge) Name() string {
 	return "kube-bridge leftovers cleanup step"
 }
 
-// NeedsToRun checks if there are and kube-bridge leftovers
-func (b *bridge) NeedsToRun() bool {
-	return false
-}
-
 // Run removes found kube-bridge leftovers
 func (b *bridge) Run() error {
 	return fmt.Errorf("not implemented")
