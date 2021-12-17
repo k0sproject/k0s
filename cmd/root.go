k0sproject/k0s

commit 22ffa9fb80c765961281bf1f4240a02f83918d35

ClusterConfig CRD: remove k0Vars in favor of DataDir var
Signed-off-by: Karen Almog <kalmog@mirantis.com>

cmd/root.go
@@ -195,7 +195,7 @@ $ k0s completion fish > ~/.config/fish/completions/k0s.fish
 }
 
 func (c *cliOpts) buildConfig() error {
-	conf, _ := yaml.Marshal(v1beta1.DefaultClusterConfig(c.K0sVars))
+	conf, _ := yaml.Marshal(v1beta1.DefaultClusterConfig(config.DataDir))
 	fmt.Print(string(conf))
 	return nil
 }

pkg/backup/manager.go
@@ -68,7 +68,6 @@ func (bm *Manager) RunBackup(cfgPath string, clusterSpec *v1beta1.ClusterSpec, v
 	}
 	logrus.Infof("archive %s created successfully", destBackupFile)
 	return nil
-
 }
 
 func (bm *Manager) discoverSteps(cfgPath string, clusterSpec *v1beta1.ClusterSpec, vars constant.CfgVars, action string, restoredConfigPath string) {
@@ -152,7 +151,7 @@ func (bm Manager) getConfigForRestore(k0sVars constant.CfgVars) (*v1beta1.Cluste
 	configFromBackup := path.Join(bm.tmpDir, "k0s.yaml")
 	_, err := os.Stat(configFromBackup)
 	if os.IsNotExist(err) {
-		return v1beta1.DefaultClusterConfig(k0sVars), nil
+		return v1beta1.DefaultClusterConfig(bm.dataDir), nil
 	}
 	logrus.Infof("Using k0s.yaml from: %s", configFromBackup)
 

pkg/component/controller/calico_test.go
@@ -4,12 +4,13 @@ import (
 	"testing"
 
 	"github.com/k0sproject/k0s/pkg/apis/v1beta1"
-	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
 )
 
 type inMemorySaver map[string][]byte
+
+var dataDir string
 
 func (i inMemorySaver) Save(dst string, content []byte) error {
 	i[dst] = content
@@ -17,8 +18,7 @@ func (i inMemorySaver) Save(dst string, content []byte) error {
 }
 
 func TestCalicoManifests(t *testing.T) {
-	k0sVars := constant.GetConfig("")
-	clusterConfig := v1beta1.DefaultClusterConfig(k0sVars)
+	clusterConfig := v1beta1.DefaultClusterConfig(dataDir)
 	clusterConfig.Spec.Network.Calico = v1beta1.DefaultCalico()
 	clusterConfig.Spec.Network.Provider = "calico"
 	clusterConfig.Spec.Network.KubeRouter = nil
@@ -52,7 +52,6 @@ func TestCalicoManifests(t *testing.T) {
 	})
 
 	t.Run("must_have_wireguard_enabled_if_config_has", func(t *testing.T) {
-
 		clusterConfig.Spec.Network.Calico.EnableWireguard = true
 		saver := inMemorySaver{}
 		crdSaver := inMemorySaver{}
@@ -86,7 +85,6 @@ func TestCalicoManifests(t *testing.T) {
 
 	t.Run("ip_autodetection", func(t *testing.T) {
 		t.Run("use_IPAutodetectionMethod_for_both_families_by_default", func(t *testing.T) {
-
 			clusterConfig.Spec.Network.Calico.IPAutodetectionMethod = "somemethod"
 			saver := inMemorySaver{}
 			crdSaver := inMemorySaver{}
@@ -106,7 +104,6 @@ func TestCalicoManifests(t *testing.T) {
 			spec.RequireContainerHasEnvVariable(t, "calico-node", "IP_AUTODETECTION_METHOD", templateContext.IPAutodetectionMethod)
 		})
 		t.Run("use_IPV6AutodetectionMethod_for_ipv6_if_specified", func(t *testing.T) {
-
 			clusterConfig.Spec.Network.Calico.IPAutodetectionMethod = "somemethod"
 			clusterConfig.Spec.Network.Calico.IPv6AutodetectionMethod = "anothermethod"
 			saver := inMemorySaver{}

pkg/component/controller/kuberouter_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/k0sproject/dig"
 	"github.com/k0sproject/k0s/internal/testutil"
 	"github.com/k0sproject/k0s/pkg/apis/v1beta1"
-	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -17,7 +16,7 @@ import (
 )
 
 func TestKubeRouterConfig(t *testing.T) {
-	cfg := v1beta1.DefaultClusterConfig(constant.CfgVars{})
+	cfg := v1beta1.DefaultClusterConfig(dataDir)
 	cfg.Spec.Network.Calico = nil
 	cfg.Spec.Network.Provider = "kuberouter"
 	cfg.Spec.Network.KubeRouter = v1beta1.DefaultKubeRouter()
@@ -54,8 +53,7 @@ func TestKubeRouterConfig(t *testing.T) {
 }
 
 func TestKubeRouterDefaultManifests(t *testing.T) {
-
-	cfg := v1beta1.DefaultClusterConfig(constant.CfgVars{})
+	cfg := v1beta1.DefaultClusterConfig(dataDir)
 	cfg.Spec.Network.Calico = nil
 	cfg.Spec.Network.Provider = "kuberouter"
 	cfg.Spec.Network.KubeRouter = v1beta1.DefaultKubeRouter()

pkg/component/controller/metricserver_test.go
@@ -16,7 +16,7 @@ import (
 func TestGetConfigWithZeroNodes(t *testing.T) {
 	fakeFactory := testutil.NewFakeClientFactory()
 
-	clusterCfg := v1beta1.DefaultClusterConfig(constant.GetConfig(""))
+	clusterCfg := v1beta1.DefaultClusterConfig(dataDir)
 	metrics, err := NewMetricServer(clusterCfg, constant.GetConfig("/foo/bar"), fakeFactory)
 	require.NoError(t, err)
 	cfg, err := metrics.getConfig()
@@ -39,7 +39,7 @@ func TestGetConfigWithSomeNodes(t *testing.T) {
 		require.NoError(t, err)
 	}
 
-	clusterCfg := v1beta1.DefaultClusterConfig(constant.GetConfig(""))
+	clusterCfg := v1beta1.DefaultClusterConfig(dataDir)
 	metrics, err := NewMetricServer(clusterCfg, constant.GetConfig("/foo/bar"), fakeFactory)
 	require.NoError(t, err)
 	cfg, err := metrics.getConfig()

pkg/apis/v1beta1/clusterconfig_types.go
@@ -24,7 +24,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/k0sproject/k0s/internal/pkg/strictyaml"
-	"github.com/k0sproject/k0s/pkg/constant"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -60,9 +59,9 @@ type ClusterConfig struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 	metav1.TypeMeta   `json:",omitempty,inline"`
 
+	DataDir string              `json:"dataDir,omitempty"`
 	Spec    *ClusterSpec        `json:"spec,omitempty"`
 	Status  ClusterConfigStatus `json:"status,omitempty"`
-	k0sVars constant.CfgVars
 }
 
 // InstallSpec defines the required fields for the `k0s install` command
@@ -108,46 +107,44 @@ func (s *SchedulerSpec) IsZero() bool {
 }
 
 // ConfigFromFile takes a file path as Input, and parses it into a ClusterConfig
-func ConfigFromFile(filename string, k0sVars constant.CfgVars) (*ClusterConfig, error) {
+func ConfigFromFile(filename string, dataDir string) (*ClusterConfig, error) {
 	buf, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read config file at %s: %w", filename, err)
 	}
-
-	return configFromString(string(buf), k0sVars)
+	return configFromString(string(buf), dataDir)
 }
 
 // ConfigFromStdin tries to read k0s.yaml config from stdin
-func ConfigFromStdin(k0sVars constant.CfgVars) (*ClusterConfig, error) {
+func ConfigFromStdin(dataDir string) (*ClusterConfig, error) {
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		return nil, fmt.Errorf("can't read configration from stdin: %v", err)
 	}
-	return configFromString(string(input), k0sVars)
+	return configFromString(string(input), dataDir)
 }
 
-func configFromString(yml string, k0sVars constant.CfgVars) (*ClusterConfig, error) {
-	// config := DefaultClusterConfig(k0sVars)
-	config := &ClusterConfig{k0sVars: k0sVars}
-	err := strictyaml.YamlUnmarshalStrictIgnoringFields([]byte(yml), &config, []string{"interval"})
+func configFromString(yml string, dataDir string) (*ClusterConfig, error) {
+	config := &ClusterConfig{}
+	err := strictyaml.YamlUnmarshalStrictIgnoringFields([]byte(yml), config, []string{"interval"})
 	if err != nil {
 		return config, err
 	}
 	if config.Spec == nil {
-		config.Spec = DefaultClusterSpec(k0sVars)
+		config.Spec = DefaultClusterSpec(dataDir)
 	}
 	return config, nil
 }
 
 // DefaultClusterConfig sets the default ClusterConfig values, when none are given
-func DefaultClusterConfig(k0sVars constant.CfgVars) *ClusterConfig {
+func DefaultClusterConfig(dataDir string) *ClusterConfig {
 	return &ClusterConfig{
 		ObjectMeta: metav1.ObjectMeta{Name: "k0s"},
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "k0s.k0sproject.io/v1beta1",
 			Kind:       "Cluster",
 		},
-		Spec: DefaultClusterSpec(k0sVars),
+		Spec: DefaultClusterSpec(dataDir),
 	}
 }
 
@@ -155,7 +152,7 @@ func DefaultClusterConfig(k0sVars constant.CfgVars) *ClusterConfig {
 func (c *ClusterConfig) UnmarshalJSON(data []byte) error {
 	c.Kind = "Cluster"
 	c.Name = "k0s"
-	c.Spec = DefaultClusterSpec(c.k0sVars)
+	c.Spec = DefaultClusterSpec(c.DataDir)
 
 	type config ClusterConfig
 	jc := (*config)(c)
@@ -167,9 +164,9 @@ func (c *ClusterConfig) UnmarshalJSON(data []byte) error {
 }
 
 // DefaultClusterSpec default settings
-func DefaultClusterSpec(k0sVars constant.CfgVars) *ClusterSpec {
+func DefaultClusterSpec(dataDir string) *ClusterSpec {
 	return &ClusterSpec{
-		Storage: DefaultStorageSpec(k0sVars),
+		Storage: DefaultStorageSpec(dataDir),
 		Network: DefaultNetwork(),
 		API:     DefaultAPISpec(),
 		ControllerManager: &ControllerManagerSpec{

pkg/apis/v1beta1/clusterconfig_types_test.go
@@ -19,18 +19,16 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/k0sproject/k0s/internal/pkg/iface"
-	"github.com/k0sproject/k0s/pkg/constant"
+	"github.com/stretchr/testify/assert"
 )
 
-var k0sVars = constant.CfgVars{}
+var dataDir string
 
 func TestClusterDefaults(t *testing.T) {
-	c, err := configFromString("apiVersion: k0s.k0sproject.io/v1beta1", k0sVars)
+	c, err := configFromString("apiVersion: k0s.k0sproject.io/v1beta1", dataDir)
 	assert.NoError(t, err)
-	assert.Equal(t, DefaultStorageSpec(constant.GetConfig("")), c.Spec.Storage)
+	assert.Equal(t, DefaultStorageSpec(dataDir), c.Spec.Storage)
 }
 
 func TestStorageDefaults(t *testing.T) {
@@ -41,7 +39,7 @@ metadata:
   name: foobar
 `
 
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	assert.Equal(t, "etcd", c.Spec.Storage.Type)
 	addr, err := iface.FirstPublicAddress()
@@ -60,7 +58,7 @@ spec:
     type: etcd
 `
 
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	assert.Equal(t, "etcd", c.Spec.Storage.Type)
 	addr, err := iface.FirstPublicAddress()
@@ -81,7 +79,7 @@ spec:
     type: etcd
 `
 
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	errors := c.Validate()
 	assert.Equal(t, 0, len(errors))
@@ -100,7 +98,7 @@ spec:
     type: etcd
 `
 
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	errors := c.Validate()
 	assert.Equal(t, 0, len(errors))
@@ -119,7 +117,7 @@ spec:
     type: etcd
 `
 
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	errors := c.Validate()
 	assert.Equal(t, 1, len(errors))
@@ -138,7 +136,7 @@ spec:
     address: 1.2.3.4
 `
 
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	assert.Equal(t, "https://foo.bar.com:6443", c.Spec.API.APIAddressURL())
 	assert.Equal(t, "https://foo.bar.com:9443", c.Spec.API.K0sControlPlaneAPIAddress())
@@ -155,7 +153,7 @@ spec:
     address: 1.2.3.4
 `
 
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	assert.Equal(t, "https://1.2.3.4:6443", c.Spec.API.APIAddressURL())
 	assert.Equal(t, "https://1.2.3.4:9443", c.Spec.API.K0sControlPlaneAPIAddress())
@@ -186,7 +184,7 @@ spec:
         anonymous:
           enabled: false
 `
-	c, err := configFromString(yamlData, k0sVars)
+	c, err := configFromString(yamlData, dataDir)
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(c.Spec.WorkerProfiles))
 	assert.Equal(t, "profile_XXX", c.Spec.WorkerProfiles[0].Name)

pkg/apis/v1beta1/images_test.go
@@ -33,9 +33,8 @@ func getConfigYAML(t *testing.T, c *ClusterConfig) []byte {
 
 func TestImagesRepoOverrideInConfiguration(t *testing.T) {
 	t.Run("if_has_repository_not_empty_add_prefix_to_all_images", func(t *testing.T) {
-		k0sVars := constant.GetConfig("")
 		t.Run("default_config", func(t *testing.T) {
-			cfg := DefaultClusterConfig(k0sVars)
+			cfg := DefaultClusterConfig(dataDir)
 			cfg.Spec.Images.Repository = "my.repo"
 			var testingConfig *ClusterConfig
 			require.NoError(t, yaml.Unmarshal(getConfigYAML(t, cfg), &testingConfig))
@@ -53,7 +52,7 @@ func TestImagesRepoOverrideInConfiguration(t *testing.T) {
 			require.Equal(t, fmt.Sprintf("my.repo/k0sproject/cni-node:%s", constant.KubeRouterCNIInstallerImageVersion), testingConfig.Spec.Images.KubeRouter.CNIInstaller.URI())
 		})
 		t.Run("config_with_custom_images", func(t *testing.T) {
-			cfg := DefaultClusterConfig(k0sVars)
+			cfg := DefaultClusterConfig(dataDir)
 			cfg.Spec.Images.Konnectivity.Image = "my-custom-image"
 			cfg.Spec.Images.Repository = "my.repo"
 			var testingConfig *ClusterConfig

commit aa82b580ba2acc391de6c5a6140114c84a17e504

Fix storage for --single regression
Kine should be used as default storage with --single option specified.
commit 22ffa9fb80c7 (ClusterConfig CRD: remove k0Vars in favor of
DataDir var) introduced a regression that caused etcd be used by default
with --single.

Fix this and refactor so that DefaultClusterConfig takes an
optional/variadic clusterConfig. This way we don't always need to
specify which default cluster spec is to be used. Then we set the kine
storage in `ValidateYaml` depending on the DefaultStorageType in
K0sVars. `ValidateYaml` needs data dir from k0s vars so it makes sense
to do this from here.

Fixes https://github.com/k0sproject/k0s/issues/1360

Signed-off-by: Natanael Copa <ncopa@mirantis.com>

cmd/root.go
@@ -195,7 +195,7 @@ $ k0s completion fish > ~/.config/fish/completions/k0s.fish
 }
 
 func (c *cliOpts) buildConfig() error {
-	conf, _ := yaml.Marshal(v1beta1.DefaultClusterConfig(config.DataDir))
+	conf, _ := yaml.Marshal(v1beta1.DefaultClusterConfig())
 	fmt.Print(string(conf))
 	return nil
 }

hack/validate-images/main.go
@@ -34,7 +34,7 @@ func main() {
 	if len(architectures) < 1 {
 		panic("No architectures given")
 	}
-	cfg := v1beta1.DefaultClusterConfig("")
+	cfg := v1beta1.DefaultClusterConfig()
 	uris := airgap.GetImageURIs(cfg.Spec.Images)
 	if err := validateImages(uris, architectures); err != nil {
 		os.Exit(1)

pkg/apis/k0s.k0sproject.io/v1beta1/clusterconfig_types.go
@@ -26,7 +26,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/k0sproject/k0s/internal/pkg/strictyaml"
-	"github.com/k0sproject/k0s/pkg/constant"
 )
 
 const (
@@ -71,9 +70,8 @@ type ClusterConfig struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 	metav1.TypeMeta   `json:",omitempty,inline"`
 
-	DataDir string              `json:"dataDir,omitempty"`
-	Spec    *ClusterSpec        `json:"spec,omitempty"`
-	Status  ClusterConfigStatus `json:"status,omitempty"`
+	Spec   *ClusterSpec        `json:"spec,omitempty"`
+	Status ClusterConfigStatus `json:"status,omitempty"`
 }
 
 // StripDefaults returns a copy of the config where the default values a nilled out
@@ -88,7 +86,7 @@ func (c *ClusterConfig) StripDefaults() *ClusterConfig {
 	if reflect.DeepEqual(copy.Spec.Scheduler, DefaultSchedulerSpec()) {
 		copy.Spec.Scheduler = nil
 	}
-	if reflect.DeepEqual(c.Spec.Storage, DefaultStorageSpec(constant.DataDirDefault)) {
+	if reflect.DeepEqual(c.Spec.Storage, DefaultStorageSpec()) {
 		c.Spec.ControllerManager = nil
 	}
 	if reflect.DeepEqual(copy.Spec.Network, DefaultNetwork()) {
@@ -165,44 +163,44 @@ func (s *SchedulerSpec) IsZero() bool {
 }
 
 // ConfigFromFile takes a file path as Input, and parses it into a ClusterConfig
-func ConfigFromFile(filename string, dataDir string) (*ClusterConfig, error) {
+func ConfigFromFile(filename string, defaultStorage ...*StorageSpec) (*ClusterConfig, error) {
 	buf, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read config file at %s: %w", filename, err)
 	}
-	return ConfigFromString(string(buf), dataDir)
+	return ConfigFromString(string(buf), defaultStorage...)
 }
 
 // ConfigFromStdin tries to read k0s.yaml config from stdin
-func ConfigFromStdin(dataDir string) (*ClusterConfig, error) {
+func ConfigFromStdin(defaultStorage ...*StorageSpec) (*ClusterConfig, error) {
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		return nil, fmt.Errorf("can't read configration from stdin: %v", err)
 	}
-	return ConfigFromString(string(input), dataDir)
+	return ConfigFromString(string(input), defaultStorage...)
 }
 
-func ConfigFromString(yml string, dataDir string) (*ClusterConfig, error) {
-	config := DefaultClusterConfig(dataDir)
+func ConfigFromString(yml string, defaultStorage ...*StorageSpec) (*ClusterConfig, error) {
+	config := DefaultClusterConfig(defaultStorage...)
 	err := strictyaml.YamlUnmarshalStrictIgnoringFields([]byte(yml), config, "interval")
 	if err != nil {
 		return config, err
 	}
 	if config.Spec == nil {
-		config.Spec = DefaultClusterSpec(dataDir)
+		config.Spec = DefaultClusterSpec(defaultStorage...)
 	}
 	return config, nil
 }
 
 // DefaultClusterConfig sets the default ClusterConfig values, when none are given
-func DefaultClusterConfig(dataDir string) *ClusterConfig {
+func DefaultClusterConfig(defaultStorage ...*StorageSpec) *ClusterConfig {
 	return &ClusterConfig{
 		ObjectMeta: metav1.ObjectMeta{Name: "k0s"},
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "k0s.k0sproject.io/v1beta1",
 			Kind:       "ClusterConfig",
 		},
-		Spec: DefaultClusterSpec(dataDir),
+		Spec: DefaultClusterSpec(defaultStorage...),
 	}
 }
 
@@ -214,7 +212,7 @@ func (c *ClusterConfig) UnmarshalJSON(data []byte) error {
 	if c.ClusterName == "" {
 		c.ClusterName = "k0s"
 	}
-	c.Spec = DefaultClusterSpec(c.DataDir)
+	c.Spec = DefaultClusterSpec()
 
 	type config ClusterConfig
 	jc := (*config)(c)
@@ -226,10 +224,17 @@ func (c *ClusterConfig) UnmarshalJSON(data []byte) error {
 }
 
 // DefaultClusterSpec default settings
-func DefaultClusterSpec(dataDir string) *ClusterSpec {
+func DefaultClusterSpec(defaultStorage ...*StorageSpec) *ClusterSpec {
+	var storage *StorageSpec
+	if defaultStorage == nil || defaultStorage[0] == nil {
+		storage = DefaultStorageSpec()
+	} else {
+		storage = defaultStorage[0]
+	}
+
 	return &ClusterSpec{
 		Extensions:        DefaultExtensions(),
-		Storage:           DefaultStorageSpec(dataDir),
+		Storage:           storage,
 		Network:           DefaultNetwork(),
 		API:               DefaultAPISpec(),
 		ControllerManager: DefaultControllerManagerSpec(),

pkg/apis/k0s.k0sproject.io/v1beta1/storage.go
@@ -49,14 +49,7 @@ type KineConfig struct {
 }
 
 // DefaultStorageSpec creates StorageSpec with sane defaults
-func DefaultStorageSpec(dataDir string) *StorageSpec {
-	k0sVars := constant.GetConfig(dataDir)
-	if k0sVars.DefaultStorageType == KineStorageType {
-		return &StorageSpec{
-			Type: KineStorageType,
-			Kine: DefaultKineConfig(dataDir),
-		}
-	}
+func DefaultStorageSpec() *StorageSpec {
 	return &StorageSpec{
 		Type: EtcdStorageType,
 		Etcd: DefaultEtcdConfig(),

pkg/backup/manager.go
@@ -151,7 +151,7 @@ func (bm Manager) getConfigForRestore(k0sVars constant.CfgVars) (*v1beta1.Cluste
 	configFromBackup := path.Join(bm.tmpDir, "k0s.yaml")
 	_, err := os.Stat(configFromBackup)
 	if os.IsNotExist(err) {
-		return v1beta1.DefaultClusterConfig(bm.dataDir), nil
+		return v1beta1.DefaultClusterConfig(), nil
 	}
 	logrus.Infof("Using k0s.yaml from: %s", configFromBackup)
 

pkg/config/config.go
@@ -116,13 +116,21 @@ func GetYamlFromFile(cfgPath string, k0sVars constant.CfgVars) (clusterConfig *v
 }
 
 func ValidateYaml(cfgPath string, k0sVars constant.CfgVars) (clusterConfig *v1beta1.ClusterConfig, err error) {
+	var storage *v1beta1.StorageSpec
+	if k0sVars.DefaultStorageType == "kine" {
+		storage = &v1beta1.StorageSpec{
+			Type: v1beta1.KineStorageType,
+			Kine: v1beta1.DefaultKineConfig(k0sVars.DataDir),
+		}
+
+	}
 	switch cfgPath {
 	case "-":
-		clusterConfig, err = v1beta1.ConfigFromStdin(k0sVars.DataDir)
+		clusterConfig, err = v1beta1.ConfigFromStdin(storage)
 	case "":
-		clusterConfig = v1beta1.DefaultClusterConfig(k0sVars.DataDir)
+		clusterConfig = v1beta1.DefaultClusterConfig(storage)
 	default:
-		clusterConfig, err = v1beta1.ConfigFromFile(cfgPath, k0sVars.DataDir)
+		clusterConfig, err = v1beta1.ConfigFromFile(cfgPath, storage)
 	}
 	if err != nil {
 		return nil, err
@@ -182,7 +190,6 @@ func ClusterConfigMinusNodeConfig(config *v1beta1.ClusterConfig) *v1beta1.Cluste
 	return &v1beta1.ClusterConfig{
 		ObjectMeta: config.ObjectMeta,
 		TypeMeta:   config.TypeMeta,
-		DataDir:    config.DataDir,
 		Spec:       clusterSpec,
 		Status:     config.Status,
 	}
@@ -194,13 +201,18 @@ func GetNodeConfig(cfgPath string, k0sVars constant.CfgVars) (*v1beta1.ClusterCo
 	if err != nil {
 		return nil, err
 	}
+	var etcdConfig *v1beta1.EtcdConfig
+	if cfg.Spec.Storage.Type == v1beta1.EtcdStorageType {
+		etcdConfig = &v1beta1.EtcdConfig{
+			PeerAddress: cfg.Spec.Storage.Etcd.PeerAddress,
+		}
+	}
+
 	clusterSpec := &v1beta1.ClusterSpec{
 		API: cfg.Spec.API,
 		Storage: &v1beta1.StorageSpec{
 			Type: cfg.Spec.Storage.Type,
-			Etcd: &v1beta1.EtcdConfig{
-				PeerAddress: cfg.Spec.Storage.Etcd.PeerAddress,
-			},
+			Etcd: etcdConfig,
 			Kine: cfg.Spec.Storage.Kine,
 		},
 		Network: &v1beta1.Network{
@@ -212,7 +224,6 @@ func GetNodeConfig(cfgPath string, k0sVars constant.CfgVars) (*v1beta1.ClusterCo
 	nodeConfig := &v1beta1.ClusterConfig{
 		ObjectMeta: cfg.ObjectMeta,
 		TypeMeta:   cfg.TypeMeta,
-		DataDir:    cfg.DataDir,
 		Spec:       clusterSpec,
 		Status:     cfg.Status,
 	}

cmd/airgap/listimages_test.go
@@ -39,7 +39,7 @@ spec:
       image: custom.io/coredns/coredns
       version: 1.0.0
 `
-	cfg, err := v1beta1.ConfigFromString(yamlData, "")
+	cfg, err := v1beta1.ConfigFromString(yamlData)
 	s.NoError(err)
 	a := cfg.Spec.Images
 

cmd/kubeconfig/kubeconfig_test.go
@@ -151,7 +151,7 @@ yJm2KSue0toWmkBFK8WMTjAvmAw3Z/qUhJRKoqCu3k6Mf8DNl6t+Uw==
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	s.NoError(err)
 	s.Equal("https://10.0.0.86:6443", config.Host)
-	_, err = v1beta1.ConfigFromString(yamlData, "")
+	_, err = v1beta1.ConfigFromString(yamlData)
 	s.NoError(err)
 }
 

pkg/apis/k0s.k0sproject.io/v1beta1/clusterconfig_types_test.go
@@ -23,19 +23,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var dataDir string
-
 func TestClusterDefaults(t *testing.T) {
-	c, err := ConfigFromString("apiVersion: k0s.k0sproject.io/v1beta1", dataDir)
+	c, err := ConfigFromString("apiVersion: k0s.k0sproject.io/v1beta1")
 	assert.NoError(t, err)
-	assert.Equal(t, DefaultStorageSpec(dataDir), c.Spec.Storage)
+	assert.Equal(t, DefaultStorageSpec(), c.Spec.Storage)
 }
 
 func TestUnknownFieldValidation(t *testing.T) {
 	_, err := ConfigFromString(`
 apiVersion: k0s.k0sproject.io/v1beta1
 kind: ClusterConfig
-unknown: 1`, dataDir)
+unknown: 1`)
 
 	assert.Error(t, err)
 }
@@ -48,7 +46,7 @@ metadata:
   name: foobar
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	assert.Equal(t, "etcd", c.Spec.Storage.Type)
 	addr, err := iface.FirstPublicAddress()
@@ -67,7 +65,7 @@ spec:
     type: etcd
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	assert.Equal(t, "etcd", c.Spec.Storage.Type)
 	addr, err := iface.FirstPublicAddress()
@@ -88,7 +86,7 @@ spec:
     type: etcd
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	errors := c.Validate()
 	assert.Equal(t, 0, len(errors))
@@ -107,7 +105,7 @@ spec:
     type: etcd
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	errors := c.Validate()
 	assert.Equal(t, 0, len(errors))
@@ -126,7 +124,7 @@ spec:
     type: etcd
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	errors := c.Validate()
 	assert.Equal(t, 1, len(errors))
@@ -145,7 +143,7 @@ spec:
     address: 1.2.3.4
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	assert.Equal(t, "https://foo.bar.com:6443", c.Spec.API.APIAddressURL())
 	assert.Equal(t, "https://foo.bar.com:9443", c.Spec.API.K0sControlPlaneAPIAddress())
@@ -162,7 +160,7 @@ spec:
     address: 1.2.3.4
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	assert.Equal(t, "https://1.2.3.4:6443", c.Spec.API.APIAddressURL())
 	assert.Equal(t, "https://1.2.3.4:9443", c.Spec.API.K0sControlPlaneAPIAddress())
@@ -191,7 +189,7 @@ spec:
         anonymous:
           enabled: false
 `
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(c.Spec.WorkerProfiles))
 	assert.Equal(t, "profile_XXX", c.Spec.WorkerProfiles[0].Name)
@@ -211,7 +209,7 @@ spec:
 }
 
 func TestStripDefaults(t *testing.T) {
-	defaultConfig := DefaultClusterConfig("")
+	defaultConfig := DefaultClusterConfig()
 	stripped := defaultConfig.StripDefaults()
 	a := assert.New(t)
 	a.Nil(stripped.Spec.API)

pkg/apis/k0s.k0sproject.io/v1beta1/images_test.go
@@ -34,7 +34,7 @@ func getConfigYAML(t *testing.T, c *ClusterConfig) []byte {
 func TestImagesRepoOverrideInConfiguration(t *testing.T) {
 	t.Run("if_has_repository_not_empty_add_prefix_to_all_images", func(t *testing.T) {
 		t.Run("default_config", func(t *testing.T) {
-			cfg := DefaultClusterConfig(dataDir)
+			cfg := DefaultClusterConfig()
 			cfg.Spec.Images.Repository = "my.repo"
 			var testingConfig *ClusterConfig
 			require.NoError(t, yaml.Unmarshal(getConfigYAML(t, cfg), &testingConfig))
@@ -52,7 +52,7 @@ func TestImagesRepoOverrideInConfiguration(t *testing.T) {
 			require.Equal(t, fmt.Sprintf("my.repo/k0sproject/cni-node:%s", constant.KubeRouterCNIInstallerImageVersion), testingConfig.Spec.Images.KubeRouter.CNIInstaller.URI())
 		})
 		t.Run("config_with_custom_images", func(t *testing.T) {
-			cfg := DefaultClusterConfig(dataDir)
+			cfg := DefaultClusterConfig()
 			cfg.Spec.Images.Konnectivity.Image = "my-custom-image"
 			cfg.Spec.Images.Repository = "my.repo"
 			var testingConfig *ClusterConfig

pkg/apis/k0s.k0sproject.io/v1beta1/network_test.go
@@ -102,7 +102,7 @@ spec:
     calico:
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	s.NoError(err)
 	n := c.Spec.Network
 
@@ -125,7 +125,7 @@ spec:
     kuberouter:
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	s.NoError(err)
 	n := c.Spec.Network
 
@@ -148,7 +148,7 @@ metadata:
 spec:
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	s.NoError(err)
 	p := c.Spec.Network.KubeProxy
 
@@ -168,7 +168,7 @@ spec:
       disabled: true
 `
 
-	c, err := ConfigFromString(yamlData, dataDir)
+	c, err := ConfigFromString(yamlData)
 	s.NoError(err)
 	p := c.Spec.Network.KubeProxy
 

pkg/apis/k0s.k0sproject.io/v1beta1/storage_test.go
@@ -18,7 +18,6 @@ package v1beta1
 import (
 	"testing"
 
-	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -91,7 +90,7 @@ spec:
   storage:
     type: kine
 `
-	c, err := ConfigFromString(yaml, constant.DataDirDefault)
+	c, err := ConfigFromString(yaml)
 	assert.NoError(t, err)
 	assert.Equal(t, "kine", c.Spec.Storage.Type)
 	assert.NotNil(t, c.Spec.Storage.Kine)

pkg/component/controller/calico_test.go
@@ -19,7 +19,7 @@ func (i inMemorySaver) Save(dst string, content []byte) error {
 }
 
 func TestCalicoManifests(t *testing.T) {
-	clusterConfig := v1beta1.DefaultClusterConfig(dataDir)
+	clusterConfig := v1beta1.DefaultClusterConfig()
 	clusterConfig.Spec.Network.Calico = v1beta1.DefaultCalico()
 	clusterConfig.Spec.Network.Provider = "calico"
 	clusterConfig.Spec.Network.KubeRouter = nil

pkg/component/controller/kuberouter_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 func TestKubeRouterConfig(t *testing.T) {
-	cfg := v1beta1.DefaultClusterConfig(dataDir)
+	cfg := v1beta1.DefaultClusterConfig()
 	cfg.Spec.Network.Calico = nil
 	cfg.Spec.Network.Provider = "kuberouter"
 	cfg.Spec.Network.KubeRouter = v1beta1.DefaultKubeRouter()
@@ -54,7 +54,7 @@ func TestKubeRouterConfig(t *testing.T) {
 }
 
 func TestKubeRouterDefaultManifests(t *testing.T) {
-	cfg := v1beta1.DefaultClusterConfig(dataDir)
+	cfg := v1beta1.DefaultClusterConfig()
 	cfg.Spec.Network.Calico = nil
 	cfg.Spec.Network.Provider = "kuberouter"
 	cfg.Spec.Network.KubeRouter = v1beta1.DefaultKubeRouter()

pkg/component/controller/metricserver_test.go
@@ -12,7 +12,7 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var cfg = v1beta1.DefaultClusterConfig("")
+var cfg = v1beta1.DefaultClusterConfig()
 
 func TestGetConfigWithZeroNodes(t *testing.T) {
 	fakeFactory := testutil.NewFakeClientFactory()