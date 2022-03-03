aff105d963df81fc1316c4b5cf4618548bab92b2
makhov <amakhov@mirantis.com>
2022-03-03 12:37:03 +0200

    Allow setting environment with the install command.
    Signed-off-by: makhov <amakhov@mirantis.com>

---
+++ pkg/install/darwin_launchd.go
@@ -0,0 +1,48 @@
+package install
+
+const launchdConfig = `<?xml version='1.0' encoding='UTF-8'?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+"http://www.apple.com/DTDs/PropertyList-1.0.dtd" >
+<plist version='1.0'>
+  <dict>
+	{{- if .Option.Environment}}
+    <key>EnvironmentVariables</key>
+	<dict>
+	{{range $k, $v := .Option.Environment -}}
+	<key>{{html $k}}</key>
+	<string>{{html $v}}</string>
+	{{end -}}
+	{{end -}}
+	</dict>
+    <key>Label</key>
+    <string>{{html .Name}}</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>{{html .Path}}</string>
+    {{range .Config.Arguments}}
+      <string>{{html .}}</string>
+    {{end}}
+    </array>
+    {{if .UserName}}<key>UserName</key>
+    <string>{{html .UserName}}</string>{{end}}
+    {{if .ChRoot}}<key>RootDirectory</key>
+    <string>{{html .ChRoot}}</string>{{end}}
+    {{if .WorkingDirectory}}<key>WorkingDirectory</key>
+    <string>{{html .WorkingDirectory}}</string>{{end}}
+    <key>SessionCreate</key>
+    <{{bool .SessionCreate}}/>
+    <key>KeepAlive</key>
+    <{{bool .KeepAlive}}/>
+    <key>RunAtLoad</key>
+    <{{bool .RunAtLoad}}/>
+    <key>Disabled</key>
+    <false/>
+    
+    <key>StandardOutPath</key>
+    <string>/usr/local/var/log/{{html .Name}}.out.log</string>
+    <key>StandardErrorPath</key>
+    <string>/usr/local/var/log/{{html .Name}}.err.log</string>
+  
+  </dict>
+</plist>
+`
