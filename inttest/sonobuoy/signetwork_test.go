81fba2c7324f5ab2beed341f6473d9febd305d38
Natanael Copa <ncopa@mirantis.com>
2022-04-25 17:19:18 +0200

    Bump kubernetes 1.21.12
    https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.21.md#changelog-since-v12111
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- inttest/sonobuoy/signetwork_test.go
+++ inttest/sonobuoy/signetwork_test.go
@@ -69,7 +69,7 @@ func (s *NetworkSuite) TestSigNetwork() {
 		`--e2e-focus=\[sig-network\].*\[Conformance\]`,
 		`--e2e-skip=\[Serial\]`,
 		"--e2e-parallel=y",
-		"--kube-conformance-image-version=v1.21.11",
+		"--kube-conformance-image-version=v1.21.12",
 	}
 	s.T().Log("running sonobuoy, this may take a while")
 	sonoFinished := make(chan bool)
