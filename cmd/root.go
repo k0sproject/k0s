82132161d80bc9cc569180c0c81c5b09ea98aa08
Karen Almog <kalmog@mirantis.com>
2022-04-26 13:53:50 +0000

    Remove global data-dir flag
    Signed-off-by: Karen Almog <kalmog@mirantis.com>

--- cmd/root.go
+++ cmd/root.go
@@ -107,10 +107,6 @@ func NewRootCmd() *cobra.Command {
 		longDesc = longDesc + "\n" + build.EulaNotice
 	}
 	cmd.Long = longDesc
-
-	// workaround for the data-dir location input for the kubectl command
-	cmd.PersistentFlags().AddFlagSet(config.GetKubeCtlFlagSet())
-
 	return cmd
 }
 
