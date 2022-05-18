b060837ae0375930e98865790521f7c14a4f4e65
Tom Wieczorek <twieczorek@mirantis.com>
2022-05-18 12:41:53 +0200

    Bump Calico to v3.23.1
    https://projectcalico.docs.tigera.io/archive/v3.23/release-notes/#v3231
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/constant/constant_shared.go
+++ pkg/constant/constant_shared.go
@@ -86,7 +86,7 @@ const (
 	CoreDNSImage                       = "k8s.gcr.io/coredns/coredns"
 	CoreDNSImageVersion                = "v1.7.0"
 	CalicoImage                        = "docker.io/calico/cni"
-	CalicoComponentImagesVersion       = "v3.23.0"
+	CalicoComponentImagesVersion       = "v3.23.1"
 	CalicoNodeImage                    = "docker.io/calico/node"
 	KubeControllerImage                = "docker.io/calico/kube-controllers"
 	KubeRouterCNIImage                 = "docker.io/cloudnativelabs/kube-router"
