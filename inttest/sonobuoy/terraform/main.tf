239b45fd15b9a90f877c80dcf4543517cea0dc65
Karen Almog <kalmog@mirantis.com>
2022-06-17 09:08:10 +0000

    Refactor terraform code for conformance tests
    Signed-off-by: Karen Almog <kalmog@mirantis.com>
    
    use k0sctl to deploy k0s on conformance hosts
    
    Signed-off-by: Karen Almog <kalmog@mirantis.com>
    
    run sonobuoy directly from inttest makefile
    
    Signed-off-by: Karen Almog <kalmog@mirantis.com>
    
    update release conformance to work with new TF code
    
    Signed-off-by: Karen Almog <kalmog@mirantis.com>

--- inttest/sonobuoy/terraform/main.tf
+++ inttest/sonobuoy/terraform/main.tf
@@ -2,25 +2,38 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# variable "sonobuoy_version" {
+#   type    = string
+#   default = "0.53.2"
+# }
+
+resource "random_id" "cluster_identifier" {
+  byte_length = 4
+}
+
+locals {
+  cluster_unique_identifier = format("%s-%s", var.cluster_name, random_id.cluster_identifier.hex)
+}
+
 resource "tls_private_key" "k8s-conformance-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "cluster-key" {
-  key_name   = format("%s_key", var.cluster_name)
+  key_name   = format("%s_key", local.cluster_unique_identifier)
   public_key = tls_private_key.k8s-conformance-key.public_key_openssh
 }
 
 // Save the private key to filesystem
 resource "local_file" "aws_private_pem" {
   file_permission = "600"
-  filename = format("%s/%s", path.module, "aws_private.pem")
-  content = tls_private_key.k8s-conformance-key.private_key_pem
+  filename        = format("%s/%s", path.module, "aws_private.pem")
+  content         = tls_private_key.k8s-conformance-key.private_key_pem
 }
 
 resource "aws_security_group" "cluster_allow_ssh" {
-  name        = format("%s-allow-ssh", var.cluster_name)
+  name        = format("%s-allow-ssh", local.cluster_unique_identifier)
   description = "Allow ssh inbound traffic"
   vpc_id      = aws_vpc.cluster-vpc.id
 
@@ -42,7 +55,7 @@ resource "aws_security_group" "cluster_allow_ssh" {
   }
 
   tags = {
-    Name = format("%s-allow-ssh", var.cluster_name)
+    Name = format("%s-allow-ssh", local.cluster_unique_identifier)
   }
 }
 
--- inttest/sonobuoy/terraform/output.tf
+++ inttest/sonobuoy/terraform/output.tf
@@ -6,8 +6,15 @@ output "worker_external_ip" {
   value = aws_instance.cluster-workers.*.public_ip
 }
 
+output "controller_count" {
+  value = var.controller_count
+}
 
 output "controller_pem" {
-  value       = local_file.aws_private_pem
-  sensitive   = true
+  value     = local_file.aws_private_pem
+  sensitive = true
+}
+
+output "cluster_name" {
+  value = local.cluster_unique_identifier
 }
