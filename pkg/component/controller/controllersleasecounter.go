2e013f5bfdf8b1c97b5bc76f6d7c2624f0edaf20
Jussi Nummelin <jnummelin@mirantis.com>
2022-08-12 07:08:36 +0000

    Use object safe hostnames for autopilot `ControllerNode` and for controller `Lease` counter.
    Hosts with capital letters breaks these currently as kube objects cannot have capitals in the names of the objects.
    
    The cases this fixes are those where we have not been able to create the corresponding objects at all and thus we do not have to do any cleanup etc..
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>
    (cherry picked from commit ecefd709918518d8b2384b2061150415f3b1a52a)
    (cherry picked from commit d19e5daf933c4e7db7e0d3a41e31d9c314d1ccac)
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit 65560e4af1f39e0c22f511986fa4c241a378b479)

--- pkg/component/controller/controllersleasecounter.go
+++ pkg/component/controller/controllersleasecounter.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/k0sproject/k0s/pkg/apis/k0s.k0sproject.io/v1beta1"
 
@@ -11,6 +10,8 @@ import (
 
 	kubeutil "github.com/k0sproject/k0s/pkg/kubernetes"
 	"github.com/k0sproject/k0s/pkg/leaderelection"
+
+	nodeutil "k8s.io/kubernetes/pkg/util/node"
 )
 
 // K0sControllersLeaseCounter implements a component that manages a lease per controller.
@@ -38,7 +39,8 @@ func (l *K0sControllersLeaseCounter) Run(ctx context.Context) error {
 	}
 
 	// hostname used to make the lease names be clear to which controller they belong to
-	holderIdentity, err := os.Hostname()
+	// follow kubelet convention for naming so we e.g. use lowercase hostname etc.
+	holderIdentity, err := nodeutil.GetHostname("")
 	if err != nil {
 		return nil
 	}
