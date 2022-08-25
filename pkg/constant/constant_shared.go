a9278abd9d31910994b9ae3d110040204dc84158
Natanael Copa <ncopa@mirantis.com>
2022-08-25 10:36:26 +0200

    Bump coredns to 1.7.1
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- pkg/constant/constant_shared.go
+++ pkg/constant/constant_shared.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -84,7 +84,7 @@ const (
 	KubeProxyImage                     = "k8s.gcr.io/kube-proxy"
 	KubeProxyImageVersion              = "v1.24.4"
 	CoreDNSImage                       = "k8s.gcr.io/coredns/coredns"
-	CoreDNSImageVersion                = "v1.7.0"
+	CoreDNSImageVersion                = "v1.7.1"
 	CalicoImage                        = "docker.io/calico/cni"
 	CalicoComponentImagesVersion       = "v3.23.3"
 	CalicoNodeImage                    = "docker.io/calico/node"
