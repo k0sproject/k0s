k0sproject/k0s

commit d141f32f9a630caec23a42c706d05ba7f54b0ae7

Early logging configuration
The long-running subcommands controller, worker and api use stdout and
info logging by default. The current setup activates those settings
quite late in the Cobra pipeline, potentially logging to the wrong
destination, or slurping away interesting debug logs.

Fix this by setting the defaults early for those subcommands via
Cobra's PersistentPreRun facility. Don't call config.GetCmdOpts()
unnecessarily in the root command's PersistentPreRun, but simply use the
global variables directly.

This relates to fd4fdffd3 in order to help making problems in the early
execution stages more visible.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

cmd/api/api.go
@@ -63,15 +63,13 @@ func NewAPICmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "api",
 		Short: "Run the controller API",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			c := command{CLIOptions: config.GetCmdOpts()}
-
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			logrus.SetOutput(os.Stdout)
-			if !c.Debug {
-				logrus.SetLevel(logrus.InfoLevel)
-			}
-
-			return c.start()
+			logrus.SetLevel(logrus.InfoLevel)
+			return config.CallParentPersistentPreRun(cmd, args)
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return (&command{CLIOptions: config.GetCmdOpts()}).start()
 		},
 	}
 	cmd.SilenceUsage = true

cmd/controller/controller.go
@@ -70,13 +70,13 @@ func NewControllerCmd() *cobra.Command {
 	or CLI flag:
 	$ k0s controller --token-file [path_to_file]
 	Note: Token can be passed either as a CLI argument or as a flag`,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			logrus.SetOutput(os.Stdout)
+			logrus.SetLevel(logrus.InfoLevel)
+			return config.CallParentPersistentPreRun(cmd, args)
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			c := command(config.GetCmdOpts())
-
-			logrus.SetOutput(os.Stdout)
-			if !c.Debug {
-				logrus.SetLevel(logrus.InfoLevel)
-			}
 
 			if len(args) > 0 {
 				c.TokenArg = args[0]

cmd/root.go
@@ -56,18 +56,16 @@ func NewRootCmd() *cobra.Command {
 		Use:   "k0s",
 		Short: "k0s - Zero Friction Kubernetes",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			c := config.GetCmdOpts()
-
-			if c.Verbose {
+			if config.Verbose {
 				logrus.SetLevel(logrus.InfoLevel)
 			}
 
-			if c.Debug {
+			if config.Debug {
 				logrus.SetLevel(logrus.DebugLevel)
 				go func() {
-					log := logrus.WithField("debug_server", c.DebugListenOn)
+					log := logrus.WithField("debug_server", config.DebugListenOn)
 					log.Debug("Starting debug server")
-					if err := http.ListenAndServe(c.DebugListenOn, nil); err != http.ErrServerClosed {
+					if err := http.ListenAndServe(config.DebugListenOn, nil); err != http.ErrServerClosed {
 						log.WithError(err).Debug("Failed to start debug server")
 					} else {
 						log.Debug("Debug server closed")

cmd/worker/worker.go
@@ -53,14 +53,13 @@ func NewWorkerCmd() *cobra.Command {
 	or CLI flag:
 	$ k0s worker --token-file [path_to_file]
 	Note: Token can be passed either as a CLI argument or as a flag`,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			logrus.SetOutput(os.Stdout)
+			logrus.SetLevel(logrus.InfoLevel)
+			return config.CallParentPersistentPreRun(cmd, args)
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			c := Command(config.GetCmdOpts())
-
-			logrus.SetOutput(os.Stdout)
-			if !c.Debug {
-				logrus.SetLevel(logrus.InfoLevel)
-			}
-
 			if len(args) > 0 {
 				c.TokenArg = args[0]
 			}

commit ed4b78d03e438dfbdc7d0590d0f0521a3b80fc99

Use stdout provided by Cobra
As done for the other two subcommands during the review of d141f32f9.

Fixes: d141f32f9 (Early logging configuration)
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

cmd/api/api.go
@@ -64,7 +64,7 @@ func NewAPICmd() *cobra.Command {
 		Use:   "api",
 		Short: "Run the controller API",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			logrus.SetOutput(os.Stdout)
+			logrus.SetOutput(cmd.OutOrStdout())
 			logrus.SetLevel(logrus.InfoLevel)
 			return config.CallParentPersistentPreRun(cmd, args)
 		},