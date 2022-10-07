k0sproject/k0s

commit 8ea562661e1878490def5f6f52dd3eb59e303e75

Write manifests atomically
Use a temporary-write-and-rename approach to write manifests to disk, so
that the appliers never see partially written files. The strategy is as
follows: Create a temporary file in the same directory as the target,
write to it, sync and close the file handle, do a chmod and a rename. As
a result, there's no more need to ignore chmod events in the stack
applier. Those events actually *can* matter, when files become readable
or unreadable for the k0s process.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

internal/pkg/file/file.go
@@ -18,9 +18,13 @@ package file
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/k0sproject/k0s/internal/pkg/users"
+
+	"go.uber.org/multierr"
 )
 
 // Exists checks if a file exists and is not a directory before we
@@ -71,15 +75,89 @@ func Copy(src, dst string) error {
 	return nil
 }
 
+// WriteAtomically will atomically create or replace a file. The contents of the
+// file will be those that the write callback writes to the Writer that gets
+// passed in. The Writer will be unbuffered. WriteAtomically will buffer the
+// contents in a hidden (i.e. its name will start with a dot), temporary file
+// (it will have a .tmp extension). When write returns without an error, the
+// temporary file will be renamed to fileName, otherwise it will be deleted
+// without touching the target file.
+//
+// Note that this function is only best-effort on Windows:
+// https://github.com/golang/go/issues/22397#issuecomment-498856679
+func WriteAtomically(fileName string, perm os.FileMode, write func(file io.Writer) error) error {
+	var fd *os.File
+	fd, err := os.CreateTemp(filepath.Dir(fileName), fmt.Sprintf(".%s.*.tmp", filepath.Base(fileName)))
+	if err != nil {
+		return err
+	}
+
+	tmpFileName := fd.Name()
+	close := true
+	defer func() {
+		remove := err != nil
+		if close {
+			err = multierr.Append(err, fd.Close())
+		}
+		if remove {
+			err = multierr.Append(err, os.Remove(tmpFileName))
+		}
+	}()
+
+	err = write(fd)
+	if err != nil {
+		return err
+	}
+
+	// https://github.com/google/renameio/blob/v2.0.0/tempfile.go#L150-L157
+	err = fd.Sync()
+	if err != nil {
+		return err
+	}
+
+	err = fd.Close()
+	close = false
+	if err != nil {
+		return err
+	}
+
+	err = os.Chmod(tmpFileName, perm)
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(tmpFileName, fileName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// WriteContentAtomically will atomically create or replace a file with the
+// given content. WriteContentAtomically will create a hidden (i.e. its name
+// will start with a dot), temporary file (it will have a .tmp extension) with
+// the given content. Afterwards, the temporary file will be renamed to
+// fileName, otherwise it will be deleted without touching the target file.
+//
+// Note that this function is only best-effort on Windows:
+// https://github.com/golang/go/issues/22397#issuecomment-498856679
+func WriteContentAtomically(fileName string, content []byte, perm os.FileMode) error {
+	return WriteAtomically(fileName, perm, func(file io.Writer) error {
+		_, err := file.Write(content)
+		return err
+	})
+}
+
 func WriteTmpFile(data string, prefix string) (path string, err error) {
 	tmpFile, err := os.CreateTemp("", prefix)
 	if err != nil {
-		return "", fmt.Errorf("cannot create temporary file: %v", err)
+		return "", fmt.Errorf("cannot create temporary file: %w", err)
 	}
 
 	text := []byte(data)
 	if _, err = tmpFile.Write(text); err != nil {
-		return "", fmt.Errorf("failed to write to temporary file: %v", err)
+		return "", fmt.Errorf("failed to write to temporary file: %w", err)
 	}
 
 	return tmpFile.Name(), nil

internal/pkg/file/file_test.go
@@ -18,7 +18,6 @@ package file
 
 import (
 	"os"
-	"path"
 	"path/filepath"
 	"testing"
 
@@ -36,7 +35,7 @@ func TestExists(t *testing.T) {
 		t.Errorf("test non-existing: got %t, wanted %t", got, want)
 	}
 
-	existingFileName := path.Join(dir, "existing")
+	existingFileName := filepath.Join(dir, "existing")
 	require.NoError(t, os.WriteFile(existingFileName, []byte{}, 0644))
 
 	// test existing

go.mod
@@ -45,6 +45,7 @@ require (
 	go.etcd.io/etcd/client/pkg/v3 v3.5.4
 	go.etcd.io/etcd/client/v3 v3.5.4
 	go.etcd.io/etcd/etcdutl/v3 v3.5.4
+	go.uber.org/multierr v1.8.0
 	go.uber.org/zap v1.23.0
 	golang.org/x/crypto v0.0.0-20220824171710-5757bc0c5503
 	golang.org/x/exp v0.0.0-20220827204233-334a2380cb91
@@ -258,7 +259,6 @@ require (
 	go.opentelemetry.io/proto/otlp v0.11.0 // indirect
 	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect

internal/pkg/templatewriter/templatewriter.go
@@ -19,11 +19,11 @@ package templatewriter
 import (
 	"fmt"
 	"io"
-	"os"
 	"text/template"
 
 	"github.com/Masterminds/sprig"
 
+	"github.com/k0sproject/k0s/internal/pkg/file"
 	"github.com/k0sproject/k0s/pkg/constant"
 )
 
@@ -35,14 +35,9 @@ type TemplateWriter struct {
 	Path     string
 }
 
-// Write writes executes the template and writes the results on disk
+// Write executes the template and writes the results on disk
 func (p *TemplateWriter) Write() error {
-	podFile, err := os.OpenFile(p.Path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, constant.CertMode)
-	if err != nil {
-		return fmt.Errorf("failed to open pod file for %s: %w", p.Name, err)
-	}
-	defer podFile.Close()
-	return p.WriteToBuffer(podFile)
+	return file.WriteAtomically(p.Path, constant.CertMode, p.WriteToBuffer)
 }
 
 // WriteToBuffer writes executed template tot he given writer

pkg/applier/applier.go
@@ -24,7 +24,8 @@ import (
 	"path"
 	"path/filepath"
 
-	"github.com/sirupsen/logrus"
+	"github.com/k0sproject/k0s/pkg/kubernetes"
+
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/cli-runtime/pkg/resource"
@@ -33,7 +34,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
 
-	"github.com/k0sproject/k0s/pkg/kubernetes"
+	"github.com/sirupsen/logrus"
 )
 
 // manifestFilePattern is the glob pattern that all applicable manifest files need to match.
@@ -105,10 +106,12 @@ func (a *Applier) Apply(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
 	files, err := filepath.Glob(path.Join(a.Dir, manifestFilePattern))
 	if err != nil {
 		return err
 	}
+
 	resources, err := a.parseFiles(files)
 	if err != nil {
 		return err

pkg/applier/stackapplier.go
@@ -108,19 +108,17 @@ func (s *StackApplier) Run(ctx context.Context) error {
 }
 
 func (*StackApplier) triggersApply(event fsnotify.Event) bool {
-	// always let the initial apply happen
+	// Always let the initial apply happen
 	if event == (fsnotify.Event{}) {
 		return true
 	}
 
-	// ignore chmods (3845479a0)
-	if event.Op == fsnotify.Chmod {
+	// Only consider events on manifest files
+	if match, _ := filepath.Match(manifestFilePattern, filepath.Base(event.Name)); !match {
 		return false
 	}
 
-	// Only consider events on manifest files
-	match, _ := filepath.Match(manifestFilePattern, filepath.Base(event.Name))
-	return match
+	return true
 }
 
 func (s *StackApplier) apply(ctx context.Context) {

pkg/component/controller/manifests.go
@@ -19,11 +19,10 @@ package controller
 import (
 	"crypto/md5"
 	"fmt"
-	"os"
-	"path"
 	"path/filepath"
 
 	"github.com/k0sproject/k0s/internal/pkg/dir"
+	"github.com/k0sproject/k0s/internal/pkg/file"
 	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/sirupsen/logrus"
 )
@@ -36,10 +35,12 @@ type FsManifestsSaver struct {
 // Save saves given manifest under the given path
 func (f FsManifestsSaver) Save(dst string, content []byte) error {
 	target := filepath.Join(f.dir, dst)
-	if err := os.WriteFile(target, content, constant.ManifestsDirMode); err != nil {
-		return fmt.Errorf("can't write manifest %s: %v", target, err)
+
+	if err := file.WriteContentAtomically(target, content, constant.CertMode); err != nil {
+		return err
 	}
-	logrus.WithField("component", "manifest-saver").Debugf("succesfully wrote %s:%s", target, hash(content))
+
+	logrus.WithField("component", "manifest-saver").Debugf("Successfully wrote %s:%s", target, hash(content))
 	return nil
 }
 
@@ -49,7 +50,7 @@ func hash(data []byte) string {
 
 // NewManifestsSaver builds new filesystem manifests saver
 func NewManifestsSaver(manifest string, dataDir string) (*FsManifestsSaver, error) {
-	manifestDir := path.Join(dataDir, "manifests", manifest)
+	manifestDir := filepath.Join(dataDir, "manifests", manifest)
 	err := dir.Init(manifestDir, constant.ManifestsDirMode)
 	if err != nil {
 		return nil, err

commit 2d79c8cce9758fcc700988f9f569781867ab7428

Use named return values in WriteAtomically
The intention was to use a named return value, so that the deferred
function would be able to modify it. Without the named value, the error
handling in the deferred function has no effect.

Moreover: Add a bunch of tests and suppress any errors about failed
removals of temporary files indicating that the files don't exist.

Fixes: 8ea562661 ("Write manifests atomically")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

internal/pkg/file/file.go
@@ -17,8 +17,10 @@ limitations under the License.
 package file
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 
@@ -85,9 +87,9 @@ func Copy(src, dst string) error {
 //
 // Note that this function is only best-effort on Windows:
 // https://github.com/golang/go/issues/22397#issuecomment-498856679
-func WriteAtomically(fileName string, perm os.FileMode, write func(file io.Writer) error) error {
+func WriteAtomically(fileName string, perm os.FileMode, write func(file io.Writer) error) (err error) {
 	var fd *os.File
-	fd, err := os.CreateTemp(filepath.Dir(fileName), fmt.Sprintf(".%s.*.tmp", filepath.Base(fileName)))
+	fd, err = os.CreateTemp(filepath.Dir(fileName), fmt.Sprintf(".%s.*.tmp", filepath.Base(fileName)))
 	if err != nil {
 		return err
 	}
@@ -100,7 +102,13 @@ func WriteAtomically(fileName string, perm os.FileMode, write func(file io.Write
 			err = multierr.Append(err, fd.Close())
 		}
 		if remove {
-			err = multierr.Append(err, os.Remove(tmpFileName))
+			removeErr := os.Remove(tmpFileName)
+			// Don't propagate any fs.ErrNotExist errors. There is no point in
+			// doing this, since the desired state is already reached: The
+			// temporary file is no longer present on the file system.
+			if removeErr != nil && !errors.Is(err, fs.ErrNotExist) {
+				err = multierr.Append(err, removeErr)
+			}
 		}
 	}()
 

internal/pkg/file/file_test.go
@@ -17,13 +17,227 @@ limitations under the License.
 package file
 
 import (
+	"errors"
+	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/multierr"
 )
+
+func TestWriteAtomically(t *testing.T) {
+
+	t.Run("filePermissions", func(t *testing.T) {
+		for _, mode := range []os.FileMode{0400, 0755, 0644, 0777} {
+			modeStr := strconv.FormatUint(uint64(mode), 8)
+			t.Run(modeStr, func(t *testing.T) {
+				dir := t.TempDir()
+				file := filepath.Join(dir, "file")
+
+				require.NoError(t, WriteAtomically(file, mode, func(file io.Writer) error {
+					_, err := file.Write([]byte(modeStr))
+					return err
+				}))
+
+				content, err := os.ReadFile(file)
+				require.NoError(t, err)
+				assert.Equal(t, []byte(modeStr), content)
+				info, err := os.Stat(file)
+				if assert.NoError(t, err) {
+					assert.Equal(t, mode, info.Mode())
+				}
+			})
+		}
+	})
+
+	// Several tests about error handling and reporting. Some of them are a bit
+	// contrived and expect the Writer to be a File pointer in order to break
+	// things in ways usual consumers of the API wouldn't be able to do, as the
+	// interface doesn't make any guarantees about the actual type of the
+	// Writer.
+
+	assertPathError := func(t *testing.T, err error, op, dir string) (pathErr *os.PathError, ok bool) {
+		t.Helper()
+		if ok = assert.True(t, errors.As(err, &pathErr), "Not a PathError: %v", err); ok {
+			assert.Equal(t, op, pathErr.Op)
+			assert.Equal(
+				t, dir, filepath.Dir(pathErr.Path),
+				"Expected the temporary file to be in the same directory as the target file",
+			)
+		}
+		return
+	}
+
+	assertDirEmpty := func(t *testing.T, dir string) bool {
+		t.Helper()
+		entries, err := os.ReadDir(dir)
+		return assert.NoError(t, err) && assert.Empty(t, entries)
+	}
+
+	t.Run("writeFails", func(t *testing.T) {
+		dir := t.TempDir()
+		file := filepath.Join(dir, "file")
+		assert.Same(t, assert.AnError, WriteAtomically(file, 0644, func(file io.Writer) error {
+			return assert.AnError
+		}))
+		assertDirEmpty(t, dir)
+	})
+
+	t.Run("tempFileClosed", func(t *testing.T) {
+		// Prove that multiple errors may be reported.
+		dir := t.TempDir()
+		file := filepath.Join(dir, "file")
+
+		errs := multierr.Errors(WriteAtomically(file, 0644, func(file io.Writer) error {
+			c, ok := file.(io.Closer)
+			require.True(t, ok, "Not closeable: %T", file)
+			require.NoError(t, c.Close())
+			return nil
+		}))
+
+		assert.Len(t, errs, 2)
+		var tempPath string
+
+		// The first error should be about the failed attempt to sync the temporary file.
+		if err, ok := assertPathError(t, errs[0], "sync", dir); ok {
+			tempPath = err.Path
+			assert.True(t, errors.Is(err, fs.ErrClosed), "Expected fs.ErrClosed: %v", err.Err)
+		}
+
+		// The second error should be about the failed attempt to close the temporary file.
+		if err, ok := assertPathError(t, errs[1], "close", dir); ok {
+			if tempPath != "" {
+				assert.Equal(t, tempPath, err.Path, "Temp paths differ between errors")
+			}
+			assert.True(t, errors.Is(err, fs.ErrClosed), "Expected fs.ErrClosed: %v", err.Err)
+		}
+
+		assertDirEmpty(t, dir)
+	})
+
+	t.Run("tempFileRemoved", func(t *testing.T) {
+		// Prove that any fs.ErrNotExist removal errors are not propagated.
+		// There is no point in doing this, since the desired state is already
+		// reached: The temporary file is no longer present on the file system.
+		dir := t.TempDir()
+		file := filepath.Join(dir, "file")
+
+		var tempPath string
+		err := WriteAtomically(file, 0644, func(file io.Writer) error {
+			n, ok := file.(interface{ Name() string })
+			require.True(t, ok, "Doesn't have a name: %T", file)
+			tempPath = n.Name()
+			require.Equal(t, dir, filepath.Dir(tempPath))
+			require.NoError(t, os.Remove(tempPath))
+			return nil
+		})
+
+		assert.Len(t, multierr.Errors(err), 1)
+
+		// The error should be about the failed chmod.
+		if err, ok := assertPathError(t, err, "chmod", dir); ok {
+			assert.Equal(t, tempPath, err.Path, "Error refers to unexpected path")
+			assert.True(t, errors.Is(err, fs.ErrNotExist), "Expected fs.ErrNotExist: %v", err.Err)
+		}
+
+		assertDirEmpty(t, dir)
+	})
+
+	t.Run("pathObstructed", func(t *testing.T) {
+		dir := t.TempDir()
+		file := filepath.Join(dir, "file")
+
+		// Obstruct the file path, so that the rename fails.
+		require.NoError(t, os.Mkdir(file, 0700))
+
+		errs := multierr.Errors(WriteAtomically(file, 0644, func(file io.Writer) error {
+			_, err := file.Write([]byte("obstructed"))
+			return err
+		}))
+
+		require.Len(t, errs, 1)
+
+		var linkErr *os.LinkError
+		if assert.True(t, errors.As(errs[0], &linkErr), "Not a LinkError: %v", linkErr) {
+			assert.Equal(t, "rename", linkErr.Op)
+			assert.Equal(
+				t, dir, filepath.Dir(linkErr.Old),
+				"Expected the temporary file to be in the same directory as the target file",
+			)
+			assert.Equal(t, file, linkErr.New)
+			assert.True(t, errors.Is(linkErr.Err, fs.ErrExist), "Expected fs.ErrExist: %v", linkErr.Err)
+		}
+
+		// Expect just the single directory that was created in order to obstruct the file path.
+		if entries, err := os.ReadDir(dir); assert.NoError(t, err) && assert.Len(t, entries, 1) {
+			e := entries[0]
+			name := e.Name()
+			assert.Equal(t, filepath.Base(file), name)
+			assert.True(t, e.IsDir(), "Not a directory: %s", name)
+		}
+	})
+
+	t.Run("tempPathObstructed", func(t *testing.T) {
+		// Prove that any non fs.ErrNotExist removal errors are propagated correctly.
+		dir := t.TempDir()
+		file := filepath.Join(dir, "file")
+
+		// Obstruct the file path, so that the rename fails.
+		require.NoError(t, os.Mkdir(file, 0700))
+
+		var tempPath string
+		errs := multierr.Errors(WriteAtomically(file, 0755, func(file io.Writer) error {
+			n, ok := file.(interface{ Name() string })
+			require.True(t, ok, "Doesn't have a name: %T", file)
+			tempPath = n.Name()
+			require.Equal(t, dir, filepath.Dir(tempPath))
+
+			// Remove the temporary file ...
+			require.NoError(t, os.Remove(tempPath))
+			// ... obstruct the temporary file path ...
+			require.NoError(t, os.Mkdir(tempPath, 0700))
+			// .. and ensure that the directory can't be removed.
+			require.NoError(t, os.WriteFile(filepath.Join(tempPath, ".keep"), []byte{}, 0600))
+
+			return nil
+		}))
+
+		// The outcome here is a bit weird, since the chmod will actually
+		// succeed, but instead it will chmod the directory, not the file. So
+		// there's no chmod error expected. But the tests will fail if the file
+		// mode given to WriteAtomically won't have the executable bit set,
+		// since the automatic temporary directory cleanup won't handle this.
+
+		require.Len(t, errs, 2)
+
+		// The first error should be about the failed rename.
+		var linkErr *os.LinkError
+		if assert.True(t, errors.As(errs[0], &linkErr), "Not a LinkError: %v", linkErr) {
+			assert.Equal(t, "rename", linkErr.Op)
+			assert.Equal(t, tempPath, linkErr.Old, "Error refers to unexpected path")
+			assert.Equal(t, file, linkErr.New)
+			assert.True(t, errors.Is(linkErr.Err, fs.ErrExist), "Expected fs.ErrExist: %v", linkErr.Err)
+		}
+
+		// The second error should be about the failed removal.
+		if err, ok := assertPathError(t, errs[1], "remove", dir); ok {
+			assert.Equal(t, tempPath, err.Path, "Error refers to unexpected path")
+			assert.True(t, errors.Is(err, fs.ErrExist), "Expected fs.ErrExist: %v", err.Err)
+		}
+
+		// Expect to see two directories left behind.
+		if entries, err := os.ReadDir(dir); assert.NoError(t, err) && assert.Len(t, entries, 2) {
+			for _, e := range entries {
+				assert.True(t, e.IsDir(), "Not a directory: %s", e.Name())
+			}
+		}
+	})
+}
 
 func TestExists(t *testing.T) {
 	dir := t.TempDir()