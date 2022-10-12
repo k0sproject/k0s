619e0b6cb4e3f8c8d9300a060c7b0eca289add20
Alexey Makhov <amakhov@mirantis.com>
2022-10-12 10:24:19 +0300

    Run conformance tests on arm64
    Signed-off-by: Alexey Makhov <amakhov@mirantis.com>

--- inttest/sonobuoy/terraform/main.tf
+++ inttest/sonobuoy/terraform/main.tf
@@ -59,7 +59,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = [format("ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-%s-server-*", var.instance_arch)]
   }
 
   filter {
