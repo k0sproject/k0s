fa93fece6b5707b7f58b7d1ae6578e9ed4c60b1e
Tom Wieczorek <twieczorek@mirantis.com>
2022-11-11 09:00:23 +0100

    Bump Calico to v3.24.5
    https://projectcalico.docs.tigera.io/archive/v3.24/release-notes/#v3245
    
    This fixes a regression in 3.24.4
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/constant/constant_shared.go
+++ pkg/constant/constant_shared.go
@@ -85,7 +85,7 @@ const (
 	CoreDNSImage                       = "docker.io/coredns/coredns"
 	CoreDNSImageVersion                = "1.10.0"
 	CalicoImage                        = "docker.io/calico/cni"
-	CalicoComponentImagesVersion       = "v3.24.4"
+	CalicoComponentImagesVersion       = "v3.24.5"
 	CalicoNodeImage                    = "docker.io/calico/node"
 	KubeControllerImage                = "docker.io/calico/kube-controllers"
 	KubeRouterCNIImage                 = "docker.io/cloudnativelabs/kube-router"
