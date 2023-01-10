2ca969baf0525d005538cbf8ee0300f2fdbddefe
Tom Wieczorek <twieczorek@mirantis.com>
2023-01-10 13:56:29 +0100

    Add docs for node-local load balancing
    * Add a new docs section about node-local load balancing, explaining its
      use and limitations, along with a walkthrough in how to setup a nllb
      enabled cluster.
    * Add the new options to the configuration reference.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/apis/k0s.k0sproject.io/v1beta1/nllb.go
+++ pkg/apis/k0s.k0sproject.io/v1beta1/nllb.go
@@ -37,8 +37,9 @@ type NodeLocalLoadBalancing struct {
 	// +optional
 	Enabled bool `json:"enabled,omitempty"`
 
-	// type indicates the type of the node-local load balancer to deploy.
-	// Currently, the only supported type is "EnvoyProxy".
+	// type indicates the type of the node-local load balancer to deploy on
+	// worker nodes. Currently, the only supported type is "EnvoyProxy".
+	// +kubebuilder:default=EnvoyProxy
 	// +optional
 	Type NllbType `json:"type,omitempty"`
 
@@ -113,11 +114,11 @@ func (n *NodeLocalLoadBalancing) IsEnabled() bool {
 // EnvoyProxy describes configuration options required for using Envoy as the
 // backing implementation for node-local load balancing.
 type EnvoyProxy struct {
-	// image specifies the OCI image that's being used to run Envoy.
+	// image specifies the OCI image that's being used for the Envoy Pod.
 	// +optional
 	Image *ImageSpec `json:"image"`
 
-	// imagePullPolicy specifies the pull policy being used to run Envoy.
+	// imagePullPolicy specifies the pull policy being used for the Envoy Pod.
 	// Defaults to the default image pull policy.
 	// +kubebuilder:validation:Enum=Always;Never;IfNotPresent
 	// +optional
