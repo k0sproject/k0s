54bff00a3add842aa280c4cab7d0310426fa6e13
Tom Wieczorek <twieczorek@mirantis.com>
2023-01-18 14:50:00 +0100

    Fix gen-bindata unit test on Windows
    On Windows, files and directories which are in use cannot be deleted.
    The test switches the process working directory to a temporary folder
    managed by the go testing framework. That temporary dir will be
    attempted to be deleted after the test, which fails, since the process
    still uses it as its working directory.
    
    Fix that by introducing the new testutil.Chdir helper that can be
    deferred to switch back to the original working directory.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

---
+++ internal/testutil/os.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2022 k0s authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package testutil
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Chdir changes the current working directory to the named directory and
+// returns a function that, when called, restores the original working
+// directory.
+//
+// Use in tests like so:
+//
+//	tmpDir := t.TempDir()
+//	defer testutil.Chdir(t, tmpDir)()
+func Chdir(t *testing.T, dir string) func() {
+	wd, err := os.Getwd()
+	require.NoError(t, err)
+	require.NoError(t, os.Chdir(dir))
+	return func() {
+		require.NoError(t, os.Chdir(wd))
+	}
+}
