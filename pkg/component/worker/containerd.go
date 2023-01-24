d01438a02e08c8e35176241cd30ee8865cd1b4a7
Jussi Nummelin <jnummelin@mirantis.com>
2023-01-24 14:19:35 +0000

    Handle containerd imports in k0s side
    If an import containes CRI plugin, append all of those (including the defaul runc config) into a single file.
    If an import does not contain CRI plugin "stuff", add those as normal imports to containerd config
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- pkg/component/worker/containerd.go
+++ pkg/component/worker/containerd.go
@@ -17,10 +17,16 @@ limitations under the License.
 package worker
 
 import (
+	"bufio"
 	"bytes"
 	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
@@ -30,21 +36,27 @@ import (
 	"github.com/k0sproject/k0s/internal/pkg/templatewriter"
 	"github.com/k0sproject/k0s/pkg/assets"
 	"github.com/k0sproject/k0s/pkg/component/manager"
+	"github.com/k0sproject/k0s/pkg/component/worker/containerd"
 	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/k0sproject/k0s/pkg/supervisor"
 )
 
+const magicMarker = "# k0s_managed=true"
+
 const confTmpl = `
-# This is a placeholder configuration for k0s managed containerD.
-# If you wish to customize the config replace this file with your custom configuration.
+# k0s_managed=true
+# This is a placeholder configuration for k0s managed containerD. 
+# If you wish to override the config, remove the first line and replace this file with your custom configuration.
 # For reference see https://github.com/containerd/containerd/blob/main/docs/man/containerd-config.toml.5.md
 version = 2
 imports = [
-	"{{ .ImportsGlob }}"
+	{{- range $i := .Imports }}
+	"{{ $i }}",
+	{{- end }}
 ]
 `
 const confPath = "/etc/k0s/containerd.toml"
-const importsPath = "/etc/k0s/containerd_imports/*.toml"
+const importsPath = "/etc/k0s/containerd.d/"
 
 // ContainerD implement the component interface to manage containerd as k0s component
 type ContainerD struct {
@@ -96,8 +108,15 @@ func (c *ContainerD) Start(_ context.Context) error {
 }
 
 func (c *ContainerD) setupConfig() error {
-	// If the config file exists, use it as-is
-	if file.Exists(confPath) {
+	// Check if the config file is user managed
+	// If it is, we should not touch it
+	k0sManaged, err := isK0sManagedConfig(confPath)
+	if err != nil {
+		return err
+	}
+
+	if !k0sManaged {
+		logrus.Infof("containerd config file %s is not k0s managed, skipping config generation", confPath)
 		return nil
 	}
 
@@ -107,14 +126,20 @@ func (c *ContainerD) setupConfig() error {
 	if err := dir.Init(filepath.Dir(importsPath), 0755); err != nil {
 		return err
 	}
+	containerDConfigurer := containerd.NewConfigurer()
+
+	imports, err := containerDConfigurer.ResolveImports()
+	if err != nil {
+		return err
+	}
 	output := bytes.NewBuffer([]byte{})
 	tw := templatewriter.TemplateWriter{
 		Name:     "containerdconfig",
 		Template: confTmpl,
 		Data: struct {
-			ImportsGlob string
+			Imports []string
 		}{
-			ImportsGlob: importsPath,
+			Imports: imports,
 		},
 	}
 	if err := tw.WriteToBuffer(output); err != nil {
@@ -126,4 +151,65 @@ func (c *ContainerD) setupConfig() error {
 // Stop stops containerD
 func (c *ContainerD) Stop() error {
 	return c.supervisor.Stop()
+}
+
+// This is the md5sum of the default k0s containerd config file before 1.27
+const pre1_27ConfigSum = "59039b43303742a5496b13fd57f9beec"
+
+// isK0sManagedConfig checks if the config file is k0s managed
+// - If the config file does not exist, it's k0s managed
+// - If the config file md5sum matches the pre 1.27 config, it's k0s managed
+// - If the config file has the magic marker, it's k0s managed
+func isK0sManagedConfig(path string) (bool, error) {
+	// If the file does not exist, it's k0s managed (new install)
+	if !file.Exists(path) {
+		return true, nil
+	}
+	pre1_27Managed, err := isPre1_27ManagedConfig(path)
+	if err != nil {
+		return false, err
+	}
+	if pre1_27Managed {
+		return true, nil
+	}
+	// Check if the config file has the magic marker
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), magicMarker) {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func isPre1_27ManagedConfig(path string) (bool, error) {
+	// Check MD5 sum of the config file
+	// If it matches the pre 1.26 config, it's k0s managed
+	md5sum := md5.New()
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+
+	if _, err := io.Copy(md5sum, f); err != nil {
+		return false, err
+	}
+
+	sum := md5sum.Sum(nil)
+
+	pre1_26ConfigSumBytes, err := hex.DecodeString(pre1_27ConfigSum)
+	if err != nil {
+		return false, err
+	}
+
+	if bytes.Equal(pre1_26ConfigSumBytes, sum) {
+		return true, nil
+	}
+
+	return false, nil
 }
