1746401e0baeb518cd59c70470261785e3a1e9a4
Juan Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>
2023-02-09 19:03:47 +0100

    Fix copyright in the codegen
    The script validates the copyright of every go file that we own as a
    codegen target. I think the most logical place to do this is in the make
    lint, however auto generated files inject a copyright from a static
    file, so we need to modify the auto generated code before the linting.
    
    So I see two options:
    1- Verify everything in the codegen
    2- Replace the autogenerated parts in the codegen and verify everything
    in the lint.
    
    Since the lint depends on the codegen, I decided that the simplicity of
    having just one script with just one call was more worthy than the
    correctness.
    
    Co-authored-by: Tom Wieczorek <twz123@users.noreply.github.com>
    Signed-off-by: Juan Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>
    (cherry picked from commit b78cc097c026a8b8202f427f4947d92b6bbbd198)

---
+++ hack/copyright.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# Copyright 2023 k0s authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+RESULT=0
+FIX=${FIX:=n}
+
+get_year(){
+    # The -1 has to be added because if a file has been added, removed and added
+    # again we'll get two lines with both dates. We only pick the newest one.
+    YEAR=$(git log -1 --diff-filter=A --pretty=format:%ad --date=format:%Y -- "$1")
+    if [ -z "$YEAR" ]; then
+	echo "FATAL!! $1 doesn't seem to be commited in the repo" 1>&2
+        exit 1
+    fi
+    echo $YEAR
+}
+
+has_basic_copyright(){
+	FILE=$1
+	grep -q -F "Copyright k0s authors" "$FILE"
+}
+
+has_date_copyright(){
+	DATE=$1
+	FILE=$2
+	grep -q -F "Copyright $DATE k0s authors" "$FILE"
+}
+
+# Deliberately do not search in docs as the date of the matches for the
+# Copyright notice aren't related to the date of the document.
+for i in $(find cmd hack internal inttest pkg static -type f -name '*.go' -not -name 'zz_generated*'); do
+    DATE=$(get_year $i)
+
+    # codegen gets the header from a static file, so instead we'll replace it every time.
+    # Also fix every file if FIX=y
+    if [[ "$FIX" == 'y' ]] ; then
+        sed -i "s/Copyright 20../Copyright $DATE/" "$i"
+    fi
+
+    if [[ $i == "pkg/apis/k0s.k0sproject.io/clientset/"* ]] || [[ $i == "pkg/apis/autopilot.k0sproject.io/v1beta2/clientset/"* ]]; then
+        if ! has_basic_copyright "$i"; then
+            echo "ERROR: $i doesn't have a proper copyright notice" 1>&2
+            RESULT=1
+        fi
+    else
+        if ! has_date_copyright "$DATE" "$i"; then
+            echo "ERROR: $i doesn't have a proper copyright notice" 1>&2
+            RESULT=1
+        fi
+    fi
+done
+
+if [[ "$RESULT" != 0 ]]; then
+    if [[ "$FIX" == 'y' ]]; then
+        echo "hack/copyright.sh can't fix the problem automatically. Manual intervention is required"
+    else
+        echo "Retry running the script with FIX=y hack/copyright.sh to see if can be fixed automatically"
+    fi
+fi
+
+exit $RESULT
