4ddd7ef5a0d3cfb7e5e7430e2836f280437a0a99
Juan Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>
2023-02-09 17:25:32 +0100

    Add copyright to files without it
    Signed-off-by: Juan Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>

--- pkg/component/worker/calico_installer_windows.go
+++ pkg/component/worker/calico_installer_windows.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 k0s authors
+
+Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package worker
 
 import (
@@ -162,7 +178,7 @@ const installCalicoPowershell = `
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http:#www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
