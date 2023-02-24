4b9e0d3a667a317f6fca11e5034a19fac2642347
Mikhail Sakhnov <msakhnov@mirantis.com>
2023-02-24 10:20:17 +0000

    Use shared prober everywhere
    Signed-off-by: Mikhail Sakhnov <msakhnov@mirantis.com>

--- pkg/component/manager/manager_test.go
+++ pkg/component/manager/manager_test.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/k0sproject/k0s/pkg/component/prober"
+	proberPackage "github.com/k0sproject/k0s/pkg/component/prober"
 	"github.com/stretchr/testify/require"
 )
 
@@ -58,7 +58,7 @@ func (f *Fake) Ready() error {
 }
 
 func TestManagerSuccess(t *testing.T) {
-	m := New(prober.NopProber{})
+	m := New(proberPackage.NopProber{})
 	require.NotNil(t, m)
 
 	ctx := context.Background()
@@ -84,7 +84,7 @@ func TestManagerSuccess(t *testing.T) {
 }
 
 func TestManagerInitFail(t *testing.T) {
-	m := New(prober.NopProber{})
+	m := New(proberPackage.NopProber{})
 	require.NotNil(t, m)
 
 	ctx := context.Background()
@@ -104,7 +104,7 @@ func TestManagerInitFail(t *testing.T) {
 }
 
 func TestManagerRunFail(t *testing.T) {
-	m := New(prober.NopProber{})
+	m := New(proberPackage.NopProber{})
 	require.NotNil(t, m)
 
 	ctx := context.Background()
@@ -135,7 +135,7 @@ func TestManagerRunFail(t *testing.T) {
 }
 
 func TestManagerHealthyFail(t *testing.T) {
-	m := New(prober.NopProber{})
+	m := New(proberPackage.NopProber{})
 	require.NotNil(t, m)
 	m.ReadyWaitDuration = 1 * time.Millisecond
 
