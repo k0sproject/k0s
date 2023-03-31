e5901c36159899977939354f723aefe7bf52e360
Jussi Nummelin <jnummelin@mirantis.com>
2023-03-31 16:23:10 +0300

    Use separate streams for pod exec
    Using the same stream for both stdout and stderr causes empty results for even commands that are not failing and are expected to have output.
    I believe this can happen as the stderr stream is empty, and thus get's quickly to EOF, it stops (or otherwise closes) the stream copying.
    
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>
    (cherry picked from commit c2f54ba970e4cdfc4a2aa122deac955e5847d2a9)

--- inttest/common/pod.go
+++ inttest/common/pod.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"bytes"
+	"fmt"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
@@ -31,14 +32,16 @@ func PodExecCmdOutput(client kubernetes.Interface, config *restclient.Config, po
 		return "", err
 	}
 
-	var b bytes.Buffer
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
 	err = exec.Stream(remotecommand.StreamOptions{
-		Stdout: &b,
-		Stderr: &b,
+		Stdout: &stdout,
+		Stderr: &stderr,
 	})
+
 	if err != nil {
-		return b.String(), err
+		return stdout.String(), fmt.Errorf("%w: %s", err, stderr.String())
 	}
 
-	return b.String(), nil
+	return stdout.String(), nil
 }
