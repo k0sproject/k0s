ee5681af47c5f96997c43d2209e0fcb7576d60f2
Natanael Copa <ncopa@mirantis.com>
2023-04-04 15:26:45 +0200

    Add test for dir.GetAll
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- internal/pkg/dir/dir_test.go
+++ internal/pkg/dir/dir_test.go
@@ -27,6 +27,24 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestGetAll(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+
+		tmpDir := t.TempDir()
+		dirs, err := dir.GetAll(tmpDir)
+		require.NoError(t, err)
+		require.Empty(t, dirs)
+	})
+	t.Run("filter dirs", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		require.NoError(t, dir.Init(filepath.Join(tmpDir, "dir1"), 0750))
+		require.NoError(t, os.WriteFile(filepath.Join(tmpDir, "file1"), []byte{}, 0600), "Unable to create file %s:", "file1")
+		dirs, err := dir.GetAll(tmpDir)
+		require.NoError(t, err)
+		require.Equal(t, []string{"dir1"}, dirs)
+	})
+}
+
 func TestCopy_empty(t *testing.T) {
 	src := t.TempDir()
 	dst := t.TempDir()
