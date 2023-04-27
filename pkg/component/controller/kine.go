ceb87abd7260f0788487c2267a67dfb80b167825
Tom Wieczorek <twieczorek@mirantis.com>
2023-04-27 15:05:32 +0200

    Bump kine to v0.10.0
    https://github.com/k3s-io/kine/releases/tag/v0.10.0
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/component/controller/kine.go
+++ pkg/component/controller/kine.go
@@ -116,7 +116,7 @@ func (k *Kine) Start(ctx context.Context) error {
 			fmt.Sprintf("--endpoint=%s", k.Config.DataSource),
 			// NB: kine doesn't parse URLs properly, so construct potentially
 			// invalid URLs that are understood by kine.
-			// https://github.com/k3s-io/kine/blob/v0.9.9/pkg/endpoint/endpoint.go#L274-L282
+			// https://github.com/k3s-io/kine/blob/v0.10.0/pkg/endpoint/endpoint.go#L275-L283
 			fmt.Sprintf("--listen-address=unix://%s", k.K0sVars.KineSocketPath),
 		},
 		UID: k.uid,
