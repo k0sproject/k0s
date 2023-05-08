4bc6456a1cb3a9f0ecb94f44413abef0b06aec15
Natanael Copa <ncopa@mirantis.com>
2023-05-08 15:45:03 +0200

    Copyright: fix follow renames
    The --follow option will also follow files that are similar enough as
    copies. Tell git to only follow files that are at least 90% equal.
    
    fixes commit 84408bff720c (Copyright: follow renames)
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- hack/copyright.sh
+++ hack/copyright.sh
@@ -22,12 +22,12 @@ FIX=${FIX:=n}
 get_year(){
     # The -1 has to be added because if a file has been added, removed and added
     # again we'll get two lines with both dates. We only pick the newest one.
-    YEAR=$(TZ=UTC git log --follow -1 --diff-filter=A --pretty=format:%ad --date=format:%Y -- "$1")
+    YEAR=$(TZ=UTC git log --follow --find-copies=90% -1 --diff-filter=A --pretty=format:%ad --date=format:%Y -- "$1")
     if [ -z "$YEAR" ]; then
         YEAR=$(TZ=UTC date +%Y)
 	    echo "WARN: $1 doesn't seem to be commited in the repo, assuming $YEAR" 1>&2
     fi
-    echo $YEAR
+    echo "$YEAR"
 }
 
 has_basic_copyright(){
@@ -62,7 +62,7 @@ for i in $(find cmd hack internal inttest pkg static -type f -name '*.go' -not -
         fi
 
         if ! has_date_copyright "$DATE" "$i"; then
-          echo "ERROR: $i doesn't have a proper copyright notice" 1>&2
+          echo "ERROR: $i doesn't have a proper copyright notice. Expected $DATE" 1>&2
           RESULT=1
         fi
         ;;
