1bc3d589785870901820b77844401286634619e6
Tom Wieczorek <twieczorek@mirantis.com>
2023-05-10 12:43:04 +0200

    Make multicontroller inttest be fail-fast
    This is to prevent noisy subsequent test error logs.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- inttest/multicontroller/multicontroller_test.go
+++ inttest/multicontroller/multicontroller_test.go
@@ -34,27 +34,29 @@ func (s *MultiControllerSuite) TestK0sGetsUp() {
 	s.T().Logf("ip address: %s", ipAddress)
 
 	s.PutFile(s.ControllerNode(0), "/tmp/k0s.yaml", fmt.Sprintf(k0sConfigWithMultiController, ipAddress))
-	s.NoError(s.InitController(0, "--config=/tmp/k0s.yaml"))
+	s.Require().NoError(s.InitController(0, "--config=/tmp/k0s.yaml"))
 
 	token, err := s.GetJoinToken("controller")
 	s.Require().NoError(err)
 	s.PutFile(s.ControllerNode(1), "/tmp/k0s.yaml", fmt.Sprintf(k0sConfigWithMultiController, ipAddress))
-	s.NoError(s.InitController(1, "--config=/tmp/k0s.yaml", token))
+	s.Require().NoError(s.InitController(1, "--config=/tmp/k0s.yaml", token))
 
 	s.PutFile(s.ControllerNode(2), "/tmp/k0s.yaml", fmt.Sprintf(k0sConfigWithMultiController, ipAddress))
-	s.NoError(s.InitController(2, "--config=/tmp/k0s.yaml", token))
-	s.NoError(s.RunWorkers())
+	s.Require().NoError(s.InitController(2, "--config=/tmp/k0s.yaml", token))
+	s.Require().NoError(s.RunWorkers())
 
 	kc, err := s.KubeClient(s.ControllerNode(0))
 	s.Require().NoError(err)
 
 	err = s.WaitForNodeReady(s.WorkerNode(0), kc)
-	s.NoError(err)
+	s.Require().NoError(err)
 
-	s.AssertSomeKubeSystemPods(kc)
+	if !s.AssertSomeKubeSystemPods(kc) {
+		return
+	}
 
 	s.T().Log("waiting to see CNI pods ready")
-	s.NoError(common.WaitForKubeRouterReady(s.Context(), kc), "CNI did not start")
+	s.Require().NoError(common.WaitForKubeRouterReady(s.Context(), kc), "CNI did not start")
 }
 
 func TestMultiControllerSuite(t *testing.T) {
