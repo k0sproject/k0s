138489813ea77b1288bb4babdd35fe30556b1920
Natanael Copa <ncopa@mirantis.com>
2023-07-12 16:43:45 +0200

    Upgrade calico to 3.26.1
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- pkg/constant/constant_shared.go
+++ pkg/constant/constant_shared.go
@@ -84,7 +84,7 @@ const (
 	EnvoyProxyImage                    = "quay.io/k0sproject/envoy-distroless"
 	EnvoyProxyImageVersion             = "v1.24.1"
 	CalicoImage                        = "quay.io/k0sproject/calico-cni"
-	CalicoComponentImagesVersion       = "v3.24.5-1"
+	CalicoComponentImagesVersion       = "v3.26.1-0"
 	CalicoNodeImage                    = "quay.io/k0sproject/calico-node"
 	KubeControllerImage                = "quay.io/k0sproject/calico-kube-controllers"
 	KubeRouterCNIImage                 = "quay.io/k0sproject/kube-router"
