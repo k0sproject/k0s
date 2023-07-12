85786097e55048509a6bb67321998d3e3b43758e
Natanael Copa <ncopa@mirantis.com>
2023-07-12 12:54:25 +0200

    Fix calico.yaml URL in get-calico.sh script and docs
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- hack/get-calico.sh
+++ hack/get-calico.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 set -e
+CALICO_VERSION="$1"
 CSPLIT_BINARY="csplit"
+
+if [ -z "$CALICO_VERSION" ]; then
+  echo "usage: $0 <VERSION>"
+  exit 1
+fi
 
 # on MacOS, we need to use the homebrew coreutils (gnu-utils) version of csplit, named gcsplit by default
 case "$(uname -s)" in
   darwin*) CSPLIT_BINARY="gcsplit" ;;
 esac
+
+if ! command -v "$CSPLIT_BINARY" > /dev/null; then
+  echo "$CSPLIT_BINARY not found" >&2
+  exit 2
+fi
 
 PATH=$PATH:$GOPATH/bin
 
@@ -14,9 +25,8 @@ DIR="static/manifests/calico"
 
 mkdir -p $DIR
 
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
-
-$CSPLIT_BINARY --digits=2 --quiet --prefix=$DIR/ calico.yaml "/---/" "{*}"
+curl --silent -L "https://raw.githubusercontent.com/projectcalico/calico/v$CALICO_VERSION/manifests/calico.yaml" \
+  | $CSPLIT_BINARY --digits=2 --quiet --prefix=$DIR/ -- - "/---/" "{*}"
 
 for f in "$DIR"/*; do
   # skip directories
@@ -35,6 +45,3 @@ for f in "$DIR"/*; do
   mkdir -p "$DIR/$kind"
   mv "$f" "$DIR/$kind/$filename.yaml"
 done
-
-# cleanup
-rm calico.yaml
