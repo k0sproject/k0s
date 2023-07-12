b635e1b5620cb08632d24f5f39bebc062ec95f0e
Natanael Copa <ncopa@mirantis.com>
2023-07-12 12:33:03 +0200

    hack/get-calico.sh: use POSIX shell
    Make script POSIX shell compatible, and make it pass shellcheck without
    any warnings.
    
    No functional changes.
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- hack/get-calico.sh
+++ hack/get-calico.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 CSPLIT_BINARY="csplit"
 
 # on MacOS, we need to use the homebrew coreutils (gnu-utils) version of csplit, named gcsplit by default
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    CSPLIT_BINARY="gcsplit"
-fi
+case "$(uname -s)" in
+    darwin*) CSPLIT_BINARY="gcsplit" ;;
+esac
 
 PATH=$PATH:$GOPATH/bin
 
@@ -18,25 +18,23 @@ curl https://docs.projectcalico.org/manifests/calico.yaml -O
 
 $CSPLIT_BINARY --digits=2 --quiet --prefix=$DIR/ calico.yaml "/---/" "{*}"
 
-for f in $DIR/*
-do
+for f in "$DIR"/*; do
     # skip directories
-    if [ -d $f ]; then
+    if [ -d "$f" ]; then
         continue
     fi
 
-    filename=$(yq eval '.metadata.name' $f)
-    kind=$(yq eval '.kind' $f)
+    filename=$(yq eval '.metadata.name' "$f")
+    kind=$(yq eval '.kind' "$f")
 
-    if [[ $filename == "null" || $kind == "null" ]]; then
-        rm $f
+    if [ "$filename" = "null" ] || [ "$kind" = "null" ]; then
+        rm "$f"
         continue
     fi
     echo "Processing $kind $filename $f"
-    mkdir -p $DIR/$kind
-    mv $f $DIR/$kind/$filename.yaml
+    mkdir -p "$DIR/$kind"
+    mv "$f" "$DIR/$kind/$filename.yaml"
 done
-
 
 # cleanup
 rm calico.yaml
