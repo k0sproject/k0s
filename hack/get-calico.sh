38747105214f4977cb90fc31b29f3783e237f57f
Natanael Copa <ncopa@mirantis.com>
2023-07-12 12:47:16 +0200

    hack/get-calico.sh: fix indent
    Follow .editroconfig style.
    
    No functional changes
    
    Signed-off-by: Natanael Copa <ncopa@mirantis.com>

--- hack/get-calico.sh
+++ hack/get-calico.sh
@@ -5,7 +5,7 @@ CSPLIT_BINARY="csplit"
 
 # on MacOS, we need to use the homebrew coreutils (gnu-utils) version of csplit, named gcsplit by default
 case "$(uname -s)" in
-    darwin*) CSPLIT_BINARY="gcsplit" ;;
+  darwin*) CSPLIT_BINARY="gcsplit" ;;
 esac
 
 PATH=$PATH:$GOPATH/bin
@@ -19,21 +19,21 @@ curl https://docs.projectcalico.org/manifests/calico.yaml -O
 $CSPLIT_BINARY --digits=2 --quiet --prefix=$DIR/ calico.yaml "/---/" "{*}"
 
 for f in "$DIR"/*; do
-    # skip directories
-    if [ -d "$f" ]; then
-        continue
-    fi
+  # skip directories
+  if [ -d "$f" ]; then
+    continue
+  fi
 
-    filename=$(yq eval '.metadata.name' "$f")
-    kind=$(yq eval '.kind' "$f")
+  filename=$(yq eval '.metadata.name' "$f")
+  kind=$(yq eval '.kind' "$f")
 
-    if [ "$filename" = "null" ] || [ "$kind" = "null" ]; then
-        rm "$f"
-        continue
-    fi
-    echo "Processing $kind $filename $f"
-    mkdir -p "$DIR/$kind"
-    mv "$f" "$DIR/$kind/$filename.yaml"
+  if [ "$filename" = "null" ] || [ "$kind" = "null" ]; then
+    rm "$f"
+    continue
+  fi
+  echo "Processing $kind $filename $f"
+  mkdir -p "$DIR/$kind"
+  mv "$f" "$DIR/$kind/$filename.yaml"
 done
 
 # cleanup
