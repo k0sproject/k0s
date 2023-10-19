k0sproject/k0s

commit a5b617d4799ec2424930c67439a8b60ff1a8ec45

Improve CoreDNS affinity check in basic inttest
The inttest sometimes fails with the following message:

    basic_test.go:217:
        	Error Trace:	/home/gh-runner/actions-runner/_work/k0s/k0s/inttest/basic/basic_test.go:217
        	            				/home/gh-runner/actions-runner/_work/k0s/k0s/inttest/basic/basic_test.go:105
        	Error:      	Should not be: ""
        	Test:       	TestBasicSuite/TestK0sGetsUp

This indicates that the test doesn't wait until the CoreDNS Pods are properly
scheduled. Maybe because it simply checks too early, or because the cluster is
not healthy.

Replace the one-shot affinity check with a watch on the Pods that waits until
two of them get scheduled. Add some logging and a better error message.

Moreover: Add label selector functionality to the Kubernetes
Watcher in a similar manner as the field selectors are
implemented. Remove WithoutFieldSelector() as it can be replaced by
WithFieldSelector(fields.Everything()). Leave a note about this in the docs.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/kubernetes/watch/watcher.go
@@ -25,6 +25,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/utils/pointer"
@@ -48,6 +49,7 @@ type Watcher[T any] struct {
 
 	includeDeletions bool
 	fieldSelector    string
+	labelSelector    string
 	errorCallback    ErrorCallback
 }
 
@@ -94,6 +96,9 @@ func FromProvider[L VersionedResource, I any](provider Provider[L], itemsFromLis
 			return list.GetResourceVersion(), itemsFromList(list), nil
 		},
 		Watch: provider.Watch,
+
+		fieldSelector: fields.Everything().String(),
+		labelSelector: labels.Everything().String(),
 	}
 }
 
@@ -138,9 +143,13 @@ func (w *Watcher[T]) WithObjectName(name string) *Watcher[T] {
 	return w.WithFieldSelector(fields.OneTermEqualSelector(metav1.ObjectNameField, name))
 }
 
-// WithFieldSelector sets the given field selector for this Watcher. Refer to
-// the [concept] for a general introduction to field selectors. To gain an
-// overview of the supported values, have a look at the usages of
+// WithFieldSelector sets the given field selector for this Watcher. The default
+// is to match everything:
+//
+//	watcher.FromClient(...).WithFieldSelector(fields.Everything())
+//
+// Refer to the [concept] for a general introduction to field selectors. To gain
+// an overview of the supported values, have a look at the usages of
 // [k8s.io/apimachinery/pkg/runtime.Scheme.AddFieldLabelConversionFunc] in the
 // [Kubernetes codebase].
 //
@@ -151,11 +160,21 @@ func (w *Watcher[T]) WithFieldSelector(selector fields.Selector) *Watcher[T] {
 	return w
 }
 
-// WithoutFieldSelector clears any field selector from this Watcher.
-// This is the default.
-func (w *Watcher[T]) WithoutFieldSelector() *Watcher[T] {
-	w.fieldSelector = ""
+// WithLabelSelector sets the given label selector for this Watcher. The default
+// is to match everything:
+//
+//	watcher.FromClient(...).WithLabelSelector(labels.Everything())
+func (w *Watcher[T]) WithLabelSelector(selector labels.Selector) *Watcher[T] {
+	w.labelSelector = selector.String()
 	return w
+}
+
+// WithLabels sets this Watcher's label selector to match exactly the given Set.
+// A nil and empty Sets are considered equivalent to labels.Everything(). It
+// does not perform any validation, which means the server will reject the
+// request if the Set contains invalid values.
+func (w *Watcher[T]) WithLabels(l labels.Set) *Watcher[T] {
+	return w.WithLabelSelector(labels.SelectorFromSet(l))
 }
 
 // WithErrorCallback sets this Watcher's error callback. It's invoked every time
@@ -239,6 +258,7 @@ func (w *Watcher[T]) list(ctx context.Context, condition Condition[T]) (*startWa
 	defer cancel()
 	resourceVersion, items, err := w.List(ctx, metav1.ListOptions{
 		FieldSelector:  w.fieldSelector,
+		LabelSelector:  w.labelSelector,
 		TimeoutSeconds: pointer.Int64(maxListDurationSecs),
 	})
 	if err != nil {

inttest/basic/basic_test.go
@@ -18,6 +18,7 @@ package basic
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"testing"
 	"time"
@@ -25,9 +26,13 @@ import (
 	"github.com/k0sproject/k0s/inttest/common"
 	"github.com/k0sproject/k0s/pkg/apis/k0s.k0sproject.io/v1beta1"
 	"github.com/k0sproject/k0s/pkg/constant"
+	"github.com/k0sproject/k0s/pkg/kubernetes/watch"
 
 	certificatesv1 "k8s.io/api/certificates/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 
@@ -208,13 +213,30 @@ func (s *BasicSuite) verifyContainerdDefaultConfig() {
 }
 
 func (s *BasicSuite) verifyCoreDNSAntiAffinity(kc *kubernetes.Clientset) {
-	opts := metav1.ListOptions{
-		LabelSelector: "k8s-app=kube-dns",
-	}
-	pods, err := kc.CoreV1().Pods("kube-system").List(s.Context(), opts)
-	s.NoError(err)
-	s.Equal(2, len(pods.Items))
-	s.NotEqual(pods.Items[0].Spec.NodeName, pods.Items[1].Spec.NodeName)
+	// Wait until both CoreDNs Pods got assigned to a node
+	pods := map[string]types.UID{}
+
+	s.NoError(watch.Pods(kc.CoreV1().Pods("kube-system")).
+		WithLabels(labels.Set{"k8s-app": "kube-dns"}).
+		WithErrorCallback(common.RetryWatchErrors(s.T().Logf)).
+		Until(s.Context(), func(pod *corev1.Pod) (bool, error) {
+			// Keep waiting if there's no node assigned yet.
+			nodeName := pod.Spec.NodeName
+			if nodeName == "" {
+				s.T().Logf("Pod %s not scheduled yet: %+v", pod.ObjectMeta.Name, pod.Status)
+				return false, nil
+			}
+
+			uid := pod.GetUID()
+			if prevUID, ok := pods[nodeName]; ok && uid != prevUID {
+				return false, errors.New("multiple CoreDNS pods scheduled on the same node")
+			}
+
+			s.T().Logf("Pod %s scheduled on %s", pod.ObjectMeta.Name, pod.Spec.NodeName)
+
+			pods[nodeName] = pod.GetUID()
+			return len(pods) > 1, nil
+		}))
 }
 
 func TestBasicSuite(t *testing.T) {

commit 75e4230bbce3246a26dbd875f616dd3612322878

Add label selector also when watching
The watcher was not respecting its label selector when watching, only
when listing.

Fixes: a5b617d47 ("Improve CoreDNS affinity check in basic inttest")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/kubernetes/watch/watcher.go
@@ -288,6 +288,7 @@ func (w *Watcher[T]) watch(ctx context.Context, resourceVersion string, conditio
 		ResourceVersion:     resourceVersion,
 		AllowWatchBookmarks: true,
 		FieldSelector:       w.fieldSelector,
+		LabelSelector:       w.labelSelector,
 		TimeoutSeconds:      pointer.Int64(maxWatchDurationSecs),
 	})
 	if err != nil {