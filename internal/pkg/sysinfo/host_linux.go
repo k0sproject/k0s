k0sproject/k0s

commit 0f474b8f98073cebdbbe85441d719bffb5a0d952

Some new sysinfo probes
* /proc file system mounted
* Executable in path: modprobe
* $(ulimit -n) -ge 65536

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

internal/pkg/sysinfo/host_linux.go
@@ -38,11 +38,15 @@ func (s *K0sSysinfoSpec) addHostSpecificProbes(p probes.Probes) {
 		return ""
 	})
 
-	s.addKernelConfigs(linux)
+	linux.AssertProcessMaxFileDescriptors(65536)
 
 	if s.WorkerRoleEnabled {
+		probes.AssertExecutablesInPath(linux, "modprobe")
+		linux.RequireProcFS()
 		addCgroups(linux)
 	}
+
+	s.addKernelConfigs(linux)
 }
 
 func (s *K0sSysinfoSpec) addKernelConfigs(linux *linux.LinuxProbes) {

internal/pkg/sysinfo/probes/executables.go
@@ -1,0 +1,38 @@
+/*
+Copyright 2022 k0s authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package probes
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func AssertExecutablesInPath(p Probes, executables ...string) {
+	for _, executable := range executables {
+		p.Set(fmt.Sprintf("executableInPath:%s", executable), func(path ProbePath, _ Probe) Probe {
+			return ProbeFn(func(r Reporter) error {
+				desc := NewProbeDesc(fmt.Sprintf("Executable in path: %s", executable), path)
+				path, err := exec.LookPath(executable)
+				if err != nil {
+					return r.Warn(desc, ErrorProp(err), "")
+				}
+
+				return r.Pass(desc, StringProp(path))
+			})
+		})
+	}
+}

internal/pkg/sysinfo/probes/linux/procfs.go
@@ -1,0 +1,60 @@
+//go:build linux
+// +build linux
+
+/*
+Copyright 2022 k0s authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package linux
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+
+	"github.com/k0sproject/k0s/internal/pkg/sysinfo/probes"
+)
+
+func (l *LinuxProbes) RequireProcFS() {
+	l.Set("procfs", func(path probes.ProbePath, _ probes.Probe) probes.Probe {
+		return probes.ProbeFn(func(r probes.Reporter) error {
+			mountPoint := "/proc"
+			desc := probes.NewProbeDesc(fmt.Sprintf("%s file system", mountPoint), path)
+
+			var st syscall.Statfs_t
+			if err := syscall.Statfs(mountPoint, &st); err != nil {
+				if os.IsNotExist(err) {
+					return r.Reject(desc, probes.ErrorProp(err), "")
+				}
+
+				return r.Error(desc, fmt.Errorf("failed to statfs %q: %w", mountPoint, err))
+			}
+
+			if st.Type != unix.PROC_SUPER_MAGIC {
+				return r.Reject(desc, procFSType(st.Type), "unexpected file system type")
+			}
+
+			return r.Pass(desc, procFSType(st.Type))
+		})
+	})
+}
+
+type procFSType int64
+
+func (t procFSType) String() string {
+	return fmt.Sprintf("mounted (0x%x)", int64(t))
+}

internal/pkg/sysinfo/probes/linux/ulimit.go
@@ -1,0 +1,47 @@
+//go:build linux
+// +build linux
+
+/*
+Copyright 2022 k0s authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package linux
+
+import (
+	"fmt"
+	"syscall"
+
+	"github.com/k0sproject/k0s/internal/pkg/sysinfo/probes"
+)
+
+func (l *LinuxProbes) AssertProcessMaxFileDescriptors(min uint64) {
+	l.Set("RLIMIT_NOFILE", func(path probes.ProbePath, _ probes.Probe) probes.Probe {
+		return probes.ProbeFn(func(r probes.Reporter) error {
+			desc := probes.NewProbeDesc("Max. file descriptors per process", path)
+
+			var rlimit syscall.Rlimit
+			if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
+				return r.Warn(desc, probes.ErrorProp(err), "")
+			}
+
+			prop := probes.StringProp(fmt.Sprintf("current: %d / max: %d", rlimit.Cur, rlimit.Max))
+			if rlimit.Cur < min {
+				return r.Warn(desc, prop, fmt.Sprintf("< %d", min))
+			}
+
+			return r.Pass(desc, prop)
+		})
+	})
+}

internal/pkg/sysinfo/probes/probes.go
@@ -74,6 +74,21 @@ type ProbedProp interface {
 	String() string
 }
 
+// StringProp is a convenience way of reporting an arbitrary string as a probed property.
+type StringProp string
+
+func (s StringProp) String() string {
+	return string(s)
+}
+
+// ErrorProp is a convenience way of reporting an arbitrary error as a probed property.
+func ErrorProp(err error) interface {
+	ProbedProp
+	error
+} {
+	return errorProp{err}
+}
+
 // Reporter receives the outcome of probes.
 type Reporter interface {
 	// Pass informs about a probe that passed.
@@ -114,6 +129,16 @@ type probeDesc struct {
 
 func (d *probeDesc) Path() ProbePath     { return d.path }
 func (d *probeDesc) DisplayName() string { return d.name }
+
+type errorProp struct{ error }
+
+func (e errorProp) String() string {
+	return e.Error()
+}
+
+func (e errorProp) Unwrap() error {
+	return e.error
+}
 
 type probes struct {
 	path   ProbePath

commit c2d5e87d78e47185a2487d8bdcbd4ee52ea710d5

Fix "executable in PATH" sysprobe
The loop over the varargs was not capturing the loop variable correctly,
so all the probes would check the same binary. Remove the loop and let
callers call the function multiple times for each executable. This is
less error prone. That bug was first introduced in 0f474b8f9, but didn't
become effective before 0f474b8f9.

Fixes: 0f474b8f9 ("Some new sysinfo probes")
Fixes: 19c800dfa ("Add sysprobe and some docs for required mount/umount")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

internal/pkg/sysinfo/host_linux.go
@@ -39,7 +39,9 @@ func (s *K0sSysinfoSpec) addHostSpecificProbes(p probes.Probes) {
 	linux.AssertAppArmor()
 
 	if s.WorkerRoleEnabled {
-		probes.AssertExecutablesInPath(linux, "modprobe", "mount", "umount")
+		probes.AssertExecutableInPath(linux, "modprobe")
+		probes.AssertExecutableInPath(linux, "mount")
+		probes.AssertExecutableInPath(linux, "umount")
 		linux.RequireProcFS()
 		addCgroups(linux)
 	}

internal/pkg/sysinfo/probes/executables.go
@@ -21,18 +21,16 @@ import (
 	"os/exec"
 )
 
-func AssertExecutablesInPath(p Probes, executables ...string) {
-	for _, executable := range executables {
-		p.Set(fmt.Sprintf("executableInPath:%s", executable), func(path ProbePath, _ Probe) Probe {
-			return ProbeFn(func(r Reporter) error {
-				desc := NewProbeDesc(fmt.Sprintf("Executable in path: %s", executable), path)
-				path, err := exec.LookPath(executable)
-				if err != nil {
-					return r.Warn(desc, ErrorProp(err), "")
-				}
+func AssertExecutableInPath(p Probes, executable string) {
+	p.Set(fmt.Sprintf("executableInPath:%s", executable), func(path ProbePath, _ Probe) Probe {
+		return ProbeFn(func(r Reporter) error {
+			desc := NewProbeDesc(fmt.Sprintf("Executable in PATH: %s", executable), path)
+			path, err := exec.LookPath(executable)
+			if err != nil {
+				return r.Warn(desc, ErrorProp(err), "")
+			}
 
-				return r.Pass(desc, StringProp(path))
-			})
+			return r.Pass(desc, StringProp(path))
 		})
-	}
+	})
 }