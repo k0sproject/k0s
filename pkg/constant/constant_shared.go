269a479c9ffd2a33981086f06a504a50ecc9410e
Tom Wieczorek <twieczorek@mirantis.com>
2023-10-26 15:28:57 +0200

    Bump envoy to v1.27.1
    Fixes CVE-2023-44487.
    
    https://github.com/envoyproxy/envoy/releases/tag/v1.27.1
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/constant/constant_shared.go
+++ pkg/constant/constant_shared.go
@@ -82,7 +82,7 @@ const (
 	CoreDNSImage                       = "quay.io/k0sproject/coredns"
 	CoreDNSImageVersion                = "1.11.1"
 	EnvoyProxyImage                    = "quay.io/k0sproject/envoy-distroless"
-	EnvoyProxyImageVersion             = "v1.24.1"
+	EnvoyProxyImageVersion             = "v1.27.1"
 	CalicoImage                        = "quay.io/k0sproject/calico-cni"
 	CalicoComponentImagesVersion       = "v3.26.1-1"
 	CalicoNodeImage                    = "quay.io/k0sproject/calico-node"
