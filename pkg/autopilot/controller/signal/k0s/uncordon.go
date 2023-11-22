b2daf6f2147d7d20c9a022075940511bb4ffd833
danj-replicated <danj@replicated.com>
2023-11-22 17:13:33 +0000

    fetch node object from apiserver when (un)cordoning
    Signed-off-by: danj-replicated <danj@replicated.com>
    (cherry picked from commit c3c94ea09115c89e3d21f7e2a2e76af5191c4255)
    (cherry picked from commit f9184f4e76e57c7c185f7cdf57fc7958a8519f5f)

--- pkg/autopilot/controller/signal/k0s/uncordon.go
+++ pkg/autopilot/controller/signal/k0s/uncordon.go
@@ -146,9 +146,11 @@ func (r *uncordoning) moveToNextState(ctx context.Context, signalNode crcli.Obje
 // unCordonNode un-cordons a node
 func (r *uncordoning) unCordonNode(ctx context.Context, signalNode crcli.Object) error {
 	logger := r.log.WithField("signalnode", signalNode.GetName()).WithField("phase", "uncordon")
-	node, ok := signalNode.(*corev1.Node)
-	if !ok {
-		return fmt.Errorf("failed to covert signalNode to Node")
+
+	//get node from client
+	node := &corev1.Node{}
+	if err := r.client.Get(ctx, crcli.ObjectKey{Name: signalNode.GetName()}, node); err != nil {
+		return fmt.Errorf("failed to get node: %w", err)
 	}
 
 	drainer := &drain.Helper{
