f408377066fb411304d4632de42e01a3ecec3fd0
Tom Wieczorek <twieczorek@mirantis.com>
2024-04-03 17:14:46 +0200

    Make the bridge cleanup step optional based on OS
    The bridge build step was a no-op on non-Linux systems. Instead of
    adding and executing a no-op step, exclude it from the list of steps
    altogether if it's not supported. Have a factory function that can
    return a step or nil, depending on whether the bridge cleanup is
    supported or not. Remove the dummy implementation entirely.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/cleanup/bridge_linux.go
+++ pkg/cleanup/bridge_linux.go
@@ -22,15 +22,19 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-type bridge struct{}
+func newBridgeStep() Step {
+	return linuxBridge{}
+}
+
+type linuxBridge struct{}
 
 // Name returns the name of the step
-func (b *bridge) Name() string {
+func (linuxBridge) Name() string {
 	return "kube-bridge leftovers cleanup step"
 }
 
 // Run removes found kube-bridge leftovers
-func (b *bridge) Run() error {
+func (linuxBridge) Run() error {
 	lnks, err := netlink.LinkList()
 	if err != nil {
 		return fmt.Errorf("failed to get link list from netlink: %w", err)
