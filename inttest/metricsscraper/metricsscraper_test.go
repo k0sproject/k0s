52515f18eabefe5349e8c4c25d5c974f702073c3
Tom Wieczorek <twieczorek@mirantis.com>
2024-05-15 17:47:36 +0200

    Parse metrics in metricsscraper job
    Use the JSON API and look for the push_time_seconds metric. Collect all
    job labels that had successful pushes.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit 2de1b0fad1e7818fa78a29ce29ff445d7841fd28)
    (cherry picked from commit ba588a6ba5c38fdd93048733ab5b81930da35d8b)

--- inttest/metricsscraper/metricsscraper_test.go
+++ inttest/metricsscraper/metricsscraper_test.go
@@ -18,14 +18,16 @@ package metricsscraper
 
 import (
 	"context"
-	"strings"
+	"encoding/json"
+	"slices"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/suite"
-
 	"github.com/k0sproject/k0s/inttest/common"
+
 	"k8s.io/apimachinery/pkg/util/wait"
+
+	"github.com/stretchr/testify/suite"
 )
 
 type MetricsScraperSuite struct {
@@ -45,7 +47,7 @@ func (s *MetricsScraperSuite) TestK0sGetsUp() {
 	s.Require().NoError(s.waitForPushgateway())
 
 	s.T().Logf("Waiting for metrics")
-	s.Require().NoError(s.waitForMetrics())
+	s.Require().NoError(s.waitForMetrics("kube-scheduler", "kube-controller-manager", "etcd"))
 }
 
 func (s *MetricsScraperSuite) waitForPushgateway() error {
@@ -57,22 +59,61 @@ func (s *MetricsScraperSuite) waitForPushgateway() error {
 	return common.WaitForDeployment(s.Context(), kc, "k0s-pushgateway", "k0s-system")
 }
 
-func (s *MetricsScraperSuite) waitForMetrics() error {
+func (s *MetricsScraperSuite) waitForMetrics(expectedJobs ...string) error {
 	kc, err := s.KubeClient(s.ControllerNode(0))
 	if err != nil {
 		return err
 	}
 
-	return wait.PollImmediateUntilWithContext(s.Context(), 5*time.Second, func(ctx context.Context) (done bool, err error) {
+	slices.Sort(expectedJobs)
 
-		b, err := kc.RESTClient().Get().AbsPath("/api/v1/namespaces/k0s-system/services/http:k0s-pushgateway:http/proxy/metrics").DoRaw(s.Context())
+	return wait.PollUntilContextCancel(s.Context(), 5*time.Second, true, func(ctx context.Context) (done bool, err error) {
+		b, err := kc.RESTClient().Get().AbsPath("/api/v1/namespaces/k0s-system/services/http:k0s-pushgateway:http/proxy/api/v1/metrics").DoRaw(s.Context())
 		if err != nil {
 			return false, nil
 		}
 
-		// wait for kube-scheduler and kube-controller-manager metrics
-		output := string(b)
-		return strings.Contains(output, `job="kube-scheduler"`) && strings.Contains(output, `job="kube-controller-manager"`) && strings.Contains(output, `job="etcd"`), nil
+		var metrics struct {
+			Data []struct {
+				// Last Unix time when changing this group in the Pushgateway succeeded.
+				PushTimeSeconds struct {
+					Metrics []struct {
+						Labels map[string]string `json:"labels"`
+						Value  string            `json:"value"`
+					} `json:"metrics"`
+				} `json:"push_time_seconds"`
+			} `json:"data"`
+			Status string `json:"status"`
+		}
+		if err := json.Unmarshal(b, &metrics); err != nil {
+			s.T().Log(err)
+			return false, nil
+		}
+
+		if metrics.Status != "success" {
+			return false, err
+		}
+
+		// Collect all the jobs that had successful pushes.
+		var jobs []string
+		for i := range metrics.Data {
+			pts := &metrics.Data[i].PushTimeSeconds
+			for i := range pts.Metrics {
+				if job, ok := pts.Metrics[i].Labels["job"]; ok {
+					if pts.Metrics[i].Value > "0" {
+						if idx, found := slices.BinarySearch(jobs, job); !found {
+							jobs = slices.Insert(jobs, idx, job)
+						}
+						break
+					}
+				}
+			}
+		}
+
+		s.T().Log("Jobs:", jobs)
+
+		// Return if the job lists match.
+		return slices.Equal(expectedJobs, jobs), nil
 	})
 }
 
