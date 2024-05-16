4e7b82aa490911f9589dfe4d74c8024bddfba060
Juan-Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>
2024-05-16 13:51:08 +0200

    Make k0s reset fail if it can't reach containerd
    Prior to this commit, if the containerd unix socket wasn't listening
    grpc.Dial would try to connect forever.
    
    This commit establishes the connection in the background and the actual
    call will fail if it has to.
    
    Also we implement a single context for all the operations so that we can
    cancel the execution with control c.
    
    Co-authored-by: Tom Wieczorek <twz123@users.noreply.github.com>
    Signed-off-by: Juan-Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>

--- pkg/container/runtime/runtime.go
+++ pkg/container/runtime/runtime.go
@@ -16,12 +16,15 @@ limitations under the License.
 
 package runtime
 
-import "net/url"
+import (
+	"context"
+	"net/url"
+)
 
 type ContainerRuntime interface {
-	ListContainers() ([]string, error)
-	RemoveContainer(id string) error
-	StopContainer(id string) error
+	ListContainers(ctx context.Context) ([]string, error)
+	RemoveContainer(ctx context.Context, id string) error
+	StopContainer(ctx context.Context, id string) error
 }
 
 func NewContainerRuntime(runtimeEndpoint *url.URL) ContainerRuntime {
