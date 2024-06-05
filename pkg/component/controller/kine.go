0c6e9d89143f43a840e47ca4fafbe00d5002c0ef
Tom Wieczorek <twieczorek@mirantis.com>
2024-06-05 17:23:20 +0200

    Bump kine to v0.11.10
    https://github.com/k3s-io/kine/releases/tag/v0.11.10
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit 5b66d27b25f35f57ae3fd7fee4a29f6b92750888)

--- pkg/component/controller/kine.go
+++ pkg/component/controller/kine.go
@@ -116,7 +116,7 @@ func (k *Kine) Start(ctx context.Context) error {
 			fmt.Sprintf("--endpoint=%s", k.Config.DataSource),
 			// NB: kine doesn't parse URLs properly, so construct potentially
 			// invalid URLs that are understood by kine.
-			// https://github.com/k3s-io/kine/blob/v0.10.1/pkg/endpoint/endpoint.go#L277-L285
+			// https://github.com/k3s-io/kine/blob/v0.11.10/pkg/endpoint/endpoint.go#L250-L258
 			fmt.Sprintf("--listen-address=unix://%s", k.K0sVars.KineSocketPath),
 			// Enable metrics on port 2380. The default is 8080, which clashes with kube-router.
 			"--metrics-bind-address=:2380",
