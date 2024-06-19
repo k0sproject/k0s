6c009c2c759dbcaa21116721a0934841b1e923f7
Tom Wieczorek <twieczorek@mirantis.com>
2024-06-19 17:35:03 +0200

    Enable and document the insecure field for Helm Repos
    The insecure field exists in the CRD for a long time already. Wire it
    up, so it can actually be used to enable TLS certificate checks.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit b6afccf1ab1b7b965eea8322072aaec60fa022ff)

--- pkg/helm/helm.go
+++ pkg/helm/helm.go
@@ -128,7 +128,7 @@ func (hc *Commands) AddRepository(repoCfg v1beta1.Repository) error {
 		CertFile:              repoCfg.CertFile,
 		KeyFile:               repoCfg.KeyFile,
 		CAFile:                repoCfg.CAFile,
-		InsecureSkipTLSverify: true,
+		InsecureSkipTLSverify: repoCfg.IsInsecure(),
 	}
 
 	r, err := repo.NewChartRepository(&c, getters)
