2b4ff4944cb11cbde9e0689e7f1571eb4f2f3aae
Tom Wieczorek <twieczorek@mirantis.com>
2024-06-20 17:50:11 +0200

    Support API resource discovery in fake client
    Take all the GVKs out of the scheme and convert those to some API
    resource lists to be added to the discovery client. The conversion is
    just a best guess, but will suffice for testing needs.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- internal/testutil/kube_client.go
+++ internal/testutil/kube_client.go
@@ -18,10 +18,18 @@ package testutil
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 
-	"k8s.io/client-go/rest"
+	k0sscheme "github.com/k0sproject/k0s/pkg/client/clientset/scheme"
+	etcdv1beta1 "github.com/k0sproject/k0s/pkg/client/clientset/typed/etcd/v1beta1"
+	k0sv1beta1 "github.com/k0sproject/k0s/pkg/client/clientset/typed/k0s/v1beta1"
+	kubeutil "github.com/k0sproject/k0s/pkg/kubernetes"
 
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/discovery/cached/memory"
 	discoveryfake "k8s.io/client-go/discovery/fake"
@@ -30,27 +38,31 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
 	kubernetesscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	restfake "k8s.io/client-go/rest/fake"
-	kubetesting "k8s.io/client-go/testing"
-
-	etcdMemberClient "github.com/k0sproject/k0s/pkg/client/clientset/typed/etcd/v1beta1"
-	cfgClient "github.com/k0sproject/k0s/pkg/client/clientset/typed/k0s/v1beta1"
-	kubeutil "github.com/k0sproject/k0s/pkg/kubernetes"
 )
 
 var _ kubeutil.ClientFactoryInterface = (*FakeClientFactory)(nil)
 
 // NewFakeClientFactory creates new client factory which uses internally only the kube fake client interface
 func NewFakeClientFactory(objects ...runtime.Object) *FakeClientFactory {
-	scheme := kubernetesscheme.Scheme
+	// Create a scheme containing all the kinds and types that k0s knows about.
+	scheme := runtime.NewScheme()
+	utilruntime.Must(kubernetesscheme.AddToScheme(scheme))
+	utilruntime.Must(k0sscheme.AddToScheme(scheme))
 
-	rawDiscovery := &discoveryfake.FakeDiscovery{Fake: &kubetesting.Fake{}}
+	// Create a dynamic fake client that can deal with all that.
+	fakeDynamic := dynamicfake.NewSimpleDynamicClient(scheme, objects...)
+	fakeDynamic.Resources = makeAPIResourceLists(scheme)
+
+	// Create a fake discovery client backed by the dynamic fake client.
+	fakeDiscovery := &discoveryfake.FakeDiscovery{Fake: &fakeDynamic.Fake}
 
 	return &FakeClientFactory{
 		Client:          fake.NewSimpleClientset(objects...),
-		DynamicClient:   dynamicfake.NewSimpleDynamicClient(scheme),
-		DiscoveryClient: memory.NewMemCacheClient(rawDiscovery),
-		RawDiscovery:    rawDiscovery,
+		DynamicClient:   fakeDynamic,
+		DiscoveryClient: memory.NewMemCacheClient(fakeDiscovery),
+		RawDiscovery:    fakeDiscovery,
 		RESTClient:      &restfake.RESTClient{},
 	}
 }
@@ -75,7 +87,7 @@ func (f *FakeClientFactory) GetDiscoveryClient() (discovery.CachedDiscoveryInter
 	return f.DiscoveryClient, nil
 }
 
-func (f *FakeClientFactory) GetConfigClient() (cfgClient.ClusterConfigInterface, error) {
+func (f *FakeClientFactory) GetConfigClient() (k0sv1beta1.ClusterConfigInterface, error) {
 	return nil, fmt.Errorf("NOT IMPLEMENTED")
 }
 
@@ -86,6 +98,57 @@ func (f *FakeClientFactory) GetRESTConfig() *rest.Config {
 	return &rest.Config{}
 }
 
-func (f *FakeClientFactory) GetEtcdMemberClient() (etcdMemberClient.EtcdMemberInterface, error) {
+func (f *FakeClientFactory) GetEtcdMemberClient() (etcdv1beta1.EtcdMemberInterface, error) {
 	return nil, fmt.Errorf("NOT IMPLEMENTED")
+}
+
+// Extracts all kinds from scheme and builds API resource lists for fake discovery clients.
+func makeAPIResourceLists(scheme *runtime.Scheme) (allResources []*metav1.APIResourceList) {
+	// Create the array of API resource lists. Ensure that the preferred version
+	// of any given group comes first. This is important for the fake discovery
+	// client, as it will bluntly pick the first version as the preferred one.
+	for _, gv := range scheme.PrioritizedVersionsAllGroups() {
+		var resources []metav1.APIResource
+		for kind, ty := range scheme.KnownTypes(gv) {
+			// Skip list kinds themselves.
+			if o, ok := reflect.New(ty).Interface().(runtime.Object); ok && meta.IsListType(o) {
+				continue
+			}
+
+			// Skip kinds that don't have an associated list kind.
+			if !scheme.Recognizes(gv.WithKind(kind + "List")) {
+				continue
+			}
+
+			plural, singular := meta.UnsafeGuessKindToResource(gv.WithKind(kind))
+			resource := metav1.APIResource{
+				Name:         plural.Resource,
+				SingularName: singular.Resource,
+				Kind:         kind,
+				Verbs:        metav1.Verbs{"get", "list", "watch", "create", "update", "patch", "delete"},
+			}
+
+			// Some duct tape for guessing cluster resources.
+			// FIXME: Is there any way to figure this out reliably? We could scan
+			// the clientsets if the factory methods have a string argument for
+			// the namespace or not.
+			switch {
+			case strings.Contains(kind, "Node"),
+				strings.Contains(kind, "Namespace"),
+				strings.Contains(kind, "Cluster"):
+				resource.Namespaced = false
+			default:
+				resource.Namespaced = true
+			}
+
+			resources = append(resources, resource)
+		}
+
+		allResources = append(allResources, &metav1.APIResourceList{
+			GroupVersion: gv.String(),
+			APIResources: resources,
+		})
+	}
+
+	return
 }
