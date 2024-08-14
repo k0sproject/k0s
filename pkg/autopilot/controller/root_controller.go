cec950b7138006e70b1f821c04e8bd513a3dfd48
Alexey Makhov <amakhov@mirantis.com>
2024-08-14 13:18:26 +0300

    Detect --hostname-override flag in Autopilot for controller+worker nodes
    Signed-off-by: Alexey Makhov <amakhov@mirantis.com>
    (cherry picked from commit 4ab9311cb4cd8f187d997556c0e43750f78b32f8)

--- pkg/autopilot/controller/root_controller.go
+++ pkg/autopilot/controller/root_controller.go
@@ -75,7 +75,7 @@ func NewRootController(cfg aproot.RootConfig, logger *logrus.Entry, enableWorker
 	c.stopSubHandler = c.stopSubControllers
 	c.leaseWatcherCreator = NewLeaseWatcher
 	c.setupHandler = func(ctx context.Context, cf apcli.FactoryInterface) error {
-		setupController := NewSetupController(c.log, cf, cfg.K0sDataDir, enableWorker)
+		setupController := NewSetupController(c.log, cf, cfg.K0sDataDir, cfg.KubeletExtraArgs, enableWorker)
 		return setupController.Run(ctx)
 	}
 
