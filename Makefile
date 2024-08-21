k0sproject/k0s

commit 47965b23c3c51c3ec1905ea77c0f1ec00588bfaf

Generate one clientset for all APIs
This is how it's usually done.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

Makefile
@@ -82,6 +82,11 @@ GO_ENV ?= docker run --rm \
 	k0sbuild.docker-image.k0s
 GO ?= $(GO_ENV) go
 
+# https://www.gnu.org/software/make/manual/make.html#index-spaces_002c-in-variable-values
+nullstring :=
+space := $(nullstring) # space now holds a single space
+comma := ,
+
 .PHONY: build
 ifeq ($(TARGET_OS),windows)
 build: k0s.exe
@@ -129,21 +134,19 @@ pkg/apis/%/.controller-gen.stamp: .k0sbuild.docker-image.k0s hack/tools/boilerpl
 	  object:headerFile=hack/tools/boilerplate.go.txt
 	touch -- '$@'
 
-codegen_targets += pkg/apis/k0s.k0sproject.io/v1beta1/.client-gen.stamp
-pkg/apis/k0s.k0sproject.io/v1beta1/.client-gen.stamp: $(shell find pkg/apis/k0s.k0sproject.io/v1beta1/ -maxdepth 1 -type f -name \*.go -not -name zz_\*.go)
-
-codegen_targets += pkg/apis/autopilot.k0sproject.io/v1beta2/.client-gen.stamp
-pkg/apis/autopilot.k0sproject.io/v1beta2/.client-gen.stamp: $(shell find pkg/apis/autopilot.k0sproject.io/v1beta2/ -maxdepth 1 -type f -name \*.go -not -name zz_\*.go)
-
-pkg/apis/%/.client-gen.stamp: .k0sbuild.docker-image.k0s hack/tools/boilerplate.go.txt embedded-bins/Makefile.variables
-	$(eval groupver := $(@:pkg/apis/%/.client-gen.stamp=%))
-	rm -rf 'pkg/apis/$(groupver)/clientset/'
-	CGO_ENABLED=0 $(GO) run k8s.io/code-generator/cmd/client-gen@v$(patsubst 1.%,0.%,$(kubernetes_version)) \
-	  --go-header-file hack/tools/boilerplate.go.txt \
-	  --input=$(groupver) \
-	  --input-base=github.com/k0sproject/k0s/pkg/apis \
-	  --clientset-name=clientset \
-	  --output-package=github.com/k0sproject/k0s/pkg/apis/$(groupver)
+clientset_input_dirs := pkg/apis/autopilot.k0sproject.io/v1beta2 pkg/apis/k0s.k0sproject.io/v1beta1
+codegen_targets += pkg/client/clientset/.client-gen.stamp
+pkg/client/clientset/.client-gen.stamp: $(shell find $(clientset_input_dirs) -type f -name \*.go -not -name \*_test.go -not -name zz_\*)
+pkg/client/clientset/.client-gen.stamp: .k0sbuild.docker-image.k0s hack/tools/boilerplate.go.txt embedded-bins/Makefile.variables
+	rm -rf -- '$(dir $@)'
+	CGO_ENABLED=0 $(GO) run k8s.io/code-generator/cmd/client-gen@v$(kubernetes_version:1.%=0.%) \
+	  --go-header-file=hack/tools/boilerplate.go.txt \
+	  --input-base='' \
+	  --input=$(subst $(space),$(comma),$(clientset_input_dirs:%=github.com/k0sproject/k0s/%)) \
+	  --output-package=github.com/k0sproject/k0s/$(patsubst %/,%,$(dir $(patsubst %/,%,$(dir $@)))) \
+	  --clientset-name=$(notdir $(patsubst %/,%,$(dir $@))) \
+	  --output-base=. \
+	  --trim-path-prefix=github.com/k0sproject/k0s
 	touch -- '$@'
 
 codegen_targets += static/zz_generated_assets.go

commit f3ebeca6500277d41af706ef93ca7ec4e791baaa

Correctly clean clientset stamp file
It has been relocated to another folder, but the clean target wasn't
adjusted accordingly.

Fixes: 47965b23c ("Generate one clientset for all APIs")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

Makefile
@@ -291,7 +291,8 @@ clean-airgap-image-bundles:
 clean: clean-gocache clean-docker-image clean-airgap-image-bundles
 	-rm -f pkg/assets/zz_generated_offsets_*.go k0s k0s.exe .bins.*stamp bindata* static/zz_generated_assets.go
 	-rm -rf $(K0S_GO_BUILD_CACHE)
-	-find pkg/apis -type f \( -name .client-gen.stamp -or -name .controller-gen.stamp \) -delete
+	-find pkg/apis -type f -name .controller-gen.stamp -delete
+	-rm pkg/client/clientset/.client-gen.stamp
 	-rm -f hack/.copyright.stamp
 	-$(MAKE) -C docs clean
 	-$(MAKE) -C embedded-bins clean