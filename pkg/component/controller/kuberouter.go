k0sproject/k0s

commit 65fcf39aa7524c9771df58052771d5de28bcf292

Add readinessProbe/minReadySeconds to kube-router
This allows for better feedback of kube-router health via the DaemonSet
resource. Without those, it's possible to observe a "healthy" DaemonSet,
even if it's not. This affects e.g. rolling updates, and, most notably
k0s's own integration tests.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/kuberouter.go
@@ -243,6 +243,7 @@ spec:
     spec:
       priorityClassName: system-node-critical
       serviceAccountName: kube-router
+      minReadySeconds: 5
       initContainers:
         - name: install-cni-bins
           image: {{ .CNIInstallerImage }}
@@ -321,12 +322,25 @@ spec:
               fieldPath: metadata.name
         - name: KUBE_ROUTER_CNI_CONF_FILE
           value: /etc/cni/net.d/10-kuberouter.conflist
+        ports:
+        - name: healthz
+          containerPort: 20244
         livenessProbe:
           httpGet:
             path: /healthz
-            port: 20244
-          initialDelaySeconds: 10
+            port: healthz
+          initialDelaySeconds: 300
+          periodSeconds: 10
+          timeoutSeconds: 10
+          failureThreshold: 6
+        readinessProbe:
+          httpGet:
+            path: /healthz
+            port: healthz
           periodSeconds: 3
+          timeoutSeconds: 3
+          failureThreshold: 3
+          successThreshold: 3
         resources:
           requests:
             cpu: 250m

commit c00fd0d8d6ad5e8321edd0bbf689ab4d544dcb8b

Remove minReadySeconds from kube-router DaemonSet
The minReadySeconds are a thing for StatefulSets. For
DaemonSets there's no such field at all. Simply remove it as
it has no effect, despite producing logs like 'unknown field
"spec.template.spec.minReadySeconds"'.

Fixes: 65fcf39aa ("Add readinessProbe/minReadySeconds to kube-router")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/kuberouter.go
@@ -245,7 +245,6 @@ spec:
     spec:
       priorityClassName: system-node-critical
       serviceAccountName: kube-router
-      minReadySeconds: 5
       initContainers:
         - name: install-cni-bins
           image: {{ .CNIInstallerImage }}