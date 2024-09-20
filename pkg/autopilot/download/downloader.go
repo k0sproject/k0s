8cb867fde9d93d037baaa6ffd9726b300e0ef663
Tom Wieczorek <twieczorek@mirantis.com>
2024-09-20 14:31:01 +0200

    Add a very long overall timeout for Autopilot downloads
    This will ensure that the download will fail at some point, even if the
    remote server is artificially slow.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/autopilot/download/downloader.go
+++ pkg/autopilot/download/downloader.go
@@ -23,6 +23,7 @@ import (
 	"hash"
 	"io"
 	"path/filepath"
+	"time"
 
 	internalhttp "github.com/k0sproject/k0s/internal/http"
 	"github.com/k0sproject/k0s/internal/pkg/file"
@@ -85,6 +86,12 @@ func (d *downloader) Download(ctx context.Context) (err error) {
 	}
 	defer func() { err = errors.Join(err, target.Close()) }()
 	targets = append(targets, target)
+
+	// Set a very long overall download timeout. This will ensure that the
+	// download will fail at some point, even if the remote server is
+	// artificially slow.
+	ctx, cancel := context.WithTimeout(ctx, 6*time.Hour)
+	defer cancel()
 
 	// Download from URL into targets.
 	if err = internalhttp.Download(ctx, d.config.URL, io.MultiWriter(targets...), downloadOpts...); err != nil {
