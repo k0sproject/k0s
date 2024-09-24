cc13b7e3ca184e56f786bdf7f8ade751de545f3a
Tom Wieczorek <twieczorek@mirantis.com>
2024-09-24 15:28:11 +0200

    Move clusterID retrieval into its own helper method
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/autopilot/controller/root_worker.go
+++ pkg/autopilot/controller/root_worker.go
@@ -86,15 +86,10 @@ func (w *rootWorker) Run(ctx context.Context) error {
 		logger.Debug("Failed to run controller manager, retrying after backoff")
 		return true
 	}, func() error {
-		cl, err := w.clientFactory.GetClient()
+		clusterID, err := w.getClusterID(ctx)
 		if err != nil {
 			return err
 		}
-		ns, err := cl.CoreV1().Namespaces().Get(ctx, metav1.NamespaceSystem, metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
-		clusterID := string(ns.UID)
 
 		restConfig, err := w.clientFactory.GetRESTConfig()
 		if err != nil {
@@ -123,4 +118,18 @@ func (w *rootWorker) Run(ctx context.Context) error {
 		}
 		return nil
 	})
+}
+
+func (w *rootWorker) getClusterID(ctx context.Context) (string, error) {
+	client, err := w.clientFactory.GetClient()
+	if err != nil {
+		return "", err
+	}
+
+	namespace, err := client.CoreV1().Namespaces().Get(ctx, metav1.NamespaceSystem, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	return string(namespace.UID), nil
 }
