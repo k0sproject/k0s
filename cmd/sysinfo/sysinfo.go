07497dcac3f6f6c0844b77de96891555bd3c48af
Ethan Mosbaugh <ethan@replicated.com>
2024-11-15 10:30:31 -0800

    feat: add output format flag to the k0s sysinfo command
    Signed-off-by: Ethan Mosbaugh <ethan@replicated.com>

--- cmd/sysinfo/sysinfo.go
+++ cmd/sysinfo/sysinfo.go
@@ -17,7 +17,9 @@ limitations under the License.
 package sysinfo
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 
@@ -28,11 +30,13 @@ import (
 	"github.com/logrusorgru/aurora/v3"
 	"github.com/spf13/cobra"
 	"k8s.io/kubectl/pkg/util/term"
+	"sigs.k8s.io/yaml"
 )
 
 func NewSysinfoCmd() *cobra.Command {
 
 	var sysinfoSpec sysinfo.K0sSysinfoSpec
+	var outputFormat string
 
 	cmd := &cobra.Command{
 		Use:   "sysinfo",
@@ -43,18 +47,18 @@ func NewSysinfoCmd() *cobra.Command {
 			probes := sysinfoSpec.NewSysinfoProbes()
 			out := cmd.OutOrStdout()
 			cli := &cliReporter{
-				w:      out,
 				colors: aurora.NewAurora(term.IsTerminal(out)),
 			}
-
 			if err := probes.Probe(cli); err != nil {
+				return err
+			}
+			if err := cli.printResults(out, outputFormat); err != nil {
 				return err
 			}
 
 			if cli.failed {
 				return errors.New("sysinfo failed")
 			}
-
 			return nil
 		},
 	}
@@ -64,66 +68,155 @@ func NewSysinfoCmd() *cobra.Command {
 	flags.BoolVar(&sysinfoSpec.ControllerRoleEnabled, "controller", true, "Include controller-specific sysinfo")
 	flags.BoolVar(&sysinfoSpec.WorkerRoleEnabled, "worker", true, "Include worker-specific sysinfo")
 	flags.StringVar(&sysinfoSpec.DataDir, "data-dir", constant.DataDirDefault, "Data Directory for k0s")
+	flags.StringVarP(&outputFormat, "output", "o", "human", "Output format. Must be one of human|yaml|json")
 
 	return cmd
 }
 
 type cliReporter struct {
-	w      io.Writer
-	colors aurora.Aurora
-	failed bool
+	results []Probe
+	colors  aurora.Aurora
+	failed  bool
 }
 
+type Probe struct {
+	Path        []string
+	DisplayName string
+	Prop        string
+	Message     string
+	Category    ProbeCategory
+	Error       error
+}
+
+type ProbeCategory string
+
+const (
+	ProbeCategoryPass     ProbeCategory = "pass"
+	ProbeCategoryWarning  ProbeCategory = "warning"
+	ProbeCategoryRejected ProbeCategory = "rejected"
+	ProbeCategoryError    ProbeCategory = "error"
+)
+
 func (r *cliReporter) Pass(p probes.ProbeDesc, v probes.ProbedProp) error {
-	prop := propString(v)
-	return r.printf("%s%s%s%s\n",
-		indent(p),
-		r.colors.BrightWhite(p.DisplayName()+": "),
-		r.colors.Green(prop),
-		buildMsg(prop, "pass", ""),
-	)
+	r.results = append(r.results, Probe{
+		Path:        probePath(p),
+		DisplayName: p.DisplayName(),
+		Prop:        propString(v),
+		Category:    ProbeCategoryPass,
+	})
+	return nil
 }
 
 func (r *cliReporter) Warn(p probes.ProbeDesc, v probes.ProbedProp, msg string) error {
-	prop := propString(v)
-	return r.printf("%s%s%s%s\n",
-		indent(p),
-		r.colors.BrightWhite(p.DisplayName()+": "),
-		r.colors.Yellow(prop),
-		buildMsg(prop, "warning", msg))
+	r.results = append(r.results, Probe{
+		Path:        probePath(p),
+		DisplayName: p.DisplayName(),
+		Prop:        propString(v),
+		Message:     msg,
+		Category:    ProbeCategoryWarning,
+	})
+	return nil
 }
 
 func (r *cliReporter) Reject(p probes.ProbeDesc, v probes.ProbedProp, msg string) error {
 	r.failed = true
-	prop := propString(v)
-	return r.printf("%s%s%s%s\n",
-		indent(p),
-		r.colors.BrightWhite(p.DisplayName()+": "),
-		r.colors.Bold(r.colors.Red(prop)),
-		buildMsg(prop, "rejected", msg))
+	r.results = append(r.results, Probe{
+		Path:        probePath(p),
+		DisplayName: p.DisplayName(),
+		Prop:        propString(v),
+		Message:     msg,
+		Category:    ProbeCategoryRejected,
+	})
+	return nil
 }
 
 func (r *cliReporter) Error(p probes.ProbeDesc, err error) error {
 	r.failed = true
-
-	errStr := "error"
-	if err != nil {
-		e := err.Error()
-		if e != "" {
-			errStr = errStr + ": " + e
-		}
-	}
-
-	return r.printf("%s%s%s\n",
-		indent(p),
-		r.colors.BrightWhite(p.DisplayName()+": "),
-		r.colors.Bold(errStr).Red(),
-	)
+	r.results = append(r.results, Probe{
+		Path:        probePath(p),
+		DisplayName: p.DisplayName(),
+		Category:    ProbeCategoryError,
+		Error:       err,
+	})
+	return nil
 }
 
-func (r *cliReporter) printf(format interface{}, args ...interface{}) error {
-	_, err := io.WriteString(r.w, aurora.Sprintf(format, args...))
+func (r *cliReporter) printResults(w io.Writer, outputFormat string) error {
+	switch outputFormat {
+	case "human":
+		return r.printHuman(w)
+	case "json":
+		enc := json.NewEncoder(w)
+		enc.SetIndent("", "  ")
+		return enc.Encode(r.results)
+	case "yaml":
+		b, err := yaml.Marshal(r.results)
+		if err != nil {
+			return err
+		}
+		_, err = io.WriteString(w, string(b))
+		return err
+	default:
+		return fmt.Errorf("unknown output format: %q", outputFormat)
+	}
+}
+
+func (r *cliReporter) printHuman(w io.Writer) error {
+	for _, p := range r.results {
+		if err := r.printOneHuman(w, p); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *cliReporter) printOneHuman(w io.Writer, p Probe) error {
+	var out string
+	switch p.Category {
+	case ProbeCategoryPass:
+		out = aurora.Sprintf("%s%s%s%s\n",
+			indent(p.Path),
+			r.colors.BrightWhite(p.DisplayName+": "),
+			r.colors.Green(p.Prop),
+			buildMsg(p.Prop, string(p.Category), p.Message))
+	case ProbeCategoryWarning:
+		out = aurora.Sprintf("%s%s%s%s\n",
+			indent(p.Path),
+			r.colors.BrightWhite(p.DisplayName+": "),
+			r.colors.Yellow(p.Prop),
+			buildMsg(p.Prop, string(p.Category), p.Message))
+	case ProbeCategoryRejected:
+		out = aurora.Sprintf("%s%s%s%s\n",
+			indent(p.Path),
+			r.colors.BrightWhite(p.DisplayName+": "),
+			r.colors.Bold(r.colors.Red(p.Prop)),
+			buildMsg(p.Prop, string(p.Category), p.Message))
+	case ProbeCategoryError:
+		errStr := "error"
+		if p.Error != nil {
+			e := p.Error.Error()
+			if e != "" {
+				errStr = errStr + ": " + e
+			}
+		}
+
+		out = aurora.Sprintf("%s%s%s\n",
+			indent(p.Path),
+			r.colors.BrightWhite(p.DisplayName+": "),
+			r.colors.Bold(errStr).Red(),
+		)
+	default:
+		return fmt.Errorf("unknown probe category %q", p.Category)
+	}
+	_, err := io.WriteString(w, out)
 	return err
+}
+
+func probePath(p probes.ProbeDesc) []string {
+	if len(p.Path()) == 0 {
+		return nil
+	}
+	return p.Path()
 }
 
 func propString(p probes.ProbedProp) string {
@@ -134,13 +227,10 @@ func propString(p probes.ProbedProp) string {
 	return p.String()
 }
 
-func indent(p probes.ProbeDesc) string {
-	count := 0
-	if p != nil {
-		count = len(p.Path()) - 1
-		if count < 1 {
-			return ""
-		}
+func indent(path []string) string {
+	count := len(path) - 1
+	if count < 1 {
+		return ""
 	}
 
 	return strings.Repeat("  ", count)
--- cmd/sysinfo/sysinfo_test.go
+++ cmd/sysinfo/sysinfo_test.go
@@ -27,13 +27,6 @@ import (
 )
 
 func TestCliReporter_Pass(t *testing.T) {
-	var buf strings.Builder
-
-	underTest := &cliReporter{
-		w:      &buf,
-		colors: aurora.NewAurora(true),
-	}
-
 	for _, data := range []struct {
 		name  string
 		desc  probes.ProbeDesc
@@ -67,10 +60,15 @@ func TestCliReporter_Pass(t *testing.T) {
 		},
 	} {
 		t.Run(data.name, func(t *testing.T) {
-			buf.Reset()
+			underTest := &cliReporter{
+				colors: aurora.NewAurora(true),
+			}
 			err := underTest.Pass(data.desc, data.prop)
 			assert.NoError(t, err)
 			assert.False(t, underTest.failed)
+			var buf strings.Builder
+			err = underTest.printHuman(&buf)
+			assert.NoError(t, err)
 			result := buf.String()
 			t.Log(result)
 			assert.Equal(t, data.xpect, result)
@@ -79,13 +77,6 @@ func TestCliReporter_Pass(t *testing.T) {
 }
 
 func TestCliReporter_Warn(t *testing.T) {
-	var buf strings.Builder
-
-	underTest := &cliReporter{
-		w:      &buf,
-		colors: aurora.NewAurora(true),
-	}
-
 	for _, data := range []struct {
 		name  string
 		desc  probes.ProbeDesc
@@ -120,10 +111,15 @@ func TestCliReporter_Warn(t *testing.T) {
 		},
 	} {
 		t.Run(data.name, func(t *testing.T) {
-			buf.Reset()
+			underTest := &cliReporter{
+				colors: aurora.NewAurora(true),
+			}
 			err := underTest.Warn(data.desc, data.prop, data.msg)
 			assert.NoError(t, err)
 			assert.False(t, underTest.failed)
+			var buf strings.Builder
+			err = underTest.printHuman(&buf)
+			assert.NoError(t, err)
 			result := buf.String()
 			t.Log(result)
 			assert.Equal(t, data.xpect, result)
@@ -132,13 +128,6 @@ func TestCliReporter_Warn(t *testing.T) {
 }
 
 func TestCliReporter_Reject(t *testing.T) {
-	var buf strings.Builder
-
-	underTest := &cliReporter{
-		w:      &buf,
-		colors: aurora.NewAurora(true),
-	}
-
 	for _, data := range []struct {
 		name  string
 		desc  probes.ProbeDesc
@@ -173,10 +162,15 @@ func TestCliReporter_Reject(t *testing.T) {
 		},
 	} {
 		t.Run(data.name, func(t *testing.T) {
-			buf.Reset()
+			underTest := &cliReporter{
+				colors: aurora.NewAurora(true),
+			}
 			err := underTest.Reject(data.desc, data.prop, data.msg)
 			assert.NoError(t, err)
 			assert.True(t, underTest.failed)
+			var buf strings.Builder
+			err = underTest.printHuman(&buf)
+			assert.NoError(t, err)
 			result := buf.String()
 			t.Log(result)
 			assert.Equal(t, data.xpect, result)
@@ -185,13 +179,6 @@ func TestCliReporter_Reject(t *testing.T) {
 }
 
 func TestCliReporter_Error(t *testing.T) {
-	var buf strings.Builder
-
-	underTest := &cliReporter{
-		w:      &buf,
-		colors: aurora.NewAurora(true),
-	}
-
 	for _, data := range []struct {
 		name  string
 		desc  probes.ProbeDesc
@@ -215,12 +202,109 @@ func TestCliReporter_Error(t *testing.T) {
 		},
 	} {
 		t.Run(data.name, func(t *testing.T) {
-			buf.Reset()
+			underTest := &cliReporter{
+				colors: aurora.NewAurora(true),
+			}
 			err := underTest.Error(data.desc, data.err)
 			assert.NoError(t, err)
 			assert.True(t, underTest.failed)
+			var buf strings.Builder
+			err = underTest.printHuman(&buf)
+			assert.NoError(t, err)
 			result := buf.String()
 			t.Log(result)
+			assert.Equal(t, data.xpect, result)
+		})
+	}
+}
+
+func TestCliReporter(t *testing.T) {
+	for _, data := range []struct {
+		name         string
+		probe        func(t *testing.T, cli *cliReporter)
+		xpectResults []Probe
+		xpect        string
+		xpectFailed  bool
+	}{
+		{
+			"success",
+			func(t *testing.T, cli *cliReporter) {
+				err := cli.Pass(&testDesc{"foo", probes.ProbePath{"bar"}}, testProp("baz"))
+				assert.NoError(t, err)
+				err = cli.Pass(&testDesc{"foo", probes.ProbePath{"bar", "baz"}}, testProp("qux"))
+				assert.NoError(t, err)
+				err = cli.Warn(&testDesc{"foo", nil}, testProp("bar"), "baz")
+				assert.NoError(t, err)
+			},
+			[]Probe{
+				{Path: []string{"bar"}, DisplayName: "foo", Prop: "baz", Message: "", Category: "pass", Error: nil},
+				{Path: []string{"bar", "baz"}, DisplayName: "foo", Prop: "qux", Message: "", Category: "pass", Error: nil},
+				{Path: []string(nil), DisplayName: "foo", Prop: "bar", Message: "baz", Category: "warning", Error: nil},
+			},
+			("\x1b[97mfoo: \x1b[0m\x1b[32mbaz\x1b[0m (pass)\n" +
+				"  \x1b[97mfoo: \x1b[0m\x1b[32mqux\x1b[0m (pass)\n" +
+				"\x1b[97mfoo: \x1b[0m\x1b[33mbar\x1b[0m (warning: baz)\n"),
+			true,
+		},
+		{
+			"has_reject",
+			func(t *testing.T, cli *cliReporter) {
+				err := cli.Pass(&testDesc{"foo", probes.ProbePath{"bar"}}, testProp("baz"))
+				assert.NoError(t, err)
+				err = cli.Pass(&testDesc{"foo", probes.ProbePath{"bar", "baz"}}, testProp("qux"))
+				assert.NoError(t, err)
+				err = cli.Warn(&testDesc{"foo", nil}, testProp("bar"), "baz")
+				assert.NoError(t, err)
+				err = cli.Reject(&testDesc{"foo", nil}, testProp("bar"), "baz")
+				assert.NoError(t, err)
+			},
+			[]Probe{
+				{Path: []string{"bar"}, DisplayName: "foo", Prop: "baz", Message: "", Category: "pass", Error: nil},
+				{Path: []string{"bar", "baz"}, DisplayName: "foo", Prop: "qux", Message: "", Category: "pass", Error: nil},
+				{Path: []string(nil), DisplayName: "foo", Prop: "bar", Message: "baz", Category: "warning", Error: nil},
+				{Path: []string(nil), DisplayName: "foo", Prop: "bar", Message: "baz", Category: "rejected", Error: nil},
+			},
+			("\x1b[97mfoo: \x1b[0m\x1b[32mbaz\x1b[0m (pass)\n" +
+				"  \x1b[97mfoo: \x1b[0m\x1b[32mqux\x1b[0m (pass)\n" +
+				"\x1b[97mfoo: \x1b[0m\x1b[33mbar\x1b[0m (warning: baz)\n" +
+				"\x1b[97mfoo: \x1b[0m\x1b[1;31mbar\x1b[0m (rejected: baz)\n"),
+			true,
+		},
+		{
+			"has_error",
+			func(t *testing.T, cli *cliReporter) {
+				err := cli.Pass(&testDesc{"foo", probes.ProbePath{"bar"}}, testProp("baz"))
+				assert.NoError(t, err)
+				err = cli.Pass(&testDesc{"foo", probes.ProbePath{"bar", "baz"}}, testProp("qux"))
+				assert.NoError(t, err)
+				err = cli.Warn(&testDesc{"foo", nil}, testProp("bar"), "baz")
+				assert.NoError(t, err)
+				err = cli.Error(&testDesc{"foo", probes.ProbePath{}}, errors.New("bar"))
+				assert.NoError(t, err)
+			},
+			[]Probe{
+				{Path: []string{"bar"}, DisplayName: "foo", Prop: "baz", Message: "", Category: "pass", Error: nil},
+				{Path: []string{"bar", "baz"}, DisplayName: "foo", Prop: "qux", Message: "", Category: "pass", Error: nil},
+				{Path: []string(nil), DisplayName: "foo", Prop: "bar", Message: "baz", Category: "warning", Error: nil},
+				{Path: []string(nil), DisplayName: "foo", Prop: "", Message: "", Category: "error", Error: errors.New("bar")},
+			},
+			("\x1b[97mfoo: \x1b[0m\x1b[32mbaz\x1b[0m (pass)\n" +
+				"  \x1b[97mfoo: \x1b[0m\x1b[32mqux\x1b[0m (pass)\n" +
+				"\x1b[97mfoo: \x1b[0m\x1b[33mbar\x1b[0m (warning: baz)\n" +
+				"\x1b[97mfoo: \x1b[0m\x1b[1;31merror: bar\x1b[0m\n"),
+			true,
+		},
+	} {
+		t.Run(data.name, func(t *testing.T) {
+			underTest := &cliReporter{
+				colors: aurora.NewAurora(true),
+			}
+			data.probe(t, underTest)
+			assert.Equal(t, data.xpectResults, underTest.results)
+			var buf strings.Builder
+			err := underTest.printResults(&buf, "human")
+			assert.NoError(t, err)
+			result := buf.String()
 			assert.Equal(t, data.xpect, result)
 		})
 	}
