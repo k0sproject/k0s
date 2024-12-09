k0sproject/k0s

commit 1e5e6b6e5d569558f11207476b873df31b07d8ee

Disable airgap list-images for dynamic config
Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

cmd/airgap/listimages.go
@@ -17,14 +17,10 @@ limitations under the License.
 package airgap
 
 import (
-	"context"
 	"fmt"
-	"time"
 
 	"github.com/k0sproject/k0s/pkg/airgap"
 	"github.com/k0sproject/k0s/pkg/config"
-	"github.com/k0sproject/k0s/pkg/kubernetes"
-	"github.com/sirupsen/logrus"
 
 	"github.com/spf13/cobra"
 )
@@ -42,18 +38,13 @@ func NewAirgapListImagesCmd() *cobra.Command {
 				return err
 			}
 
-			adminClientFactory := kubernetes.NewAdminClientFactory(opts.K0sVars.AdminKubeConfigPath)
+			if opts.EnableDynamicConfig {
+				return fmt.Errorf("dynamic config is not supported for airgap list-images")
+			}
 
-			ctx, cancel := context.WithTimeout(cmd.Context(), 2*time.Minute)
-			defer cancel()
-
-			clusterConfig, err := opts.K0sVars.FetchDynamicConfig(ctx, adminClientFactory)
+			clusterConfig, err := opts.K0sVars.NodeConfig()
 			if err != nil {
-				logrus.WithError(err).Warn("Failed to get cluster config, falling back to local config")
-				clusterConfig, err = opts.K0sVars.NodeConfig()
-				if err != nil {
-					return fmt.Errorf("failed to get local config: %w", err)
-				}
+				return fmt.Errorf("failed to get config: %w", err)
 			}
 
 			for _, uri := range airgap.GetImageURIs(clusterConfig.Spec, all) {

commit b83ce67369e14e0399880cc5525a82ba95136357

Don't check for dynamic config when listing images
There's no --enable-dynamic-config flag for k0s airgap list-images:

$ k0s airgap list-images --enable-dynamic-config
Error: unknown flag: --enable-dynamic-config

Fixes: 1e5e6b6e5 ("Disable airgap list-images for dynamic config")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

cmd/airgap/listimages.go
@@ -17,7 +17,6 @@ limitations under the License.
 package airgap
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/k0sproject/k0s/pkg/airgap"
@@ -37,10 +36,6 @@ func NewAirgapListImagesCmd() *cobra.Command {
 			opts, err := config.GetCmdOpts(cmd)
 			if err != nil {
 				return err
-			}
-
-			if opts.EnableDynamicConfig {
-				return errors.New("dynamic config is not supported for airgap list-images")
 			}
 
 			clusterConfig, err := opts.K0sVars.NodeConfig()