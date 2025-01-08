k0sproject/k0s

commit 4a5886e5157a5c9508f8db3a28980aa246f70dc0

Reject unknown sub-commands
Cobra has the property of not reporting any unrecognized sub-commands
and simply displaying the help page for any command that doesn't have a
run function set.

Add a unit test that probes all sub-commands to properly reject unknown
sub-commands. Whenever a sub-command is added that accepts arguments and
doesn't declare them as ValidArgs, it must be added to the ignore list
at the beginning of that test.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

cmd/airgap/airgap.go
@@ -26,6 +26,8 @@ func NewAirgapCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "airgap",
 		Short: "Manage airgap setup",
+		Args:  cobra.NoArgs,
+		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
 	}
 
 	cmd.AddCommand(NewAirgapListImagesCmd())

cmd/config/config.go
@@ -24,10 +24,29 @@ func NewConfigCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "config",
 		Short: "Configuration related sub-commands",
+		Args:  cobra.NoArgs,
+		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
 	}
 	cmd.AddCommand(NewCreateCmd())
 	cmd.AddCommand(NewEditCmd())
 	cmd.AddCommand(NewStatusCmd())
 	cmd.AddCommand(NewValidateCmd())
 	return cmd
+}
+
+func reExecKubectl(cmd *cobra.Command, kubectlArgs ...string) error {
+	args := []string{"kubectl"}
+	if dataDir, err := cmd.Flags().GetString("data-dir"); err != nil {
+		return err
+	} else if dataDir != "" {
+		args = append(args, "--data-dir", dataDir)
+	}
+
+	root := cmd.Root()
+	root.SetArgs(append(args, kubectlArgs...))
+
+	silenceErrors := root.SilenceErrors
+	defer func() { root.SilenceErrors = silenceErrors }()
+	root.SilenceErrors = true // So that errors aren't printed twice.
+	return root.Execute()
 }

cmd/etcd/etcd.go
@@ -30,6 +30,7 @@ func NewEtcdCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "etcd",
 		Short: "Manage etcd cluster",
+		Args:  cobra.NoArgs,
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			if err := config.CallParentPersistentPreRun(cmd, args); err != nil {
 				return err
@@ -51,6 +52,7 @@ func NewEtcdCmd() *cobra.Command {
 			}
 			return nil
 		},
+		Run: func(*cobra.Command, []string) { /* Enforce arg validation. */ },
 	}
 	cmd.AddCommand(etcdLeaveCmd())
 	cmd.AddCommand(etcdListCmd())

cmd/install/install.go
@@ -40,6 +40,8 @@ func NewInstallCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "install",
 		Short: "Install k0s on a brand-new system. Must be run as root (or with sudo)",
+		Args:  cobra.NoArgs,
+		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
 	}
 
 	cmd.AddCommand(installControllerCmd(&installFlags))

cmd/kubeconfig/kubeconfig.go
@@ -26,6 +26,8 @@ func NewKubeConfigCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "kubeconfig [command]",
 		Short: "Create a kubeconfig file for a specified user",
+		Args:  cobra.NoArgs,
+		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
 	}
 	cmd.AddCommand(kubeconfigCreateCmd())
 	cmd.AddCommand(kubeConfigAdminCmd())

cmd/token/token.go
@@ -28,6 +28,8 @@ func NewTokenCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "token",
 		Short: "Manage join tokens",
+		Args:  cobra.NoArgs,
+		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
 	}
 
 	cmd.AddCommand(tokenCreateCmd())

cmd/airgap/listimages.go
@@ -32,7 +32,8 @@ func NewAirgapListImagesCmd() *cobra.Command {
 		Use:     "list-images",
 		Short:   "List image names and version needed for air-gap install",
 		Example: `k0s airgap list-images`,
-		RunE: func(cmd *cobra.Command, args []string) error {
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
 			opts, err := config.GetCmdOpts(cmd)
 			if err != nil {
 				return err

cmd/airgap/listimages_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package airgap
+package airgap_test
 
 import (
 	"errors"
@@ -26,6 +26,7 @@ import (
 	"testing"
 	"testing/iotest"
 
+	"github.com/k0sproject/k0s/cmd"
 	internalio "github.com/k0sproject/k0s/internal/io"
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 
@@ -46,15 +47,12 @@ func TestAirgapListImages(t *testing.T) {
 
 	t.Run("HonorsIOErrors", func(t *testing.T) {
 		var writes uint
-		underTest := NewAirgapListImagesCmd()
-		underTest.SetIn(iotest.ErrReader(errors.New("unexpected read from standard input")))
+		underTest, _, stderr := newAirgapListImagesCmdWithConfig(t, "")
 		underTest.SilenceUsage = true // Cobra writes usage to stdout on errors 🤔
 		underTest.SetOut(internalio.WriterFunc(func(p []byte) (int, error) {
 			writes++
 			return 0, assert.AnError
 		}))
-		var stderr strings.Builder
-		underTest.SetErr(&stderr)
 
 		assert.Same(t, assert.AnError, underTest.Execute())
 		assert.Equal(t, uint(1), writes, "Expected a single write to stdout")
@@ -127,8 +125,8 @@ func newAirgapListImagesCmdWithConfig(t *testing.T, config string, args ...strin
 	require.NoError(t, os.WriteFile(configFile, []byte(config), 0644))
 
 	out, err = new(strings.Builder), new(strings.Builder)
-	cmd := NewAirgapListImagesCmd()
-	cmd.SetArgs(append([]string{"--config=" + configFile}, args...))
+	cmd := cmd.NewRootCmd()
+	cmd.SetArgs(append([]string{"airgap", "--config=" + configFile, "list-images"}, args...))
 	cmd.SetIn(iotest.ErrReader(errors.New("unexpected read from standard input")))
 	cmd.SetOut(out)
 	cmd.SetErr(err)

commit 3a74c39a9794a7af1a15fbe8ed91172ba6253ac9

Show help again for non-leaf subcommands
Adding a run function to enable argument validation also disabled the
default behavior of displaying the help text. Restore this by explicitly
returning the "help requested" error.

Fixes: 4a5886e51 ("Reject unknown sub-commands")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

cmd/airgap/airgap.go
@@ -17,9 +17,10 @@ limitations under the License.
 package airgap
 
 import (
-	"github.com/spf13/cobra"
-
 	"github.com/k0sproject/k0s/pkg/config"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func NewAirgapCmd() *cobra.Command {
@@ -27,7 +28,7 @@ func NewAirgapCmd() *cobra.Command {
 		Use:   "airgap",
 		Short: "Manage airgap setup",
 		Args:  cobra.NoArgs,
-		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
+		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
 
 	cmd.AddCommand(NewAirgapListImagesCmd())

cmd/config/config.go
@@ -18,6 +18,7 @@ package config
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func NewConfigCmd() *cobra.Command {
@@ -25,7 +26,7 @@ func NewConfigCmd() *cobra.Command {
 		Use:   "config",
 		Short: "Configuration related sub-commands",
 		Args:  cobra.NoArgs,
-		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
+		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
 	cmd.AddCommand(NewCreateCmd())
 	cmd.AddCommand(NewEditCmd())

cmd/etcd/etcd.go
@@ -24,6 +24,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/config"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func NewEtcdCmd() *cobra.Command {
@@ -52,7 +53,7 @@ func NewEtcdCmd() *cobra.Command {
 			}
 			return nil
 		},
-		Run: func(*cobra.Command, []string) { /* Enforce arg validation. */ },
+		RunE: func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
 	cmd.AddCommand(etcdLeaveCmd())
 	cmd.AddCommand(etcdListCmd())

cmd/install/install.go
@@ -25,6 +25,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/install"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 type command config.CLIOptions
@@ -41,7 +42,7 @@ func NewInstallCmd() *cobra.Command {
 		Use:   "install",
 		Short: "Install k0s on a brand-new system. Must be run as root (or with sudo)",
 		Args:  cobra.NoArgs,
-		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
+		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
 
 	cmd.AddCommand(installControllerCmd(&installFlags))

cmd/kubeconfig/kubeconfig.go
@@ -20,6 +20,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/config"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func NewKubeConfigCmd() *cobra.Command {
@@ -27,7 +28,7 @@ func NewKubeConfigCmd() *cobra.Command {
 		Use:   "kubeconfig [command]",
 		Short: "Create a kubeconfig file for a specified user",
 		Args:  cobra.NoArgs,
-		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
+		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
 	cmd.AddCommand(kubeconfigCreateCmd())
 	cmd.AddCommand(kubeConfigAdminCmd())

cmd/token/token.go
@@ -22,6 +22,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/token"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func NewTokenCmd() *cobra.Command {
@@ -29,7 +30,7 @@ func NewTokenCmd() *cobra.Command {
 		Use:   "token",
 		Short: "Manage join tokens",
 		Args:  cobra.NoArgs,
-		Run:   func(*cobra.Command, []string) { /* Enforce arg validation. */ },
+		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
 
 	cmd.AddCommand(tokenCreateCmd())