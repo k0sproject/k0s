k0sproject/k0s

commit 79b95965a055334c3d6e991fd4e12367dcd0e0fb

Bump Kubernetes to v1.31.0
https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.31.md#changelog-since-v1300

* Adapt fake client's ObjectTracker to k/k PR 125560
  The interface changed upstream, add the variadic option arguments and
  missing Patch and apply methods. Moreover, remove the capacity from
  the external channel for Watch. This could introduce double-buffering.

* Bump controller-runtime to v0.19.0
  https://github.com/kubernetes-sigs/controller-runtime/releases/tag/v0.19.0
  This is the version which is compatible with Kubernetes 1.31.

* Skip name checks when restarting Autpoilot root controller
  If the autpoilot root controller is started multiple times due
  to reconfiguration events, this means that all controller-runtime
  controllers have already been successfully registered to another
  manager during the initial startup. However, controller-runtime
  maintains a global checklist of controller names and doesn't currently
  provide a way to unregister names from discarded managers. So it's
  necessary to suppress the global name check each time things are
  restarted for reconfiguration.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/autopilot/controller/root_controller.go
@@ -32,6 +32,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	cr "sigs.k8s.io/controller-runtime"
 	crcli "sigs.k8s.io/controller-runtime/pkg/client"
 	crman "sigs.k8s.io/controller-runtime/pkg/manager"
@@ -56,6 +57,8 @@ type rootController struct {
 	stopSubHandler         subControllerStopFunc
 	leaseWatcherCreator    leaseWatcherCreatorFunc
 	setupHandler           setupFunc
+
+	initialized bool
 }
 
 var _ aproot.Root = (*rootController)(nil)
@@ -157,6 +160,16 @@ func (c *rootController) startSubControllerRoutine(ctx context.Context, logger *
 		HealthProbeBindAddress: c.cfg.HealthProbeBindAddr,
 	}
 
+	// If this controller is already initialized, this means that all
+	// controller-runtime controllers have already been successfully registered
+	// to another manager. However, controller-runtime maintains a global
+	// checklist of controller names and doesn't currently provide a way to
+	// unregister names from discarded managers. So it's necessary to suppress
+	// the global name check whenever things are restarted for reconfiguration.
+	if c.initialized {
+		managerOpts.Controller.SkipNameValidation = ptr.To(true)
+	}
+
 	mgr, err := cr.NewManager(c.autopilotClientFactory.RESTConfig(), managerOpts)
 	if err != nil {
 		logger.WithError(err).Error("unable to start controller manager")
@@ -216,6 +229,9 @@ func (c *rootController) startSubControllerRoutine(ctx context.Context, logger *
 		logger.WithError(err).Error("unable to register 'update' controllers")
 		return err
 	}
+
+	// All the controller-runtime controllers have been registered.
+	c.initialized = true
 
 	// The controller-runtime start blocks until the context is cancelled.
 	if err := mgr.Start(ctx); err != nil {

.github/workflows/publish-docs-manual.yml
@@ -3,7 +3,7 @@ on:
   workflow_dispatch:
     inputs:
       version:
-        description: 'Version tag (e.g."v1.30.4+k0s.0")'
+        description: 'Version tag (e.g."v1.31.0+k0s.0")'
         required: true
 
 env:

embedded-bins/Makefile.variables
@@ -22,7 +22,7 @@ containerd_build_shim_go_cgo_enabled = 0
 #containerd_build_go_ldflags =
 containerd_build_go_ldflags_extra = "-w -s -extldflags=-static"
 
-kubernetes_version = 1.30.4
+kubernetes_version = 1.31.0
 helm_version = 3.11.1
 kubernetes_buildimage = $(golang_buildimage)
 kubernetes_build_go_tags = "providerless"

examples/bootloose-ha-controllers/Dockerfile
@@ -2,7 +2,7 @@ FROM quay.io/k0sproject/bootloose-ubuntu18.04
 
 ADD k0s.service /etc/systemd/system/k0s.service
 
-RUN curl --proto '=https' --tlsv1.2 --retry 5 --retry-all-errors -Lfo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.30.4/bin/linux/amd64/kubectl && \
+RUN curl --proto '=https' --tlsv1.2 --retry 5 --retry-all-errors -Lfo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.31.0/bin/linux/amd64/kubectl && \
     chmod +x /usr/local/bin/kubectl
 
 ENV KUBECONFIG=/var/lib/k0s/pki/admin.conf

go.mod
@@ -63,22 +63,22 @@ require (
 
 // Kubernetes
 require (
-	k8s.io/api v0.30.4
-	k8s.io/apiextensions-apiserver v0.30.4
-	k8s.io/apimachinery v0.30.4
-	k8s.io/cli-runtime v0.30.4
-	k8s.io/client-go v0.30.4
-	k8s.io/cloud-provider v0.30.4
-	k8s.io/component-base v0.30.4
-	k8s.io/component-helpers v0.30.4
-	k8s.io/cri-api v0.30.4
-	k8s.io/kube-aggregator v0.30.4
-	k8s.io/kubectl v0.30.4
-	k8s.io/kubelet v0.30.4
-	k8s.io/kubernetes v1.30.4
-	k8s.io/mount-utils v0.30.4
-	k8s.io/utils v0.0.0-20230726121419-3b25d923346b
-	sigs.k8s.io/controller-runtime v0.18.5
+	k8s.io/api v0.31.0
+	k8s.io/apiextensions-apiserver v0.31.0
+	k8s.io/apimachinery v0.31.0
+	k8s.io/cli-runtime v0.31.0
+	k8s.io/client-go v0.31.0
+	k8s.io/cloud-provider v0.31.0
+	k8s.io/component-base v0.31.0
+	k8s.io/component-helpers v0.31.0
+	k8s.io/cri-api v0.31.0
+	k8s.io/kube-aggregator v0.31.0
+	k8s.io/kubectl v0.31.0
+	k8s.io/kubelet v0.31.0
+	k8s.io/kubernetes v1.31.0
+	k8s.io/mount-utils v0.31.0
+	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8
+	sigs.k8s.io/controller-runtime v0.19.0
 	sigs.k8s.io/yaml v1.4.0
 )
 
@@ -93,12 +93,12 @@ require (
 	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
 	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/NYTimes/gziphandler v1.1.1 // indirect
-	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230305170008-8188dc5388df // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/briandowns/spinner v1.19.0 // indirect
-	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chai2010/gettext-go v1.0.2 // indirect
 	github.com/containerd/cgroups v1.1.0 // indirect
@@ -135,8 +135,8 @@ require (
 	github.com/exponent-io/jsonpath v0.0.0-20151013193312-d6023ce2651d // indirect
 	github.com/fatih/camelcase v1.0.0 // indirect
 	github.com/fatih/color v1.14.1 // indirect
-	github.com/felixge/httpsnoop v1.0.3 // indirect
-	github.com/fvbommel/sortorder v1.1.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-errors/errors v1.4.2 // indirect
@@ -153,7 +153,7 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.2 // indirect
-	github.com/google/cel-go v0.17.8 // indirect
+	github.com/google/cel-go v0.20.1 // indirect
 	github.com/google/certificate-transparency-go v1.1.4 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
@@ -164,7 +164,7 @@ require (
 	github.com/gosuri/uitable v0.0.4 // indirect
 	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
@@ -189,14 +189,13 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mattn/go-sqlite3 v1.14.19 // indirect
-	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/locker v1.0.1 // indirect
-	github.com/moby/spdystream v0.2.0 // indirect
-	github.com/moby/sys/mountinfo v0.6.2 // indirect
+	github.com/moby/spdystream v0.4.0 // indirect
+	github.com/moby/sys/mountinfo v0.7.1 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
 	github.com/moby/sys/signal v0.7.0 // indirect
 	github.com/moby/sys/symlink v0.2.0 // indirect
@@ -210,14 +209,15 @@ require (
 	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/opencontainers/runc v1.1.13 // indirect
 	github.com/opencontainers/selinux v1.11.0 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.18.0 // indirect
-	github.com/prometheus/client_model v0.6.0 // indirect
-	github.com/prometheus/common v0.45.0 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rubenv/sql-migrate v1.5.2 // indirect
@@ -230,6 +230,7 @@ require (
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
 	github.com/weppos/publicsuffix-go v0.15.1-0.20210511084619-b1f36a2d6c0b // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
@@ -244,15 +245,15 @@ require (
 	go.etcd.io/etcd/raft/v3 v3.5.15 // indirect
 	go.etcd.io/etcd/server/v3 v3.5.15 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.46.1 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.45.0 // indirect
-	go.opentelemetry.io/otel v1.21.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.20.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.20.0 // indirect
-	go.opentelemetry.io/otel/metric v1.21.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
-	go.opentelemetry.io/otel/trace v1.21.0 // indirect
-	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.53.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.28.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	go.starlark.net v0.0.0-20230525235612-a134d8f9ddca // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
@@ -264,58 +265,59 @@ require (
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20240227224415-6ceb2ff114de // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240604185151-ef581f913117 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/apiserver v0.30.4 // indirect
-	k8s.io/controller-manager v0.30.4 // indirect
-	k8s.io/klog/v2 v2.120.1 // indirect
-	k8s.io/kms v0.30.4 // indirect
+	k8s.io/apiserver v0.31.0 // indirect
+	k8s.io/controller-manager v0.31.0 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/kms v0.31.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
-	k8s.io/metrics v0.30.4 // indirect
+	k8s.io/metrics v0.31.0 // indirect
 	oras.land/oras-go v1.2.5 // indirect
-	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.29.0 // indirect
+	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.30.3 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
-	sigs.k8s.io/kustomize/api v0.13.5-0.20230601165947-6ce0bf390ce3 // indirect
-	sigs.k8s.io/kustomize/kustomize/v5 v5.0.4-0.20230601165947-6ce0bf390ce3 // indirect
-	sigs.k8s.io/kustomize/kyaml v0.14.3-0.20230601165947-6ce0bf390ce3 // indirect
+	sigs.k8s.io/kustomize/api v0.17.2 // indirect
+	sigs.k8s.io/kustomize/kustomize/v5 v5.4.2 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.17.1 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 )
 
 // Replacements duplicated from upstream Kubernetes
 replace (
-	// https://github.com/kubernetes/kubernetes/blob/v1.30.4/go.mod#L239-L270
-	k8s.io/api => k8s.io/api v0.30.4
-	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.30.4
-	k8s.io/apimachinery => k8s.io/apimachinery v0.30.4
-	k8s.io/apiserver => k8s.io/apiserver v0.30.4
-	k8s.io/cli-runtime => k8s.io/cli-runtime v0.30.4
-	k8s.io/client-go => k8s.io/client-go v0.30.4
-	k8s.io/cloud-provider => k8s.io/cloud-provider v0.30.4
-	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.30.4
-	k8s.io/code-generator => k8s.io/code-generator v0.30.4
-	k8s.io/component-base => k8s.io/component-base v0.30.4
-	k8s.io/component-helpers => k8s.io/component-helpers v0.30.4
-	k8s.io/controller-manager => k8s.io/controller-manager v0.30.4
-	k8s.io/cri-api => k8s.io/cri-api v0.30.4
-	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.30.4
-	k8s.io/dynamic-resource-allocation => k8s.io/dynamic-resource-allocation v0.30.4
-	k8s.io/endpointslice => k8s.io/endpointslice v0.30.4
-	k8s.io/kms => k8s.io/kms v0.30.4
-	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.30.4
-	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.30.4
-	k8s.io/kube-proxy => k8s.io/kube-proxy v0.30.4
-	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.30.4
-	k8s.io/kubectl => k8s.io/kubectl v0.30.4
-	k8s.io/kubelet => k8s.io/kubelet v0.30.4
-	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.30.4
-	k8s.io/metrics => k8s.io/metrics v0.30.4
-	k8s.io/mount-utils => k8s.io/mount-utils v0.30.4
-	k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.30.4
-	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.30.4
-	k8s.io/sample-cli-plugin => k8s.io/sample-cli-plugin v0.30.4
-	k8s.io/sample-controller => k8s.io/sample-controller v0.30.4
+	// https://github.com/kubernetes/kubernetes/blob/v1.31.0/go.mod#L227-L258
+	k8s.io/api => k8s.io/api v0.31.0
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.31.0
+	k8s.io/apimachinery => k8s.io/apimachinery v0.31.0
+	k8s.io/apiserver => k8s.io/apiserver v0.31.0
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.31.0
+	k8s.io/client-go => k8s.io/client-go v0.31.0
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.31.0
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.31.0
+	k8s.io/code-generator => k8s.io/code-generator v0.31.0
+	k8s.io/component-base => k8s.io/component-base v0.31.0
+	k8s.io/component-helpers => k8s.io/component-helpers v0.31.0
+	k8s.io/controller-manager => k8s.io/controller-manager v0.31.0
+	k8s.io/cri-api => k8s.io/cri-api v0.31.0
+	k8s.io/cri-client => k8s.io/cri-client v0.31.0
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.31.0
+	k8s.io/dynamic-resource-allocation => k8s.io/dynamic-resource-allocation v0.31.0
+	k8s.io/endpointslice => k8s.io/endpointslice v0.31.0
+	k8s.io/kms => k8s.io/kms v0.31.0
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.31.0
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.31.0
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.31.0
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.31.0
+	k8s.io/kubectl => k8s.io/kubectl v0.31.0
+	k8s.io/kubelet => k8s.io/kubelet v0.31.0
+	k8s.io/metrics => k8s.io/metrics v0.31.0
+	k8s.io/mount-utils => k8s.io/mount-utils v0.31.0
+	k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.31.0
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.31.0
+	k8s.io/sample-cli-plugin => k8s.io/sample-cli-plugin v0.31.0
+	k8s.io/sample-controller => k8s.io/sample-controller v0.31.0
 )

hack/tool/go.mod
@@ -26,9 +26,10 @@ require (
 	github.com/carlmjohnson/versioninfo v0.22.5 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/creasty/defaults v1.7.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/davidmz/go-pageant v1.0.2 // indirect
 	github.com/denisbrodbeck/machineid v1.0.1 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -75,6 +76,7 @@ require (
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/tidwall/transform v0.0.0-20201103190739-32f242e2dbde // indirect
 	github.com/urfave/cli/v2 v2.27.2 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	github.com/xtgo/uuid v0.0.0-20140804021211-a0b114877d4c // indirect
 	github.com/zclconf/go-cty v1.14.4 // indirect
@@ -87,10 +89,10 @@ require (
 	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/apimachinery v0.30.4 // indirect
-	k8s.io/client-go v0.30.4 // indirect
+	k8s.io/apimachinery v0.31.0 // indirect
+	k8s.io/client-go v0.31.0 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
-	k8s.io/utils v0.0.0-20240502163921-fe8a2dddb1d0 // indirect
+	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect

commit 2eb6c639a54753bac7ef8a6822cc0c30a3501ff6

Always skip controller-runtime name validation
Controller-runtime maintains a global checklist of controller names and
does not currently provide a way to unregister the controller names used
by discarded managers. The autopilot controller and worker components
accidentally share some controller names. So it's necessary to suppress
the global name check because the order in which components are started
is not fully guaranteed for k0s controller nodes running an embedded
worker.

Fixes: 79b95965a ("Bump Kubernetes to v1.31.0")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/autopilot/controller/root_controller.go
@@ -37,6 +37,7 @@ import (
 	"k8s.io/utils/ptr"
 	cr "sigs.k8s.io/controller-runtime"
 	crcli "sigs.k8s.io/controller-runtime/pkg/client"
+	crconfig "sigs.k8s.io/controller-runtime/pkg/config"
 	crman "sigs.k8s.io/controller-runtime/pkg/manager"
 	crmetricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	crwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -59,8 +60,6 @@ type rootController struct {
 	stopSubHandler         subControllerStopFunc
 	leaseWatcherCreator    leaseWatcherCreatorFunc
 	setupHandler           setupFunc
-
-	initialized bool
 }
 
 var _ aproot.Root = (*rootController)(nil)
@@ -153,6 +152,17 @@ func (c *rootController) Run(ctx context.Context) error {
 func (c *rootController) startSubControllerRoutine(ctx context.Context, logger *logrus.Entry, event LeaseEventStatus) error {
 	managerOpts := crman.Options{
 		Scheme: scheme,
+		Controller: crconfig.Controller{
+			// Controller-runtime maintains a global checklist of controller
+			// names and does not currently provide a way to unregister the
+			// controller names used by discarded managers. The autopilot
+			// controller and worker components accidentally share some
+			// controller names. So it's necessary to suppress the global name
+			// check because the order in which components are started is not
+			// fully guaranteed for k0s controller nodes running an embedded
+			// worker.
+			SkipNameValidation: ptr.To(true),
+		},
 		WebhookServer: crwebhook.NewServer(crwebhook.Options{
 			Port: c.cfg.ManagerPort,
 		}),
@@ -160,16 +170,6 @@ func (c *rootController) startSubControllerRoutine(ctx context.Context, logger *
 			BindAddress: c.cfg.MetricsBindAddr,
 		},
 		HealthProbeBindAddress: c.cfg.HealthProbeBindAddr,
-	}
-
-	// If this controller is already initialized, this means that all
-	// controller-runtime controllers have already been successfully registered
-	// to another manager. However, controller-runtime maintains a global
-	// checklist of controller names and doesn't currently provide a way to
-	// unregister names from discarded managers. So it's necessary to suppress
-	// the global name check whenever things are restarted for reconfiguration.
-	if c.initialized {
-		managerOpts.Controller.SkipNameValidation = ptr.To(true)
 	}
 
 	mgr, err := cr.NewManager(c.autopilotClientFactory.RESTConfig(), managerOpts)
@@ -231,9 +231,6 @@ func (c *rootController) startSubControllerRoutine(ctx context.Context, logger *
 		logger.WithError(err).Error("unable to register 'update' controllers")
 		return err
 	}
-
-	// All the controller-runtime controllers have been registered.
-	c.initialized = true
 
 	// The controller-runtime start blocks until the context is cancelled.
 	if err := mgr.Start(ctx); err != nil {

pkg/autopilot/controller/root_worker.go
@@ -29,7 +29,9 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	k8sretry "k8s.io/client-go/util/retry"
+	"k8s.io/utils/ptr"
 	cr "sigs.k8s.io/controller-runtime"
+	crconfig "sigs.k8s.io/controller-runtime/pkg/config"
 	crman "sigs.k8s.io/controller-runtime/pkg/manager"
 	crmetricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	crwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -61,6 +63,17 @@ func (w *rootWorker) Run(ctx context.Context) error {
 
 	managerOpts := crman.Options{
 		Scheme: scheme,
+		Controller: crconfig.Controller{
+			// Controller-runtime maintains a global checklist of controller
+			// names and does not currently provide a way to unregister the
+			// controller names used by discarded managers. The autopilot
+			// controller and worker components accidentally share some
+			// controller names. So it's necessary to suppress the global name
+			// check because the order in which components are started is not
+			// fully guaranteed for k0s controller nodes running an embedded
+			// worker.
+			SkipNameValidation: ptr.To(true),
+		},
 		WebhookServer: crwebhook.NewServer(crwebhook.Options{
 			Port: w.cfg.ManagerPort,
 		}),