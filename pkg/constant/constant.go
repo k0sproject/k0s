3515d94b9fa34e5b42545704261c5f2f8be14525
Tom Wieczorek <twieczorek@mirantis.com>
2025-02-05 15:20:16 +0100

    Bump Envoy to v1.30.9
    https://github.com/envoyproxy/envoy/releases/tag/v1.30.8
    https://github.com/envoyproxy/envoy/releases/tag/v1.30.9
    
    Fixes CVE-2024-53269 and CVE-2024-53270.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -90,7 +90,7 @@ const (
 	CoreDNSImage                       = "quay.io/k0sproject/coredns"
 	CoreDNSImageVersion                = "1.11.4"
 	EnvoyProxyImage                    = "quay.io/k0sproject/envoy-distroless"
-	EnvoyProxyImageVersion             = "v1.30.7"
+	EnvoyProxyImageVersion             = "v1.30.9"
 	CalicoImage                        = "quay.io/k0sproject/calico-cni"
 	CalicoComponentImagesVersion       = "v3.27.3-0"
 	CalicoNodeImage                    = "quay.io/k0sproject/calico-node"
