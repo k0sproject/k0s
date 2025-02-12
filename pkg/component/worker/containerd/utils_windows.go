2f9e4a3910530c1758b81e78012ad22ced92b904
Tom Wieczorek <twieczorek@mirantis.com>
2025-02-12 08:43:12 +0100

    Run containerd as a supervised process on Windows
    This simplifies the component code quite a bit, and removes the
    PowerShell dependency. This way, containerd on Windows is no different
    than on Linux, modulo the following:
    
    - There are no signals on Windows, so the SIGHUP trick that tells
      containerd to reload its configuration won't work. In this case, fall
      back to a full stop/start cycle.
    
    - There's a bug in containerd 1.x on Windows that prevents it from
      properly responding to Ctrl+C and Ctrl+Break events. Apply the 2.x
      one-liner patch to the 1.x codebase so the Supervisor can stop it.
    
    - On Windows, containerd requires the special
      SeAssignPrimaryTokenPrivilege, which allows processes to logon any
      user without knowing the password. This privilege is not granted to
      administrator accounts, it is only granted to the built-in SYSTEM
      and LOCAL SERVICE accounts. Since containerd will inherit k0s's
      privileges, it can only function properly if k0s itself has this
      privilege. Therefore, k0s must be run as a service. Consequently,
      running a k0s worker in a terminal with a standard administrator
      account will result in containerd throwing permission errors when
      running containers. This is an acceptable trade-off IMO, as any real
      setup will involve using Windows services anyway. For quick manual
      testing, dealing with the container errors is acceptable.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/component/worker/containerd/utils_windows.go
+++ pkg/component/worker/containerd/utils_windows.go
@@ -5,8 +5,6 @@ package containerd
 
 import (
 	"net/url"
-	"os"
-	"os/exec"
 	"path/filepath"
 )
 
@@ -19,37 +17,4 @@ func Endpoint(runDir string) *url.URL {
 		Scheme: "npipe",
 		Path:   filepath.ToSlash(Address(runDir)),
 	}
-}
-
-// PowerShell struct
-type PowerShell struct {
-	powerShell string
-	err        error
-}
-
-// New create new session
-func NewPowershell() *PowerShell {
-	ps, err := exec.LookPath("powershell.exe")
-	return &PowerShell{
-		powerShell: ps,
-		err:        err,
-	}
-}
-
-func (p *PowerShell) execute(args ...string) error {
-	if p.err != nil {
-		return p.err
-	}
-	args = append([]string{"-NoProfile", "-NonInteractive"}, args...)
-	cmd := exec.Command(p.powerShell, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
-
-func winExecute(args ...string) error {
-	ps := NewPowershell()
-
-	r := ps.execute(args...)
-	return r
 }
