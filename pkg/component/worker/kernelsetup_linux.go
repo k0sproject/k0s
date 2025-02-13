642b0557f741c29db5b2e48ab58a034e68a8cdb2
Ethan Mosbaugh <emosbaugh@gmail.com>
2025-02-13 07:14:46 -0800

    fix log format string
    Co-authored-by: Tom Wieczorek <twz123@users.noreply.github.com>
    Signed-off-by: Ethan Mosbaugh <emosbaugh@gmail.com>
    Signed-off-by: Ethan Mosbaugh <ethan@replicated.com>

--- pkg/component/worker/kernelsetup_linux.go
+++ pkg/component/worker/kernelsetup_linux.go
@@ -45,7 +45,7 @@ func hasFilesystem(filesystem string) bool {
 func modprobe(module string) {
 	out, err := exec.Command("modprobe", module).CombinedOutput()
 	if err != nil {
-		logrus.Warnf("failed to load %s kernel module: %s, out=%q", module, err.Error(), string(out))
+		logrus.WithError(err).Warnf("failed to load kernel module %s: %s", module, out)
 	}
 }
 
