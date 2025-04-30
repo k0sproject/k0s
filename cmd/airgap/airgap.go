k0sproject/k0s

commit f9773809eb490955a38f14e02051bab139d5dee1

Encapsulate debug flag handling
Introduce internal.DebugFlags to replace debug and verbose logging
flag handling. Move the flag definitions from the "PersistentFlagSet"
to a struct method. Remove the debug flag handling from the root
command and move it to the subcommands. This allows for the removal
of CallPersistentPreRun, since the commands no longer rely on proper
chaining of the PersistentPreRun hooks.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

cmd/config/config.go
@@ -17,21 +17,30 @@ limitations under the License.
 package config
 
 import (
+	"github.com/k0sproject/k0s/cmd/internal"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
 func NewConfigCmd() *cobra.Command {
+	var debugFlags internal.DebugFlags
+
 	cmd := &cobra.Command{
-		Use:   "config",
-		Short: "Configuration related sub-commands",
-		Args:  cobra.NoArgs,
-		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
+		Use:              "config",
+		Short:            "Configuration related sub-commands",
+		Args:             cobra.NoArgs,
+		PersistentPreRun: debugFlags.Run,
+		RunE:             func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
+
+	debugFlags.AddToFlagSet(cmd.PersistentFlags())
+
 	cmd.AddCommand(NewCreateCmd())
 	cmd.AddCommand(NewEditCmd())
 	cmd.AddCommand(NewStatusCmd())
 	cmd.AddCommand(NewValidateCmd())
+
 	return cmd
 }
 

cmd/config/create.go
@@ -17,13 +17,15 @@ limitations under the License.
 package config
 
 import (
-	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"sigs.k8s.io/yaml"
-
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	k0sscheme "github.com/k0sproject/k0s/pkg/client/clientset/scheme"
 	"github.com/k0sproject/k0s/pkg/config"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"sigs.k8s.io/yaml"
 )
 
 func NewCreateCmd() *cobra.Command {
@@ -57,7 +59,11 @@ func NewCreateCmd() *cobra.Command {
 	}
 
 	flags := cmd.Flags()
-	flags.AddFlagSet(config.GetPersistentFlagSet())
+	config.GetPersistentFlagSet().VisitAll(func(f *pflag.Flag) {
+		f.Hidden = true
+		f.Deprecated = "it has no effect and will be removed in a future release"
+		cmd.PersistentFlags().AddFlag(f)
+	})
 	flags.BoolVar(&includeImages, "include-images", false, "include the default images in the output")
 
 	return cmd

cmd/config/validate.go
@@ -26,6 +26,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/config"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func NewValidateCmd() *cobra.Command {
@@ -62,7 +63,11 @@ func NewValidateCmd() *cobra.Command {
 	}
 
 	flags := cmd.Flags()
-	flags.AddFlagSet(config.GetPersistentFlagSet())
+	config.GetPersistentFlagSet().VisitAll(func(f *pflag.Flag) {
+		f.Hidden = true
+		f.Deprecated = "it has no effect and will be removed in a future release"
+		cmd.PersistentFlags().AddFlag(f)
+	})
 	flags.AddFlagSet(config.FileInputFlag())
 	_ = cmd.MarkFlagRequired("config")
 

cmd/token/preshared.go
@@ -19,7 +19,6 @@ package token
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -28,6 +27,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
 	bootstraptokenv1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/bootstraptoken/v1"
 
@@ -36,6 +36,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/token"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func preSharedCmd() *cobra.Command {
@@ -52,38 +53,29 @@ func preSharedCmd() *cobra.Command {
 		Short:   "Generates token and secret and stores them as a files",
 		Example: `k0s token pre-shared --role worker --cert <path>/<to>/ca.crt --url https://<controller-ip>:<port>/`,
 		Args:    cobra.NoArgs,
-		PreRunE: func(cmd *cobra.Command, _ []string) error {
-			err := checkTokenRole(preSharedRole)
-			if err != nil {
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			if err := checkTokenRole(preSharedRole); err != nil {
 				return err
 			}
-			if certPath == "" {
-				return errors.New("please, provide --cert argument")
-			}
-			if joinURL == "" {
-				return errors.New("please, provide --url argument")
-			}
 
-			return nil
-		},
-		RunE: func(cmd *cobra.Command, _ []string) error {
 			t, err := createSecret(preSharedRole, validity, outDir)
 			if err != nil {
 				return err
 			}
 
-			err = createKubeConfig(t, preSharedRole, joinURL, certPath, outDir)
-			if err != nil {
-				return err
-			}
-			return nil
+			return createKubeConfig(t, preSharedRole, joinURL, certPath, outDir)
 		},
 	}
 
 	flags := cmd.Flags()
-	flags.AddFlagSet(config.GetPersistentFlagSet())
+	config.GetPersistentFlagSet().VisitAll(func(f *pflag.Flag) {
+		f.Hidden = true
+		flags.AddFlag(f)
+	})
 	flags.StringVar(&certPath, "cert", "", "path to the CA certificate file")
+	runtime.Must(cobra.MarkFlagRequired(flags, "cert"))
 	flags.StringVar(&joinURL, "url", "", "url of the api server to join")
+	runtime.Must(cobra.MarkFlagRequired(flags, "url"))
 	flags.StringVar(&preSharedRole, "role", "worker", "token role. valid values: worker, controller. Default: worker")
 	flags.StringVar(&outDir, "out", ".", "path to the output directory. Default: current dir")
 	flags.DurationVar(&validity, "valid", 0, "how long token is valid, in Go duration format")

cmd/token/token.go
@@ -19,6 +19,7 @@ package token
 import (
 	"fmt"
 
+	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/pkg/token"
 
 	"github.com/spf13/cobra"
@@ -26,12 +27,17 @@ import (
 )
 
 func NewTokenCmd() *cobra.Command {
+	var debugFlags internal.DebugFlags
+
 	cmd := &cobra.Command{
-		Use:   "token",
-		Short: "Manage join tokens",
-		Args:  cobra.NoArgs,
-		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
+		Use:              "token",
+		Short:            "Manage join tokens",
+		Args:             cobra.NoArgs,
+		PersistentPreRun: debugFlags.Run,
+		RunE:             func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
+
+	debugFlags.AddToFlagSet(cmd.PersistentFlags())
 
 	cmd.AddCommand(tokenListCmd())
 	cmd.AddCommand(tokenInvalidateCmd())

cmd/airgap/listimages.go
@@ -19,6 +19,7 @@ package airgap
 import (
 	"fmt"
 
+	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/pkg/airgap"
 	"github.com/k0sproject/k0s/pkg/config"
 
@@ -26,13 +27,17 @@ import (
 )
 
 func NewAirgapListImagesCmd() *cobra.Command {
-	var all bool
+	var (
+		debugFlags internal.DebugFlags
+		all        bool
+	)
 
 	cmd := &cobra.Command{
-		Use:     "list-images",
-		Short:   "List image names and version needed for air-gap install",
-		Example: `k0s airgap list-images`,
-		Args:    cobra.NoArgs,
+		Use:              "list-images",
+		Short:            "List image names and version needed for air-gap install",
+		Example:          `k0s airgap list-images`,
+		Args:             cobra.NoArgs,
+		PersistentPreRun: debugFlags.Run,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			opts, err := config.GetCmdOpts(cmd)
 			if err != nil {
@@ -53,6 +58,8 @@ func NewAirgapListImagesCmd() *cobra.Command {
 			return nil
 		},
 	}
+
+	debugFlags.AddToFlagSet(cmd.PersistentFlags())
 
 	flags := cmd.Flags()
 	flags.AddFlagSet(config.GetPersistentFlagSet())

cmd/api/api.go
@@ -32,7 +32,7 @@ import (
 	"strings"
 	"time"
 
-	internallog "github.com/k0sproject/k0s/internal/pkg/log"
+	"github.com/k0sproject/k0s/cmd/internal"
 	mw "github.com/k0sproject/k0s/internal/pkg/middleware"
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	"github.com/k0sproject/k0s/pkg/config"
@@ -52,17 +52,15 @@ import (
 )
 
 func NewAPICmd() *cobra.Command {
+	var debugFlags internal.DebugFlags
+
 	cmd := &cobra.Command{
 		Use:   "api",
 		Short: "Run the controller API",
 		Long: `Run the controller API.
 Reads the runtime configuration from standard input.`,
-		Args: cobra.NoArgs,
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			logrus.SetOutput(cmd.OutOrStdout())
-			internallog.SetInfoLevel()
-			return config.CallParentPersistentPreRun(cmd, args)
-		},
+		Args:             cobra.NoArgs,
+		PersistentPreRun: debugFlags.Run,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			var run func() error
 
@@ -75,6 +73,8 @@ Reads the runtime configuration from standard input.`,
 			return run()
 		},
 	}
+
+	debugFlags.LongRunning().AddToFlagSet(cmd.PersistentFlags())
 
 	flags := cmd.Flags()
 	config.GetPersistentFlagSet().VisitAll(func(f *pflag.Flag) {

cmd/backup/backup_unix.go
@@ -25,6 +25,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/internal/pkg/dir"
 	k0sv1beta1 "github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	"github.com/k0sproject/k0s/pkg/backup"
@@ -38,12 +39,16 @@ import (
 type command config.CLIOptions
 
 func NewBackupCmd() *cobra.Command {
-	var savePath string
+	var (
+		debugFlags internal.DebugFlags
+		savePath   string
+	)
 
 	cmd := &cobra.Command{
-		Use:   "backup",
-		Short: "Back-Up k0s configuration. Must be run as root (or with sudo)",
-		Args:  cobra.NoArgs,
+		Use:              "backup",
+		Short:            "Back-Up k0s configuration. Must be run as root (or with sudo)",
+		Args:             cobra.NoArgs,
+		PersistentPreRun: debugFlags.Run,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			opts, err := config.GetCmdOpts(cmd)
 			if err != nil {
@@ -60,6 +65,8 @@ func NewBackupCmd() *cobra.Command {
 			return c.backup(nodeConfig, savePath, cmd.OutOrStdout())
 		},
 	}
+
+	debugFlags.AddToFlagSet(cmd.PersistentFlags())
 
 	flags := cmd.Flags()
 	flags.AddFlagSet(config.GetPersistentFlagSet())

commit 79109e45b75e9bacce8fcff0f9dcc88b9f76e3b8

Fix backwards compat for debug flag handling
The current version of the debug flag handling rejects debug flags
on the airgap level, i.e. "k0s airgap -v" produces an error, which was
valid in 1.32.

Add back the debug flags as hidden/deprecated on that level, and add
explicit support for them on the level where they're actually used. Do
a similar change for the config and token subcommands, so that the flags
are only available for subcommands for which they make sense.

Fixes: f9773809e ("Encapsulate debug flag handling")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

cmd/airgap/airgap.go
@@ -17,6 +17,7 @@ limitations under the License.
 package airgap
 
 import (
+	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/pkg/config"
 
 	"github.com/spf13/cobra"
@@ -32,6 +33,7 @@ func NewAirgapCmd() *cobra.Command {
 	}
 
 	var deprecatedFlags pflag.FlagSet
+	(&internal.DebugFlags{}).AddToFlagSet(&deprecatedFlags)
 	deprecatedFlags.AddFlagSet(config.GetPersistentFlagSet())
 	deprecatedFlags.AddFlagSet(config.FileInputFlag())
 	deprecatedFlags.VisitAll(func(f *pflag.Flag) {

cmd/config/config.go
@@ -17,24 +17,17 @@ limitations under the License.
 package config
 
 import (
-	"github.com/k0sproject/k0s/cmd/internal"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
 func NewConfigCmd() *cobra.Command {
-	var debugFlags internal.DebugFlags
-
 	cmd := &cobra.Command{
-		Use:              "config",
-		Short:            "Configuration related sub-commands",
-		Args:             cobra.NoArgs,
-		PersistentPreRun: debugFlags.Run,
-		RunE:             func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
+		Use:   "config",
+		Short: "Configuration related sub-commands",
+		Args:  cobra.NoArgs,
+		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
-
-	debugFlags.AddToFlagSet(cmd.PersistentFlags())
 
 	cmd.AddCommand(NewCreateCmd())
 	cmd.AddCommand(NewEditCmd())

cmd/config/create.go
@@ -17,6 +17,7 @@ limitations under the License.
 package config
 
 import (
+	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	k0sscheme "github.com/k0sproject/k0s/pkg/client/clientset/scheme"
 	"github.com/k0sproject/k0s/pkg/config"
@@ -29,12 +30,16 @@ import (
 )
 
 func NewCreateCmd() *cobra.Command {
-	var includeImages bool
+	var (
+		debugFlags    internal.DebugFlags
+		includeImages bool
+	)
 
 	cmd := &cobra.Command{
-		Use:   "create",
-		Short: "Output the default k0s configuration yaml to stdout",
-		Args:  cobra.NoArgs,
+		Use:              "create",
+		Short:            "Output the default k0s configuration yaml to stdout",
+		Args:             cobra.NoArgs,
+		PersistentPreRun: debugFlags.Run,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			config := v1beta1.DefaultClusterConfig()
 			if !includeImages {
@@ -58,13 +63,15 @@ func NewCreateCmd() *cobra.Command {
 		},
 	}
 
-	flags := cmd.Flags()
+	pflags := cmd.PersistentFlags()
+	debugFlags.AddToFlagSet(pflags)
 	config.GetPersistentFlagSet().VisitAll(func(f *pflag.Flag) {
 		f.Hidden = true
 		f.Deprecated = "it has no effect and will be removed in a future release"
-		cmd.PersistentFlags().AddFlag(f)
+		pflags.AddFlag(f)
 	})
-	flags.BoolVar(&includeImages, "include-images", false, "include the default images in the output")
+
+	cmd.Flags().BoolVar(&includeImages, "include-images", false, "include the default images in the output")
 
 	return cmd
 }

cmd/config/validate.go
@@ -22,6 +22,7 @@ import (
 	"io"
 	"os"
 
+	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	"github.com/k0sproject/k0s/pkg/config"
 
@@ -30,12 +31,15 @@ import (
 )
 
 func NewValidateCmd() *cobra.Command {
+	var debugFlags internal.DebugFlags
+
 	cmd := &cobra.Command{
 		Use:   "validate",
 		Short: "Validate k0s configuration",
 		Long: `Example:
    k0s config validate --config path_to_config.yaml`,
-		Args: cobra.NoArgs,
+		Args:             cobra.NoArgs,
+		PersistentPreRun: debugFlags.Run,
 		RunE: func(cmd *cobra.Command, _ []string) (err error) {
 			var bytes []byte
 
@@ -62,13 +66,15 @@ func NewValidateCmd() *cobra.Command {
 		},
 	}
 
-	flags := cmd.Flags()
+	pflags := cmd.PersistentFlags()
+	debugFlags.AddToFlagSet(pflags)
 	config.GetPersistentFlagSet().VisitAll(func(f *pflag.Flag) {
 		f.Hidden = true
 		f.Deprecated = "it has no effect and will be removed in a future release"
-		cmd.PersistentFlags().AddFlag(f)
+		pflags.AddFlag(f)
 	})
-	flags.AddFlagSet(config.FileInputFlag())
+
+	cmd.Flags().AddFlagSet(config.FileInputFlag())
 	_ = cmd.MarkFlagRequired("config")
 
 	return cmd

cmd/token/preshared.go
@@ -31,6 +31,7 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	bootstraptokenv1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/bootstraptoken/v1"
 
+	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/internal/pkg/file"
 	"github.com/k0sproject/k0s/pkg/config"
 	"github.com/k0sproject/k0s/pkg/token"
@@ -67,11 +68,15 @@ func preSharedCmd() *cobra.Command {
 		},
 	}
 
-	flags := cmd.Flags()
+	var deprecatedFlags pflag.FlagSet
+	(&internal.DebugFlags{}).AddToFlagSet(&deprecatedFlags)
+	deprecatedFlags.AddFlagSet(config.GetPersistentFlagSet())
 	config.GetPersistentFlagSet().VisitAll(func(f *pflag.Flag) {
 		f.Hidden = true
-		flags.AddFlag(f)
+		cmd.PersistentFlags().AddFlag(f)
 	})
+
+	flags := cmd.Flags()
 	flags.StringVar(&certPath, "cert", "", "path to the CA certificate file")
 	runtime.Must(cobra.MarkFlagRequired(flags, "cert"))
 	flags.StringVar(&joinURL, "url", "", "url of the api server to join")

cmd/token/token.go
@@ -19,7 +19,6 @@ package token
 import (
 	"fmt"
 
-	"github.com/k0sproject/k0s/cmd/internal"
 	"github.com/k0sproject/k0s/pkg/token"
 
 	"github.com/spf13/cobra"
@@ -27,17 +26,12 @@ import (
 )
 
 func NewTokenCmd() *cobra.Command {
-	var debugFlags internal.DebugFlags
-
 	cmd := &cobra.Command{
-		Use:              "token",
-		Short:            "Manage join tokens",
-		Args:             cobra.NoArgs,
-		PersistentPreRun: debugFlags.Run,
-		RunE:             func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
+		Use:   "token",
+		Short: "Manage join tokens",
+		Args:  cobra.NoArgs,
+		RunE:  func(*cobra.Command, []string) error { return pflag.ErrHelp }, // Enforce arg validation
 	}
-
-	debugFlags.AddToFlagSet(cmd.PersistentFlags())
 
 	cmd.AddCommand(tokenListCmd())
 	cmd.AddCommand(tokenInvalidateCmd())