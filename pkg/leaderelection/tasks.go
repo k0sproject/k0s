k0sproject/k0s

commit d69e33d051adae5475a378994fb40b533ff4650c

Introduce leaderelection.RunLeaderTasks
This is a useful abstraction that runs a callback whose context gets
cancelled as soon as the lead gets lost.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/leaderelection/tasks.go
@@ -1,0 +1,59 @@
+/*
+Copyright 2024 k0s authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package leaderelection
+
+import (
+	"context"
+	"errors"
+)
+
+// Indicates that the previously gained lead has been lost.
+var ErrLostLead = errors.New("lost the lead")
+
+// Returns the current leader election status. Whenever the status becomes
+// outdated, the returned expired channel will be closed.
+type StatusFunc func() (current Status, expired <-chan struct{})
+
+// Runs the provided tasks function when the lead is taken. It continuously
+// monitors the leader election status using statusFunc. When the lead is taken,
+// the tasks function is called with a context that is cancelled either when the
+// lead has been lost or ctx is done. After the tasks function returns, the
+// process is repeated until ctx is done.
+func RunLeaderTasks(ctx context.Context, statusFunc StatusFunc, tasks func(context.Context)) {
+	for {
+		status, statusExpired := statusFunc()
+
+		if status == StatusLeading {
+			ctx, cancel := context.WithCancelCause(ctx)
+			go func() {
+				select {
+				case <-statusExpired:
+					cancel(ErrLostLead)
+				case <-ctx.Done():
+				}
+			}()
+
+			tasks(ctx)
+		}
+
+		select {
+		case <-statusExpired:
+		case <-ctx.Done():
+			return
+		}
+	}
+}

pkg/component/controller/leaderelector/leasepool.go
@@ -18,6 +18,7 @@ package leaderelector
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"sync"
 
@@ -79,44 +80,25 @@ func (l *LeasePool) Start(context.Context) error {
 		return err
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancelCause(context.Background())
 	var wg sync.WaitGroup
 
 	wg.Add(2)
 	go func() { defer wg.Done(); client.Run(ctx, l.status.Set) }()
-	go func() { defer wg.Done(); l.invokeCallbacks(ctx.Done()) }()
+	go func() { defer wg.Done(); l.invokeCallbacks(ctx) }()
 
-	l.stop = func() { cancel(); wg.Wait() }
+	l.stop = func() { cancel(errors.New("lease pool is stopping")); wg.Wait() }
 	return nil
 }
 
-func (l *LeasePool) invokeCallbacks(done <-chan struct{}) {
-	var lastStatus leaderelection.Status
-
-	for {
-		status, statusChanged := l.status.Peek()
-
-		if status != lastStatus {
-			lastStatus = status
-			if status == leaderelection.StatusLeading {
-				l.log.Info("acquired leader lease")
-				runCallbacks(l.acquiredLeaseCallbacks)
-			} else {
-				l.log.Info("lost leader lease")
-				runCallbacks(l.lostLeaseCallbacks)
-			}
-		}
-
-		select {
-		case <-statusChanged:
-		case <-done:
-			l.log.Info("Lease pool is stopping")
-			if status == leaderelection.StatusLeading {
-				runCallbacks(l.lostLeaseCallbacks)
-			}
-			return
-		}
-	}
+func (l *LeasePool) invokeCallbacks(ctx context.Context) {
+	leaderelection.RunLeaderTasks(ctx, l.status.Peek, func(leaderCtx context.Context) {
+		l.log.Info("acquired leader lease")
+		runCallbacks(l.acquiredLeaseCallbacks)
+		<-leaderCtx.Done()
+		l.log.Infof("lost leader lease (%v)", context.Cause(ctx))
+		runCallbacks(l.lostLeaseCallbacks)
+	})
 }
 
 func runCallbacks(callbacks []func()) {

commit 731d61b0854b1d1415e1fa758c255487f603b327

Fix spelling in comment
Fixes: d69e33d05 ("Introduce leaderelection.RunLeaderTasks")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/leaderelection/tasks.go
@@ -30,7 +30,7 @@ type StatusFunc func() (current Status, expired <-chan struct{})
 
 // Runs the provided tasks function when the lead is taken. It continuously
 // monitors the leader election status using statusFunc. When the lead is taken,
-// the tasks function is called with a context that is cancelled either when the
+// the tasks function is called with a context that is canceled either when the
 // lead has been lost or ctx is done. After the tasks function returns, the
 // process is repeated until ctx is done.
 func RunLeaderTasks(ctx context.Context, statusFunc StatusFunc, tasks func(context.Context)) {