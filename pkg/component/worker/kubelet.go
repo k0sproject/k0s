a713cb5de507a52470daa15056bf9c2cd3299adb
Tom Wieczorek <twieczorek@mirantis.com>
2025-06-26 09:18:01 +0200

    Properly initialize kubelet directories on Windows
    The staging code for kubelet.exe was short-circuiting the Init method,
    thus skipping the rest of its code. Instead, only return early on error
    conditions, just like the code for Linux does it.
    
    Fixes: ec6053af4 ("Stage iptables binaries in a separate component")
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit 324c06c09149635be3fbdc5130a0bd2f0993f2d8)
    (cherry picked from commit a86bc113333e24d98c99605cc30b618b1dc91f0d)

--- pkg/component/worker/kubelet.go
+++ pkg/component/worker/kubelet.go
@@ -72,8 +72,9 @@ var _ manager.Component = (*Kubelet)(nil)
 func (k *Kubelet) Init(_ context.Context) error {
 
 	if runtime.GOOS == "windows" {
-		err := assets.Stage(k.K0sVars.BinDir, "kubelet.exe", constant.BinDirMode)
-		return err
+		if err := assets.Stage(k.K0sVars.BinDir, "kubelet.exe", constant.BinDirMode); err != nil {
+			return err
+		}
 	}
 
 	if runtime.GOOS == "linux" {
