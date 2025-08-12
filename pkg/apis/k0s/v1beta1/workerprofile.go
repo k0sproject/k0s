357386bdebdafd7ccfb6acd0339c01299596b568
Tom Wieczorek <twieczorek@mirantis.com>
2025-08-12 11:26:36 +0200

    Semantic kubelet config validation for worker profiles
    Use the kubelet's config validation code to verify the semantic
    correctness of the worker profiles.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/apis/k0s/v1beta1/workerprofile.go
+++ pkg/apis/k0s/v1beta1/workerprofile.go
@@ -5,13 +5,19 @@ package v1beta1
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	kubeletv1beta1 "k8s.io/kubelet/config/v1beta1"
+	"k8s.io/kubernetes/cmd/kubelet/app/options"
+	kubeletscheme "k8s.io/kubernetes/pkg/kubelet/apis/config/scheme"
+	kubeletvalidation "k8s.io/kubernetes/pkg/kubelet/apis/config/validation"
 )
 
 // WorkerProfiles profiles collection
@@ -59,7 +65,7 @@ func (wp *WorkerProfile) Validate(path *field.Path) (errs []error) {
 func (wp *WorkerProfile) validateConfig(path *field.Path) []error {
 	var errs []error
 
-	// Decode the kubelet config.
+	// Decode the versioned kubelet config.
 	kubeletCfg := &kubeletv1beta1.KubeletConfiguration{}
 	if err := json.Unmarshal(wp.Config.Raw, kubeletCfg); err != nil {
 		errs = append(errs, (*shortenedFieldError)(field.Invalid(path, wp.Config.Raw, err.Error())))
@@ -88,6 +94,47 @@ func (wp *WorkerProfile) validateConfig(path *field.Path) []error {
 	}
 	if kubeletCfg.StaticPodURL != "" {
 		errs = append(errs, reservedField("staticPodURL"))
+	}
+
+	// Get the kubelet scheme and apply the defaults to the versioned config.
+	scheme, _, err := kubeletscheme.NewSchemeAndCodecs()
+	if err != nil {
+		errs = append(errs, fmt.Errorf("failed to get kubelet scheme: %w", err))
+		return errs
+	}
+	scheme.Default(kubeletCfg)
+
+	// Create a new kubelet config and use the scheme to convert the versioned config into it.
+	validatedCfg, err := options.NewKubeletConfiguration()
+	if err != nil {
+		errs = append(errs, fmt.Errorf("failed to get kubelet default configuration: %w", err))
+		return errs
+	}
+	if err := scheme.Convert(kubeletCfg, validatedCfg, nil); err != nil {
+		errs = append(errs, (*shortenedFieldError)(field.Invalid(path, wp.Config.Raw, err.Error())))
+		return errs
+	}
+
+	// Run the kubelet's own config validation. Pass in the Kubernetes default
+	// feature gate settings. These will be automatically merged with the
+	// feature gates specified in the kubelet configuration by the validation
+	// function.
+	defaultGates := utilfeature.DefaultMutableFeatureGate.DeepCopyAndReset()
+	if err := kubeletvalidation.ValidateKubeletConfiguration(validatedCfg, defaultGates); err != nil {
+		// Flatten aggregate validation errors
+		var (
+			agg            utilerrors.Aggregate
+			validationErrs []error
+		)
+		if errors.As(err, &agg) {
+			validationErrs = agg.Errors()
+		} else {
+			validationErrs = []error{err}
+		}
+
+		for _, err := range validationErrs {
+			errs = append(errs, (*shortenedFieldError)(field.Invalid(path, wp.Config.Raw, err.Error())))
+		}
 	}
 
 	return errs
