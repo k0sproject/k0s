f39522407edd4ccfb4ec1463bc9ba251e92de479
Tom Wieczorek <twieczorek@mirantis.com>
2025-09-19 17:37:50 +0200

    Implement awaitTermination for Supervisor proc handles
    On newer kernels which support pidfd_open, it's actually possible to
    wait for arbitrary processes. Use pidfds in conjunction with eventfds
    to implement a cancellable procedure that waits for process termination
    without requiring userspace polling.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/supervisor/supervisor.go
+++ pkg/supervisor/supervisor.go
@@ -309,7 +309,14 @@ func (s *Supervisor) isK0sManaged(ph procHandle) (bool, error) {
 }
 
 func (s *Supervisor) awaitTermination(ctx context.Context, ph procHandle) error {
-	s.log.Debug("Polling for process termination")
+	err := ph.awaitTermination(ctx)
+	if !errors.Is(err, errors.ErrUnsupported) {
+		return err
+	}
+
+	// Fall back to polling (old Linux kernels don't have the required syscalls).
+	s.log.WithError(err).Debug("Polling for process termination")
+
 	backoff := wait.Backoff{
 		Duration: 25 * time.Millisecond,
 		Cap:      3 * time.Second,
