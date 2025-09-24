a39ff0fa642c739e3f0130efe36ace70ce77a2ef
Juan-Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>
2025-09-24 13:13:57 +0200

    Bump kubernetes to v1.34.1
    https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.34.md#changelog-since-v1340
    
    Signed-off-by: Juan-Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>

--- pkg/apis/k0s/v1beta1/kubeproxy.go
+++ pkg/apis/k0s/v1beta1/kubeproxy.go
@@ -39,7 +39,7 @@ type KubeProxy struct {
 }
 
 // KubeProxyIPTablesConfiguration contains iptables-related kube-proxy configuration
-// @see https://github.com/kubernetes/kube-proxy/blob/v0.34.0/config/v1alpha1/types.go#L27-L48
+// @see https://github.com/kubernetes/kube-proxy/blob/v0.34.1/config/v1alpha1/types.go#L27-L48
 type KubeProxyIPTablesConfiguration struct {
 	MasqueradeBit      *int32 `json:"masqueradeBit,omitempty"`
 	MasqueradeAll      bool   `json:"masqueradeAll,omitempty"`
@@ -51,7 +51,7 @@ type KubeProxyIPTablesConfiguration struct {
 }
 
 // KubeProxyIPVSConfiguration contains ipvs-related kube-proxy configuration
-// @see https://github.com/kubernetes/kube-proxy/blob/v0.34.0/config/v1alpha1/types.go#L52-L78
+// @see https://github.com/kubernetes/kube-proxy/blob/v0.34.1/config/v1alpha1/types.go#L52-L78
 type KubeProxyIPVSConfiguration struct {
 	// +optional
 	SyncPeriod metav1.Duration `json:"syncPeriod"`
@@ -69,7 +69,7 @@ type KubeProxyIPVSConfiguration struct {
 }
 
 // KubeProxyNFTablesConfiguration contains nftables-related kube-proxy configuration
-// @see https://github.com/kubernetes/kube-proxy/blob/v0.34.0/config/v1alpha1/types.go#L82-L97
+// @see https://github.com/kubernetes/kube-proxy/blob/v0.34.1/config/v1alpha1/types.go#L82-L97
 type KubeProxyNFTablesConfiguration struct {
 	// +optional
 	SyncPeriod    metav1.Duration `json:"syncPeriod"`
