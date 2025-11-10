bccd00ec230b641e0068ef673d604140ce0c2190
Tom Wieczorek <twieczorek@mirantis.com>
2025-11-10 11:27:43 +0100

    Add unit tests for starting and stopping the API
    Let the API try to get its logger from the command's context. This
    allows for injecting a test logger during unit testing, and a safe way
    to intercept and inspec the logs.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -27,6 +27,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/config"
 	"github.com/k0sproject/k0s/pkg/constant"
 	"github.com/k0sproject/k0s/pkg/etcd"
+	"github.com/k0sproject/k0s/pkg/k0scontext"
 	kubeutil "github.com/k0sproject/k0s/pkg/kubernetes"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -52,12 +53,15 @@ Reads the runtime configuration from standard input.`,
 		PersistentPreRun: debugFlags.Run,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			ctx := cmd.Context()
+			log := k0scontext.ValueOrElse(ctx, func() logrus.FieldLogger {
+				return logrus.StandardLogger()
+			})
 
 			var server *http.Server
 
-			if runtimeConfig, err := loadRuntimeConfig(cmd.InOrStdin()); err != nil {
+			if runtimeConfig, err := loadRuntimeConfig(log, cmd.InOrStdin()); err != nil {
 				return err
-			} else if server, err = buildServer(runtimeConfig.Spec.K0sVars, runtimeConfig.Spec.NodeConfig); err != nil {
+			} else if server, err = buildServer(log, runtimeConfig.Spec.K0sVars, runtimeConfig.Spec.NodeConfig); err != nil {
 				return err
 			}
 
@@ -67,7 +71,7 @@ Reads the runtime configuration from standard input.`,
 			}
 			defer server.Close()
 
-			logrus.Info("Listening on ", server.Addr, ", start serving")
+			log.Info("Listening on ", server.Addr, ", start serving")
 
 			doneServing := make(chan struct{})
 			go func() {
@@ -80,7 +84,7 @@ Reads the runtime configuration from standard input.`,
 				return fmt.Errorf("unexpected server error: %w", err)
 
 			case <-ctx.Done():
-				logrus.Info("Shutting down server: ", context.Cause(ctx))
+				log.Info("Shutting down server: ", context.Cause(ctx))
 
 				ctx, cancel := context.WithTimeout(context.TODO(), 3*time.Second)
 				defer cancel()
@@ -93,7 +97,7 @@ Reads the runtime configuration from standard input.`,
 					return fmt.Errorf("unexpected error after server shutdown: %w", err)
 				}
 
-				logrus.Info("Good bye")
+				log.Info("Good bye")
 				return nil
 			}
 		},
@@ -112,8 +116,8 @@ Reads the runtime configuration from standard input.`,
 	return cmd
 }
 
-func loadRuntimeConfig(stdin io.Reader) (*config.RuntimeConfig, error) {
-	logrus.Info("Reading runtime configuration from standard input")
+func loadRuntimeConfig(log logrus.FieldLogger, stdin io.Reader) (*config.RuntimeConfig, error) {
+	log.Info("Reading runtime configuration from standard input")
 	bytes, err := io.ReadAll(stdin)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read from standard input: %w", err)
@@ -127,7 +131,7 @@ func loadRuntimeConfig(stdin io.Reader) (*config.RuntimeConfig, error) {
 	return runtimeConfig, nil
 }
 
-func buildServer(k0sVars *config.CfgVars, nodeConfig *v1beta1.ClusterConfig) (*http.Server, error) {
+func buildServer(log logrus.FieldLogger, k0sVars *config.CfgVars, nodeConfig *v1beta1.ClusterConfig) (*http.Server, error) {
 	// Single kube client for whole lifetime of the API
 	client, err := kubeutil.NewClientFromFile(k0sVars.AdminKubeConfigPath)
 	if err != nil {
@@ -143,12 +147,12 @@ func buildServer(k0sVars *config.CfgVars, nodeConfig *v1beta1.ClusterConfig) (*h
 		// Only mount the etcd handler if we're running on internal etcd storage
 		// by default the mux will return 404 back which the caller should handle
 		mux.Handle(prefix+"/etcd/members", mw.AllowMethods(http.MethodPost)(
-			authMiddleware(etcdHandler(k0sVars.CertRootDir, k0sVars.EtcdCertDir), secrets, "controller-join")))
+			authMiddleware(etcdHandler(log, k0sVars.CertRootDir, k0sVars.EtcdCertDir), log, secrets, "controller-join")))
 	}
 
 	if storage.IsJoinable() {
 		mux.Handle(prefix+"/ca", mw.AllowMethods(http.MethodGet)(
-			authMiddleware(caHandler(k0sVars.CertRootDir), secrets, "controller-join")))
+			authMiddleware(caHandler(k0sVars.CertRootDir), log, secrets, "controller-join")))
 	}
 
 	ipAddr, bindAddressSpecified := nodeConfig.Spec.API.ExtraArgs["bind-address"]
@@ -177,7 +181,7 @@ func buildServer(k0sVars *config.CfgVars, nodeConfig *v1beta1.ClusterConfig) (*h
 	}, nil
 }
 
-func etcdHandler(certRootDir, etcdCertDir string) http.Handler {
+func etcdHandler(log logrus.FieldLogger, certRootDir, etcdCertDir string) http.Handler {
 	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
 		ctx := req.Context()
 		var etcdReq v1beta1.EtcdRequest
@@ -186,7 +190,7 @@ func etcdHandler(certRootDir, etcdCertDir string) http.Handler {
 			sendError(err, resp)
 			return
 		}
-		logrus.Infof("etcd API, adding new member: %s", etcdReq.PeerAddress)
+		log.Infof("etcd API, adding new member: %s", etcdReq.PeerAddress)
 		err = etcdReq.Validate()
 		if err != nil {
 			sendError(err, resp)
@@ -278,7 +282,7 @@ func caHandler(certRootDir string) http.Handler {
 // We need to validate:
 //   - that we find a secret with the ID
 //   - that the token matches whats inside the secret
-func isValidToken(ctx context.Context, secrets clientcorev1.SecretInterface, rawTokenString, usage string) bool {
+func isValidToken(ctx context.Context, log logrus.FieldLogger, secrets clientcorev1.SecretInterface, rawTokenString, usage string) bool {
 	tokenString, err := bootstraptokenv1.NewBootstrapTokenString(rawTokenString)
 	if err != nil {
 		return false
@@ -288,14 +292,14 @@ func isValidToken(ctx context.Context, secrets clientcorev1.SecretInterface, raw
 	secret, err := secrets.Get(ctx, secretName, metav1.GetOptions{})
 	if err != nil {
 		if !apierrors.IsNotFound(err) {
-			logrus.WithError(err).Error("Failed to get bootstrap token with ID ", tokenString.ID)
+			log.WithError(err).Error("Failed to get bootstrap token with ID ", tokenString.ID)
 		}
 		return false
 	}
 
 	token, err := bootstraptokenv1.BootstrapTokenFromSecret(secret)
 	if err != nil {
-		logrus.WithError(err).Errorf("Bootstrap token with ID %s is malformed", tokenString.ID)
+		log.WithError(err).Errorf("Bootstrap token with ID %s is malformed", tokenString.ID)
 		return false
 	}
 
@@ -317,12 +321,12 @@ func isValidToken(ctx context.Context, secrets clientcorev1.SecretInterface, raw
 	}
 }
 
-func authMiddleware(next http.Handler, secrets clientcorev1.SecretInterface, usage string) http.Handler {
+func authMiddleware(next http.Handler, log logrus.FieldLogger, secrets clientcorev1.SecretInterface, usage string) http.Handler {
 	unauthorizedErr := errors.New("go away")
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token, ok := strings.CutPrefix(r.Header.Get("Authorization"), "Bearer ")
-		if ok && isValidToken(r.Context(), secrets, token, usage) {
+		if ok && isValidToken(r.Context(), log, secrets, token, usage) {
 			next.ServeHTTP(w, r)
 		} else {
 			sendError(unauthorizedErr, w, http.StatusUnauthorized)
