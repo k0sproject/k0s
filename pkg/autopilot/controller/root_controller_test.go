7c3be056215ca757525c72fd916bd9a91b59b791
Tom Wieczorek <twieczorek@mirantis.com>
2025-12-11 12:07:41 +0100

    Refactor mode switch AP test to use synctest
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/autopilot/controller/root_controller_test.go
+++ pkg/autopilot/controller/root_controller_test.go
@@ -7,17 +7,18 @@ package controller
 
 import (
 	"context"
+	"sync"
 	"testing"
-	"time"
+	"testing/synctest"
 
 	aptu "github.com/k0sproject/k0s/internal/autopilot/testutil"
-	"github.com/k0sproject/k0s/internal/testutil"
 	apcli "github.com/k0sproject/k0s/pkg/autopilot/client"
 	aproot "github.com/k0sproject/k0s/pkg/autopilot/controller/root"
 	"github.com/k0sproject/k0s/pkg/leaderelection"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -41,77 +42,73 @@ func (le fakeLeaderElector) Run(ctx context.Context, callback func(leaderelectio
 // TestModeSwitch tests the scenario of losing + re-acquiring the kubernetes lease.
 // This toggle should result in sub-controllers being shutdown and then restarted.
 func TestModeSwitch(t *testing.T) {
-	logger := logrus.New().WithField("app", "autopilot-test")
-	clientFactory := aptu.NewFakeClientFactory()
+	synctest.Test(t, func(t *testing.T) {
+		logger := logrus.New().WithField("app", "autopilot-test")
+		clientFactory := aptu.NewFakeClientFactory()
 
-	rootControllerInterface, err := NewRootController(aproot.RootConfig{}, logger, false, testutil.NewFakeClientFactory(), clientFactory)
-	assert.NoError(t, err)
+		rootControllerInterface, err := NewRootController(aproot.RootConfig{}, logger, false, clientFactory.Unwrap(), clientFactory)
+		assert.NoError(t, err)
 
-	rootController, ok := rootControllerInterface.(*rootController)
-	assert.True(t, ok)
-	assert.NotEmpty(t, rootController)
+		rootController, ok := rootControllerInterface.(*rootController)
+		assert.True(t, ok)
+		assert.NotEmpty(t, rootController)
 
-	awaitFirstStart := make(chan struct{})
-	var seenEvents []string
+		var seenEvents []string
 
-	// Override the important portions of leasewatcher, and provide wrappers to the start/stop
-	// sub-controller handlers for invocation counting.
-	leaseEventStatusCh := make(fakeLeaderElector)
-	rootController.newLeaderElector = func(c leaderelection.Config) (leaderElector, error) {
-		return &leaseEventStatusCh, nil
-	}
-	rootController.startSubHandler = func(ctx context.Context, event leaderelection.Status) (context.CancelFunc, *errgroup.Group) {
-		if len(seenEvents) < 1 {
-			close(awaitFirstStart)
+		// Override the important portions of leasewatcher, and provide wrappers to the start/stop
+		// sub-controller handlers for invocation counting.
+		leaseEventStatusCh := make(fakeLeaderElector)
+		rootController.newLeaderElector = func(c leaderelection.Config) (leaderElector, error) {
+			return &leaseEventStatusCh, nil
 		}
-		seenEvents = append(seenEvents, "start: "+event.String())
-		return rootController.startSubControllers(ctx, event)
-	}
-	rootController.startSubHandlerRoutine = func(ctx context.Context, logger *logrus.Entry, event leaderelection.Status) error {
-		<-ctx.Done()
-		return nil
-	}
-	rootController.stopSubHandler = func(cancel context.CancelFunc, g *errgroup.Group, event leaderelection.Status) {
-		seenEvents = append(seenEvents, "stop: "+event.String())
-		rootController.stopSubControllers(cancel, g, event)
-	}
-	rootController.setupHandler = func(ctx context.Context, cf apcli.FactoryInterface) error {
-		return nil
-	}
+		rootController.startSubHandler = func(ctx context.Context, event leaderelection.Status) (context.CancelFunc, *errgroup.Group) {
+			seenEvents = append(seenEvents, "start: "+event.String())
+			return rootController.startSubControllers(ctx, event)
+		}
+		rootController.startSubHandlerRoutine = func(ctx context.Context, logger *logrus.Entry, event leaderelection.Status) error {
+			<-ctx.Done()
+			return nil
+		}
+		rootController.stopSubHandler = func(cancel context.CancelFunc, g *errgroup.Group, event leaderelection.Status) {
+			seenEvents = append(seenEvents, "stop: "+event.String())
+			rootController.stopSubControllers(cancel, g, event)
+		}
+		rootController.setupHandler = func(ctx context.Context, cf apcli.FactoryInterface) error {
+			return nil
+		}
 
-	ctx, cancel := context.WithCancel(t.Context())
+		var wg sync.WaitGroup
+		ctx, cancel := context.WithCancel(t.Context())
+		t.Cleanup(wg.Wait)
+		t.Cleanup(cancel)
+		wg.Go(func() { assert.NoError(t, rootController.Run(ctx)) })
 
-	// Send alternating lease events, as well as one that is considered redundant
+		// Send alternating lease events, as well as one that is considered redundant
 
-	go func() {
-		logger.Info("Awaiting first start")
-		<-awaitFirstStart
-
-		logger.Info("Sending acquired")
-		leaseEventStatusCh <- leaderelection.StatusLeading
-
-		logger.Info("Sending acquired (again)")
-		leaseEventStatusCh <- leaderelection.StatusLeading
-
-		time.Sleep(1 * time.Second)
-		logger.Info("Canceling context")
-		cancel()
-	}()
-
-	assert.NoError(t, rootController.Run(ctx))
-
-	assert.Equal(t, []string{
 		// The controller will always start in pending state.
-		"start: pending",
+		logger.Info("Awaiting first start")
+		synctest.Wait()
+		require.Equal(t, []string{"start: pending"}, seenEvents)
+		seenEvents = seenEvents[0:0]
 
 		// The first leading status is observed.
-		"stop: leading",
-		"start: leading",
+		logger.Info("Sending acquired")
+		leaseEventStatusCh <- leaderelection.StatusLeading
+		synctest.Wait()
+		require.Equal(t, []string{"stop: leading", "start: leading"}, seenEvents)
+		seenEvents = seenEvents[0:0]
 
 		// The second leading status is ignored, as the controller is already in
 		// the right state.
+		logger.Info("Sending acquired (again)")
+		leaseEventStatusCh <- leaderelection.StatusLeading
+		synctest.Wait()
+		require.Empty(t, seenEvents)
 
 		// Finally, the context gets canceled and the controller shuts down.
-		"stop: leading",
-	}, seenEvents)
+		logger.Info("Canceling context")
+		cancel()
+		synctest.Wait()
+		require.Equal(t, []string{"stop: leading"}, seenEvents)
+	})
 }
