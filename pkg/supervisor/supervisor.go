k0sproject/k0s

commit 12d0d91bb8354f002a006cd2a0639a5ac34e97e5

Check for new process startup in Supervisor PID test
Otherwise, the process might not terminate gracefully. Moreover, this is
in fact something worth checking.

Also fix some log statements around this.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/supervisor/supervisor.go
@@ -82,7 +82,12 @@ func (s *Supervisor) processWaitQuit(ctx context.Context) bool {
 			select {
 			case <-time.After(s.TimeoutStop):
 				continue
-			case <-waitresult:
+			case err := <-waitresult:
+				if err != nil {
+					s.log.WithError(err).Error("Failed to wait for process")
+				} else {
+					s.log.Info("Process exited: ", s.cmd.ProcessState)
+				}
 				return true
 			}
 		}
@@ -90,7 +95,7 @@ func (s *Supervisor) processWaitQuit(ctx context.Context) bool {
 		if err != nil {
 			s.log.WithError(err).Warn("Failed to wait for process")
 		} else {
-			s.log.Warnf("Process exited: %s", s.cmd.ProcessState)
+			s.log.Warnf("Process exited: ", s.cmd.ProcessState)
 		}
 	}
 	return false

pkg/supervisor/supervisor_test.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/k0sproject/k0s/internal/pkg/dir"
 	"github.com/k0sproject/k0s/internal/testutil/pingpong"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -274,6 +275,9 @@ func TestCleanupPIDFile_Gracefully(t *testing.T) {
 
 	// Expect the previous process to be gracefully terminated.
 	assert.NoError(t, prevCmd.Wait())
+
+	// Expect the new process to be started.
+	assert.NoError(t, pingPong.AwaitPing())
 
 	// Stop the supervisor and check if the PID file is gone.
 	s.Stop()

commit 1beaff4f44dbcdd567c290c028c9d7e18884cec8

Fix warning log in supervisor
This is not a printf style logging.

Fixes: 12d0d91bb ("Check for new process startup in Supervisor PID test")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/supervisor/supervisor.go
@@ -90,7 +90,7 @@ func (s *Supervisor) processWaitQuit(ctx context.Context) bool {
 		if err != nil {
 			s.log.WithError(err).Warn("Failed to wait for process")
 		} else {
-			s.log.Warnf("Process exited: ", s.cmd.ProcessState)
+			s.log.Warn("Process exited: ", s.cmd.ProcessState)
 		}
 	}
 	return false