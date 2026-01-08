afb7660dde7b1723b20d01d5c282d3623cfe8e65
Tom Wieczorek <twieczorek@mirantis.com>
2026-01-08 17:21:49 +0100

    Categorize AP controller readiness probe errors
    The previous implementation of the readiness probe didn't categorize
    the different errors that could occur when probing the controllers in
    any way. Callers could not distinguish between transient and fundamental
    problems.
    
    This change classifies probe failures into the following categories:
    
    - Target resolution failed (e.g. missing ControlNode)
    - Readiness probe unsuccessful
    - Everything else
    
    The probe records the worst error encountered and cancels in-flight
    probes as soon as a target resolution failure is detected. Callers can
    then decide to treat those error categories differently.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit bf4bb4d4468fe79d0109f4b8d896fff0a1805b73)

--- pkg/autopilot/controller/readyprober.go
+++ pkg/autopilot/controller/readyprober.go
@@ -9,24 +9,35 @@ import (
 	"cmp"
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	apv1beta2 "github.com/k0sproject/k0s/pkg/apis/autopilot/v1beta2"
 	k0sclientset "github.com/k0sproject/k0s/pkg/client/clientset"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/sirupsen/logrus"
-	"golang.org/x/sync/errgroup"
 	k8sprobe "k8s.io/kubernetes/pkg/probe"
 	k8shttpprobe "k8s.io/kubernetes/pkg/probe/http"
 )
 
 const readyzURLFormat = "https://%s/readyz?verbose"
+
+var (
+	// Indicates that a readiness probe failed figure out its target.
+	errReadyProbeTargetResolutionFailed = errors.New("target resolution failed")
+
+	// Indicates that a readiness probe has been performed, but the result was
+	// not successful.
+	errUnsuccessfulReadyProbe = errors.New("ready probe didn't indicate success")
+)
 
 type readyProber struct {
 	k8sAPIPort int
@@ -47,16 +58,41 @@ func newReadyProber(logger *logrus.Entry, client k0sclientset.Interface, tlsConf
 	}
 }
 
-// Probes the given targets concurrently. Returns the first target probe error
-// that is encountered, if any.
+// Runs probes for all targets concurrently and returns the most severe error,
+// prioritizing failed target resolutions and canceling the rest.
 func (p *readyProber) probeTargets(ctx context.Context, targets []apv1beta2.PlanCommandTargetStatus) error {
-	g, ctx := errgroup.WithContext(ctx)
+	var (
+		wg     sync.WaitGroup
+		mu     sync.Mutex
+		result error
+	)
 
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 	for _, target := range targets {
-		g.Go(func() error { return p.probeOne(ctx, target) })
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := p.probeOne(ctx, target)
+			if err == nil {
+				return
+			}
+
+			targetResolutionFailed := errors.Is(err, errReadyProbeTargetResolutionFailed)
+			if targetResolutionFailed {
+				cancel() // Be fail-fast
+			}
+
+			mu.Lock()
+			defer mu.Unlock()
+			if result == nil || (targetResolutionFailed && !errors.Is(result, errReadyProbeTargetResolutionFailed)) {
+				result = fmt.Errorf("while probing %s: %w", target.Name, err)
+			}
+		}()
 	}
 
-	return g.Wait()
+	wg.Wait()
+	return result
 }
 
 // probeOne will lookup the IP address of a target, and then proceed to query a
@@ -68,28 +104,31 @@ func (p readyProber) probeOne(ctx context.Context, target apv1beta2.PlanCommandT
 	defer cancel()
 	controlnode, err := p.client.AutopilotV1beta2().ControlNodes().Get(ctx, target.Name, metav1.GetOptions{})
 	if err != nil {
-		return err
+		if apierrors.IsNotFound(err) {
+			return fmt.Errorf("%w: no such ControlNode: %w", errReadyProbeTargetResolutionFailed, err)
+		}
+
+		return fmt.Errorf("failed to get ControlNode: %w", err)
 	}
 
 	address := controlnode.Status.GetInternalIP()
 	if address == "" {
-		return fmt.Errorf("no internal IP address found for %s", target.Name)
+		return errors.New("no internal IP address found for ControlNode")
 	}
 
 	probe := k8shttpprobe.NewWithTLSConfig(p.tlsConfig, false /* followNonLocalRedirects */)
 	url := fmt.Sprintf(readyzURLFormat, net.JoinHostPort(address, strconv.Itoa(p.k8sAPIPort)))
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return fmt.Errorf("unable to create HTTP request for '%s': %w", url, err)
+		return fmt.Errorf("unable to create HTTP request for %s:%d: %w", address, p.k8sAPIPort, err)
 	}
 	// The body content is not interesting at the moment.
-	res, _, err := probe.Probe(req, p.timeout)
+	res, msg, err := probe.Probe(req, p.timeout)
 	if err != nil {
-		return fmt.Errorf("failed to HTTP probe '%s/%s': %w", target.Name, address, err)
+		return fmt.Errorf("failed to probe %s: %w", url, err)
 	}
-
 	if res != k8sprobe.Success {
-		return fmt.Errorf("failed to probe '%s/%s': result=%v", target.Name, address, res)
+		return fmt.Errorf("%w: %s: %s", errUnsuccessfulReadyProbe, res, msg)
 	}
 
 	p.log.Infof("Probing %s done: %v", target.Name, res)
