31da9cf635044858405aca8d6d747cf01b0c00fc
Tom Wieczorek <twieczorek@mirantis.com>
2026-01-13 17:05:12 +0100

    Merge SAN list generation into a single place
    Some parts were in the API package and others were in the certificate
    manager. Since the certificate manager is the only component that deals
    with this, it makes sense to keep it all together in one place.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- internal/pkg/iface/iface.go
+++ internal/pkg/iface/iface.go
@@ -11,38 +11,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// AllAddresses returns a list of all network addresses on a node
-func AllAddresses() ([]string, error) {
-	addresses, err := CollectAllIPs()
-	if err != nil {
-		return nil, err
-	}
-	strings := make([]string, len(addresses))
-	for i, addr := range addresses {
-		strings[i] = addr.String()
-	}
-	return strings, nil
-}
-
-// CollectAllIPs returns a list of all network addresses on a node
-func CollectAllIPs() (addresses []net.IP, err error) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return nil, fmt.Errorf("failed to list network interfaces: %w", err)
-	}
-
-	for _, a := range addrs {
-		// check the address type and skip if loopback
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil || ipnet.IP.To16() != nil {
-				addresses = append(addresses, ipnet.IP)
-			}
-		}
-	}
-
-	return addresses, nil
-}
-
 // FirstPublicAddress return the first found non-local IPv4 address that's not part of pod network
 // if any interface does not have any IPv4 address then return the first found non-local IPv6 address
 func FirstPublicAddress() (string, error) {
