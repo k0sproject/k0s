2b10ff038bb4996b9659d45ffdcf6ec97a493683
Kimmo Lehto <klehto@mirantis.com>
2026-01-14 15:14:42 +0200

    Windows privilege escalation fix
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- pkg/uninstall/privileges_windows.go
+++ pkg/uninstall/privileges_windows.go
@@ -13,9 +13,9 @@ import (
 )
 
 func ensurePrivileges() error {
-	token, err := windows.OpenCurrentProcessToken()
+	token, err := openImpersonationToken()
 	if err != nil {
-		return fmt.Errorf("failed to open process token: %w", err)
+		return err
 	}
 	defer token.Close()
 
@@ -32,4 +32,18 @@ func ensurePrivileges() error {
 		return errors.New("this command must be run with administrator privileges")
 	}
 	return nil
+}
+
+func openImpersonationToken() (windows.Token, error) {
+	var processToken windows.Token
+	if err := windows.OpenProcessToken(windows.CurrentProcess(), windows.TOKEN_QUERY|windows.TOKEN_DUPLICATE, &processToken); err != nil {
+		return 0, fmt.Errorf("failed to open process token: %w", err)
+	}
+	defer processToken.Close()
+
+	var impersonationToken windows.Token
+	if err := windows.DuplicateTokenEx(processToken, windows.TOKEN_QUERY, nil, windows.SecurityIdentification, windows.TokenImpersonation, &impersonationToken); err != nil {
+		return 0, fmt.Errorf("failed to duplicate process token: %w", err)
+	}
+	return impersonationToken, nil
 }
