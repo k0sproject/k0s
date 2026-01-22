k0sproject/k0s

commit a5f7d00c1ef188c10f19ba65521785cdea82d3f1

Make K0S_UPDATE_FROM_PATH optional
If K0S_UPDATE_FROM_PATH hasn't been set, the current executable path
will be filled in automatically. This allows the environment variable to
be used as an indicator of an update from a different k0s version than
the one currently being tested.

Also remove K0S_UPDATE_FROM_BIN, as K0S_UPDATE_FROM_PATH can just be
used directly.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

inttest/common/bootloosesuite.go
@@ -1518,26 +1518,31 @@ func (s *BootlooseSuite) maybeAddBinPath(volumes []config.Volume) ([]config.Volu
 		return nil, fmt.Errorf("%s: is a directory: %q", exePathErrMsg, exePath)
 	}
 
-	updateFromBinPath := os.Getenv("K0S_UPDATE_FROM_PATH")
-	if updateFromBinPath != "" {
-		volumes = append(volumes, config.Volume{
-			Type:        "bind",
-			Source:      updateFromBinPath,
-			Destination: k0sBindMountFullPath,
-			ReadOnly:    true,
-		}, config.Volume{
-			Type:        "bind",
-			Source:      exePath,
-			Destination: k0sNewBindMountFullPath,
-			ReadOnly:    true,
-		})
+	fromK0sPath, updatingFromOtherVersion := os.LookupEnv("K0S_UPDATE_FROM_PATH")
+	if updatingFromOtherVersion {
+		errMsg := "failed to locate k0s executable to update from (check the K0S_UPDATE_FROM_PATH environment variable)"
+		if fromK0sPath, err = filepath.Abs(fromK0sPath); err != nil {
+			return nil, fmt.Errorf("%s: %w", errMsg, err)
+		} else if fileInfo, err := os.Stat(fromK0sPath); err != nil {
+			return nil, fmt.Errorf("%s: %w", errMsg, err)
+		} else if fileInfo.IsDir() {
+			return nil, fmt.Errorf("%s: is a directory: %q", exePathErrMsg, fromK0sPath)
+		}
 	} else {
-		volumes = append(volumes, config.Volume{
-			Type:        "bind",
-			Source:      exePath,
-			Destination: k0sBindMountFullPath,
-			ReadOnly:    true,
-		})
+		fromK0sPath = exePath
 	}
+
+	volumes = append(volumes, config.Volume{
+		Type:        "bind",
+		Source:      fromK0sPath,
+		Destination: k0sBindMountFullPath,
+		ReadOnly:    true,
+	}, config.Volume{
+		Type:        "bind",
+		Source:      exePath,
+		Destination: k0sNewBindMountFullPath,
+		ReadOnly:    true,
+	})
+
 	return volumes, nil
 }

hack/tools/run-autopilot-matrix-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Usage:
 #   TARGET_ARCH=arm64 run-autopilot-matrix-tests.sh check-ap-ha3x3,check-ap-single v1.24.2+k0s.0,v1.24.3+k0s.0
@@ -22,7 +22,7 @@ while IFS=',' read -ra VERSION; do
 
     while IFS=',' read -ra TESTARR; do
       for test in "${TESTARR[@]}"; do
-        make -C inttest ${test} K0S_UPDATE_FROM_BIN=../k0s-${ver}
+        make -C inttest ${test} K0S_UPDATE_FROM_PATH="$(realpath "../k0s-${ver}")"
       done
     done <<< "$TESTS"
   done

inttest/Makefile
@@ -68,12 +68,7 @@ get-conformance-results: bin/sonobuoy
 
 TIMEOUT ?= 6m
 
-check-ap-controllerworker: K0S_UPDATE_FROM_BIN ?= ../k0s
-check-ap-controllerworker: export K0S_UPDATE_FROM_PATH ?= $(realpath $(K0S_UPDATE_FROM_BIN))
-
 # Autopilot 3x3 HA test can take a while to run
-check-ap-ha3x3: K0S_UPDATE_FROM_BIN ?= ../k0s
-check-ap-ha3x3: export K0S_UPDATE_FROM_PATH ?= $(realpath $(K0S_UPDATE_FROM_BIN))
 check-ap-ha3x3: export K0S_UPDATE_TO_VERSION ?= $(shell ../k0s version)
 
 check-ap-updater: .update-server.stamp

commit 14989f951ff68d1628ce9fc5316600f3b0b7f155

Fix error message related to version update inttests
This was a copy/paste error that I only noticed when backporting the
flawed commit to release-1.33.

Fixes: a5f7d00c1 ("Make K0S_UPDATE_FROM_PATH optional")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

inttest/common/bootloosesuite.go
@@ -1563,7 +1563,7 @@ func (s *BootlooseSuite) maybeAddBinPath(volumes []config.Volume) ([]config.Volu
 		} else if fileInfo, err := os.Stat(fromK0sPath); err != nil {
 			return nil, fmt.Errorf("%s: %w", errMsg, err)
 		} else if fileInfo.IsDir() {
-			return nil, fmt.Errorf("%s: is a directory: %q", exePathErrMsg, fromK0sPath)
+			return nil, fmt.Errorf("%s: is a directory: %q", errMsg, fromK0sPath)
 		}
 	} else {
 		fromK0sPath = exePath