k0sproject/k0s

commit 98727e458430b43796e968a5cf4bcd7e407b96c7

Revamp Calico manifests for Windows
Add all the Windows images to the ClusterConfig, and propagate the
Windows pause image via worker profiles.

Use templating for calico windows manifests instead of hardcoded CIDRs etc.

Provide the windows-specific default worker profile name directly in
the Cobra command, so that it's available right from the beginning, and
will be listed correctly in the help output as well.

Co-Authored-By: Jussi Nummelin <jnummelin@mirantis.com>
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/constant/constant.go
@@ -64,32 +64,36 @@ const (
 
 	/* Image Constants */
 
-	KonnectivityImage                  = "quay.io/k0sproject/apiserver-network-proxy-agent"
-	KonnectivityImageVersion           = "v0.33.0"
-	PushGatewayImage                   = "quay.io/k0sproject/pushgateway-ttl"
-	PushGatewayImageVersion            = "1.4.0-k0s.0"
-	MetricsImage                       = "quay.io/k0sproject/metrics-server"
-	MetricsImageVersion                = "v0.7.2-0"
-	KubePauseContainerImage            = "quay.io/k0sproject/pause"
-	KubePauseContainerImageVersion     = "3.10.1"
-	KubeProxyImage                     = "quay.io/k0sproject/kube-proxy"
-	KubeProxyImageVersion              = "v1.34.1"
-	KubeProxyWindowsImage              = "sigwindowstools/kube-proxy"
-	KubeProxyWindowsImageVersion       = "v1.27.1-calico-hostproces"
-	CoreDNSImage                       = "quay.io/k0sproject/coredns"
-	CoreDNSImageVersion                = "1.12.2-1"
-	EnvoyProxyImage                    = "quay.io/k0sproject/envoy-distroless"
-	EnvoyProxyImageVersion             = "v1.34.3"
-	CalicoImage                        = "quay.io/k0sproject/calico-cni"
-	CalicoComponentImagesVersion       = "v3.29.6-0"
-	CalicoNodeImage                    = "quay.io/k0sproject/calico-node"
-	CalicoNodeWindowsImage             = "calico/windows"
-	CalicoNodeWindowsImageVersion      = "v3.23.5"
-	KubeControllerImage                = "quay.io/k0sproject/calico-kube-controllers"
-	KubeRouterCNIImage                 = "quay.io/k0sproject/kube-router"
-	KubeRouterCNIImageVersion          = "v2.6.0-iptables1.8.11-0"
-	KubeRouterCNIInstallerImage        = "quay.io/k0sproject/cni-node"
-	KubeRouterCNIInstallerImageVersion = "1.7.1-k0s.0"
+	KonnectivityImage                     = "quay.io/k0sproject/apiserver-network-proxy-agent"
+	KonnectivityImageVersion              = "v0.33.0"
+	PushGatewayImage                      = "quay.io/k0sproject/pushgateway-ttl"
+	PushGatewayImageVersion               = "1.4.0-k0s.0"
+	MetricsImage                          = "quay.io/k0sproject/metrics-server"
+	MetricsImageVersion                   = "v0.7.2-0"
+	KubePauseContainerImage               = "quay.io/k0sproject/pause"
+	KubePauseContainerImageVersion        = "3.10.1"
+	KubePauseWindowsContainerImage        = "k8s.gcr.io/pause"
+	KubePauseWindowsContainerImageVersion = "3.10.1"
+	KubeProxyImage                        = "quay.io/k0sproject/kube-proxy"
+	KubeProxyImageVersion                 = "v1.34.1"
+	KubeProxyWindowsImage                 = "docker.io/sigwindowstools/kube-proxy"
+	KubeProxyWindowsImageVersion          = "v1.34.1-calico-hostprocess"
+	CoreDNSImage                          = "quay.io/k0sproject/coredns"
+	CoreDNSImageVersion                   = "1.12.2-1"
+	EnvoyProxyImage                       = "quay.io/k0sproject/envoy-distroless"
+	EnvoyProxyImageVersion                = "v1.34.3"
+	CalicoCNIImage                        = "quay.io/k0sproject/calico-cni"
+	CalicoComponentImagesVersion          = "v3.29.6-0"
+	CalicoCNIWindowsImage                 = "docker.io/calico/cni-windows"
+	CalicoCNIWindowsImageVersion          = "v3.29.6"
+	CalicoNodeImage                       = "quay.io/k0sproject/calico-node"
+	CalicoNodeWindowsImage                = "docker.io/calico/node-windows"
+	CalicoNodeWindowsImageVersion         = "v3.29.6"
+	KubeControllerImage                   = "quay.io/k0sproject/calico-kube-controllers"
+	KubeRouterCNIImage                    = "quay.io/k0sproject/kube-router"
+	KubeRouterCNIImageVersion             = "v2.6.0-iptables1.8.11-0"
+	KubeRouterCNIInstallerImage           = "quay.io/k0sproject/cni-node"
+	KubeRouterCNIInstallerImageVersion    = "1.7.1-k0s.0"
 
 	/* Controller component names */
 

cmd/worker/worker.go
@@ -272,10 +272,6 @@ func (c *Command) Start(ctx context.Context, nodeName apitypes.NodeName, kubelet
 		componentManager.Add(ctx, worker.NewOCIBundleReconciler(c.K0sVars))
 	}
 
-	if c.WorkerProfile == "default" && runtime.GOOS == "windows" {
-		c.WorkerProfile = "default-windows"
-	}
-
 	if controller == nil && runtime.GOOS == "linux" {
 		componentManager.Add(ctx, &iptables.Component{
 			IPTablesMode: c.IPTablesMode,

pkg/apis/k0s/v1beta1/api.go
@@ -10,12 +10,14 @@ import (
 	"net/url"
 	"strconv"
 
-	"github.com/asaskevich/govalidator"
+	"github.com/k0sproject/k0s/internal/pkg/iface"
+	k0snet "github.com/k0sproject/k0s/internal/pkg/net"
+	"github.com/k0sproject/k0s/internal/pkg/stringslice"
+
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
-	"github.com/k0sproject/k0s/internal/pkg/iface"
-	"github.com/k0sproject/k0s/internal/pkg/stringslice"
+	"github.com/asaskevich/govalidator"
 )
 
 var _ Validateable = (*APISpec)(nil)
@@ -84,6 +86,21 @@ func (a *APISpec) APIAddress() string {
 		return a.ExternalHost()
 	}
 	return a.Address
+}
+
+func (a *APISpec) APIServerHostPort() (*k0snet.HostPort, error) {
+	if a.ExternalAddress != "" {
+		if ip := net.ParseIP(a.ExternalAddress); ip != nil {
+			return k0snet.NewHostPort(a.ExternalAddress, uint16(a.Port))
+		}
+		hostPort, err := k0snet.ParseHostPortWithDefault(a.ExternalAddress, uint16(a.Port))
+		if err != nil {
+			return nil, fmt.Errorf("external address is invalid: %w", err)
+		}
+		return hostPort, nil
+	}
+
+	return k0snet.NewHostPort(a.Address, uint16(a.Port))
 }
 
 func (a *APISpec) ExternalHost() string {

pkg/apis/k0s/v1beta1/images.go
@@ -54,12 +54,13 @@ func (s *ImageSpec) URI() string {
 
 // ClusterImages sets docker images for addon components
 type ClusterImages struct {
-	Konnectivity  *ImageSpec `json:"konnectivity,omitempty"`
-	PushGateway   *ImageSpec `json:"pushgateway,omitempty"`
-	MetricsServer *ImageSpec `json:"metricsserver,omitempty"`
-	KubeProxy     *ImageSpec `json:"kubeproxy,omitempty"`
-	CoreDNS       *ImageSpec `json:"coredns,omitempty"`
-	Pause         *ImageSpec `json:"pause,omitempty"`
+	Konnectivity  *ImageSpec        `json:"konnectivity,omitempty"`
+	PushGateway   *ImageSpec        `json:"pushgateway,omitempty"`
+	MetricsServer *ImageSpec        `json:"metricsserver,omitempty"`
+	KubeProxy     *ImageSpec        `json:"kubeproxy,omitempty"`
+	CoreDNS       *ImageSpec        `json:"coredns,omitempty"`
+	Pause         *ImageSpec        `json:"pause,omitempty"`
+	Windows       *WindowsImageSpec `json:"windows,omitempty"`
 
 	Calico     *CalicoImageSpec     `json:"calico,omitempty"`
 	KubeRouter *KubeRouterImageSpec `json:"kuberouter,omitempty"`
@@ -111,6 +112,7 @@ func (ci *ClusterImages) Validate(path *field.Path) (errs field.ErrorList) {
 	errs = append(errs, ci.Pause.Validate(path.Child("pause"))...)
 	errs = append(errs, ci.Calico.Validate(path.Child("calico"))...)
 	errs = append(errs, ci.KubeRouter.Validate(path.Child("kuberouter"))...)
+	errs = append(errs, ci.Windows.Validate(path.Child("windows"))...)
 	return
 }
 
@@ -128,17 +130,36 @@ func (ci *ClusterImages) overrideImageRepositories() {
 	override(ci.Calico.CNI)
 	override(ci.Calico.Node)
 	override(ci.Calico.KubeControllers)
+	override(ci.Calico.Windows.CNI)
+	override(ci.Calico.Windows.Node)
 	override(ci.KubeRouter.CNI)
 	override(ci.KubeRouter.CNIInstaller)
 	override(ci.Pause)
 	override(ci.PushGateway)
+	override(ci.Windows.KubeProxy)
+	override(ci.Windows.Pause)
+}
+
+type WindowsImageSpec struct {
+	KubeProxy *ImageSpec `json:"kubeproxy,omitempty"`
+	Pause     *ImageSpec `json:"pause,omitempty"`
+}
+
+func (s *WindowsImageSpec) Validate(path *field.Path) (errs field.ErrorList) {
+	if s == nil {
+		return
+	}
+	errs = append(errs, s.KubeProxy.Validate(path.Child("kubeproxy"))...)
+	errs = append(errs, s.Pause.Validate(path.Child("pause"))...)
+	return
 }
 
 // CalicoImageSpec config group for calico related image settings
 type CalicoImageSpec struct {
-	CNI             *ImageSpec `json:"cni,omitempty"`
-	Node            *ImageSpec `json:"node,omitempty"`
-	KubeControllers *ImageSpec `json:"kubecontrollers,omitempty"`
+	CNI             *ImageSpec              `json:"cni,omitempty"`
+	Node            *ImageSpec              `json:"node,omitempty"`
+	KubeControllers *ImageSpec              `json:"kubecontrollers,omitempty"`
+	Windows         *CalicoWindowsImageSpec `json:"windows,omitempty"`
 }
 
 func (s *CalicoImageSpec) Validate(path *field.Path) (errs field.ErrorList) {
@@ -148,6 +169,21 @@ func (s *CalicoImageSpec) Validate(path *field.Path) (errs field.ErrorList) {
 	errs = append(errs, s.CNI.Validate(path.Child("cni"))...)
 	errs = append(errs, s.Node.Validate(path.Child("node"))...)
 	errs = append(errs, s.KubeControllers.Validate(path.Child("kubecontrollers"))...)
+	errs = append(errs, s.Windows.Validate(path.Child("windows"))...)
+	return
+}
+
+type CalicoWindowsImageSpec struct {
+	CNI  *ImageSpec `json:"cni,omitempty"`
+	Node *ImageSpec `json:"node,omitempty"`
+}
+
+func (s *CalicoWindowsImageSpec) Validate(path *field.Path) (errs field.ErrorList) {
+	if s == nil {
+		return
+	}
+	errs = append(errs, s.CNI.Validate(path.Child("cni"))...)
+	errs = append(errs, s.Node.Validate(path.Child("node"))...)
 	return
 }
 
@@ -192,7 +228,7 @@ func DefaultClusterImages() *ClusterImages {
 		},
 		Calico: &CalicoImageSpec{
 			CNI: &ImageSpec{
-				Image:   constant.CalicoImage,
+				Image:   constant.CalicoCNIImage,
 				Version: constant.CalicoComponentImagesVersion,
 			},
 			Node: &ImageSpec{
@@ -202,6 +238,16 @@ func DefaultClusterImages() *ClusterImages {
 			KubeControllers: &ImageSpec{
 				Image:   constant.KubeControllerImage,
 				Version: constant.CalicoComponentImagesVersion,
+			},
+			Windows: &CalicoWindowsImageSpec{
+				CNI: &ImageSpec{
+					Image:   constant.CalicoCNIWindowsImage,
+					Version: constant.CalicoCNIWindowsImageVersion,
+				},
+				Node: &ImageSpec{
+					Image:   constant.CalicoNodeWindowsImage,
+					Version: constant.CalicoNodeWindowsImageVersion,
+				},
 			},
 		},
 		KubeRouter: &KubeRouterImageSpec{
@@ -217,6 +263,16 @@ func DefaultClusterImages() *ClusterImages {
 		Pause: &ImageSpec{
 			Image:   constant.KubePauseContainerImage,
 			Version: constant.KubePauseContainerImageVersion,
+		},
+		Windows: &WindowsImageSpec{
+			KubeProxy: &ImageSpec{
+				Image:   constant.KubeProxyWindowsImage,
+				Version: constant.KubeProxyWindowsImageVersion,
+			},
+			Pause: &ImageSpec{
+				Image:   constant.KubePauseWindowsContainerImage,
+				Version: constant.KubePauseWindowsContainerImageVersion,
+			},
 		},
 	}
 }

pkg/apis/k0s/v1beta1/zz_generated.deepcopy.go
@@ -155,6 +155,11 @@ func (in *CalicoImageSpec) DeepCopyInto(out *CalicoImageSpec) {
 		*out = new(ImageSpec)
 		**out = **in
 	}
+	if in.Windows != nil {
+		in, out := &in.Windows, &out.Windows
+		*out = new(CalicoWindowsImageSpec)
+		(*in).DeepCopyInto(*out)
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CalicoImageSpec.
@@ -163,6 +168,31 @@ func (in *CalicoImageSpec) DeepCopy() *CalicoImageSpec {
 		return nil
 	}
 	out := new(CalicoImageSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CalicoWindowsImageSpec) DeepCopyInto(out *CalicoWindowsImageSpec) {
+	*out = *in
+	if in.CNI != nil {
+		in, out := &in.CNI, &out.CNI
+		*out = new(ImageSpec)
+		**out = **in
+	}
+	if in.Node != nil {
+		in, out := &in.Node, &out.Node
+		*out = new(ImageSpec)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CalicoWindowsImageSpec.
+func (in *CalicoWindowsImageSpec) DeepCopy() *CalicoWindowsImageSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(CalicoWindowsImageSpec)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -348,6 +378,11 @@ func (in *ClusterImages) DeepCopyInto(out *ClusterImages) {
 		in, out := &in.Pause, &out.Pause
 		*out = new(ImageSpec)
 		**out = **in
+	}
+	if in.Windows != nil {
+		in, out := &in.Windows, &out.Windows
+		*out = new(WindowsImageSpec)
+		(*in).DeepCopyInto(*out)
 	}
 	if in.Calico != nil {
 		in, out := &in.Calico, &out.Calico
@@ -1241,6 +1276,31 @@ func (in VirtualServers) DeepCopy() VirtualServers {
 	out := new(VirtualServers)
 	in.DeepCopyInto(out)
 	return *out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *WindowsImageSpec) DeepCopyInto(out *WindowsImageSpec) {
+	*out = *in
+	if in.KubeProxy != nil {
+		in, out := &in.KubeProxy, &out.KubeProxy
+		*out = new(ImageSpec)
+		**out = **in
+	}
+	if in.Pause != nil {
+		in, out := &in.Pause, &out.Pause
+		*out = new(ImageSpec)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new WindowsImageSpec.
+func (in *WindowsImageSpec) DeepCopy() *WindowsImageSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(WindowsImageSpec)
+	in.DeepCopyInto(out)
+	return out
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.

pkg/component/controller/calico.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/k0sproject/k0s/internal/pkg/dir"
 	"github.com/k0sproject/k0s/internal/pkg/file"
+	k0snet "github.com/k0sproject/k0s/internal/pkg/net"
 	"github.com/k0sproject/k0s/internal/pkg/templatewriter"
 	"github.com/k0sproject/k0s/internal/sync/value"
 )
@@ -61,8 +62,7 @@ type calicoConfig struct {
 }
 
 type calicoNodeConfig struct {
-	KubeAPIHost     string
-	KubeAPIPort     int
+	APIServer       *k0snet.HostPort
 	ServiceCIDRIPv4 string
 	ClusterDNSIP    string
 }
@@ -81,6 +81,7 @@ type calicoClusterConfig struct {
 	EnvVars              map[string]string
 
 	CalicoCNIImage             string
+	CalicoCNIWindowsImage      string
 	CalicoNodeImage            string
 	CalicoNodeWindowsImage     string
 	CalicoKubeControllersImage string
@@ -97,11 +98,15 @@ func NewCalico(nodeConfig *v1beta1.ClusterConfig, manifestsDir string, hasWindow
 		return nil, err
 	}
 
+	apiServer, err := nodeConfig.Spec.API.APIServerHostPort()
+	if err != nil {
+		return nil, err
+	}
+
 	return &Calico{
 		log: logrus.WithFields(logrus.Fields{"component": "calico"}),
 		nodeConfig: calicoNodeConfig{
-			KubeAPIHost:     nodeConfig.Spec.API.ExternalHost(),
-			KubeAPIPort:     nodeConfig.Spec.API.ExternalPort(),
+			APIServer:       apiServer,
 			ServiceCIDRIPv4: nodeConfig.Spec.Network.ServiceCIDR,
 			ClusterDNSIP:    dnsAddress,
 		},
@@ -294,8 +299,9 @@ func (c *Calico) getConfig(clusterConfig *v1beta1.ClusterConfig) (*calicoCluster
 		EnableIPv4:                 isDualStack || primaryAFIPv4,
 		EnableIPv6:                 isDualStack || !primaryAFIPv4,
 		CalicoCNIImage:             clusterConfig.Spec.Images.Calico.CNI.URI(),
+		CalicoCNIWindowsImage:      clusterConfig.Spec.Images.Calico.Windows.CNI.URI(),
 		CalicoNodeImage:            clusterConfig.Spec.Images.Calico.Node.URI(),
-		CalicoNodeWindowsImage:     constant.CalicoNodeWindowsImage + ":" + constant.CalicoNodeWindowsImageVersion,
+		CalicoNodeWindowsImage:     clusterConfig.Spec.Images.Calico.Windows.Node.URI(),
 		CalicoKubeControllersImage: clusterConfig.Spec.Images.Calico.KubeControllers.URI(),
 		Overlay:                    clusterConfig.Spec.Network.Calico.Overlay,
 		IPAutodetectionMethod:      clusterConfig.Spec.Network.Calico.IPAutodetectionMethod,

pkg/component/controller/kubeproxy.go
@@ -230,7 +230,7 @@ func (k *KubeProxy) getConfig(clusterConfig *v1beta1.ClusterConfig) (*proxyConfi
 		ClusterCIDR:          clusterConfig.Spec.Network.BuildPodCIDR(),
 		ControlPlaneEndpoint: controlPlaneEndpoint,
 		Image:                clusterConfig.Spec.Images.KubeProxy.URI(),
-		WindowsImage:         constant.KubeProxyWindowsImage + ":" + constant.KubeProxyWindowsImageVersion,
+		WindowsImage:         clusterConfig.Spec.Images.Windows.KubeProxy.URI(),
 		PullPolicy:           clusterConfig.Spec.Images.DefaultPullPolicy,
 		DualStack:            clusterConfig.Spec.Network.DualStack.Enabled,
 		Mode:                 clusterConfig.Spec.Network.KubeProxy.Mode,

pkg/component/controller/workerconfig/reconciler.go
@@ -494,6 +494,7 @@ func (r *Reconciler) buildConfigMaps(snapshot *snapshot) ([]*corev1.ConfigMap, e
 	workerProfiles["default"] = workerProfile
 
 	workerProfile = r.buildProfile(snapshot)
+	workerProfile.PauseImage = snapshot.pauseWindowsImage.DeepCopy()
 	workerProfile.KubeletConfiguration.CgroupsPerQOS = ptr.To(false)
 	workerProfile.KubeletConfiguration.KubeReservedCgroup = ""
 	workerProfile.KubeletConfiguration.KubeletCgroups = ""

commit 2378621d79662484e8e848496248b37e49efff28

Use registry.k8s.io for Windows pause image
See: bf9f9f6ea ("Switch from k8s.gcr.io to registry.k8s.io")
Fixes: 98727e458 ("Revamp Calico manifests for Windows")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/constant/constant.go
@@ -77,7 +77,7 @@ const (
 	MetricsImageVersion                   = "v0.7.2-0"
 	KubePauseContainerImage               = "quay.io/k0sproject/pause"
 	KubePauseContainerImageVersion        = "3.10.1"
-	KubePauseWindowsContainerImage        = "k8s.gcr.io/pause"
+	KubePauseWindowsContainerImage        = "registry.k8s.io/pause"
 	KubePauseWindowsContainerImageVersion = "3.10.1"
 	KubeProxyImage                        = "quay.io/k0sproject/kube-proxy"
 	KubeProxyImageVersion                 = "v1.35.0"