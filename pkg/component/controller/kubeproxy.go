9e56f32a2c9799effbc8f876baaf03b62b6ad783
Tom Wieczorek <twieczorek@mirantis.com>
2026-04-07 17:57:22 +0200

    Remove default values from kube-proxy configuration
    kube-proxy applies its own defaults for zero/unset fields, so there's no
    need to repeat them in k0s's configuration.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit 0061c4c54a0bdb6c4f70bc25a06d75f5658fc336)
    (cherry picked from commit a49bb5806d2c32d6654ec4ed7240f9baffa7f442)
    (cherry picked from commit 377e21039070946c92961eb20953f9b9eb7cbce4)

--- pkg/component/controller/kubeproxy.go
+++ pkg/component/controller/kubeproxy.go
@@ -282,15 +282,9 @@ data:
         tokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
   config.conf: |-
     apiVersion: kubeproxy.config.k8s.io/v1alpha1
-    bindAddress: 0.0.0.0
     clientConnection:
-      acceptContentTypes: ""
-      burst: 0
-      contentType: ""
       kubeconfig: /var/lib/kube-proxy/kubeconfig.conf
-      qps: 0
     clusterCIDR: {{ .ClusterCIDR }}
-    configSyncPeriod: 0s
     featureGates:
 {{- range $key, $value := .FeatureGates }}
       {{ $key }}: {{ $value }}
@@ -298,11 +292,6 @@ data:
     mode: "{{ .Mode }}"
     conntrack:
       maxPerCore: 0
-      min: null
-      tcpCloseWaitTimeout: null
-      tcpEstablishedTimeout: null
-    detectLocalMode: ""
-    enableProfiling: false
     healthzBindAddress: ""
     hostnameOverride: ""
     iptables: {{ .IPTables }}
@@ -311,14 +300,6 @@ data:
     kind: KubeProxyConfiguration
     metricsBindAddress: {{ .MetricsBindAddress }}
     nodePortAddresses: {{ .NodePortAddresses }}
-    oomScoreAdj: null
-    portRange: ""
-    showHiddenMetricsForVersion: ""
-    udpIdleTimeout: 0s
-    winkernel:
-      enableDSR: false
-      networkName: ""
-      sourceVip: ""
 ---
 apiVersion: apps/v1
 kind: DaemonSet
