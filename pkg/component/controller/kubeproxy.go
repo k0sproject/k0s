twz123/k0s-testo

Roll kube-proxy DaemonSet on ConfigMap changes
The kube-proxy DaemonSet consumes its configuration from a ConfigMap,
but nothing in the pod template changes when that ConfigMap is updated.
That can leave existing pods running with stale configuration until some
unrelated restart happens.

Hash the rendered kube-proxy ConfigMap and add the hash to the pod
template labels so configuration changes trigger a DaemonSet rollout.
Extend the kube-proxy tests to cover the rendered config and the hash
change behavior through the component's public API.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/kubeproxy.go
@@ -6,7 +6,10 @@ package controller
 import (
 	"bufio"
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -162,16 +165,29 @@ func (k *KubeProxy) updateManifests(cfg *proxyConfig, includeWindows bool) error
 		Do(func(unbuffered file.AtomicWriter) error {
 			buf := bufio.NewWriter(unbuffered)
 
+			templateData := struct {
+				*kubeProxyTemplateData
+				ConfigHash string
+			}{
+				kubeProxyTemplateData: &cfg.TemplateData,
+			}
+
 			if configMap, err := cfg.ConfigMapData.toConfigMap(); err != nil {
 				return err
-			} else if err := applier.CodecFor(kubernetesscheme.Scheme).Encode(configMap, buf); err != nil {
-				return err
+			} else {
+				hasher := sha256.New()
+				buf := io.MultiWriter(hasher, buf)
+				codec := applier.CodecFor(kubernetesscheme.Scheme)
+				if err := codec.Encode(configMap, buf); err != nil {
+					return err
+				}
+				templateData.ConfigHash = hex.EncodeToString(hasher.Sum(nil))
 			}
 
 			if err := (&templatewriter.TemplateWriter{
 				Name:     "kube-proxy",
 				Template: proxyTemplate,
-				Data:     &cfg.TemplateData,
+				Data:     &templateData,
 			}).WriteToBuffer(buf); err != nil {
 				return err
 			}
@@ -185,7 +201,7 @@ func (k *KubeProxy) updateManifests(cfg *proxyConfig, includeWindows bool) error
 				if err := (&templatewriter.TemplateWriter{
 					Name:     "kube-proxy-windows",
 					Template: string(proxyWindowsTemplate),
-					Data:     &cfg.TemplateData,
+					Data:     &templateData,
 				}).WriteToBuffer(buf); err != nil {
 					return err
 				}
@@ -426,6 +442,7 @@ spec:
     metadata:
       labels:
         k8s-app: kube-proxy
+        k0sproject.io/config-hash: {{ .ConfigHash }}
       annotations:
         prometheus.io/scrape: 'true'
         prometheus.io/port: '10249'

pkg/component/controller/kubeproxy_test.go
@@ -4,6 +4,7 @@
 package controller
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"slices"
@@ -14,6 +15,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	"github.com/k0sproject/k0s/pkg/config"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -27,13 +29,50 @@ import (
 )
 
 func TestKubeProxyConfig_FeatureGates(t *testing.T) {
-	k0sVars, err := config.NewCfgVars(nil, t.TempDir())
-	require.NoError(t, err)
 	cfg := v1beta1.DefaultClusterConfig()
 	cfg.Spec.FeatureGates = v1beta1.FeatureGates{
 		{Name: "Feature0", Enabled: true, Components: []string{"kube-proxy"}},
 		{Name: "Feature1", Enabled: false, Components: []string{"kube-proxy"}},
 	}
+
+	_, manifestsDir := startComponent(t, cfg)
+	_, resources := awaitUpdate(t, manifestsDir, nil)
+	configMap := findKubeProxyConfigMap(t, resources)
+
+	var kubeProxyConfigData unstructured.Unstructured
+	require.NoError(t, yaml.Unmarshal([]byte(configMap.Data["config.conf"]), &kubeProxyConfigData.Object))
+	assert.Equal(t, kubeproxyv1alpha1.SchemeGroupVersion.String(), kubeProxyConfigData.GetAPIVersion())
+	assert.Equal(t, "KubeProxyConfiguration", kubeProxyConfigData.GetKind())
+
+	renderedFeatureGates, ok := kubeProxyConfigData.Object["featureGates"].(map[string]any)
+	require.True(t, ok)
+	assert.Equal(t, map[string]any{
+		"Feature0": true,
+		"Feature1": false,
+	}, renderedFeatureGates)
+}
+
+func TestKubeProxyConfig_HashChangesWhenConfigMapChanges(t *testing.T) {
+	cfg := v1beta1.DefaultClusterConfig()
+	underTest, manifestsDir := startComponent(t, cfg)
+
+	stat, resources := awaitUpdate(t, manifestsDir, nil)
+	initialDaemonSet := findKubeProxyDaemonSet(t, resources)
+
+	cfg.Spec.FeatureGates = v1beta1.FeatureGates{
+		{Name: "Feature0", Enabled: true, Components: []string{"kube-proxy"}},
+	}
+	require.NoError(t, underTest.Reconcile(t.Context(), cfg))
+
+	_, resources = awaitUpdate(t, manifestsDir, stat)
+	updatedDaemonSet := findKubeProxyDaemonSet(t, resources)
+
+	assert.NotEqual(t, initialDaemonSet.Spec.Template.Labels["k0sproject.io/config-hash"], updatedDaemonSet.Spec.Template.Labels["k0sproject.io/config-hash"])
+}
+
+func startComponent(t *testing.T, cfg *v1beta1.ClusterConfig) (*KubeProxy, string) {
+	k0sVars, err := config.NewCfgVars(nil, t.TempDir())
+	require.NoError(t, err)
 	noWindowsNodes := func() (*bool, <-chan struct{}) {
 		return ptr.To(false), nil
 	}
@@ -44,35 +83,72 @@ func TestKubeProxyConfig_FeatureGates(t *testing.T) {
 	t.Cleanup(func() { assert.NoError(t, underTest.Stop()) })
 	require.NoError(t, underTest.Reconcile(t.Context(), cfg))
 
-	manifestPath := filepath.Join(k0sVars.ManifestsDir, "kubeproxy", "kube-proxy.yaml")
-	var manifestData []byte
+	return underTest, k0sVars.ManifestsDir
+}
+
+func awaitUpdate(t *testing.T, manifestsDir string, prev os.FileInfo) (os.FileInfo, []*unstructured.Unstructured) {
+	manifestPath := filepath.Join(manifestsDir, "kubeproxy", "kube-proxy.yaml")
+
+	var (
+		stat         os.FileInfo
+		manifestData []byte
+	)
 	require.EventuallyWithT(t, func(t *assert.CollectT) {
-		manifestData, err = os.ReadFile(manifestPath)
-		assert.NoError(t, err)
+		f, err := os.OpenFile(manifestPath, os.O_RDONLY, 0)
+		if !assert.NoError(t, err) {
+			return
+		}
+		defer f.Close()
+
+		stat, err = f.Stat()
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		if prev != nil && !assert.True(t, prev.ModTime().Before(stat.ModTime()) || prev.Size() != stat.Size(), "manifest unchanged") {
+			return
+		}
+
+		manifestData, err = io.ReadAll(f)
+		if !assert.NoError(t, err) || !assert.Len(t, manifestData, int(stat.Size())) {
+			return
+		}
 	}, 5*time.Second, 50*time.Millisecond)
 
 	resources, err := testutil.ParseManifests(manifestData)
 	require.NoError(t, err)
+	return stat, resources
+}
 
-	configMapIdx := slices.IndexFunc(resources, func(u *unstructured.Unstructured) bool {
+func findKubeProxyConfigMap(t *testing.T, resources []*unstructured.Unstructured) *corev1.ConfigMap {
+	t.Helper()
+
+	idx := slices.IndexFunc(resources, func(u *unstructured.Unstructured) bool {
 		return u.GetAPIVersion() == corev1.SchemeGroupVersion.String() &&
 			u.GetKind() == "ConfigMap" &&
 			u.GetNamespace() == "kube-system" &&
 			u.GetName() == "kube-proxy"
 	})
-	require.GreaterOrEqual(t, configMapIdx, 0, "kube-proxy ConfigMap not found")
-	var cm corev1.ConfigMap
-	require.NoError(t, runtime.DefaultUnstructuredConverter.FromUnstructured(resources[configMapIdx].Object, &cm))
+	require.GreaterOrEqual(t, idx, 0, "kube-proxy ConfigMap not found")
 
-	var kubeProxyConfigData unstructured.Unstructured
-	require.NoError(t, yaml.Unmarshal([]byte(cm.Data["config.conf"]), &kubeProxyConfigData.Object))
-	assert.Equal(t, kubeproxyv1alpha1.SchemeGroupVersion.String(), kubeProxyConfigData.GetAPIVersion())
-	assert.Equal(t, "KubeProxyConfiguration", kubeProxyConfigData.GetKind())
+	var configMap corev1.ConfigMap
+	require.NoError(t, runtime.DefaultUnstructuredConverter.FromUnstructured(resources[idx].Object, &configMap))
 
-	renderedFeatureGates, ok := kubeProxyConfigData.Object["featureGates"].(map[string]any)
-	require.True(t, ok)
-	assert.Equal(t, map[string]any{
-		"Feature0": true,
-		"Feature1": false,
-	}, renderedFeatureGates)
+	return &configMap
+}
+
+func findKubeProxyDaemonSet(t *testing.T, resources []*unstructured.Unstructured) *appsv1.DaemonSet {
+	t.Helper()
+
+	idx := slices.IndexFunc(resources, func(u *unstructured.Unstructured) bool {
+		return u.GetAPIVersion() == appsv1.SchemeGroupVersion.String() &&
+			u.GetKind() == "DaemonSet" &&
+			u.GetNamespace() == "kube-system" &&
+			u.GetName() == "kube-proxy"
+	})
+	require.GreaterOrEqual(t, idx, 0, "kube-proxy DaemonSet not found")
+
+	var daemonSet appsv1.DaemonSet
+	require.NoError(t, runtime.DefaultUnstructuredConverter.FromUnstructured(resources[idx].Object, &daemonSet))
+	return &daemonSet
 }

k0sproject/k0s

(cherry picked from commit 18e320e2f9a6ec5a9ec69f89af3bc1fbd2576688)

pkg/component/controller/kubeproxy.go
@@ -6,7 +6,10 @@ package controller
 import (
 	"bufio"
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -162,16 +165,29 @@ func (k *KubeProxy) updateManifests(cfg *proxyConfig, includeWindows bool) error
 		Do(func(unbuffered file.AtomicWriter) error {
 			buf := bufio.NewWriter(unbuffered)
 
+			templateData := struct {
+				*kubeProxyTemplateData
+				ConfigHash string
+			}{
+				kubeProxyTemplateData: &cfg.TemplateData,
+			}
+
 			if configMap, err := cfg.ConfigMapData.toConfigMap(); err != nil {
 				return err
-			} else if err := applier.CodecFor(kubernetesscheme.Scheme).Encode(configMap, buf); err != nil {
-				return err
+			} else {
+				hasher := sha256.New()
+				buf := io.MultiWriter(hasher, buf)
+				codec := applier.CodecFor(kubernetesscheme.Scheme)
+				if err := codec.Encode(configMap, buf); err != nil {
+					return err
+				}
+				templateData.ConfigHash = base64.RawURLEncoding.EncodeToString(hasher.Sum(nil))
 			}
 
 			if err := (&templatewriter.TemplateWriter{
 				Name:     "kube-proxy",
 				Template: proxyTemplate,
-				Data:     &cfg.TemplateData,
+				Data:     &templateData,
 			}).WriteToBuffer(buf); err != nil {
 				return err
 			}
@@ -185,7 +201,7 @@ func (k *KubeProxy) updateManifests(cfg *proxyConfig, includeWindows bool) error
 				if err := (&templatewriter.TemplateWriter{
 					Name:     "kube-proxy-windows",
 					Template: string(proxyWindowsTemplate),
-					Data:     &cfg.TemplateData,
+					Data:     &templateData,
 				}).WriteToBuffer(buf); err != nil {
 					return err
 				}
@@ -426,6 +442,7 @@ spec:
       labels:
         k8s-app: kube-proxy
       annotations:
+        k0sproject.io/config-hash: {{ .ConfigHash }}
         prometheus.io/scrape: 'true'
         prometheus.io/port: '10249'
     spec:

pkg/component/controller/kubeproxy_test.go
@@ -4,6 +4,7 @@
 package controller
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"slices"
@@ -14,6 +15,7 @@ import (
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	"github.com/k0sproject/k0s/pkg/config"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -27,13 +29,50 @@ import (
 )
 
 func TestKubeProxyConfig_FeatureGates(t *testing.T) {
-	k0sVars, err := config.NewCfgVars(nil, t.TempDir())
-	require.NoError(t, err)
 	cfg := v1beta1.DefaultClusterConfig()
 	cfg.Spec.FeatureGates = v1beta1.FeatureGates{
 		{Name: "Feature0", Enabled: true, Components: []string{"kube-proxy"}},
 		{Name: "Feature1", Enabled: false, Components: []string{"kube-proxy"}},
 	}
+
+	_, manifestsDir := startComponent(t, cfg)
+	_, resources := awaitUpdate(t, manifestsDir, nil)
+	configMap := findKubeProxyConfigMap(t, resources)
+
+	var kubeProxyConfigData unstructured.Unstructured
+	require.NoError(t, yaml.Unmarshal([]byte(configMap.Data["config.conf"]), &kubeProxyConfigData.Object))
+	assert.Equal(t, kubeproxyv1alpha1.SchemeGroupVersion.String(), kubeProxyConfigData.GetAPIVersion())
+	assert.Equal(t, "KubeProxyConfiguration", kubeProxyConfigData.GetKind())
+
+	renderedFeatureGates, ok := kubeProxyConfigData.Object["featureGates"].(map[string]any)
+	require.True(t, ok)
+	assert.Equal(t, map[string]any{
+		"Feature0": true,
+		"Feature1": false,
+	}, renderedFeatureGates)
+}
+
+func TestKubeProxyConfig_HashChangesWhenConfigMapChanges(t *testing.T) {
+	cfg := v1beta1.DefaultClusterConfig()
+	underTest, manifestsDir := startComponent(t, cfg)
+
+	stat, resources := awaitUpdate(t, manifestsDir, nil)
+	initialDaemonSet := findKubeProxyDaemonSet(t, resources)
+
+	cfg.Spec.FeatureGates = v1beta1.FeatureGates{
+		{Name: "Feature0", Enabled: true, Components: []string{"kube-proxy"}},
+	}
+	require.NoError(t, underTest.Reconcile(t.Context(), cfg))
+
+	_, resources = awaitUpdate(t, manifestsDir, stat)
+	updatedDaemonSet := findKubeProxyDaemonSet(t, resources)
+
+	assert.NotEqual(t, initialDaemonSet.Spec.Template.Annotations["k0sproject.io/config-hash"], updatedDaemonSet.Spec.Template.Annotations["k0sproject.io/config-hash"])
+}
+
+func startComponent(t *testing.T, cfg *v1beta1.ClusterConfig) (*KubeProxy, string) {
+	k0sVars, err := config.NewCfgVars(nil, t.TempDir())
+	require.NoError(t, err)
 	noWindowsNodes := func() (*bool, <-chan struct{}) {
 		return ptr.To(false), nil
 	}
@@ -44,35 +83,72 @@ func TestKubeProxyConfig_FeatureGates(t *testing.T) {
 	t.Cleanup(func() { assert.NoError(t, underTest.Stop()) })
 	require.NoError(t, underTest.Reconcile(t.Context(), cfg))
 
-	manifestPath := filepath.Join(k0sVars.ManifestsDir, "kubeproxy", "kube-proxy.yaml")
-	var manifestData []byte
+	return underTest, k0sVars.ManifestsDir
+}
+
+func awaitUpdate(t *testing.T, manifestsDir string, prev os.FileInfo) (os.FileInfo, []*unstructured.Unstructured) {
+	manifestPath := filepath.Join(manifestsDir, "kubeproxy", "kube-proxy.yaml")
+
+	var (
+		stat         os.FileInfo
+		manifestData []byte
+	)
 	require.EventuallyWithT(t, func(t *assert.CollectT) {
-		manifestData, err = os.ReadFile(manifestPath)
-		assert.NoError(t, err)
+		f, err := os.OpenFile(manifestPath, os.O_RDONLY, 0)
+		if !assert.NoError(t, err) {
+			return
+		}
+		defer f.Close()
+
+		stat, err = f.Stat()
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		if prev != nil && !assert.True(t, prev.ModTime().Before(stat.ModTime()) || prev.Size() != stat.Size(), "manifest unchanged") {
+			return
+		}
+
+		manifestData, err = io.ReadAll(f)
+		if !assert.NoError(t, err) || !assert.Len(t, manifestData, int(stat.Size())) {
+			return
+		}
 	}, 5*time.Second, 50*time.Millisecond)
 
 	resources, err := testutil.ParseManifests(manifestData)
 	require.NoError(t, err)
+	return stat, resources
+}
 
-	configMapIdx := slices.IndexFunc(resources, func(u *unstructured.Unstructured) bool {
+func findKubeProxyConfigMap(t *testing.T, resources []*unstructured.Unstructured) *corev1.ConfigMap {
+	t.Helper()
+
+	idx := slices.IndexFunc(resources, func(u *unstructured.Unstructured) bool {
 		return u.GetAPIVersion() == corev1.SchemeGroupVersion.String() &&
 			u.GetKind() == "ConfigMap" &&
 			u.GetNamespace() == "kube-system" &&
 			u.GetName() == "kube-proxy"
 	})
-	require.GreaterOrEqual(t, configMapIdx, 0, "kube-proxy ConfigMap not found")
-	var cm corev1.ConfigMap
-	require.NoError(t, runtime.DefaultUnstructuredConverter.FromUnstructured(resources[configMapIdx].Object, &cm))
+	require.GreaterOrEqual(t, idx, 0, "kube-proxy ConfigMap not found")
 
-	var kubeProxyConfigData unstructured.Unstructured
-	require.NoError(t, yaml.Unmarshal([]byte(cm.Data["config.conf"]), &kubeProxyConfigData.Object))
-	assert.Equal(t, kubeproxyv1alpha1.SchemeGroupVersion.String(), kubeProxyConfigData.GetAPIVersion())
-	assert.Equal(t, "KubeProxyConfiguration", kubeProxyConfigData.GetKind())
+	var configMap corev1.ConfigMap
+	require.NoError(t, runtime.DefaultUnstructuredConverter.FromUnstructured(resources[idx].Object, &configMap))
 
-	renderedFeatureGates, ok := kubeProxyConfigData.Object["featureGates"].(map[string]any)
-	require.True(t, ok)
-	assert.Equal(t, map[string]any{
-		"Feature0": true,
-		"Feature1": false,
-	}, renderedFeatureGates)
+	return &configMap
+}
+
+func findKubeProxyDaemonSet(t *testing.T, resources []*unstructured.Unstructured) *appsv1.DaemonSet {
+	t.Helper()
+
+	idx := slices.IndexFunc(resources, func(u *unstructured.Unstructured) bool {
+		return u.GetAPIVersion() == appsv1.SchemeGroupVersion.String() &&
+			u.GetKind() == "DaemonSet" &&
+			u.GetNamespace() == "kube-system" &&
+			u.GetName() == "kube-proxy"
+	})
+	require.GreaterOrEqual(t, idx, 0, "kube-proxy DaemonSet not found")
+
+	var daemonSet appsv1.DaemonSet
+	require.NoError(t, runtime.DefaultUnstructuredConverter.FromUnstructured(resources[idx].Object, &daemonSet))
+	return &daemonSet
 }