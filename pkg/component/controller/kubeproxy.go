twz123/k0s-testo

Roll kube-proxy DaemonSet on ConfigMap changes
The kube-proxy DaemonSet consumes its configuration from a ConfigMap,
but nothing in the pod template changes when that ConfigMap is updated.
That can leave existing pods running with stale configuration until some
unrelated restart happens.

Hash the rendered kube-proxy ConfigMap and add the hash to the pod
template labels so configuration changes trigger a DaemonSet rollout.
Extend the kube-proxy tests to cover the rendered config and the hash
change behavior through the component's public API.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/kubeproxy.go
@@ -6,7 +6,10 @@ package controller
 import (
 	"bufio"
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -162,16 +165,29 @@ func (k *KubeProxy) updateManifests(cfg *proxyConfig, includeWindows bool) error
 		Do(func(unbuffered file.AtomicWriter) error {
 			buf := bufio.NewWriter(unbuffered)
 
+			templateData := struct {
+				*kubeProxyTemplateData
+				ConfigHash string
+			}{
+				kubeProxyTemplateData: &cfg.TemplateData,
+			}
+
 			if configMap, err := cfg.ConfigMapData.toConfigMap(); err != nil {
 				return err
-			} else if err := applier.CodecFor(kubernetesscheme.Scheme).Encode(configMap, buf); err != nil {
-				return err
+			} else {
+				hasher := sha256.New()
+				buf := io.MultiWriter(hasher, buf)
+				codec := applier.CodecFor(kubernetesscheme.Scheme)
+				if err := codec.Encode(configMap, buf); err != nil {
+					return err
+				}
+				templateData.ConfigHash = hex.EncodeToString(hasher.Sum(nil))
 			}
 
 			if err := (&templatewriter.TemplateWriter{
 				Name:     "kube-proxy",
 				Template: proxyTemplate,
-				Data:     &cfg.TemplateData,
+				Data:     &templateData,
 			}).WriteToBuffer(buf); err != nil {
 				return err
 			}
@@ -185,7 +201,7 @@ func (k *KubeProxy) updateManifests(cfg *proxyConfig, includeWindows bool) error
 				if err := (&templatewriter.TemplateWriter{
 					Name:     "kube-proxy-windows",
 					Template: string(proxyWindowsTemplate),
-					Data:     &cfg.TemplateData,
+					Data:     &templateData,
 				}).WriteToBuffer(buf); err != nil {
 					return err
 				}
@@ -426,6 +442,7 @@ spec:
     metadata:
       labels:
         k8s-app: kube-proxy
+        k0sproject.io/config-hash: {{ .ConfigHash }}
       annotations:
         prometheus.io/scrape: 'true'
         prometheus.io/port: '10249'

k0sproject/k0s

(cherry picked from commit 18e320e2f9a6ec5a9ec69f89af3bc1fbd2576688)
(cherry picked from commit e3cf6f253832078ddca45559c608de30aefc0019)
(cherry picked from commit cc001aa10d9d9f253110d6379ac1df76cd67e791)

pkg/component/controller/kubeproxy.go
@@ -19,7 +19,10 @@ package controller
 import (
 	"bufio"
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -98,16 +101,29 @@ func (k *KubeProxy) Reconcile(_ context.Context, clusterConfig *v1beta1.ClusterC
 		Do(func(unbuffered file.AtomicWriter) error {
 			buf := bufio.NewWriter(unbuffered)
 
+			templateData := struct {
+				*kubeProxyTemplateData
+				ConfigHash string
+			}{
+				kubeProxyTemplateData: &cfg.TemplateData,
+			}
+
 			if configMap, err := cfg.ConfigMapData.toConfigMap(); err != nil {
 				return err
-			} else if err := applier.CodecFor(kubernetesscheme.Scheme).Encode(configMap, buf); err != nil {
-				return err
+			} else {
+				hasher := sha256.New()
+				buf := io.MultiWriter(hasher, buf)
+				codec := applier.CodecFor(kubernetesscheme.Scheme)
+				if err := codec.Encode(configMap, buf); err != nil {
+					return err
+				}
+				templateData.ConfigHash = base64.RawURLEncoding.EncodeToString(hasher.Sum(nil))
 			}
 
 			if err := (&templatewriter.TemplateWriter{
 				Name:     "kube-proxy",
 				Template: proxyTemplate,
-				Data:     &cfg.TemplateData,
+				Data:     &templateData,
 			}).WriteToBuffer(buf); err != nil {
 				return err
 			}
@@ -349,6 +365,7 @@ spec:
       labels:
         k8s-app: kube-proxy
       annotations:
+        k0sproject.io/config-hash: {{ .ConfigHash }}
         prometheus.io/scrape: 'true'
         prometheus.io/port: '10249'
     spec: