k0sproject/k0s

commit 4f8f2438ecbf05f9624c975d5a885f58d5ded790

Manage draining and cordoning via Autopilot controller
Instead of letting each node handle its own cordon/drain/uncordon
process, let the Autopilot controller handle it. This will allow for
the future removal of some very broad RBAC roles currently granted to
all workers.

These roles need to be kept during the migration period to prevent
cluster upgrades from getting stuck when updating from an old,
self-managed k0s version to a new, centrally managed one.

Use the node lease object to carry over information about how a node
handles cordoning. Access to node lease objects is granted by the node
authorizer and does not require special RBAC roles.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/autopilot/controller/root_controller.go
@@ -103,6 +103,7 @@ func (c *rootController) Run(ctx context.Context) error {
 	le, err := c.newLeaderElector(&leaderelection.LeaseConfig{
 		Namespace: apconst.AutopilotNamespace,
 		Name:      apconst.AutopilotNamespace + "-controller",
+		Labels:    map[string]string{apconst.CentralCordoningLabel: c.cfg.InvocationID},
 		Identity:  c.cfg.InvocationID,
 		Client:    kubeClient.CoordinationV1(),
 	})
@@ -236,7 +237,7 @@ func (c *rootController) startSubControllerRoutine(ctx context.Context, logger *
 	}
 	clusterID := string(ns.UID)
 
-	if err := signal.RegisterControllers(ctx, logger, mgr, delegateMap[apdel.ControllerDelegateController], c.cfg.K0sDataDir, clusterID); err != nil {
+	if err := signal.RegisterControllers(ctx, logger, mgr, delegateMap[apdel.ControllerDelegateController], c.cfg.K0sDataDir, c.enableWorker, clusterID, event, c.cfg.InvocationID); err != nil {
 		logger.WithError(err).Error("unable to register signal controllers")
 		return err
 	}

pkg/autopilot/controller/root_worker.go
@@ -14,6 +14,7 @@ import (
 	apdel "github.com/k0sproject/k0s/pkg/autopilot/controller/delegate"
 	aproot "github.com/k0sproject/k0s/pkg/autopilot/controller/root"
 	"github.com/k0sproject/k0s/pkg/autopilot/controller/signal"
+	"github.com/k0sproject/k0s/pkg/leaderelection"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -110,7 +111,7 @@ func (w *rootWorker) Run(ctx context.Context) error {
 			return fmt.Errorf("unable to register indexers: %w", err)
 		}
 
-		if err := signal.RegisterControllers(ctx, logger, mgr, apdel.NodeControllerDelegate(), w.cfg.K0sDataDir, clusterID); err != nil {
+		if err := signal.RegisterControllers(ctx, logger, mgr, apdel.NodeControllerDelegate(), w.cfg.K0sDataDir, true, clusterID, leaderelection.StatusPending, w.cfg.InvocationID); err != nil {
 			return fmt.Errorf("unable to register signal controllers: %w", err)
 		}
 

pkg/autopilot/controller/signal/init.go
@@ -12,6 +12,7 @@ import (
 	apdel "github.com/k0sproject/k0s/pkg/autopilot/controller/delegate"
 	"github.com/k0sproject/k0s/pkg/autopilot/controller/signal/airgap"
 	"github.com/k0sproject/k0s/pkg/autopilot/controller/signal/k0s"
+	"github.com/k0sproject/k0s/pkg/leaderelection"
 
 	"github.com/sirupsen/logrus"
 	crman "sigs.k8s.io/controller-runtime/pkg/manager"
@@ -19,8 +20,8 @@ import (
 
 // RegisterControllers registers all of the autopilot controllers used by both controller
 // and worker modes.
-func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, k0sDataDir, clusterID string) error {
-	if err := k0s.RegisterControllers(ctx, logger, mgr, delegate, clusterID); err != nil {
+func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, k0sDataDir string, enableWorker bool, clusterID string, leaseStatus leaderelection.Status, invocationID string) error {
+	if err := k0s.RegisterControllers(ctx, logger, mgr, delegate, enableWorker, clusterID, leaseStatus, invocationID); err != nil {
 		return fmt.Errorf("unable to register k0s controllers: %w", err)
 	}
 

pkg/autopilot/controller/signal/k0s/cordon.go
@@ -18,6 +18,8 @@ import (
 	apdel "github.com/k0sproject/k0s/pkg/autopilot/controller/delegate"
 	apsigpred "github.com/k0sproject/k0s/pkg/autopilot/controller/signal/common/predicate"
 	apsigv2 "github.com/k0sproject/k0s/pkg/autopilot/signaling/v2"
+	kubeutil "github.com/k0sproject/k0s/pkg/kubernetes"
+	"github.com/k0sproject/k0s/pkg/leaderelection"
 
 	cr "sigs.k8s.io/controller-runtime"
 	crcli "sigs.k8s.io/controller-runtime/pkg/client"
@@ -27,6 +29,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/kubectl/pkg/drain"
 )
@@ -35,10 +39,9 @@ const Cordoning = "Cordoning"
 
 // cordoningEventFilter creates a controller-runtime predicate that governs which objects
 // will make it into reconciliation, and which will be ignored.
-func cordoningEventFilter(hostname string, handler apsigpred.ErrorHandler) crpred.Predicate {
+func cordoningEventFilter(handler apsigpred.ErrorHandler) crpred.Predicate {
 	return crpred.And(
 		crpred.AnnotationChangedPredicate{},
-		apsigpred.SignalNamePredicate(hostname),
 		apsigpred.NewSignalDataPredicateAdapter(handler).And(
 			signalDataUpdateCommandK0sPredicate(),
 			apsigpred.SignalDataStatusPredicate(Cordoning),
@@ -58,8 +61,10 @@ type cordonUncordon struct {
 	log          *logrus.Entry
 	client       crcli.Client
 	delegate     apdel.ControllerDelegate
-	clientset    *kubernetes.Clientset
+	clientset    kubernetes.Interface
 	currentState string
+	nodeName     types.NodeName
+	leaseStatus  leaderelection.Status
 	do           func(*drain.Helper, *corev1.Node) error
 	nextState    string
 }
@@ -70,7 +75,7 @@ type cordonUncordon struct {
 // This controller is only interested when autopilot signaling annotations have
 // moved to a `Cordoning` status. At this point, it will attempt to cordong & drain
 // the node.
-func registerCordoning(logger *logrus.Entry, mgr crman.Manager, eventFilter crpred.Predicate, delegate apdel.ControllerDelegate) error {
+func registerCordoning(logger *logrus.Entry, mgr crman.Manager, eventFilter crpred.Predicate, delegate apdel.ControllerDelegate, nodeName types.NodeName, leaseStatus leaderelection.Status) error {
 	name := strings.ToLower(delegate.Name()) + "_k0s_cordoning"
 	logger.Info("Registering reconciler: ", name)
 
@@ -91,6 +96,8 @@ func registerCordoning(logger *logrus.Entry, mgr crman.Manager, eventFilter crpr
 				delegate:     delegate,
 				clientset:    clientset,
 				currentState: Cordoning,
+				nodeName:     nodeName,
+				leaseStatus:  leaseStatus,
 				do:           cordonAndDrainNode,
 				nextState:    ApplyingUpdate,
 			},
@@ -115,6 +122,14 @@ func (r *cordonUncordon) Reconcile(ctx context.Context, req cr.Request) (cr.Resu
 		logger.Debug("Ignoring signal status ", signalData.Status.Status)
 		return cr.Result{}, nil
 	}
+
+	if reason, err := r.isIgnored(ctx, signalNode); err != nil {
+		return cr.Result{}, fmt.Errorf("failed to determine if this node should be ignored: %w", err)
+	} else if reason != "" {
+		logger.Debug("Ignoring this node: ", reason)
+		return cr.Result{}, nil
+	}
+
 	if !needsCordoning(signalNode) {
 		logger.Infof("ignoring non worker node")
 
@@ -127,6 +142,54 @@ func (r *cordonUncordon) Reconcile(ctx context.Context, req cr.Request) (cr.Resu
 	}
 
 	return cr.Result{}, r.moveToNextState(ctx, signalNode)
+}
+
+// Determines whether the request should be ignored. This enables the migration
+// from Autopilot deployments that manage (un-)cordoning on each individual node
+// to deployments that manage (un-)cordoning via the Autopilot controller.
+//
+// TODO: Remove in v1.36+
+func (r *cordonUncordon) isIgnored(ctx context.Context, signalNode crcli.Object) (reason string, _ error) {
+	if r.leaseStatus == leaderelection.StatusLeading {
+		if types.NodeName(signalNode.GetName()) == r.nodeName {
+			// It's us and we're leading. Go for it!
+			return "", nil
+		}
+
+		if !needsCordoning(signalNode) {
+			// If it's not a worker node, there's no node lease to be checked.
+			return "", nil
+		}
+
+		// Check the node lease if it needs to be reconciled by us.
+		nodeLease, err := r.clientset.CoordinationV1().Leases(corev1.NamespaceNodeLease).Get(ctx, signalNode.GetName(), metav1.GetOptions{})
+		if err != nil {
+			return "", fmt.Errorf("failed to get node lease: %w", err)
+		}
+
+		label, ident := nodeLease.Labels[apconst.CentralCordoningLabel], nodeLease.Spec.HolderIdentity
+		if label == "" || (ident != nil && label != *ident) {
+			return "node manages cordoning on its own", nil
+		}
+
+		return "", nil
+	}
+
+	// Check if the current autopilot leader is managing the reconciliation for us.
+	apLease, err := r.clientset.CoordinationV1().Leases(apconst.AutopilotNamespace).Get(ctx, apconst.AutopilotNamespace+"-controller", metav1.GetOptions{})
+	if err != nil {
+		return "", fmt.Errorf("failed to get autopilot controller lease: %w", err)
+	}
+
+	ident := apLease.Spec.HolderIdentity
+	switch {
+	case ident == nil || *ident == "" || !kubeutil.IsValidLease(*apLease):
+		return "", errors.New("autopilot controller lease is invalid")
+	case apLease.Labels[apconst.CentralCordoningLabel] == *ident:
+		return "autopilot controller takes care of reconciliation", nil
+	default:
+		return "", nil
+	}
 }
 
 func (r *cordonUncordon) moveToNextState(ctx context.Context, signalNode crcli.Object) error {

pkg/autopilot/controller/signal/k0s/init.go
@@ -11,18 +11,26 @@ import (
 	"os"
 	"path/filepath"
 
+	autopilotv1beta2 "github.com/k0sproject/k0s/pkg/apis/autopilot/v1beta2"
 	apcomm "github.com/k0sproject/k0s/pkg/autopilot/common"
+	"github.com/k0sproject/k0s/pkg/autopilot/constant"
 	apdel "github.com/k0sproject/k0s/pkg/autopilot/controller/delegate"
 	apsigpred "github.com/k0sproject/k0s/pkg/autopilot/controller/signal/common/predicate"
 	"github.com/k0sproject/k0s/pkg/component/status"
+	"github.com/k0sproject/k0s/pkg/leaderelection"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	applycoordinationv1 "k8s.io/client-go/applyconfigurations/coordination/v1"
 
 	"github.com/sirupsen/logrus"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	crman "sigs.k8s.io/controller-runtime/pkg/manager"
+	crpred "sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 // RegisterControllers registers all of the autopilot controllers used for updating `k0s`
 // to the controller-runtime manager.
-func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, clusterID string) error {
+func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, enableWorker bool, clusterID string, leaseStatus leaderelection.Status, invocationID string) error {
 	logger = logger.WithField("controller", delegate.Name())
 
 	hostname, err := apcomm.FindEffectiveHostname()
@@ -42,6 +50,15 @@ func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Ma
 		return getK0sVersion(status.DefaultSocketPath)
 	}
 
+	if enableWorker {
+		if err := mgr.GetClient().Apply(ctx, applycoordinationv1.
+			Lease(hostname, corev1.NamespaceNodeLease).
+			WithLabels(map[string]string{constant.CentralCordoningLabel: invocationID}),
+			client.FieldOwner("k0s/autopilot"), client.ForceOwnership); err != nil {
+			return fmt.Errorf("unable to apply lease labels: %w", err)
+		}
+	}
+
 	if err := registerSignalController(logger, mgr, signalControllerEventFilter(hostname, apsigpred.DefaultErrorHandler(logger, "k0s signal")), delegate, clusterID, k0sVersionHandler); err != nil {
 		return fmt.Errorf("unable to register signal controller: %w", err)
 	}
@@ -50,8 +67,25 @@ func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Ma
 		return fmt.Errorf("unable to register downloading controller: %w", err)
 	}
 
-	if err := registerCordoning(logger, mgr, cordoningEventFilter(hostname, apsigpred.DefaultErrorHandler(logger, "k0s cordoning")), delegate); err != nil {
+	// Man, I wish there would be a saner way to do this!
+	var nodeDelegate apdel.ControllerDelegate
+	if _, isController := delegate.CreateObject().(*autopilotv1beta2.ControlNode); isController {
+		nodeDelegate = apdel.NodeControllerDelegate()
+	}
+
+	cordoningEventFilter := cordoningEventFilter(apsigpred.DefaultErrorHandler(logger, "k0s cordoning"))
+	if leaseStatus != leaderelection.StatusLeading {
+		cordoningEventFilter = crpred.And(apsigpred.SignalNamePredicate(hostname), cordoningEventFilter)
+	}
+
+	if err := registerCordoning(logger, mgr, cordoningEventFilter, delegate, types.NodeName(hostname), leaseStatus); err != nil {
 		return fmt.Errorf("unable to register cordoning controller: %w", err)
+	}
+
+	if nodeDelegate != nil {
+		if err := registerCordoning(logger, mgr, cordoningEventFilter, nodeDelegate, types.NodeName(hostname), leaseStatus); err != nil {
+			return fmt.Errorf("unable to register cordoning node controller: %w", err)
+		}
 	}
 
 	if err := registerApplyingUpdate(logger, mgr, applyingUpdateEventFilter(hostname, apsigpred.DefaultErrorHandler(logger, "k0s applying-update")), delegate, k0sBinaryDir); err != nil {
@@ -66,8 +100,19 @@ func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Ma
 		return fmt.Errorf("unable to register restarted controller: %w", err)
 	}
 
-	if err := registerUncordoning(logger, mgr, unCordoningEventFilter(hostname, apsigpred.DefaultErrorHandler(logger, "k0s uncordoning")), delegate); err != nil {
+	unCordoningEventFilter := unCordoningEventFilter(apsigpred.DefaultErrorHandler(logger, "k0s uncordoning"))
+	if leaseStatus != leaderelection.StatusLeading {
+		unCordoningEventFilter = crpred.And(apsigpred.SignalNamePredicate(hostname), unCordoningEventFilter)
+	}
+
+	if err := registerUncordoning(logger, mgr, unCordoningEventFilter, delegate, types.NodeName(hostname), leaseStatus); err != nil {
 		return fmt.Errorf("unable to register uncordoning controller: %w", err)
+	}
+
+	if nodeDelegate != nil {
+		if err := registerUncordoning(logger, mgr, unCordoningEventFilter, nodeDelegate, types.NodeName(hostname), leaseStatus); err != nil {
+			return fmt.Errorf("unable to register uncordoning node controller: %w", err)
+		}
 	}
 
 	return nil

pkg/autopilot/constant/static.go
@@ -7,6 +7,7 @@ const (
 	AutopilotName                      = "autopilot"
 	AutopilotNamespace                 = "k0s-autopilot"
 	K0sTempFilename                    = "k0s.tmp"
+	CentralCordoningLabel              = "autopilot.k0sproject.io/central-cordoning"
 	K0SControlNodeModeAnnotation       = "autopilot.k0sproject.io/mode"
 	K0SControlNodeModeController       = "controller"
 	K0SControlNodeModeControllerWorker = "controller+worker"

pkg/autopilot/controller/signal/k0s/uncordon.go
@@ -12,9 +12,11 @@ import (
 	apdel "github.com/k0sproject/k0s/pkg/autopilot/controller/delegate"
 	apsigcomm "github.com/k0sproject/k0s/pkg/autopilot/controller/signal/common"
 	apsigpred "github.com/k0sproject/k0s/pkg/autopilot/controller/signal/common/predicate"
+	"github.com/k0sproject/k0s/pkg/leaderelection"
 
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/kubectl/pkg/drain"
 	cr "sigs.k8s.io/controller-runtime"
@@ -27,10 +29,9 @@ const UnCordoning = "UnCordoning"
 
 // unCordoningEventFilter creates a controller-runtime predicate that governs which objects
 // will make it into reconciliation, and which will be ignored.
-func unCordoningEventFilter(hostname string, handler apsigpred.ErrorHandler) crpred.Predicate {
+func unCordoningEventFilter(handler apsigpred.ErrorHandler) crpred.Predicate {
 	return crpred.And(
 		crpred.AnnotationChangedPredicate{},
-		apsigpred.SignalNamePredicate(hostname),
 		apsigpred.NewSignalDataPredicateAdapter(handler).And(
 			signalDataUpdateCommandK0sPredicate(),
 			apsigpred.SignalDataStatusPredicate(UnCordoning),
@@ -52,7 +53,7 @@ func unCordoningEventFilter(hostname string, handler apsigpred.ErrorHandler) crp
 // This controller is only interested when autopilot signaling annotations have
 // moved to a `Cordoning` status. At this point, it will attempt to cordong & drain
 // the node.
-func registerUncordoning(logger *logrus.Entry, mgr crman.Manager, eventFilter crpred.Predicate, delegate apdel.ControllerDelegate) error {
+func registerUncordoning(logger *logrus.Entry, mgr crman.Manager, eventFilter crpred.Predicate, delegate apdel.ControllerDelegate, nodeName types.NodeName, leaseStatus leaderelection.Status) error {
 	name := strings.ToLower(delegate.Name()) + "_k0s_uncordoning"
 	logger.Info("Registering reconciler: ", name)
 
@@ -73,6 +74,8 @@ func registerUncordoning(logger *logrus.Entry, mgr crman.Manager, eventFilter cr
 				delegate:     delegate,
 				clientset:    clientset,
 				currentState: UnCordoning,
+				nodeName:     nodeName,
+				leaseStatus:  leaseStatus,
 				do:           uncordonNode,
 				nextState:    apsigcomm.Completed,
 			},

pkg/component/controller/autopilot.yaml
@@ -3,3 +3,30 @@ apiVersion: v1
 kind: Namespace
 metadata:
   name: k0s-autopilot
+---
+# Required to check if the controller is managing cordoning of nodes.
+# TODO: Remove in v1.36+
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: system:nodes:autopilot
+  namespace: k0s-autopilot
+rules:
+  - apiGroups: [coordination.k8s.io]
+    resources: [leases]
+    verbs: [get]
+    resourceNames: [k0s-autopilot-controller]
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: system:nodes:autopilot
+  namespace: k0s-autopilot
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: system:nodes:autopilot
+subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: Group
+    name: system:nodes

commit d489364400e43fd6a1b9e54847d1501b5886adf2

Fix Autopilot labeling for worker node leases
The original idea was to stamp each worker's node lease with the
central-cordoning label set to k0s's current invocation ID, then have
the Autopilot controller confirm that the worker is running a compatible
k0s version by comparing the label's value against the lease's holder
identity. This is the same pattern that is used with the Autopilot
controller lease. However, the node leases are very much different
from the controller lease. Their holder identity is the node name,
not the k0s invocation ID. I completely mixed that up. Moreover, the
controller lease is used for leader election, whereas the node leases
are used for heartbeats. The holder identity has much less of a meaning
for the latter. In particular, the kubelet, which is the component
ultimately owning the lease, will only ever set the holder identity when
creating the lease for the first time. If it finds an existing lease, it
doesn't touch anything but the renewal time. Having Autopilot apply the
central-cordoning label during startup results in the holder identity
being unset for newly created nodes.

On top of that, the invocation ID was never wired into the worker's
Autopilot configuration, so the label value was always set to the empty
string on pure worker nodes. Not that it would've helped much if it
were.

Fix this by treating the label's presence (not its value) as the signal.
Old k0s workers that don't have the label continue to manage their
own cordoning; new workers that carry the label are handled centrally.
This means that there's no way for k0s to detect downgrades: If k0s is
downgraded to a version that predates central cordoning, the label will
remain on the lease and the controller will continue to treat the worker
as if it supports central cordoning.

Remove the now unused parts of the invocation ID wiring in the Autopilot
guts, and, just for completeness, set the invocation ID into the
Autopilot worker config.

Additionally, put a little band-aid on the potentially unset holder
identity. Patch it to the node name if it's not set after applying the
label.

Fixes: 4f8f2438e ("Manage draining and cordoning via Autopilot controller")

pkg/autopilot/controller/root_controller.go
@@ -237,7 +237,7 @@ func (c *rootController) startSubControllerRoutine(ctx context.Context, logger *
 	}
 	clusterID := string(ns.UID)
 
-	if err := signal.RegisterControllers(ctx, logger, mgr, delegateMap[apdel.ControllerDelegateController], c.cfg.K0sDataDir, c.enableWorker, clusterID, event, c.cfg.InvocationID); err != nil {
+	if err := signal.RegisterControllers(ctx, logger, mgr, delegateMap[apdel.ControllerDelegateController], c.cfg.K0sDataDir, c.enableWorker, clusterID, event); err != nil {
 		logger.WithError(err).Error("unable to register signal controllers")
 		return err
 	}

pkg/autopilot/controller/root_worker.go
@@ -111,7 +111,7 @@ func (w *rootWorker) Run(ctx context.Context) error {
 			return fmt.Errorf("unable to register indexers: %w", err)
 		}
 
-		if err := signal.RegisterControllers(ctx, logger, mgr, apdel.NodeControllerDelegate(), w.cfg.K0sDataDir, true, clusterID, leaderelection.StatusPending, w.cfg.InvocationID); err != nil {
+		if err := signal.RegisterControllers(ctx, logger, mgr, apdel.NodeControllerDelegate(), w.cfg.K0sDataDir, true, clusterID, leaderelection.StatusPending); err != nil {
 			return fmt.Errorf("unable to register signal controllers: %w", err)
 		}
 

pkg/autopilot/controller/signal/init.go
@@ -21,8 +21,8 @@ import (
 
 // RegisterControllers registers all of the autopilot controllers used by both controller
 // and worker modes.
-func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, k0sDataDir string, enableWorker bool, clusterID string, leaseStatus leaderelection.Status, invocationID string) error {
-	if err := k0s.RegisterControllers(ctx, logger, mgr, delegate, enableWorker, clusterID, leaseStatus, invocationID); err != nil {
+func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, k0sDataDir string, enableWorker bool, clusterID string, leaseStatus leaderelection.Status) error {
+	if err := k0s.RegisterControllers(ctx, logger, mgr, delegate, enableWorker, clusterID, leaseStatus); err != nil {
 		return fmt.Errorf("unable to register k0s controllers: %w", err)
 	}
 

pkg/autopilot/controller/signal/k0s/cordon.go
@@ -167,8 +167,15 @@ func (r *cordonUncordon) isIgnored(ctx context.Context, signalNode crcli.Object)
 			return "", fmt.Errorf("failed to get node lease: %w", err)
 		}
 
-		label, ident := nodeLease.Labels[apconst.CentralCordoningLabel], nodeLease.Spec.HolderIdentity
-		if label == "" || (ident != nil && label != *ident) {
+		// Since the node lease is a heartbeat lease and not a leader lease, the
+		// holder identity is cosmetic and always equals the lease name. Hence,
+		// we can't pull the same trick as we do with the Autopilot controller
+		// lease, where we compare the annotation's value with the current
+		// holder identity. This means we can't detect stale annotations, e.g.
+		// after downgrading from a k0s version that supports central cordoning
+		// to one that doesn't. It's a pity, but there's not much we can do
+		// about it. There's no practical way for us to figure this out.
+		if _, exists := nodeLease.Labels[apconst.CentralCordoningLabel]; !exists {
 			return "node manages cordoning on its own", nil
 		}
 

pkg/autopilot/controller/signal/k0s/init.go
@@ -7,6 +7,7 @@ package k0s
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -18,7 +19,11 @@ import (
 	apsigpred "github.com/k0sproject/k0s/pkg/autopilot/controller/signal/common/predicate"
 	"github.com/k0sproject/k0s/pkg/component/status"
 	"github.com/k0sproject/k0s/pkg/leaderelection"
+
+	coordinationv1 "k8s.io/api/coordination/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	applycoordinationv1 "k8s.io/client-go/applyconfigurations/coordination/v1"
 
@@ -30,7 +35,7 @@ import (
 
 // RegisterControllers registers all of the autopilot controllers used for updating `k0s`
 // to the controller-runtime manager.
-func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, enableWorker bool, clusterID string, leaseStatus leaderelection.Status, invocationID string) error {
+func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Manager, delegate apdel.ControllerDelegate, enableWorker bool, clusterID string, leaseStatus leaderelection.Status) error {
 	logger = logger.WithField("controller", delegate.Name())
 
 	hostname, err := apcomm.FindEffectiveHostname()
@@ -51,11 +56,50 @@ func RegisterControllers(ctx context.Context, logger *logrus.Entry, mgr crman.Ma
 	}
 
 	if enableWorker {
-		if err := mgr.GetClient().Apply(ctx, applycoordinationv1.
+		// Stamp the node lease with the central cordoning label to signal to
+		// the autopilot controller that this worker supports central cordoning.
+		// Note that if k0s is downgraded to a version that no longer supports
+		// central cordoning, the label will remain in place, and there will be
+		// no way to detect that this has become stale.
+		c := mgr.GetClient()
+		if err := c.Apply(ctx, applycoordinationv1.
 			Lease(hostname, corev1.NamespaceNodeLease).
-			WithLabels(map[string]string{constant.CentralCordoningLabel: invocationID}),
+			WithLabels(map[string]string{constant.CentralCordoningLabel: ""}),
 			client.FieldOwner("k0s/autopilot"), client.ForceOwnership); err != nil {
 			return fmt.Errorf("unable to apply lease labels: %w", err)
+		}
+
+		// The kubelet only sets the holder identity when creating a new lease
+		// from scratch, not when renewing an existing one. The above apply call
+		// may very well happen before the kubelet has had a chance to create
+		// the lease. As a result, the kubelet finds an existing lease in place
+		// and only updates the renewal time, leaving the holder identity unset.
+		//
+		// Work around this by sending a conditional patch that will set the
+		// holder identity if it has not already been set. If the identity has
+		// already been set, the patch will fail with an "Invalid" error. We
+		// treat this as a success.
+		patch, err := json.Marshal([]struct {
+			Op    string `json:"op"`
+			Path  string `json:"path"`
+			Value string `json:"value,omitempty"`
+		}{
+			{"test", "/spec/holderIdentity", ""},
+			{"add", "/spec/holderIdentity", hostname},
+		})
+		if err != nil {
+			return err
+		}
+
+		if err := c.Patch(
+			ctx,
+			&coordinationv1.Lease{ObjectMeta: metav1.ObjectMeta{
+				Name:      hostname,
+				Namespace: corev1.NamespaceNodeLease,
+			}},
+			client.RawPatch(types.JSONPatchType, patch),
+		); err != nil && !apierrors.IsInvalid(err) {
+			return fmt.Errorf("failed to ensure that a holder identity is set: %w", err)
 		}
 	}
 

pkg/component/worker/autopilot.go
@@ -74,6 +74,7 @@ func (a *Autopilot) Start(ctx context.Context) error {
 
 	log.Info("Autopilot client factory created, booting up worker root controller")
 	autopilotRoot, err := apcont.NewRootWorker(aproot.RootConfig{
+		InvocationID:        a.K0sVars.InvocationID,
 		KubeConfig:          a.K0sVars.KubeletAuthConfigPath,
 		K0sDataDir:          a.K0sVars.DataDir,
 		Mode:                "worker",