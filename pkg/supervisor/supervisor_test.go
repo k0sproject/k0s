k0sproject/k0s

commit 739eebd64a3683bd6820daa5ddae252d9e211e93

Bail out after stop timeout
Previously, the shutdown code looped endlessly until the child process
finished, requesting graceful termination over and over again. Change
this to a single request-termination -> wait -> bail-out logic. This
is to ensure that k0s won't hang when the supervised processes can't be
terminated for whichever reason: the code will terminate, at least after
the timeout expired.

Use a buffered channel for the wait result, so that the goroutine
will be able to exit, even if nothing reads from the channel anymore.
Introduce fine-grained error reporting to differentiate shutdown
outcomes (graceful shutdown, forced kill, failure, and so on).

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/supervisor/supervisor.go
@@ -15,6 +15,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -54,7 +55,7 @@ const k0sManaged = "_K0S_MANAGED=yes"
 // processWaitQuit waits for a process to exit or a shut down signal
 // returns true if shutdown is requested
 func (s *Supervisor) processWaitQuit(ctx context.Context, cmd *exec.Cmd) bool {
-	waitresult := make(chan error)
+	waitresult := make(chan error, 1)
 	go func() {
 		waitresult <- cmd.Wait()
 	}()
@@ -63,29 +64,14 @@ func (s *Supervisor) processWaitQuit(ctx context.Context, cmd *exec.Cmd) bool {
 
 	select {
 	case <-ctx.Done():
-		for {
-			s.log.Debugf("Requesting graceful termination (%v)", context.Cause(ctx))
-			if err := requestGracefulTermination(cmd.Process); err != nil {
-				if errors.Is(err, os.ErrProcessDone) {
-					s.log.Info("Failed to request graceful termination: process has already terminated")
-				} else {
-					s.log.WithError(err).Error("Failed to request graceful termination")
-				}
-			} else {
-				s.log.Info("Requested graceful termination")
-			}
-			select {
-			case <-time.After(s.TimeoutStop):
-				continue
-			case err := <-waitresult:
-				if err != nil {
-					s.log.WithError(err).Error("Failed to wait for process")
-				} else {
-					s.log.Info("Process exited: ", s.cmd.ProcessState)
-				}
-				return true
-			}
+		s.log.Debugf("Attempting to terminate supervised process (%v)", context.Cause(ctx))
+		if err := s.terminateSupervisedProcess(cmd, waitresult); err != nil {
+			s.log.WithError(err).Error("Error while terminating process")
+		} else {
+			s.log.Info("Process terminated successfully")
 		}
+		return true
+
 	case err := <-waitresult:
 		var exitErr *exec.ExitError
 		state := cmd.ProcessState
@@ -99,6 +85,61 @@ func (s *Supervisor) processWaitQuit(ctx context.Context, cmd *exec.Cmd) bool {
 			s.log.WithError(err).Error("Failed to wait for process: ", state)
 		}
 		return false
+	}
+}
+
+func (s *Supervisor) terminateSupervisedProcess(cmd *exec.Cmd, waitresult <-chan error) error {
+	err := requestGracefulTermination(cmd.Process)
+	switch {
+	case err == nil:
+		// Termination request sent, wait for process to finish.
+		s.log.Debug("Awaiting graceful process termination for ", s.TimeoutStop)
+
+		select {
+		case err := <-waitresult:
+			var exitErr *exec.ExitError
+			switch {
+			case err == nil:
+				return nil
+			case errors.As(err, &exitErr):
+				if status, ok := exitErr.Sys().(syscall.WaitStatus); ok && status.Signal() == syscall.SIGTERM {
+					return errors.New("process terminated without handling SIGTERM")
+				}
+				return exitErr
+			default:
+				return fmt.Errorf("failed to wait for process: %w", err)
+			}
+
+		case <-time.After(s.TimeoutStop):
+			err = fmt.Errorf("timed out after %s while waiting for process to terminate", s.TimeoutStop)
+		}
+
+		return err
+
+	case errors.Is(err, os.ErrProcessDone):
+		// The process has finished even before the termination could be requested.
+		select {
+		case err = <-waitresult:
+			var exitErr *exec.ExitError
+			state := cmd.ProcessState
+			switch {
+			case errors.As(err, &exitErr):
+				state = exitErr.ProcessState
+				fallthrough
+			case err == nil:
+				err = errors.New(state.String())
+			default:
+				return fmt.Errorf("failed to wait for process: %s (%w)", state, err)
+			}
+		default:
+			err = errors.New("process state unavailable")
+		}
+
+		return fmt.Errorf("process terminated before graceful termination could be requested: %w", err)
+
+	default:
+		// Something else went wrong
+		return fmt.Errorf("failed to request graceful termination: %w", err)
 	}
 }
 

commit 9fa8acc1cd35e0359204edc46257141369d7bd24

Only remove PID file if process actually terminated
The supervisor removed the PID file unconditionally whenever the
processWaitQuit method returned. After removing the kill path from the
supervisor, supervised processes could remain alive after returning,
making all the PID file cleanup logic during supervisor startup mostly
futile.

Move the file removal into the goroutine that reaps the process. That's
supposedly the best place for it.

Fixes: 739eebd64a ("Bail out after stop timeout")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/supervisor/supervisor.go
@@ -57,10 +57,13 @@ const k0sManaged = "_K0S_MANAGED=yes"
 func (s *Supervisor) processWaitQuit(ctx context.Context, cmd *exec.Cmd) bool {
 	waitresult := make(chan error, 1)
 	go func() {
-		waitresult <- cmd.Wait()
+		defer close(waitresult)
+		err := cmd.Wait()
+		defer func() { waitresult <- err }()
+		if err := os.Remove(s.PidFile); err != nil && !errors.Is(err, os.ErrNotExist) {
+			s.log.WithError(err).Error("Failed to remove PID file")
+		}
 	}()
-
-	defer os.Remove(s.PidFile)
 
 	select {
 	case <-ctx.Done():
@@ -77,10 +80,12 @@ func (s *Supervisor) processWaitQuit(ctx context.Context, cmd *exec.Cmd) bool {
 		}
 		return true
 
-	case err := <-waitresult:
+	case err, ok := <-waitresult:
 		var exitErr *exec.ExitError
 		state := cmd.ProcessState
 		switch {
+		case !ok:
+			s.log.Error("Failed to wait for process: ", state)
 		case errors.As(err, &exitErr):
 			state = exitErr.ProcessState
 			fallthrough

pkg/supervisor/supervisor_test.go
@@ -461,6 +461,51 @@ func TestCleanupPIDFile_BogusPIDFile(t *testing.T) {
 	assert.ErrorContains(t, s.Supervise(t.Context()), `"rubbish": invalid`)
 }
 
+func TestStop_PIDFileRemainsOnTerminationTimeout(t *testing.T) {
+	switch runtime.GOOS {
+	case "darwin":
+		t.Skip("PID file cleanup not implemented on macOS")
+	}
+
+	// Prepare a process that won't terminate gracefully.
+	pingPong := pingpong.New(t)
+	pingPong.IgnoreGracefulTerminationRequests = true
+
+	// Start to supervise the process.
+	runDir := t.TempDir()
+	s := Supervisor{
+		Name:           t.Name(),
+		BinPath:        pingPong.BinPath(),
+		RunDir:         runDir,
+		Args:           pingPong.BinArgs(),
+		TimeoutStop:    10 * time.Millisecond,
+		TimeoutRespawn: 1 * time.Hour,
+	}
+
+	// Await process startup.
+	require.NoError(t, s.Supervise(t.Context()))
+	require.NoError(t, pingPong.AwaitPing())
+
+	// Expect the previous process to still be alive at the end of the test.
+	t.Cleanup(func() {
+		assert.NoError(t, pingPong.SendPong())
+	})
+
+	// Ensure the PID file is present on disk.
+	pidFilePath := filepath.Join(s.RunDir, s.Name+".pid")
+	pidStat, err := os.Stat(pidFilePath)
+	require.NoError(t, err)
+	require.True(t, pidStat.Mode().IsRegular())
+
+	// Stop the supervisor. The process should survive that.
+	assert.NoError(t, s.Stop())
+
+	// PID file should be unchanged.
+	if newPidStat, err := os.Stat(pidFilePath); assert.NoError(t, err, "While ensuring the existence of the PID file") {
+		assert.Equal(t, pidStat, newPidStat)
+	}
+}
+
 type cmd struct {
 	binPath string
 	binArgs []string