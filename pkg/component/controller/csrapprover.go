c37283eef0f5991d312bfd2ce778577844d41baf
Tom Wieczorek <twieczorek@mirantis.com>
2026-09-09 09:47:10 +0200

    Verify requester identity and SANs before approving CSRs
    CSRApprover only called upstream's ValidateKubeletServingCSR before
    approving kubelet-serving certificate requests. That validation is
    deliberately limited to the certificate's basic information. It doesn't
    have access to the containing CSR or the cluster. Therefore, it cannot
    verify the username of the CSR's requesting user or the requested SANs
    against the node's actual addresses.
    
    As a consequence, any identity allowed to create CSRs could obtain
    serving certificates to impersonate arbitrary (even non-existing) nodes
    with arbitrary SANs.
    
    Require that the requesting user is a node identity using upstream's
    default NodeIdentifier. Additionally, look up the node and reject
    requests for any DNS names or IP addresses that the node doesn't
    report in its status. This mirrors how the kubelet builds its serving
    certificate requests, so legitimate requests are unaffected, whereas the
    SANs were never verified before.
    
    Describe the csr-approver component and the conditions under which it
    approves CSRs in the docs.
    
    Fixes: d51a2e19803 ("Use upstream validation logic for kubelet serving CSRs")
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit 92c1dd7c3e3022b669bcaf53f5e3488946f1f71d)
    (cherry picked from commit e9d6ae560282b24d4f5a64c0c4ccd74e3544ea0f)
    (cherry picked from commit 99d8f6a82c79daccdd0985c87a016fc7035abf08)

--- pkg/component/controller/csrapprover.go
+++ pkg/component/controller/csrapprover.go
@@ -8,6 +8,8 @@ import (
 	"crypto/x509"
 	"errors"
 	"fmt"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
@@ -19,11 +21,16 @@ import (
 	authorizationv1 "k8s.io/api/authorization/v1"
 	certificatesv1 "k8s.io/api/certificates/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apiserver/pkg/authentication/user"
 	clientset "k8s.io/client-go/kubernetes"
 	certificates "k8s.io/kubernetes/pkg/apis/certificates"
+	"k8s.io/kubernetes/pkg/auth/nodeidentifier"
+	utilsnet "k8s.io/utils/net"
 
 	"github.com/sirupsen/logrus"
 )
@@ -36,6 +43,7 @@ type CSRApprover struct {
 	KubeClientFactory kubeutil.ClientFactoryInterface
 	leaderElector     leaderelector.Interface
 	clientset         clientset.Interface
+	nodeIdentifier    nodeidentifier.NodeIdentifier
 }
 
 var _ manager.Component = (*CSRApprover)(nil)
@@ -46,6 +54,7 @@ func NewCSRApprover(c *v1beta1.ClusterConfig, leaderElector leaderelector.Interf
 		ClusterConfig:     c,
 		leaderElector:     leaderElector,
 		KubeClientFactory: kubeClientFactory,
+		nodeIdentifier:    nodeidentifier.NewDefaultNodeIdentifier(),
 	}
 }
 
@@ -95,7 +104,6 @@ type csrCheckErr struct{ error }
 
 func (e csrCheckErr) Unwrap() error { return e.error }
 
-// Majority of this code has been adapted from https://github.com/kontena/kubelet-rubber-stamp
 func (a *CSRApprover) approveCSR(ctx context.Context) error {
 	if !a.leaderElector.IsLeader() {
 		a.log.Debug("not the leader, can't approve certificates")
@@ -117,7 +125,7 @@ func (a *CSRApprover) approveCSR(ctx context.Context) error {
 			continue
 		}
 
-		cr, err := a.ensureKubeletServingCert(ctx, &csr)
+		cr, err := a.authorizeKubeletServingCSR(ctx, &csr)
 		if err != nil {
 			select {
 			case <-ctx.Done():
@@ -175,8 +183,19 @@ func (a *CSRApprover) authorizeCSRCreation(ctx context.Context, csr *certificate
 	return sar.Status.Allowed, nil
 }
 
-func (a *CSRApprover) ensureKubeletServingCert(ctx context.Context, csr *certificatesv1.CertificateSigningRequest) (*x509.CertificateRequest, error) {
+// Checks that the CSR is a well-formed kubelet-serving certificate request that
+// was submitted by the very node whose identity it requests, that the requester
+// is allowed to create such requests, and that it only requests names and
+// addresses that the node reports for itself.
+func (a *CSRApprover) authorizeKubeletServingCSR(ctx context.Context, csr *certificatesv1.CertificateSigningRequest) (*x509.CertificateRequest, error) {
+	// This only checks the shape of the request. Whether the requester is
+	// actually entitled to impersonate the requested identity is up to k0s.
 	cr, err := validateKubeletServingCSR(&csr.Spec)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeName, err := a.verifyNodeIdentity(csr, cr)
 	if err != nil {
 		return nil, err
 	}
@@ -185,6 +204,10 @@ func (a *CSRApprover) ensureKubeletServingCert(ctx context.Context, csr *certifi
 		return nil, csrCheckErr{fmt.Errorf("SubjectAccessReview failed: %w", err)}
 	} else if !approved {
 		return nil, errors.New("requesting user is not allowed to create certificate signing requests")
+	}
+
+	if err := a.verifyNode(ctx, nodeName, cr); err != nil {
+		return nil, err
 	}
 
 	return cr, nil
@@ -208,6 +231,66 @@ func validateKubeletServingCSR(spec *certificatesv1.CertificateSigningRequestSpe
 	return cr, nil
 }
 
+func (a *CSRApprover) verifyNodeIdentity(csr *certificatesv1.CertificateSigningRequest, cr *x509.CertificateRequest) (string, error) {
+	requestingUser := (*requestingUserInfo)(&csr.Spec)
+	nodeName, isNode := a.nodeIdentifier.NodeIdentity(requestingUser)
+	if !isNode {
+		return "", fmt.Errorf("not requested by a node identity: user %q in group(s) %q", requestingUser.GetName(), requestingUser.GetGroups())
+	}
+	if errs := validation.IsDNS1123Subdomain(nodeName); len(errs) > 0 {
+		return "", fmt.Errorf("requesting node name %q is invalid: %s", nodeName, strings.Join(errs, "; "))
+	}
+
+	// The CSR must have been created by the user it requests a certificate for.
+	if requested, requesting := cr.Subject.CommonName, csr.Spec.Username; requested != requesting {
+		return "", fmt.Errorf("requested subject's common name %q doesn't match requesting user name %q", requested, requesting)
+	}
+	return nodeName, nil
+}
+
+func (a *CSRApprover) verifyNode(ctx context.Context, nodeName string, cr *x509.CertificateRequest) error {
+	node, err := a.clientset.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return err
+		}
+		return csrCheckErr{fmt.Errorf("failed to get node %q: %w", nodeName, err)}
+	}
+
+	// The certificate may only be valid for the names and addresses that the
+	// node reports for itself. This mirrors how the kubelet builds its serving
+	// certificate requests. Normalize IP addresses on both sides to mitigate
+	// different but otherwise equivalent string notations.
+	var (
+		nodeAddresses []string
+		errs          []error
+	)
+	for _, address := range node.Status.Addresses {
+		address := address.Address
+		// Kubernetes uses ParseIPSloppy for backwards compatibility.
+		if ip := utilsnet.ParseIPSloppy(address); ip != nil {
+			address = ip.String()
+		}
+		if !slices.Contains(nodeAddresses, address) {
+			nodeAddresses = append(nodeAddresses, address)
+		}
+	}
+	for i, dnsName := range cr.DNSNames {
+		if !slices.Contains(nodeAddresses, dnsName) {
+			errs = append(errs, fmt.Errorf("DNSNames[%d]: forbidden: %q: is not a node address of %s", i, dnsName, nodeName))
+		}
+	}
+	for i, address := range cr.IPAddresses {
+		if address := address.String(); !slices.Contains(nodeAddresses, address) {
+			errs = append(errs, fmt.Errorf("IPAddresses[%d]: forbidden: %q: is not a node address of %s", i, address, nodeName))
+		}
+	}
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
+	return nil
+}
+
 func getCertApprovalCondition(status *certificatesv1.CertificateSigningRequestStatus) (approved bool, denied bool) {
 	for _, c := range status.Conditions {
 		if c.Type == certificatesv1.CertificateApproved {
@@ -227,4 +310,22 @@ func appendApprovalCondition(csr *certificatesv1.CertificateSigningRequest, mess
 		Message: message,
 		Status:  corev1.ConditionTrue,
 	})
+}
+
+type requestingUserInfo certificatesv1.CertificateSigningRequestSpec
+
+var _ user.Info = (*requestingUserInfo)(nil) // Also pins the user package import for godoc.
+
+func (i *requestingUserInfo) GetName() string     { return i.Username } // GetName implements [user.Info].
+func (i *requestingUserInfo) GetUID() string      { return i.UID }      // GetUID implements [user.Info].
+func (i *requestingUserInfo) GetGroups() []string { return i.Groups }   // GetGroups implements [user.Info].
+
+// GetExtra implements [user.Info].
+func (i *requestingUserInfo) GetExtra() map[string][]string {
+	// Need a deep clone to adapt the types ¯\_(ツ)_/¯
+	extra := make(map[string][]string, len(i.Extra))
+	for k, v := range i.Extra {
+		extra[k] = slices.Clone(v)
+	}
+	return extra
 }
