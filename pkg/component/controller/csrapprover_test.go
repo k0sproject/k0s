595b15d877cb2e6b860f60c6eb237a10a23193fd
Tom Wieczorek <twieczorek@mirantis.com>
2026-09-09 09:21:46 +0200

    Rewrite CSR approver unit test
    Only use the exported component API to test the implementation. This
    makes the test more robust against implementation changes later on.
    
    Add table-driven test cases that cover the different success, failure
    and rejection scenarios. Adjust the implementation slightly so that the
    test can inject a logger to better track error conditions. Rename some
    package imports to use more idiomatic names.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>
    (cherry picked from commit 1ad54c6e6c357b0d723397ebb4375752378273f9)

--- pkg/component/controller/csrapprover_test.go
+++ pkg/component/controller/csrapprover_test.go
@@ -1,84 +1,164 @@
 // SPDX-FileCopyrightText: 2021 k0s authors
 // SPDX-License-Identifier: Apache-2.0
 
-package controller
+package controller_test
 
 import (
 	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
+	"net"
 	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
+	"github.com/k0sproject/k0s/pkg/component/controller"
+	"github.com/k0sproject/k0s/pkg/component/controller/leaderelector"
+	"github.com/k0sproject/k0s/pkg/k0scontext"
+
+	authorizationv1 "k8s.io/api/authorization/v1"
+	certv1 "k8s.io/api/certificates/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/k0sproject/k0s/internal/testutil"
-	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
-	"github.com/k0sproject/k0s/pkg/component/controller/leaderelector"
+	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
-	certv1 "k8s.io/api/certificates/v1"
-	core "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/stretchr/testify/require"
+	kubernetesfake "k8s.io/client-go/kubernetes/fake"
+	k8stesting "k8s.io/client-go/testing"
 )
 
-func TestBasicCRSApprover(t *testing.T) {
-	fakeFactory := testutil.NewFakeClientFactory()
-
-	client, err := fakeFactory.GetClient()
-	assert.NoError(t, err)
-
-	ctx := t.Context()
-
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req := pemWithPrivateKey(privateKey)
-
-	csrReq := &certv1.CertificateSigningRequest{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "csrapprover_test",
-		},
-		Spec: certv1.CertificateSigningRequestSpec{
-			Request:    req,
-			SignerName: "kubernetes.io/kubelet-serving",
-		},
-	}
-
-	newCsr, err := client.CertificatesV1().CertificateSigningRequests().Create(ctx, csrReq, metav1.CreateOptions{})
-	assert.NoError(t, err)
-
-	config := &v1beta1.ClusterConfig{
-		Spec: &v1beta1.ClusterSpec{
-			API: &v1beta1.APISpec{
-				Address:         "1.2.3.4",
-				ExternalAddress: "get.k0s.sh",
+func TestCSRApprover(t *testing.T) {
+	node := &corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{Name: "csr-approver-test-node"},
+		Status: corev1.NodeStatus{
+			Addresses: []corev1.NodeAddress{
+				{Type: corev1.NodeHostName, Address: "csr-approver-test-node"},
+				{Type: corev1.NodeInternalIP, Address: "10.0.0.1"},
 			},
 		},
 	}
-	c := NewCSRApprover(config, leaderelector.Off(), fakeFactory)
 
-	assert.NoError(t, c.Init(ctx))
-	assert.NoError(t, c.approveCSR(ctx))
-
-	csr, err := client.CertificatesV1().CertificateSigningRequests().Get(ctx, newCsr.Name, metav1.GetOptions{})
-	assert.NoError(t, err)
-	assert.NotNil(t, csr)
-	assert.Equal(t, newCsr.Name, csr.Name)
-	for _, c := range csr.Status.Conditions {
-		assert.True(t, c.Type == certv1.CertificateApproved && c.Reason == "Autoapproved by K0S CSRApprover" && c.Status == core.ConditionTrue)
+	validTemplate := func() *x509.CertificateRequest {
+		return &x509.CertificateRequest{
+			Subject: pkix.Name{
+				CommonName:   "system:node:csr-approver-test-node",
+				Organization: []string{"system:nodes"},
+			},
+			DNSNames:    []string{"csr-approver-test-node"},
+			IPAddresses: []net.IP{{10, 0, 0, 1}},
+		}
 	}
-}
 
-func pemWithPrivateKey(pk crypto.PrivateKey) []byte {
-	template := &x509.CertificateRequest{
-		Subject: pkix.Name{
-			CommonName:   "something",
-			Organization: []string{"test"},
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+
+	for _, tt := range []struct {
+		testCase          string
+		template          *x509.CertificateRequest // gets defaulted to a valid template if nil
+		noReviewAutoAllow bool
+		expectedErr       string
+	}{
+		{
+			testCase: "node requesting its own certificate",
 		},
+		{
+			testCase:          "SubjectAccessReview fails",
+			noReviewAutoAllow: true,
+			expectedErr:       "failed to perform SubjectAccessReview",
+		},
+		{
+			testCase: "certificate that is not a kubelet-serving certificate",
+			template: func() *x509.CertificateRequest {
+				t := validTemplate()
+				t.Subject.Organization = nil
+				return t
+			}(),
+			expectedErr: "subject organization is not system:nodes",
+		},
+	} {
+		t.Run(tt.testCase, func(t *testing.T) {
+			if tt.template == nil {
+				tt.template = validTemplate()
+			}
+
+			synctest.Test(t, func(t *testing.T) {
+				fakeFactory := testutil.NewFakeClientFactory(node)
+				client := fakeFactory.Client.(*kubernetesfake.Clientset)
+
+				if !tt.noReviewAutoAllow {
+					// Allow the SubjectAccessReview, so that it's actually the
+					// approver's own checks that decide about the approval.
+					client.PrependReactor("create", "subjectaccessreviews", func(action k8stesting.Action) (bool, runtime.Object, error) {
+						sar := action.(k8stesting.CreateAction).GetObject().(*authorizationv1.SubjectAccessReview)
+						sar.Status.Allowed = true
+						return true, sar, nil
+					})
+				}
+
+				csr := &certv1.CertificateSigningRequest{
+					ObjectMeta: metav1.ObjectMeta{Name: "csrapprover_test"},
+					Spec: certv1.CertificateSigningRequestSpec{
+						Request:    pemWithTemplate(tt.template, privateKey),
+						SignerName: certv1.KubeletServingSignerName,
+						Username:   "system:node:csr-approver-test-node",
+						Groups:     []string{"system:nodes"},
+						Usages:     []certv1.KeyUsage{certv1.UsageDigitalSignature, certv1.UsageServerAuth},
+					},
+				}
+				_, err := client.CertificatesV1().CertificateSigningRequests().Create(t.Context(), csr, metav1.CreateOptions{})
+				require.NoError(t, err)
+
+				logger, logs := test.NewNullLogger()
+				ctx := k0scontext.WithValue[logrus.FieldLogger](t.Context(), logger)
+				underTest := controller.NewCSRApprover(&v1beta1.ClusterConfig{}, leaderelector.Off(), fakeFactory)
+				require.NoError(t, underTest.Init(ctx))
+				require.NoError(t, underTest.Start(ctx))
+				t.Cleanup(func() { assert.NoError(t, underTest.Stop()) })
+
+				time.Sleep(10 * time.Second)
+				synctest.Wait()
+
+				csr, err = client.CertificatesV1().CertificateSigningRequests().Get(t.Context(), csr.Name, metav1.GetOptions{})
+				require.NoError(t, err)
+
+				var approvedCond *certv1.CertificateSigningRequestCondition
+				for _, c := range csr.Status.Conditions {
+					if c.Type == certv1.CertificateApproved {
+						approvedCond = &c
+						break
+					}
+				}
+				if tt.expectedErr != "" {
+					assert.Nil(t, approvedCond, "Expected no approved condition at all")
+				} else if assert.NotNil(t, approvedCond, "Expected an approved condition") {
+					assert.Equalf(t, corev1.ConditionTrue, approvedCond.Status, "Unexpected status in approved condition: %v", approvedCond)
+				}
+
+				entries := logs.AllEntries()
+				require.Len(t, entries, 1, "Expected exactly one log message")
+				loggedMsg, loggedErr := entries[0].Message, entries[0].Data[logrus.ErrorKey]
+				if tt.expectedErr == "" {
+					if loggedErr != nil {
+						assert.NoErrorf(t, loggedErr.(error), "Message: %s", loggedMsg)
+					}
+				} else {
+					if assert.NotNilf(t, loggedErr, "No error was logged: %s", loggedMsg) {
+						assert.ErrorContainsf(t, loggedErr.(error), tt.expectedErr, "Message: %s", loggedMsg)
+					}
+				}
+			})
+		})
 	}
-	return pemWithTemplate(template, pk)
 }
 
 func pemWithTemplate(template *x509.CertificateRequest, key crypto.PrivateKey) []byte {
