k0sproject/k0s

commit 743f8afede6e54603a7cfd113128e89642b0b738

Unify view of the world for the fake clients
In order for the typed clients to share the same view of the world
as the dynamic client, they all must share the same (wrapped) object
tracker. Create typed clients using the dynamic client's object tracker
and transform between typed and unstructured objects.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

internal/testutil/fakeclient/tracker.go
@@ -1,0 +1,255 @@
+/*
+Copyright 2024 k0s authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fakeclient
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"unsafe"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+	discoveryfake "k8s.io/client-go/discovery/fake"
+	"k8s.io/client-go/dynamic/fake"
+	"k8s.io/client-go/testing"
+)
+
+// Creates a new fake clientset backed by the given ObjectTracker.
+// This should only be used with the auto-generated fake clientsets.
+// Discovery() will return nil.
+func NewClientset[T any](discovery *discoveryfake.FakeDiscovery, tracker testing.ObjectTracker) *T {
+	p := new(T)
+
+	// Assume that a pointer to T is a fake client.
+	c := any(p).(testing.FakeClient)
+
+	// This wire code is adopted from the generated fake clients.
+	c.AddReactor("*", "*", testing.ObjectReaction(tracker))
+	c.AddWatchReactor("*", func(action testing.Action) (bool, watch.Interface, error) {
+		watch, err := tracker.Watch(action.GetResource(), action.GetNamespace())
+		if err != nil {
+			return false, nil, err
+		}
+		return true, watch, nil
+	})
+
+	// Set the fake clientset's discovery and tracker.
+	ty := reflect.TypeOf(p).Elem()
+	for i := 0; i < ty.NumField(); i++ {
+		f := ty.Field(i)
+		if f.Name == "discovery" && f.Type == reflect.TypeFor[*discoveryfake.FakeDiscovery]() {
+			*(**discoveryfake.FakeDiscovery)(unsafe.Add(unsafe.Pointer(p), f.Offset)) = discovery
+		} else if f.Name == "tracker" && f.Type == reflect.TypeFor[testing.ObjectTracker]() {
+			*(*testing.ObjectTracker)(unsafe.Add(unsafe.Pointer(p), f.Offset)) = tracker
+		}
+	}
+
+	return p
+}
+
+func TypedObjectTrackerFrom(scheme *runtime.Scheme, dynamicClient *fake.FakeDynamicClient) *TransformingObjectTracker {
+	return &TransformingObjectTracker{
+		Inner: dynamicClient.Tracker(),
+		Internalize: func(o runtime.Object) (runtime.Object, error) {
+			return toUnstructured(scheme, o)
+		},
+		Externalize: func(o runtime.Object, gvk schema.GroupVersionKind) (runtime.Object, error) {
+			return fromUnstructured(scheme, o, gvk)
+		},
+	}
+}
+
+type TransformingObjectTracker struct {
+	Inner       testing.ObjectTracker
+	Internalize func(runtime.Object) (runtime.Object, error)
+	Externalize func(runtime.Object, schema.GroupVersionKind) (runtime.Object, error)
+}
+
+var _ testing.ObjectTracker = (*TransformingObjectTracker)(nil)
+
+// Add implements testing.ObjectTracker.
+func (t *TransformingObjectTracker) Add(obj runtime.Object) error {
+	internal, err := t.Internalize(obj)
+	if err != nil {
+		return fmt.Errorf("failed to internalize object: %w", err)
+	}
+	return t.Inner.Add(internal)
+}
+
+// Create implements testing.ObjectTracker.
+func (t *TransformingObjectTracker) Create(gvr schema.GroupVersionResource, obj runtime.Object, ns string) error {
+	internal, err := t.Internalize(obj)
+	if err != nil {
+		return fmt.Errorf("failed to internalize object: %w", err)
+	}
+	return t.Inner.Create(gvr, internal, ns)
+}
+
+// Delete implements testing.ObjectTracker.
+func (t *TransformingObjectTracker) Delete(gvr schema.GroupVersionResource, ns string, name string) error {
+	return t.Inner.Delete(gvr, ns, ns)
+}
+
+// Get implements testing.ObjectTracker.
+func (t *TransformingObjectTracker) Get(gvr schema.GroupVersionResource, ns string, name string) (runtime.Object, error) {
+	obj, err := t.Inner.Get(gvr, ns, name)
+	if err != nil {
+		return nil, err
+	}
+
+	external, err := t.Externalize(obj, obj.GetObjectKind().GroupVersionKind())
+	if err != nil {
+		return obj, fmt.Errorf("failed to externalize object: %w", err)
+	}
+
+	return external, nil
+}
+
+// List implements testing.ObjectTracker.
+func (t *TransformingObjectTracker) List(gvr schema.GroupVersionResource, gvk schema.GroupVersionKind, ns string) (runtime.Object, error) {
+	obj, err := t.Inner.List(gvr, gvk, ns)
+	if err != nil {
+		return nil, err
+	}
+
+	external, err := t.Externalize(obj, gvk)
+	if err != nil {
+		return obj, fmt.Errorf("failed to externalize object: %w", err)
+	}
+
+	return external, nil
+}
+
+// Update implements testing.ObjectTracker.
+func (t *TransformingObjectTracker) Update(gvr schema.GroupVersionResource, obj runtime.Object, ns string) error {
+	internal, err := t.Internalize(obj)
+	if err != nil {
+		return fmt.Errorf("failed to internalize object: %w", err)
+	}
+	return t.Inner.Update(gvr, internal, ns)
+}
+
+// Watch implements testing.ObjectTracker.
+func (t *TransformingObjectTracker) Watch(gvr schema.GroupVersionResource, ns string) (watch.Interface, error) {
+	w, err := t.Inner.Watch(gvr, ns)
+	if err != nil {
+		return w, err
+	}
+
+	internal := w.ResultChan()
+	external := make(chan watch.Event, cap(internal))
+
+	go func() {
+		defer close(external)
+		for e := range internal {
+			if e.Object != nil {
+				gvk := e.Object.GetObjectKind().GroupVersionKind()
+				if external, err := t.Externalize(e.Object, gvk); err == nil {
+					e.Object = external
+				} else {
+					e = watch.Event{
+						Type: watch.Error,
+						Object: &metav1.Status{
+							Status:  metav1.StatusFailure,
+							Message: err.Error(),
+							Code:    http.StatusInternalServerError,
+						},
+					}
+				}
+			}
+
+			external <- e
+		}
+	}()
+
+	return &watcher{external, w.Stop}, nil
+}
+
+type watcher struct {
+	result <-chan watch.Event
+	stop   func()
+}
+
+// ResultChan implements watch.Interface.
+func (w *watcher) ResultChan() <-chan watch.Event { return w.result }
+
+// Stop implements watch.Interface.
+func (w *watcher) Stop() { w.stop() }
+
+func toUnstructured(scheme *runtime.Scheme, obj runtime.Object) (runtime.Object, error) {
+	var u unstructured.Unstructured
+	if err := scheme.Convert(obj, &u, nil); err != nil {
+		return obj, err
+	}
+
+	if meta.IsListType(obj) {
+		if u.IsList() {
+			return u.ToList()
+		}
+		return obj, fmt.Errorf("not an unstructured list: %T", obj)
+	}
+
+	return &u, nil
+}
+
+func fromUnstructured(scheme *runtime.Scheme, obj runtime.Object, gvk schema.GroupVersionKind) (runtime.Object, error) {
+	if !meta.IsListType(obj) {
+		external, err := scheme.New(gvk)
+		if err != nil {
+			return obj, err
+		}
+
+		err = scheme.Convert(obj, external, nil)
+		if err != nil {
+			return obj, err
+		}
+
+		external.GetObjectKind().SetGroupVersionKind(gvk)
+		return external, nil
+	}
+
+	var items []runtime.Object
+	if err := meta.EachListItem(obj, func(obj runtime.Object) error {
+		if typed, err := fromUnstructured(scheme, obj, gvk); err != nil {
+			return err
+		} else {
+			items = append(items, typed)
+			return nil
+		}
+	}); err != nil {
+		return obj, err
+	}
+
+	listGVK := gvk
+	listGVK.Kind = listGVK.Kind + "List"
+
+	list, err := scheme.New(listGVK)
+	if err != nil {
+		return obj, err
+	}
+
+	if err := meta.SetList(list, items); err != nil {
+		return obj, err
+	}
+
+	return list, nil
+}

internal/testutil/kube_client.go
@@ -17,13 +17,16 @@ limitations under the License.
 package testutil
 
 import (
-	"fmt"
+	"errors"
 	"reflect"
 	"strings"
 
+	"github.com/k0sproject/k0s/internal/testutil/fakeclient"
+	k0sfake "github.com/k0sproject/k0s/pkg/client/clientset/fake"
 	k0sscheme "github.com/k0sproject/k0s/pkg/client/clientset/scheme"
 	etcdv1beta1 "github.com/k0sproject/k0s/pkg/client/clientset/typed/etcd/v1beta1"
 	k0sv1beta1 "github.com/k0sproject/k0s/pkg/client/clientset/typed/k0s/v1beta1"
+	"github.com/k0sproject/k0s/pkg/constant"
 	kubeutil "github.com/k0sproject/k0s/pkg/kubernetes"
 
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -36,10 +39,9 @@ import (
 	"k8s.io/client-go/dynamic"
 	dynamicfake "k8s.io/client-go/dynamic/fake"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/fake"
+	kubernetesfake "k8s.io/client-go/kubernetes/fake"
 	kubernetesscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	restfake "k8s.io/client-go/rest/fake"
 )
 
 var _ kubeutil.ClientFactoryInterface = (*FakeClientFactory)(nil)
@@ -58,21 +60,29 @@ func NewFakeClientFactory(objects ...runtime.Object) *FakeClientFactory {
 	// Create a fake discovery client backed by the dynamic fake client.
 	fakeDiscovery := &discoveryfake.FakeDiscovery{Fake: &fakeDynamic.Fake}
 
+	// In order for the typed clients to share the same view of the world as the
+	// dynamic client, they all must share the same (wrapped) object tracker. We
+	// create typed clients using the dynamic client's object tracker and
+	// transform between typed and unstructured objects.
+	tracker := fakeclient.TypedObjectTrackerFrom(scheme, fakeDynamic)
+	kubeClients := fakeclient.NewClientset[kubernetesfake.Clientset](fakeDiscovery, tracker)
+	k0sClients := fakeclient.NewClientset[k0sfake.Clientset](fakeDiscovery, tracker)
+
 	return &FakeClientFactory{
-		Client:          fake.NewSimpleClientset(objects...),
-		DynamicClient:   fakeDynamic,
-		DiscoveryClient: memory.NewMemCacheClient(fakeDiscovery),
-		RawDiscovery:    fakeDiscovery,
-		RESTClient:      &restfake.RESTClient{},
+		DynamicClient:    fakeDynamic,
+		Client:           kubeClients,
+		DiscoveryClient:  memory.NewMemCacheClient(fakeDiscovery),
+		ConfigClient:     k0sClients.K0sV1beta1().ClusterConfigs(constant.ClusterConfigNamespace),
+		EtcdMemberClient: k0sClients.EtcdV1beta1().EtcdMembers(),
 	}
 }
 
 type FakeClientFactory struct {
-	Client          kubernetes.Interface
-	DynamicClient   dynamic.Interface
-	DiscoveryClient discovery.CachedDiscoveryInterface
-	RawDiscovery    *discoveryfake.FakeDiscovery
-	RESTClient      rest.Interface
+	DynamicClient    *dynamicfake.FakeDynamicClient
+	Client           kubernetes.Interface
+	DiscoveryClient  discovery.CachedDiscoveryInterface
+	ConfigClient     k0sv1beta1.ClusterConfigInterface
+	EtcdMemberClient etcdv1beta1.EtcdMemberInterface
 }
 
 func (f *FakeClientFactory) GetClient() (kubernetes.Interface, error) {
@@ -88,18 +98,19 @@ func (f *FakeClientFactory) GetDiscoveryClient() (discovery.CachedDiscoveryInter
 }
 
 func (f *FakeClientFactory) GetConfigClient() (k0sv1beta1.ClusterConfigInterface, error) {
-	return nil, fmt.Errorf("NOT IMPLEMENTED")
+	return f.ConfigClient, nil
 }
 
 func (f *FakeClientFactory) GetRESTClient() (rest.Interface, error) {
-	return f.RESTClient, nil
+	return nil, errors.ErrUnsupported
 }
-func (f *FakeClientFactory) GetRESTConfig() *rest.Config {
+
+func (f FakeClientFactory) GetRESTConfig() *rest.Config {
 	return &rest.Config{}
 }
 
-func (f *FakeClientFactory) GetEtcdMemberClient() (etcdv1beta1.EtcdMemberInterface, error) {
-	return nil, fmt.Errorf("NOT IMPLEMENTED")
+func (f FakeClientFactory) GetEtcdMemberClient() (etcdv1beta1.EtcdMemberInterface, error) {
+	return f.EtcdMemberClient, nil
 }
 
 // Extracts all kinds from scheme and builds API resource lists for fake discovery clients.

commit adff518919ec51af0ca45ebae961b2cd75ac5151

Fix copy pasta in the fake client tracker's delete implementation
It passed the namespace instead of the name.

Fixes: 743f8afede6 ("Unify view of the world for the fake clients")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

internal/testutil/fakeclient/tracker.go
@@ -86,12 +86,12 @@ func (t *TransformingObjectTracker) Create(gvr schema.GroupVersionResource, obj 
 }
 
 // Delete implements testing.ObjectTracker.
-func (t *TransformingObjectTracker) Delete(gvr schema.GroupVersionResource, ns string, name string, opts ...metav1.DeleteOptions) error {
-	return t.Inner.Delete(gvr, ns, ns, opts...)
+func (t *TransformingObjectTracker) Delete(gvr schema.GroupVersionResource, ns, name string, opts ...metav1.DeleteOptions) error {
+	return t.Inner.Delete(gvr, ns, name, opts...)
 }
 
 // Get implements testing.ObjectTracker.
-func (t *TransformingObjectTracker) Get(gvr schema.GroupVersionResource, ns string, name string, opts ...metav1.GetOptions) (runtime.Object, error) {
+func (t *TransformingObjectTracker) Get(gvr schema.GroupVersionResource, ns, name string, opts ...metav1.GetOptions) (runtime.Object, error) {
 	obj, err := t.Inner.Get(gvr, ns, name, opts...)
 	if err != nil {
 		return nil, err