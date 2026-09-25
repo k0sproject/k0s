k0sproject/k0s

commit 780c87bb82e60b0c36f79eba25a77a3c738ffb98

Implement rawArgs for kube-apiserver
Signed-off-by: Juan-Luis de Sousa-Valadas Castaño <jvaladas@mirantis.com>

pkg/component/controller/apiserver.go
@@ -159,6 +159,7 @@ func (a *APIServer) Start(_ context.Context) error {
 	for name, value := range args {
 		apiServerArgs = append(apiServerArgs, fmt.Sprintf("--%s=%s", name, value))
 	}
+	apiServerArgs = append(apiServerArgs, a.ClusterConfig.Spec.API.RawArgs...)
 
 	a.supervisor = &supervisor.Supervisor{
 		Name:    kubeAPIComponentName,

inttest/extraargs/extraargs_test.go
@@ -88,7 +88,8 @@ spec:
   api:
     extraArgs:
       disable-admission-plugins: PodSecurity
-      enable-admission-plugins: NamespaceLifecycle,LimitRanger,ServiceAccount,TaintNodesByCondition,Priority,DefaultTolerationSeconds,DefaultStorageClass,StorageObjectInUseProtection,PersistentVolumeClaimResize,RuntimeClass,CertificateApproval,CertificateSigning,CertificateSubjectRestriction,DefaultIngressClass,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota
+    rawArgs:
+    - --enable-admission-plugins=NamespaceLifecycle,LimitRanger,ServiceAccount,TaintNodesByCondition,Priority,DefaultTolerationSeconds,DefaultStorageClass,StorageObjectInUseProtection,PersistentVolumeClaimResize,RuntimeClass,CertificateApproval,CertificateSigning,CertificateSubjectRestriction,DefaultIngressClass,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota
   storage:
     etcd:
       extraArgs:

pkg/apis/k0s/v1beta1/api.go
@@ -35,6 +35,10 @@ type APISpec struct {
 	// Map of key-values (strings) for any extra arguments to pass down to Kubernetes api-server process
 	ExtraArgs map[string]string `json:"extraArgs,omitempty"`
 
+	// Slice of strings with raw arguments to pass to the kube-apiserver process
+	// These arguments will be appended to the `ExtraArgs` and aren't validated at all.
+	RawArgs []string `json:"rawArgs,omitempty"`
+
 	// Custom port for k0s-api server to listen on (default: 9443)
 	// +kubebuilder:validation:Minimum=1
 	// +kubebuilder:validation:Maximum=65535

pkg/apis/k0s/v1beta1/zz_generated.deepcopy.go
@@ -21,6 +21,11 @@ func (in *APISpec) DeepCopyInto(out *APISpec) {
 			(*out)[key] = val
 		}
 	}
+	if in.RawArgs != nil {
+		in, out := &in.RawArgs, &out.RawArgs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	if in.SANs != nil {
 		in, out := &in.SANs, &out.SANs
 		*out = make([]string, len(*in))

static/_crds/k0s/k0s.k0sproject.io_clusterconfigs.yaml
@@ -85,6 +85,13 @@ spec:
                     maximum: 65535
                     minimum: 1
                     type: integer
+                  rawArgs:
+                    description: |-
+                      Slice of strings with raw arguments to pass to the kube-apiserver process
+                      These arguments will be appended to the `ExtraArgs` and aren't validated at all.
+                    items:
+                      type: string
+                    type: array
                   sans:
                     description: List of additional addresses to push to API servers
                       serving the certificate

commit 3c291f8ec2a805aa8db6b8628226ffaa77d75019

Fix precedence of etcd flags for kube-apiserver
The etcd flags were appended to the command line after everything else,
including the rawArgs. This made them the only flags that users couldn't
override. An etcd flag in extraArgs triggered the "overriding" warning,
but was ignored because kube-apiserver takes the last occurrence of a
flag. This contradicts the rawArgs documentation, which states that they
are appended to extraArgs, and how every other component handles them.

Add the etcd flags to the flag map before merging the extra args, so
they're treated like any other flag set by k0s. Extra args may override
them, and raw args come last.

Fixes: 780c87bb82e ("Implement rawArgs for kube-apiserver")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/apiserver.go
@@ -138,6 +138,10 @@ func (a *APIServer) buildSupervisor() (*supervisor.Supervisor, error) {
 
 	args["api-audiences"] = strings.Join(apiAudiences, ",")
 
+	if err := addEtcdArgs(args, a.NodeConfig.Spec.Storage, a.K0sVars); err != nil {
+		return nil, err
+	}
+
 	for name, value := range a.NodeConfig.Spec.API.ExtraArgs {
 		if _, ok := args[name]; ok {
 			logrus.Warnf("overriding apiserver flag with user provided value: %s", name)
@@ -237,12 +241,6 @@ func (a *APIServer) buildSupervisor() (*supervisor.Supervisor, error) {
 		sup.RequiredPrivileges.BindsPrivilegedPorts = true
 		logrus.Infof("API port %d is less than 1024, granting privilege to bind to privileged ports", a.NodeConfig.Spec.API.Port)
 	}
-
-	etcdArgs, err := getEtcdArgs(a.NodeConfig.Spec.Storage, a.K0sVars)
-	if err != nil {
-		return nil, err
-	}
-	sup.Args = append(sup.Args, etcdArgs...)
 
 	return sup, nil
 }
@@ -346,30 +344,28 @@ func authenticationConfigHasAnonymous(path string) (bool, error) {
 	return len(authConfig.Anonymous) > 0 && string(authConfig.Anonymous) != "null", nil
 }
 
-func getEtcdArgs(storage *v1beta1.StorageSpec, k0sVars *config.CfgVars) ([]string, error) {
-	var args []string
-
+// Adds the flags that connect kube-apiserver to its storage backend.
+func addEtcdArgs(args stringmap.StringMap, storage *v1beta1.StorageSpec, k0sVars *config.CfgVars) error {
 	switch storage.Type {
 	case v1beta1.KineStorageType:
 		sockURL := url.URL{
 			Scheme: "unix", OmitHost: true,
 			Path: filepath.ToSlash(k0sVars.KineSocketPath),
 		} // kine endpoint
-		args = append(args, "--etcd-servers="+sockURL.String())
+		args["etcd-servers"] = sockURL.String()
 	case v1beta1.EtcdStorageType:
-		args = append(args, "--etcd-servers="+storage.Etcd.GetEndpointsAsString())
+		args["etcd-servers"] = storage.Etcd.GetEndpointsAsString()
 		if storage.Etcd.IsTLSEnabled() {
-			args = append(args,
-				"--etcd-cafile="+storage.Etcd.GetCaFilePath(k0sVars.EtcdCertDir),
-				"--etcd-certfile="+storage.Etcd.GetCertFilePath(k0sVars.CertRootDir),
-				"--etcd-keyfile="+storage.Etcd.GetKeyFilePath(k0sVars.CertRootDir))
+			args["etcd-cafile"] = storage.Etcd.GetCaFilePath(k0sVars.EtcdCertDir)
+			args["etcd-certfile"] = storage.Etcd.GetCertFilePath(k0sVars.CertRootDir)
+			args["etcd-keyfile"] = storage.Etcd.GetKeyFilePath(k0sVars.CertRootDir)
 		}
 		if storage.Etcd.IsExternalClusterUsed() {
-			args = append(args, "--etcd-prefix="+storage.Etcd.ExternalCluster.EtcdPrefix)
+			args["etcd-prefix"] = storage.Etcd.ExternalCluster.EtcdPrefix
 		}
 	default:
-		return nil, fmt.Errorf("invalid storage type: %s", storage.Type)
+		return fmt.Errorf("invalid storage type: %s", storage.Type)
 	}
 
-	return args, nil
+	return nil
 }

pkg/component/controller/apiserver_test.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/k0sproject/k0s/internal/pkg/stringmap"
 	"github.com/k0sproject/k0s/pkg/apis/k0s/v1beta1"
 	"github.com/k0sproject/k0s/pkg/config"
 
@@ -95,90 +96,95 @@ anonymous: null
 	})
 }
 
-func (a *apiServerSuite) TestGetEtcdArgs() {
+func (a *apiServerSuite) TestAddEtcdArgs() {
 	k0sVars := &config.CfgVars{
 		KineSocketPath: "/run/k0s/kine/kine.sock:2379",
 		CertRootDir:    "/var/lib/k0s/pki",
 		EtcdCertDir:    "/var/lib/k0s/pki/etcd",
 	}
 
-	a.Run("kine", func() {
-		storageSpec := &v1beta1.StorageSpec{
-			Type: "kine",
-			Kine: v1beta1.DefaultKineConfig("/var/lib/k0s"),
-		}
-
-		result, err := getEtcdArgs(storageSpec, k0sVars)
-
-		require := a.Require()
-		require.NoError(err)
-		require.Len(result, 1)
-		require.Contains(result[0], "--etcd-servers=unix:/run/k0s/kine/kine.sock:2379")
-	})
-
-	a.Run("internal etcd cluster", func() {
-		storageSpec := &v1beta1.StorageSpec{
-			Etcd: &v1beta1.EtcdConfig{
-				PeerAddress: "192.168.68.104",
+	for _, tt := range []struct {
+		name     string
+		storage  *v1beta1.StorageSpec
+		expected stringmap.StringMap
+	}{
+		{
+			"kine",
+			&v1beta1.StorageSpec{
+				Type: "kine",
+				Kine: v1beta1.DefaultKineConfig("/var/lib/k0s"),
 			},
-			Type: "etcd",
-		}
-
-		result, err := getEtcdArgs(storageSpec, k0sVars)
-
-		require := a.Require()
-		require.NoError(err)
-		require.Len(result, 4)
-		require.Contains(result[0], "--etcd-servers=https://127.0.0.1:2379")
-		require.Contains(result[1], "--etcd-cafile="+filepath.FromSlash("/var/lib/k0s/pki/etcd/ca.crt"))
-		require.Contains(result[2], "--etcd-certfile="+filepath.FromSlash("/var/lib/k0s/pki/apiserver-etcd-client.crt"))
-		require.Contains(result[3], "--etcd-keyfile="+filepath.FromSlash("/var/lib/k0s/pki/apiserver-etcd-client.key"))
-	})
-
-	a.Run("external etcd cluster with TLS", func() {
-		storageSpec := &v1beta1.StorageSpec{
-			Etcd: &v1beta1.EtcdConfig{
-				ExternalCluster: &v1beta1.ExternalCluster{
-					Endpoints:      []string{"https://192.168.10.10:2379", "https://192.168.10.11:2379"},
-					EtcdPrefix:     "k0s-tenant-1",
-					CaFile:         "/etc/pki/CA/ca.crt",
-					ClientCertFile: "/etc/pki/tls/certs/etcd-client.crt",
-					ClientKeyFile:  "/etc/pki/tls/private/etcd-client.key",
+			stringmap.StringMap{
+				"etcd-servers": "unix:/run/k0s/kine/kine.sock:2379",
+			},
+		},
+		{
+			"internal etcd cluster",
+			&v1beta1.StorageSpec{
+				Type: "etcd",
+				Etcd: &v1beta1.EtcdConfig{
+					PeerAddress: "192.168.68.104",
 				},
 			},
-			Type: "etcd",
-		}
-
-		result, err := getEtcdArgs(storageSpec, k0sVars)
-
-		require := a.Require()
-		require.NoError(err)
-		require.Len(result, 5)
-		require.Contains(result[0], "--etcd-servers=https://192.168.10.10:2379,https://192.168.10.11:2379")
-		require.Contains(result[1], "--etcd-cafile=/etc/pki/CA/ca.crt")
-		require.Contains(result[2], "--etcd-certfile=/etc/pki/tls/certs/etcd-client.crt")
-		require.Contains(result[3], "--etcd-keyfile=/etc/pki/tls/private/etcd-client.key")
-		require.Contains(result[4], "--etcd-prefix=k0s-tenant-1")
-	})
-
-	a.Run("external etcd cluster without TLS", func() {
-		storageSpec := &v1beta1.StorageSpec{
-			Etcd: &v1beta1.EtcdConfig{
-				ExternalCluster: &v1beta1.ExternalCluster{
-					Endpoints:  []string{"http://192.168.10.10:2379", "http://192.168.10.11:2379"},
-					EtcdPrefix: "k0s-tenant-1",
+			stringmap.StringMap{
+				"etcd-servers":  "https://127.0.0.1:2379",
+				"etcd-cafile":   filepath.FromSlash("/var/lib/k0s/pki/etcd/ca.crt"),
+				"etcd-certfile": filepath.FromSlash("/var/lib/k0s/pki/apiserver-etcd-client.crt"),
+				"etcd-keyfile":  filepath.FromSlash("/var/lib/k0s/pki/apiserver-etcd-client.key"),
+			},
+		},
+		{
+			"external etcd cluster with TLS",
+			&v1beta1.StorageSpec{
+				Type: "etcd",
+				Etcd: &v1beta1.EtcdConfig{
+					ExternalCluster: &v1beta1.ExternalCluster{
+						Endpoints:      []string{"https://192.168.10.10:2379", "https://192.168.10.11:2379"},
+						EtcdPrefix:     "k0s-tenant-1",
+						CaFile:         "/etc/pki/CA/ca.crt",
+						ClientCertFile: "/etc/pki/tls/certs/etcd-client.crt",
+						ClientKeyFile:  "/etc/pki/tls/private/etcd-client.key",
+					},
 				},
 			},
-			Type: "etcd",
-		}
+			stringmap.StringMap{
+				"etcd-servers":  "https://192.168.10.10:2379,https://192.168.10.11:2379",
+				"etcd-cafile":   "/etc/pki/CA/ca.crt",
+				"etcd-certfile": "/etc/pki/tls/certs/etcd-client.crt",
+				"etcd-keyfile":  "/etc/pki/tls/private/etcd-client.key",
+				"etcd-prefix":   "k0s-tenant-1",
+			},
+		},
+		{
+			"external etcd cluster without TLS",
+			&v1beta1.StorageSpec{
+				Type: "etcd",
+				Etcd: &v1beta1.EtcdConfig{
+					ExternalCluster: &v1beta1.ExternalCluster{
+						Endpoints:  []string{"http://192.168.10.10:2379", "http://192.168.10.11:2379"},
+						EtcdPrefix: "k0s-tenant-1",
+					},
+				},
+			},
+			stringmap.StringMap{
+				"etcd-servers": "http://192.168.10.10:2379,http://192.168.10.11:2379",
+				"etcd-prefix":  "k0s-tenant-1",
+			},
+		},
+	} {
+		a.Run(tt.name, func() {
+			args := make(stringmap.StringMap)
+			a.Require().NoError(addEtcdArgs(args, tt.storage, k0sVars))
+			a.Equal(tt.expected, args)
+		})
+	}
 
-		result, err := getEtcdArgs(storageSpec, k0sVars)
+	a.Run("invalid storage type", func() {
+		storageSpec := &v1beta1.StorageSpec{Type: "bogus"}
 
-		require := a.Require()
-		require.NoError(err)
-		require.Len(result, 2)
-		require.Contains(result[0], "--etcd-servers=http://192.168.10.10:2379,http://192.168.10.11:2379")
-		require.Contains(result[1], "--etcd-prefix=k0s-tenant-1")
+		args := make(stringmap.StringMap)
+		a.ErrorContains(addEtcdArgs(args, storageSpec, k0sVars), "invalid storage type: bogus")
+		a.Empty(args)
 	})
 }
 